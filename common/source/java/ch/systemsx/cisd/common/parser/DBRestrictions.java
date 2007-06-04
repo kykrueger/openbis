098835b29ae631af316f0ee808f6777a1e4c3505
brinn <brinn>
2007-06-04 06:20:53 +0000

    add: - class DBRestrictions and tests for column length and constraints checking
    SVN: 288

---
+++ common/source/java/ch/systemsx/cisd/common/parser/DBRestrictions.java
@@ -0,0 +1,258 @@
+/*
+ * Copyright 2007 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.common.parser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+import ch.systemsx.cisd.common.exceptions.UserFailureException;
+import ch.systemsx.cisd.common.logging.LogCategory;
+import ch.systemsx.cisd.common.logging.LogFactory;
+
+/**
+ * A parser for SQL92 domain manipulation language scripts. The result are columns lengths and checked constraints.
+ * 
+ * @author Bernd Rinn
+ */
+public class DBRestrictions
+{
+    private static final String CREATE_DOMAIN_PREFIX = "create domain ";
+
+    private static final Pattern VARCHAR_PATTERN = Pattern.compile("varchar\\(([0-9]+)\\)");
+
+    private static final Pattern CREATE_TABLE_PATTERN = Pattern.compile("create table ([a-z,0-9,_]+) \\((.+)\\)");
+
+    private static final Pattern CHECK_CONSTRAINT_PATTERN =
+            Pattern
+                    .compile("alter table ([a-z,0-9,_]+) add constraint [a-z,0-9,_]+ check \\(([a-z,0-9,_]+) in \\((.+)\\)\\)");
+
+    private static final Logger operationLog = LogFactory.getLogger(LogCategory.OPERATION, DBRestrictions.class);
+
+    // @Private
+    final Map<String, DBTableRestrictions> tableMap = new HashMap<String, DBTableRestrictions>();
+
+    /**
+     * A class that holds the restrictions put upon columns in a database table.
+     */
+    public static class DBTableRestrictions
+    {
+        final Map<String, Integer> columnLengthMap = new HashMap<String, Integer>();
+
+        final Map<String, Set<String>> checkedConstraintsMap = new HashMap<String, Set<String>>();
+
+        public int getLength(String columnName)
+        {
+            final Integer columnLength = columnLengthMap.get(columnName);
+            assert columnLength != null : "Illegal column " + columnName;
+            return columnLength;
+        }
+
+        public Set<String> getCheckedConstaint(String columnName)
+        {
+            return checkedConstraintsMap.get(columnName);
+        }
+    }
+
+    public DBRestrictions(String dmlScript)
+    {
+        final List<String> normalizedDmlScript = normalize(dmlScript);
+        final Map<String, Integer> domains = parseDomains(normalizedDmlScript);
+        parseColumnLength(normalizedDmlScript, domains);
+        parserCheckedConstraints(normalizedDmlScript);
+    }
+
+    // @Private
+    static List<String> normalize(String dmlScript)
+    {
+        final List<String> list = new ArrayList<String>();
+        final SQLCommandTokenizer normalizer = new SQLCommandTokenizer(dmlScript);
+        String command;
+        do
+        {
+            command = normalizer.getNextCommand();
+            if (command != null)
+            {
+                list.add(command);
+            }
+        } while (command != null);
+        return list;
+    }
+
+    // @Private
+    static Map<String, Integer> parseDomains(List<String> dmlScript)
+    {
+        final Map<String, Integer> domains = new HashMap<String, Integer>();
+        for (String line : dmlScript)
+        {
+            if (line.startsWith(CREATE_DOMAIN_PREFIX))
+            {
+                final String[] words = StringUtils.split(line.substring(CREATE_DOMAIN_PREFIX.length()), ' ');
+                if (words.length != 3 || "as".equals(words[1]) == false)
+                {
+                    operationLog.warn("line \"" + line + "\" looks like domain definition, but is ill-formed.");
+                    continue;
+                }
+                final Matcher varCharMatcher = VARCHAR_PATTERN.matcher(words[2]);
+                if (varCharMatcher.matches())
+                {
+                    domains.put(words[0], Integer.parseInt(varCharMatcher.group(1)));
+                }
+            }
+        }
+
+        return domains;
+    }
+
+    private void parseColumnLength(List<String> dmlScript, Map<String, Integer> domains)
+    {
+        for (String line : dmlScript)
+        {
+            final Matcher createTableMatcher = CREATE_TABLE_PATTERN.matcher(line);
+            if (createTableMatcher.matches())
+            {
+                final String tableName = createTableMatcher.group(1);
+                final String tableDefinition = createTableMatcher.group(2);
+                final String[] columnDefinitions = StringUtils.split(tableDefinition, ',');
+                for (String columnDefinition : columnDefinitions)
+                {
+                    parseColumnDefinition(columnDefinition, tableName, domains);
+                }
+            }
+        }
+    }
+
+    private void parseColumnDefinition(String columnDefinition, final String tableName, Map<String, Integer> domains)
+            throws NumberFormatException
+    {
+        final String[] words = StringUtils.split(columnDefinition, ' ');
+        if (words.length < 2)
+        {
+            operationLog.warn("Invalid column definition \"" + columnDefinition + "\" for table " + tableName);
+            return;
+        }
+        final String columnName = words[0];
+        final String type = words[1];
+        final Integer domainLength = domains.get(type);
+        if (domainLength != null)
+        {
+            getTableRestrictions(tableName).columnLengthMap.put(columnName, domainLength);
+        } else
+        {
+            final Matcher varCharMatcher = VARCHAR_PATTERN.matcher(type);
+            if (varCharMatcher.matches())
+            {
+                getTableRestrictions(tableName).columnLengthMap.put(columnName, Integer.parseInt(varCharMatcher
+                        .group(1)));
+            }
+        }
+    }
+
+    private void parserCheckedConstraints(List<String> dmlScript)
+    {
+        for (String line : dmlScript)
+        {
+            final Matcher checkedConstraintMatcher = CHECK_CONSTRAINT_PATTERN.matcher(line);
+            if (checkedConstraintMatcher.matches())
+            {
+                final String tableName = checkedConstraintMatcher.group(1);
+                final String columnName = checkedConstraintMatcher.group(2);
+                final String alternativesStr = checkedConstraintMatcher.group(3);
+                final String[] alternatives = StringUtils.split(alternativesStr, ',');
+                final Set<String> alternativeSet = new HashSet<String>();
+                for (String alternative : alternatives)
+                {
+                    if (alternative.charAt(0) != '\'' || alternative.charAt(alternative.length() - 1) != '\'')
+                    {
+                        operationLog.warn("Invalid alternatives definition \"" + alternative + "\" for column "
+                                + columnName + " of table " + tableName);
+                        continue;
+                    }
+                    alternativeSet.add(alternative.substring(1, alternative.length() - 1));
+                }
+                getTableRestrictions(tableName).checkedConstraintsMap.put(columnName, alternativeSet);
+            }
+        }
+    }
+
+    /**
+     * @return The table restrictions of <var>tableName</var>
+     */
+    // @Private
+    DBTableRestrictions getTableRestrictions(String tableName)
+    {
+        DBTableRestrictions table = tableMap.get(tableName);
+        if (table == null)
+        {
+            table = new DBTableRestrictions();
+            tableMap.put(tableName, table);
+        }
+        return table;
+    }
+
+    /**
+     * Check <var>value</var> against the restrictions (maximal length and possible check constraints) of column
+     * <var>columnName</var> of table <var>tableName</var>.
+     * 
+     * @throws UserFailureException If the <var>value</var> violates the constraints.
+     */
+    public void check(String tableName, String columnName, String value) throws UserFailureException
+    {
+        final DBTableRestrictions restrictions = tableMap.get(tableName);
+        assert restrictions != null : "Illegal table " + tableName;
+        final int maxLength = restrictions.getLength(columnName);
+        if (value == null)
+        {
+            // TODO 2007-06-04, Bernd Rinn: add correct 'NON NULL' check
+            return;
+        }
+        final Set<String> checkedConstraint = restrictions.getCheckedConstaint(columnName);
+        if (checkedConstraint != null && checkedConstraint.contains(value) == false)
+        {
+            throw UserFailureException.fromTemplate(
+                    "Value '%s' is not one of the allowed alternatives %s of column %s.%s.", value,
+                    toString(checkedConstraint), tableName, columnName);
+        }
+        if (value.length() > maxLength)
+        {
+            throw UserFailureException.fromTemplate("Value '%s' is longer than the maximum length %d of column %s.%s.",
+                    value, maxLength, tableName, columnName);
+        }
+    }
+
+    private String toString(Set<String> alternatives)
+    {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("{ ");
+        for (String alternative : alternatives)
+        {
+            builder.append(alternative);
+            builder.append(", ");
+        }
+        builder.setLength(builder.length() - 2);
+        builder.append(" }");
+        return builder.toString();
+    }
+}
---
+++ common/sourceTest/java/ch/systemsx/cisd/common/parser/DBRestrictionsTest.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2007 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.common.parser;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import ch.systemsx.cisd.common.exceptions.UserFailureException;
+import ch.systemsx.cisd.common.logging.LogInitializer;
+import ch.systemsx.cisd.common.utilities.FileUtilities;
+
+/**
+ * Test cases for the {@link DBRestrictions}.
+ * 
+ * @author Bernd Rinn
+ */
+public class DBRestrictionsTest
+{
+
+    static
+    {
+        LogInitializer.init();
+    }
+
+    @Test
+    public void testNormalize()
+    {
+        final List<String> normalizedList = DBRestrictions.normalize(" a  1 ;; B\t2;\n\nC; \n--D\n E ");
+        assertEquals(Arrays.asList("a 1", "b 2", "c", "e"), normalizedList);
+    }
+
+    @Test
+    public void testGetDomains()
+    {
+        final List<String> domainScript =
+                Arrays.asList("create table sometable", "create domain user_id as varchar(15)",
+                        "create domain bla for varchar(0)", "create domain code as varchar(8)",
+                        "create domain description_80 as varchar(81)");
+        final Map<String, Integer> domains = DBRestrictions.parseDomains(domainScript);
+        final Map<String, Integer> expectedDomains = new HashMap<String, Integer>();
+        expectedDomains.put("user_id", 15);
+        expectedDomains.put("code", 8);
+        expectedDomains.put("description_80", 81);
+        assertEquals(expectedDomains, domains);
+    }
+
+    @Test
+    public void testColumnLengths()
+    {
+        final String sqlScript =
+                FileUtilities.loadToString(getClass(), "/ch/systemsx/cisd/common/parser/DBRestrictionsTest.sql");
+        assert sqlScript != null;
+        final DBRestrictions parser = new DBRestrictions(sqlScript);
+
+        assertEquals(10, parser.getTableRestrictions("contacts").getLength("cnta_type"));
+        assertEquals(30, parser.getTableRestrictions("contacts").getLength("firstname"));
+        assertEquals(1, parser.getTableRestrictions("contacts").getLength("midinitial"));
+        assertEquals(30, parser.getTableRestrictions("contacts").getLength("lastname"));
+        assertEquals(50, parser.getTableRestrictions("contacts").getLength("email"));
+        assertEquals(15, parser.getTableRestrictions("contacts").getLength("user_id"));
+
+        assertEquals(8, parser.getTableRestrictions("material_types").getLength("code"));
+        assertEquals(80, parser.getTableRestrictions("material_types").getLength("description"));
+
+        assertEquals(50, parser.getTableRestrictions("materials").getLength("name"));
+        assertEquals(4, parser.getTableRestrictions("materials").getLength("mate_sub_type"));
+    }
+    
+    @Test(expectedExceptions = AssertionError.class)
+    public void testInvalidTable()
+    {
+        final DBRestrictions parser = new DBRestrictions("");
+        assertEquals(Integer.MAX_VALUE, parser.getTableRestrictions("doesnotexit").getLength("doesnotexist"));
+    }
+    
+    @Test(expectedExceptions = AssertionError.class)
+    public void testInvalidColumn()
+    {
+        final DBRestrictions parser = new DBRestrictions("create table tab (a integer, b varchar(1))");
+        assertEquals(Integer.MAX_VALUE, parser.getTableRestrictions("tab").getLength("doesnotexist"));
+    }
+    
+    @Test
+    public void testCheckedConstraints()
+    {
+        final String sqlScript =
+            FileUtilities.loadToString(getClass(), "/ch/systemsx/cisd/common/parser/DBRestrictionsTest.sql");
+        assert sqlScript != null;
+        final DBRestrictions parser = new DBRestrictions(sqlScript);
+
+        assertEquals(new HashSet<String>(Arrays.asList("PERS", "ORGA")), parser.getTableRestrictions("contacts")
+                .getCheckedConstaint("cnta_type"));
+        assertEquals(new HashSet<String>(Arrays.asList("STOB", "MATE")), parser.getTableRestrictions("materials")
+                .getCheckedConstaint("mate_sub_type"));
+    }
+    
+    @Test
+    public void testCheckOK()
+    {
+        final String sqlScript =
+            FileUtilities.loadToString(getClass(), "/ch/systemsx/cisd/common/parser/DBRestrictionsTest.sql");
+        assert sqlScript != null;
+        final DBRestrictions parser = new DBRestrictions(sqlScript);
+
+        parser.check("contacts", "cnta_type", "ORGA");
+        parser.check("material_types", "code", "somecode");
+    }
+
+    @Test(expectedExceptions = UserFailureException.class)
+    public void testCheckViolateLength()
+    {
+        final String sqlScript =
+            FileUtilities.loadToString(getClass(), "/ch/systemsx/cisd/common/parser/DBRestrictionsTest.sql");
+        assert sqlScript != null;
+        final DBRestrictions parser = new DBRestrictions(sqlScript);
+
+        try
+        {
+            parser.check("material_types", "code", "somecode1");
+        } catch (UserFailureException ex)
+        {
+            System.err.println(ex.getMessage());
+            throw ex;
+        }
+    }
+
+    @Test(expectedExceptions = UserFailureException.class)
+    public void testCheckAlternatives()
+    {
+        final String sqlScript =
+            FileUtilities.loadToString(getClass(), "/ch/systemsx/cisd/common/parser/DBRestrictionsTest.sql");
+        assert sqlScript != null;
+        final DBRestrictions parser = new DBRestrictions(sqlScript);
+
+        try
+        {
+            parser.check("materials", "mate_sub_type", "stob");
+        } catch (UserFailureException ex)
+        {
+            System.err.println(ex.getMessage());
+            throw ex;
+        }
+    }
+}
