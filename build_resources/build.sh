764523a76fe1531cc6d7efc57eeadc55e890d308
felmer <felmer>
2007-06-26 09:29:47 +0000

    allow private or public builds
    SVN: 682

--- build_resources/build.sh
+++ build_resources/build.sh
@@ -1,9 +1,16 @@
 #! /bin/sh
 
 if [ $# -eq 0 ]; then
-    echo "Usage is $0 <project name> [<version>]"
+    echo "Usage is $0 [--private] <project name> [<version>]"
     exit 1
 fi
+
+protocol=http
+if [ $1 -eq "--private" ]; then
+		protocol=svn+ssh
+		shift
+fi
+repository=$protocol://source.systemsx.ch/repos
 
 name=$1
 version=trunk
@@ -18,6 +25,7 @@ build_dir=$cdir/tmp
 rm -rf $build_dir
 mkdir $build_dir
 ant -lib $dir/lib/ecj.jar -f $dir/ant/build.xml \
+		-DrepositoryRoot=$repository
     -Dname=$name \
     -Dversion=$version \
     -Ddir=$build_dir \
