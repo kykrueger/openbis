aafdc9b3165d26d14e8dc030331696fd43b9300f
ribeaudc <ribeaudc>
2007-08-07 10:59:15 +0000

    add: - Unit test for 'ConverterPool' - default conversion for predefined primitive resp. java types remove: - 'IdentityStringConverter' no longer needed
    SVN: 1254

--- common/source/java/ch/systemsx/cisd/common/converter/ConverterPool.java
+++ common/source/java/ch/systemsx/cisd/common/converter/ConverterPool.java
@@ -42,9 +42,7 @@ public final class ConverterPool
 
     private final static Map<Class<?>, Converter<?>> createConverters()
     {
-        final Map<Class<?>, Converter<?>> converterMap = new HashMap<Class<?>, Converter<?>>();
-        converterMap.put(String.class, new IdentityStringConverter());
-        return converterMap;
+        return new HashMap<Class<?>, Converter<?>>();
     }
 
     /** Returns <code>instance</code>. */
@@ -54,10 +52,59 @@ public final class ConverterPool
     }
 
     /**
+     * Default conversion if no specific <code>Converter</code> has been defined.
+     */
+    @SuppressWarnings("unchecked")
+    private final static <T> T defaultConvert(String value, Class<T> type)
+    {
+        if (value == null)
+        {
+            return null;
+        }
+        if (type.equals(Integer.class) || type.equals(Integer.TYPE))
+        {
+            return (T) Integer.valueOf(value);
+        } else if (type.equals(Long.class) || type.equals(Long.TYPE))
+        {
+            return (T) Long.valueOf(value);
+        } else if (type.equals(Float.class) || type.equals(Float.TYPE))
+        {
+            return (T) Float.valueOf(value);
+        } else if (type.equals(Double.class) || type.equals(Double.TYPE))
+        {
+            return (T) Double.valueOf(value);
+        } else if (type.equals(Byte.class) || type.equals(Byte.TYPE))
+        {
+            return (T) Byte.valueOf(value);
+        } else if (type.equals(Short.class) || type.equals(Short.TYPE))
+        {
+            return (T) Short.valueOf(value);
+        } else if (type.equals(Character.class) || type.equals(Character.TYPE))
+        {
+            if (value.length() > 0)
+            {
+                return (T) Character.valueOf(value.charAt(0));
+            } else
+            {
+                return (T) new Character('\0');
+            }
+        } else if (type.equals(String.class))
+        {
+            return (T) value;
+        } else if (type.equals(Boolean.class) || type.equals(Boolean.TYPE))
+        {
+            return (T) Boolean.valueOf(value);
+        }
+        throw new IllegalArgumentException("No converter for type '" + type.getCanonicalName() + "'.");
+    }
+
+    /**
      * Registers given <code>Converter</code> for given <code>Class</code>.
      */
     public final <T> void registerConverter(Class<T> type, Converter<T> converter)
     {
+        assert type != null;
+        assert converter != null;
         converters.put(type, converter);
     }
 
@@ -66,31 +113,37 @@ public final class ConverterPool
      */
     public final <T> void unregisterConverter(Class<T> type)
     {
+        assert type != null;
         converters.remove(type);
     }
 
-    /** 
+    /**
      * Performs the conversion.
      * 
-     *  @throws IllegalArgumentException If there is no converter for <var>type</var>.
+     * @throws IllegalArgumentException If there is no converter for <var>type</var>.
      */
     public final <T> T convert(String value, Class<T> type)
     {
+        assert type != null;
         final Converter<T> converter = getConverter(type);
+        T returned = null;
         if (converter == null)
         {
-            throw new IllegalArgumentException("No converter for type '" + type.getCanonicalName() + "'.");
-        }
-        if (value == null)
+            returned = defaultConvert(value, type);
+        } else
         {
-            converter.getDefaultValue();
+            returned = converter.convert(value);
         }
-        return converter.convert(value);
+        if (returned == null && converter != null)
+        {
+            return converter.getDefaultValue();
+        }
+        return returned;
     }
 
-    // The setter registerConverter() will ensure that no converter can be entered that is of the wrong type. 
+    // The setter registerConverter() will ensure that no converter can be entered that is of the wrong type.
     @SuppressWarnings("unchecked")
-    private final <T> Converter<T> getConverter(Class<T> type)
+    final <T> Converter<T> getConverter(Class<T> type)
     {
         return (Converter<T>) converters.get(type);
     }
