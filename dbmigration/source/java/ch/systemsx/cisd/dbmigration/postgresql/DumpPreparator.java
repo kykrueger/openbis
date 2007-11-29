2736d508498af0ad0fb0253703b29c66e8d2ba47
felmer <felmer>
2007-11-29 16:13:08 +0000

    LMS-148 DumpPreparator with test
    SVN: 2859

---
+++ dbmigration/source/java/ch/systemsx/cisd/dbmigration/postgresql/DumpPreparator.java
@@ -0,0 +1,365 @@
+/*
+ * Copyright 2007 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.dbmigration.postgresql;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.JFileChooser;
+
+/**
+ * Helper application which creates from a PostgreSQL dump file to be used to setup the database.
+ * <p> 
+ * <b>Important:</b> When creating the dump file with <code>pg_dump</code> use the option <b>--no-owner</b>.
+ * <p>
+ * Note, that this is a stand alone class which only uses J2EE library classes. 
+ *
+ * @author Franz-Josef Elmer
+ */
+public class DumpPreparator
+{
+    private static final Set<String> FILTERED_SCHEMA_LINES =
+            new LinkedHashSet<String>(Arrays.asList("SET client_encoding = 'UTF8';",
+                                                    "COMMENT ON SCHEMA public IS 'Standard public schema';", 
+                                                    "CREATE PROCEDURAL LANGUAGE plpgsql;"));
+    
+    public static void main(String[] args) throws IOException
+    {
+        if (args.length < 1)
+        {
+            System.out.println("Usage: java ch.systemsx.cisd.dbmigration.postgresql.DumpPreparator "
+                    + "<main folder> [<dump file>]");
+            System.exit(1);
+        }
+        File destination = new File(args[0]);
+        File dumpFile;
+        if (args.length == 1)
+        {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setSelectedFile(new File(System.getProperty("user.dir")));
+            int response = fileChooser.showOpenDialog(null);
+            if (response != JFileChooser.APPROVE_OPTION)
+            {
+                System.exit(0);
+            }
+            dumpFile = fileChooser.getSelectedFile();
+        } else
+        {
+            dumpFile = new File(args[1]);
+        }
+        System.out.println("Parsing PostgreSQL dump file " + dumpFile.getAbsolutePath());
+        Reader reader = new FileReader(dumpFile);
+        try
+        {
+            createUploadFiles(reader, destination);
+        } finally {
+            reader.close();
+        }
+        System.out.println("Dump preparation successfully finished.");
+    }
+    
+    static void createUploadFiles(Reader dumpReader, File destinationFolder) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(dumpReader);
+        String line;
+        State state = State.SCHEMA;
+        UploadFileManager uploadFileManager = new UploadFileManager(destinationFolder, FILTERED_SCHEMA_LINES);
+        while ((line = reader.readLine()) != null)
+        {
+            if (line.length() != 0 && line.startsWith("--") == false)
+            {
+                state = state.processLine(line, uploadFileManager);
+            }
+        }
+        uploadFileManager.save();
+    }
+    
+    private static enum State
+    {
+        SCHEMA()
+        {
+            @Override
+            State processLine(String line, UploadFileManager manager)
+            {
+                if (COPY_PATTERN.matcher(line).matches())
+                {
+                    return COPY_LINE.processLine(line, manager);
+                }
+                manager.addSchemaLine(line);
+                return this;
+            }
+        },
+        COPY_LINE
+        {
+            @Override
+            State processLine(String line, UploadFileManager manager)
+            {
+                Matcher matcher = COPY_PATTERN.matcher(line);
+                if (matcher.matches())
+                {
+                    manager.createUploadFile(matcher.group(1));
+                } else {
+                    throw new IllegalArgumentException("Couldn't extract table name from the following line: " + line);
+                }
+                return IN_COPY;
+            }
+        }, 
+        IN_COPY
+        {
+            @Override
+            State processLine(String line, UploadFileManager manager)
+            {
+                return manager.addTableLine(line) ? COPY : this;
+            }
+        },
+        COPY
+        {
+            @Override
+            State processLine(String line, UploadFileManager manager)
+            {
+                if (COPY_PATTERN.matcher(line).matches())
+                {
+                    return COPY_LINE.processLine(line, manager);
+                }
+                return FIX.processLine(line, manager);
+            }
+        },
+        FIX
+        {
+            @Override
+            State processLine(String line, UploadFileManager manager)
+            {
+                manager.addFinishLine(line);
+                return this;
+            }
+        };
+        
+        private static final Pattern COPY_PATTERN = Pattern.compile("COPY (\\w*).*");
+
+        State processLine(String line, UploadFileManager manager)
+        {
+            return this;
+        }
+
+    }
+    
+    private static class UploadFileManager
+    {
+        private static final String VERSION_LOGS_TABLE = "database_version_logs";
+        private static final Pattern VERSION_PATTERN = Pattern.compile("(\\d{3})\\t.*");
+        
+        private final File destinationFolder;
+        private final Set<String> filteredSchemaLines;
+        private final StringWriter schemaScript = new StringWriter();
+        private final PrintWriter schemaPrinter = new PrintWriter(schemaScript, true);
+        private final StringWriter finishScript = new StringWriter();
+        private final PrintWriter finishPrinter = new PrintWriter(finishScript, true);
+        private final Map<String, Table> tables = new LinkedHashMap<String, Table>();
+        
+        private Table currentTable;
+        
+        UploadFileManager(File destinationFolder, Set<String> filteredSchemaLines)
+        {
+            this.destinationFolder = destinationFolder;
+            this.filteredSchemaLines = filteredSchemaLines;
+        }
+
+        void addSchemaLine(String line)
+        {
+            if (filteredSchemaLines.contains(line) == false)
+            {
+                schemaPrinter.println(line);
+            }
+        }
+        
+        void addFinishLine(String line)
+        {
+            finishPrinter.println(line);
+        }
+
+        void createUploadFile(String tableName)
+        {
+            currentTable = new Table(tableName);
+            tables.put(tableName, currentTable);
+        }
+        
+        boolean addTableLine(String line)
+        {
+            if (currentTable == null)
+            {
+                throw new IllegalStateException("No table created to add the following line: " + line);
+            }
+            if (line.startsWith("\\."))
+            {
+                return true;
+            }
+            currentTable.addRow(line);
+            return false; 
+        }
+        
+        void save() throws IOException
+        {
+            String databaseVersion = getDatabaseVersion();
+            File folder = createFolder(databaseVersion);
+            writeTo(folder, "schema-" + databaseVersion + ".sql", Arrays.asList(schemaScript.toString()));
+            for (Table table : tables.values())
+            {
+                List<String> rows = table.getRows();
+                if (rows.size() > 0)
+                {
+                    writeTo(folder, table.getUploadFileName(), rows);
+                }
+            }
+            writeTo(folder, "finish-" + databaseVersion + ".sql", Arrays.asList(finishScript.toString()));
+        }
+
+        private void writeTo(File folder, String fileName, List<String> lines) throws IOException
+        {
+            FileWriter writer = null;
+            try
+            {
+                File file = new File(folder, fileName);
+                System.out.println("   create " + file.getAbsolutePath());
+                writer = new FileWriter(file);
+                PrintWriter printWriter = new PrintWriter(writer, true);
+                for (String line : lines)
+                {
+                    printWriter.println(line);
+                }
+            } finally
+            {
+                if (writer != null)
+                {
+                    writer.close();
+                }
+            }
+        }
+
+        private File createFolder(String databaseVersion)
+        {
+            File folder = new File(destinationFolder, databaseVersion);
+            if (folder.exists())
+            {
+                if (folder.isDirectory())
+                {
+                    File[] files = folder.listFiles(new FileFilter()
+                        {
+                            public boolean accept(File pathname)
+                            {
+                                if (pathname.isDirectory())
+                                {
+                                    return false;
+                                }
+                                String name = pathname.getName();
+                                return name.endsWith(".tsv") || name.endsWith(".sql");
+                            }
+                        });
+                    for (File file : files)
+                    {
+                        if (file.delete() == false)
+                        {
+                            throw new IllegalStateException("Couldn't delete file '" + file.getAbsolutePath()
+                                    + "' for some unknown reasons.");
+                        }
+                    }
+                } else
+                {
+                    throw new IllegalStateException("Is not a directory: " + folder.getAbsolutePath());
+                }
+            } else
+            {
+                if (folder.mkdirs() == false)
+                {
+                    throw new IllegalStateException("Couldn't create folder '" + folder.getAbsolutePath()
+                            + "' for some unknown reason.");
+                }
+            }
+            return folder;
+        }
+        
+        private String getDatabaseVersion()
+        {
+            Table logTable = tables.get(VERSION_LOGS_TABLE);
+            if (logTable == null)
+            {
+                throw new IllegalStateException("Table '" + VERSION_LOGS_TABLE + "' missing.");
+            }
+            List<String> rows = logTable.getRows();
+            String result = "000";
+            for (String row : rows)
+            {
+                Matcher matcher = VERSION_PATTERN.matcher(row);
+                if (matcher.matches() == false)
+                {
+                    throw new IllegalArgumentException("Row does not start with a version number: " + row);
+                }
+                String v = matcher.group(1);
+                if (v.compareTo(result) > 0)
+                {
+                    result = v;
+                }
+            }
+            return result;
+        }
+    }
+    
+    private static class Table
+    {
+        private static int counter;
+        
+        private final String uploadFileName;
+        private final List<String> rows = new ArrayList<String>();
+
+        Table(String tableName)
+        {
+            uploadFileName = String.format("%03d=%s.tsv", ++counter, tableName);
+        }
+        
+        void addRow(String line)
+        {
+            rows.add(line);
+        }
+
+        public final String getUploadFileName()
+        {
+            return uploadFileName;
+        }
+
+        public final List<String> getRows()
+        {
+            return rows;
+        }
+
+    }
+
+}
---
+++ dbmigration/sourceTest/java/ch/systemsx/cisd/dbmigration/postgresql/DumpPreparatorTest.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2007 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.dbmigration.postgresql;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.testng.annotations.Test;
+
+import ch.systemsx.cisd.common.utilities.FileUtilities;
+
+/**
+ * 
+ *
+ * @author Franz-Josef Elmer
+ */
+public class DumpPreparatorTest
+{
+    private static final File TEST_FOLDER = new File("targets");
+    
+    private static final String EXAMPLE = "--\n" + 
+            "-- PostgreSQL database dump\n" + 
+            "--\n" + 
+            "\n" + 
+            "-- Started on 2007-11-28 13:36:10\n" + 
+            "\n" + 
+            "SET client_encoding = \'UTF8\';\n" + 
+            "SET standard_conforming_strings = off;\n" + 
+            "COMMENT ON SCHEMA public IS 'Standard public schema';\n" + 
+            "CREATE PROCEDURAL LANGUAGE plpgsql;\n" + 
+            "--\n" + 
+            "-- TOC entry 1908 (class 0 OID 326942)\n" + 
+            "-- Dependencies: 1374\n" + 
+            "-- Data for Name: data; Type: TABLE DATA; Schema: public; Owner: felmer\n" + 
+            "--\n" + 
+            "\n" + 
+            "COPY data (id, registration_timestamp, obty_id) FROM stdin;\n" + 
+            "\\.\n" + 
+            "\n" + 
+            "\n" + 
+            "--\n" + 
+            "-- TOC entry 1890 (class 0 OID 326868)\n" + 
+            "-- Dependencies: 1356\n" + 
+            "-- Data for Name: data_types; Type: TABLE DATA; Schema: public; Owner: felmer\n" + 
+            "--\n" + 
+            "\n" + 
+            "COPY data_types (id, code, description) FROM stdin;\n" + 
+            "1\tVARCHAR\tVariable length character\n" + 
+            "2\tINTEGER\tInteger\n" + 
+            "3\tREAL\tReal number, i.e. an inexact, variable-precision numeric type\n" + 
+            "\\.\n" + 
+            "\n" + 
+            "\n" + 
+            "--\n" + 
+            "-- TOC entry 1891 (class 0 OID 326873)\n" + 
+            "-- Dependencies: 1357\n" + 
+            "-- Data for Name: data_values; Type: TABLE DATA; Schema: public; Owner: felmer\n" + 
+            "--\n" + 
+            "\n" + 
+            "COPY data_values (id, data_id, saco_id, value) FROM stdin;\n" + 
+            "\\.\n" + 
+            "\n" + 
+            "\n" + 
+            "--\n" + 
+            "-- TOC entry 1887 (class 0 OID 326837)\n" + 
+            "-- Dependencies: 1353\n" + 
+            "-- Data for Name: database_version_logs; Type: TABLE DATA; Schema: public; Owner: felmer\n" + 
+            "--\n" + 
+            "\n" + 
+            "COPY database_version_logs (db_version, module_name, run_status) FROM stdin;\n" + 
+            "011\tsource/sql/postgresql/010/schema-010.sql\tSUCCESS 2007-11-22 08:46:04.25\n" + 
+            "010\tsource/sql/postgresql/010/data-010.sql\tSUCCESS 2007-11-22 08:46:04.53\n" + 
+            "\\.\n" + 
+            "\n" + 
+            "\n" + 
+            "\n" + 
+            "--\n" + 
+            "-- TOC entry 1823 (class 2606 OID 327015)\n" + 
+            "-- Dependencies: 1374 1374\n" + 
+            "-- Name: data_pk; Type: CONSTRAINT; Schema: public; Owner: felmer; Tablespace: \n" + 
+            "--\n" + 
+            "\n" + 
+            "ALTER TABLE ONLY data\n" + 
+            "    ADD CONSTRAINT data_pk PRIMARY KEY (id);\n" + 
+            "\n" + 
+            "-- Completed on 2007-11-28 13:36:13\n" + 
+            "\n" + 
+            "--\n" + 
+            "-- PostgreSQL database dump complete\n" + 
+            "--\n";
+    
+    @Test
+    public void test() throws IOException
+    {
+        StringReader reader = new StringReader(EXAMPLE);
+        DumpPreparator.createUploadFiles(reader, TEST_FOLDER);
+        
+        File folder = new File(TEST_FOLDER, "011");
+        assertEquals(true, folder.exists());
+        assertEquals(true, folder.isDirectory());
+        assertEquals("SET standard_conforming_strings = off;\n\n", 
+                     FileUtilities.loadToString(new File(folder, "schema-011.sql")));
+        assertEquals("1\tVARCHAR\tVariable length character\n" 
+                     + "2\tINTEGER\tInteger\n" 
+                     + "3\tREAL\tReal number, i.e. an inexact, variable-precision numeric type\n", 
+                     FileUtilities.loadToString(new File(folder, "002=data_types.tsv")));
+        assertEquals("011\tsource/sql/postgresql/010/schema-010.sql\tSUCCESS 2007-11-22 08:46:04.25\n"
+                     + "010\tsource/sql/postgresql/010/data-010.sql\tSUCCESS 2007-11-22 08:46:04.53\n", 
+                     FileUtilities.loadToString(new File(folder, "004=database_version_logs.tsv")));
+        assertEquals("ALTER TABLE ONLY data\n" 
+                     + "    ADD CONSTRAINT data_pk PRIMARY KEY (id);\n\n", 
+                     FileUtilities.loadToString(new File(folder, "finish-011.sql")));
+        assertEquals(4, folder.listFiles().length);
+    }
+}
