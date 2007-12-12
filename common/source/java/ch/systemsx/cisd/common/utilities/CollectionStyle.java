1e3bea72e0428cccf8f58a3799ca380c713d2e7b
ribeaudc <ribeaudc>
2007-12-12 08:46:45 +0000

    fix: - Smoke tests.
    SVN: 3045

--- common/source/java/ch/systemsx/cisd/common/utilities/CollectionStyle.java
+++ common/source/java/ch/systemsx/cisd/common/utilities/CollectionStyle.java
@@ -8,7 +8,8 @@ package ch.systemsx.cisd.common.utilities;
 public enum CollectionStyle
 {
     /** Default <code>CollectionStyle</code>. */
-    DEFAULT_COLLECTION_STYLE("[", "]", ", "), NO_BOUNDARY_COLLECTION_STYLE("", "", ", ");
+    DEFAULT_COLLECTION_STYLE("[", "]", ", "), NO_BOUNDARY_COLLECTION_STYLE("", "", ", "), NICE_COLLECTION_STYLE("'",
+            "'", "', '");
 
     private final String collectionStart;
 
