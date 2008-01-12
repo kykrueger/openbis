d04e8708397599c92283086945a46b3e7b338431
brinn <brinn>
2008-01-12 11:29:15 +0000

    add possibility to build other revisions than HEAD
    SVN: 3485

--- build_resources/build.sh
+++ build_resources/build.sh
@@ -1,16 +1,23 @@
 #! /bin/sh
 
 if [ $# -eq 0 ]; then
-    echo "Usage is $0 [--private] <project name> [<version>]"
+    echo "Usage is $0 [--private] [--revision <revision>] <project name> [<version>]"
     exit 1
 fi
 
 protocol=http
 if [ "$1" = "--private" ]; then
-		protocol=svn+ssh
-		shift
+	protocol=svn+ssh
+	shift
 fi
 repository=$protocol://source.systemsx.ch/repos
+if [ "$1" = "--revision" ]; then
+	shift
+	revision="$1"
+	shift
+else
+	revision="HEAD"
+fi
 
 name=$1
 version=trunk
@@ -27,6 +34,7 @@ mkdir $build_dir
 ant -lib $dir/lib/ecj.jar -f $dir/ant/build.xml \
 		-DrepositoryRoot=$repository \
     -Dname=$name \
+    -drevision=$revision \
     -Dversion=$version \
     -Ddir=$build_dir \
     -Dresult.dir=$cdir \
