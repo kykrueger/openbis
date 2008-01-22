d6ee5929ca5e8871c2552f9790f21f63808d9e3e
felmer <felmer>
2008-01-22 10:46:11 +0000

    LogInvocationHandler moved out of DomainModel to project 'common' (together with LogAnnotation).
    SVN: 3699

---
+++ common/source/java/ch/systemsx/cisd/common/logging/LogInvocationHandler.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2008 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.common.logging;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.UndeclaredThrowableException;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Invocation handler used to log invocations.
+ * 
+ *
+ * @author Franz-Josef Elmer
+ */
+public final class LogInvocationHandler implements InvocationHandler
+{
+    private final Object object;
+    private final String name;
+    private final Class<?> classUsedToNameLogger;
+
+    /**
+     * Creates a new instance.
+     *
+     * @param object Object whos invocations should be logged.
+     * @param name Meaningful name of <code>object</code>. Will be used in the log message.
+     * @param classUsedToNameLogger Class used to specify the name of the logger.
+     */
+    public LogInvocationHandler(Object object, String name, Class<?> classUsedToNameLogger)
+    {
+        this.object = object;
+        this.name = name;
+        this.classUsedToNameLogger = classUsedToNameLogger;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
+    {
+        long time = System.currentTimeMillis();
+        Throwable throwable = null;
+        try
+        {
+            return method.invoke(object, args);
+        } catch (InvocationTargetException e)
+        {
+            throwable = e.getCause();
+            throw throwable;
+        } catch (UndeclaredThrowableException e)
+        {
+            throwable = e.getCause();
+            throw throwable;
+        } catch (Throwable t)
+        {
+            throwable = t;
+            throw t;
+        } finally
+        {
+            StringBuilder builder = new StringBuilder(throwable == null ? "Successful" : "Failed");
+            builder.append(" invocation of ");
+            builder.append(name).append('.').append(method.getName()).append('(');
+            if (args != null)
+            {
+                for (int i = 0; i < args.length; i++)
+                {
+                    builder.append(args[i]);
+                    if (i < args.length - 1)
+                    {
+                        builder.append(", ");
+                    }
+                }
+            }
+            builder.append(") took ").append(System.currentTimeMillis() - time).append(" msec");
+            Logger logger = createLogger(method);
+            if (throwable == null)
+            {
+                logger.info(builder);
+            } else
+            {
+                logger.error(builder, throwable);
+            }
+        }
+    }
+
+    private Logger createLogger(Method method)
+    {
+        LogAnnotation annotation = method.getAnnotation(LogAnnotation.class);
+        LogCategory logCategory = annotation == null ? LogCategory.ACCESS : annotation.logCategory();
+        return LogFactory.getLogger(logCategory, classUsedToNameLogger);
+    }
+}
