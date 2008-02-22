abfe89b5e5c212c33d6b3c734e8950cc4e9f6c37
brinn <brinn>
2008-02-22 20:27:03 +0000

    change: make the initiation of the moving process robust with respect to deviating clocks on Datamover host and data producer host add: test for QuietPeriodFileFilter
    SVN: 4429

--- datamover/source/java/ch/systemsx/cisd/datamover/utils/QuietPeriodFileFilter.java
+++ datamover/source/java/ch/systemsx/cisd/datamover/utils/QuietPeriodFileFilter.java
@@ -17,45 +17,207 @@
 package ch.systemsx.cisd.datamover.utils;
 
 import java.io.FileFilter;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+import ch.systemsx.cisd.common.logging.LogCategory;
+import ch.systemsx.cisd.common.logging.LogFactory;
 import ch.systemsx.cisd.common.utilities.StoreItem;
 import ch.systemsx.cisd.datamover.filesystem.intf.IFileStore;
 import ch.systemsx.cisd.datamover.intf.ITimingParameters;
 
 /**
  * A {@link FileFilter} that picks all entries that haven't been changed for longer than some given quiet period.
+ * <p>
+ * Note that the system is designed to be robust when the clocks of the host that runs the datamover and the host that
+ * writes the files are out of sync. To this end, we are <i>never<i> comparing times as provided by the datamover host
+ * clock with last modification times. Instead we wait for some quiet period as defined solely by the clock of the
+ * datamover host. After that time we require the last modification time to be not younger than the last time we checked
+ * before we {@link #accept(StoreItem)} the path.
  * 
  * @author Bernd Rinn
  */
 public class QuietPeriodFileFilter
 {
+    // @Private
+    static final int CLEANUP_TO_QUIET_PERIOD_RATIO = 10;
 
-    private static final long SAFETY_MARGIN_MILLIS = 2000L;
+    // @Private
+    final static int MAX_CALLS_BEFORE_CLEANUP = 10000;
+
+    private static final String TIME_FORMAT_TEMPLATE = "%#$tY-%#$tm-%#$td %#$tH:%#$tM:%#$tS";
+
+    private final static Logger operationLog = LogFactory.getLogger(LogCategory.OPERATION, QuietPeriodFileFilter.class);
+
+    private final static Logger machineLog = LogFactory.getLogger(LogCategory.MACHINE, QuietPeriodFileFilter.class);
 
     private final long quietPeriodMillis;
 
-    private final IFileStore store;
+    private final long cleanUpPeriodMillis;
+
+    private final IFileStore fileStore;
+
+    private final Map<StoreItem, PathCheckRecord> pathMap;
+
+    private final ICurrentTimeProvider timeProvider;
+
+    private int callCounter;
+
+    /**
+     * A value object that holds the information about the last check performed for a path.
+     */
+    private static final class PathCheckRecord
+    {
+        final private long timeChecked;
+
+        final private long timeOfLastModification;
+
+        PathCheckRecord(final long timeChecked, final long timeLastChanged)
+        {
+            this.timeChecked = timeChecked;
+            this.timeOfLastModification = timeLastChanged;
+        }
+
+        /**
+         * The time when the entry was checked.
+         */
+        public long getTimeChecked()
+        {
+            return timeChecked;
+        }
+
+        /**
+         * The newest last modification time found during the check.
+         */
+        public long getTimeOfLastModification()
+        {
+            return timeOfLastModification;
+        }
+    }
+
+    /**
+     * Provider for the current time in milli-seconds since the Epoch. Overwrite this in unit-tests.
+     */
+    // @Private
+    interface ICurrentTimeProvider
+    {
+        long getCurrentTimeMillis();
+    }
 
     /**
      * Creates a <var>QuietPeriodFileFilter</var>.
      * 
-     * @param store The store in which items reside
+     * @param fileStore Used to check when a pathname was changed.
      * @param timingParameters The timing parameter object to get the quiet period from.
      */
-    public QuietPeriodFileFilter(IFileStore store, ITimingParameters timingParameters)
+    public QuietPeriodFileFilter(IFileStore fileStore, ITimingParameters timingParameters)
     {
-        assert store != null;
-        assert timingParameters != null;
+        this(fileStore, timingParameters, new ICurrentTimeProvider()
+            {
+                public long getCurrentTimeMillis()
+                {
+                    return System.currentTimeMillis();
+                }
+            });
+    }
 
-        this.store = store;
+    /**
+     * Creates a <var>QuietPeriodFileFilter</var>.
+     * 
+     * @param fileStore Used to check when a pathname was changed.
+     * @param timingParameters The timing parameter object to get the quiet period from.
+     * @param timeProvider A role that provides access to the current time.
+     */
+    public QuietPeriodFileFilter(IFileStore fileStore, ITimingParameters timingParameters,
+            ICurrentTimeProvider timeProvider)
+    {
+        assert timingParameters != null;
+        assert timingParameters.getQuietPeriodMillis() > 0;
+        assert fileStore != null;
+        assert timeProvider != null;
+
         this.quietPeriodMillis = timingParameters.getQuietPeriodMillis();
-        assert quietPeriodMillis > 0;
+        this.cleanUpPeriodMillis = CLEANUP_TO_QUIET_PERIOD_RATIO * quietPeriodMillis;
+        this.fileStore = fileStore;
+        this.timeProvider = timeProvider;
+        this.pathMap = new HashMap<StoreItem, PathCheckRecord>();
+        this.callCounter = 0;
     }
 
     public boolean accept(StoreItem item)
     {
-        final long stopWhenFindYoungerRelative = quietPeriodMillis - SAFETY_MARGIN_MILLIS;
-        return (System.currentTimeMillis() - store.lastChangedRelative(item, stopWhenFindYoungerRelative)) > quietPeriodMillis;
+        final long now = timeProvider.getCurrentTimeMillis();
+        try
+        {
+            final PathCheckRecord checkRecordOrNull = pathMap.get(item);
+            if (checkRecordOrNull == null) // new item
+            {
+                pathMap.put(item, new PathCheckRecord(now, fileStore.lastChanged(item, 0L)));
+                return false;
+            }
+            if (now - checkRecordOrNull.getTimeChecked() < quietPeriodMillis) // no need to check yet
+            {
+                return false;
+            }
+            final long oldLastChanged = checkRecordOrNull.getTimeOfLastModification();
+            final long newLastChanged = fileStore.lastChanged(item, oldLastChanged);
+            if (newLastChanged != oldLastChanged)
+            {
+                pathMap.put(item, new PathCheckRecord(now, newLastChanged));
+                if (newLastChanged < oldLastChanged) // that is not supposed to happen
+                {
+                    machineLog.error(getClockProblemLogMessage(item.getName(), checkRecordOrNull
+                            .getTimeChecked(), oldLastChanged, now, newLastChanged));
+                }
+                return false;
+            }
+            pathMap.remove(item);
+            return true;
+        } finally
+        {
+            if (++callCounter >= MAX_CALLS_BEFORE_CLEANUP)
+            {
+                cleanUpVanishedPaths(now);
+                callCounter = 0;
+            }
+        }
+    }
+
+    private void cleanUpVanishedPaths(long now)
+    {
+        final Iterator<Map.Entry<StoreItem, PathCheckRecord>> iter = pathMap.entrySet().iterator();
+        while(iter.hasNext())
+        {
+            final Map.Entry<StoreItem, PathCheckRecord> entry = iter.next();
+            final long timeSinceLastCheckMillis = now - entry.getValue().getTimeChecked();
+            if (timeSinceLastCheckMillis > cleanUpPeriodMillis)
+            {
+                final StoreItem item = entry.getKey();
+                operationLog.warn("Path '" + item.getName() + "' hasn't been checked for "
+                        + (timeSinceLastCheckMillis / 1000.0) + " s, removing from path map.");
+                iter.remove();
+            }
+        }
+    }
+
+    // @Private
+    static String getClockProblemLogMessage(final String pathname, final long oldCheck, final long oldLastChanged,
+            final long newCheck, final long newLastChanged)
+    {
+        return String.format("Last modification time of path '%1$s' jumped back: check at " + getTimeFormat(2)
+                + " -> last modification time " + getTimeFormat(3) + ", check at later time " + getTimeFormat(4)
+                + " -> last modification time " + getTimeFormat(5) + " (which is %6$d ms younger)", pathname, oldCheck,
+                oldLastChanged, newCheck, newLastChanged, (oldLastChanged - newLastChanged));
+    }
+
+    // @Private
+    static String getTimeFormat(int position)
+    {
+        return StringUtils.replace(TIME_FORMAT_TEMPLATE, "#", Integer.toString(position));
     }
 
 }
---
+++ datamover/sourceTest/java/ch/systemsx/cisd/datamover/utils/QuietPeriodFileFilterTest.java
@@ -0,0 +1,287 @@
+/*
+ * Copyright 2008 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.datamover.utils;
+
+import static org.testng.AssertJUnit.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.regex.Pattern;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import ch.systemsx.cisd.common.logging.LogCategory;
+import ch.systemsx.cisd.common.logging.LogMonitoringAppender;
+import ch.systemsx.cisd.common.utilities.StoreItem;
+import ch.systemsx.cisd.datamover.filesystem.intf.IFileStore;
+import ch.systemsx.cisd.datamover.intf.ITimingParameters;
+import ch.systemsx.cisd.datamover.utils.QuietPeriodFileFilter.ICurrentTimeProvider;
+
+/**
+ * Test cases for the quiet period file filter.
+ * 
+ * @author Bernd Rinn
+ */
+public class QuietPeriodFileFilterTest
+{
+    private final static String PATH_NAME = "testPath";
+
+    private final static StoreItem ITEM = new StoreItem(PATH_NAME);
+
+    private final static long QUIET_PERIOD_MILLIS = 100L;
+
+    private Mockery context;
+
+    private QuietPeriodFileFilter.ICurrentTimeProvider timeProvider;
+
+    private ITimingParameters timingParameters;
+
+    private IFileStore fileStore;
+
+    private QuietPeriodFileFilter filterUnderTest;
+
+    @BeforeMethod
+    public void setUp()
+    {
+        context = new Mockery();
+        timeProvider = context.mock(ICurrentTimeProvider.class);
+        timingParameters = context.mock(ITimingParameters.class);
+        fileStore = context.mock(IFileStore.class);
+        context.checking(new Expectations()
+            {
+                {
+                    atLeast(1).of(timingParameters).getQuietPeriodMillis();
+                    will(returnValue(QUIET_PERIOD_MILLIS));
+                }
+            });
+        filterUnderTest = new QuietPeriodFileFilter(fileStore, timingParameters, timeProvider);
+    }
+
+    @AfterMethod
+    public void tearDown()
+    {
+        // To following line of code should also be called at the end of each test method.
+        // Otherwise one do not known which test failed.
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void testGetTimeFormat()
+    {
+        assertEquals("%0$tY-%0$tm-%0$td %0$tH:%0$tM:%0$tS", QuietPeriodFileFilter.getTimeFormat(0));
+        assertEquals("%17$tY-%17$tm-%17$td %17$tH:%17$tM:%17$tS", QuietPeriodFileFilter.getTimeFormat(17));
+    }
+
+    @Test
+    public void testGetClockProblemLogMessage()
+    {
+        final String expected =
+                "Last modification time of path 'bla' jumped back: check at 1970-01-01 01:00:01 -> "
+                        + "last modification time 1970-01-01 01:00:50, check at later time 1970-01-01 01:00:10 -> "
+                        + "last modification time 1970-01-01 01:00:30 (which is 20000 ms younger)";
+        assertEquals(expected, QuietPeriodFileFilter.getClockProblemLogMessage("bla", 1000L, 50000L, 10000L, 30000L));
+    }
+
+    @Test
+    public void testInitialAcceptCall()
+    {
+        // 1000 - 500 > 100, but the initial call will never accept
+        final long nowMillis = 1000L;
+        final long pathLastChangedMillis = 500L;
+        context.checking(new Expectations()
+            {
+                {
+                    one(timeProvider).getCurrentTimeMillis();
+                    will(returnValue(nowMillis));
+                    one(fileStore).lastChanged(ITEM, 0L);
+                    will(returnValue(pathLastChangedMillis));
+                }
+            });
+        assertFalse(filterUnderTest.accept(ITEM));
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void testTwoAcceptCallsUntilAccept()
+    {
+        final long nowMillis1 = 1000L;
+        final long nowMillis2 = 1100L;
+        final long pathLastChangedMillis = 500L;
+        context.checking(new Expectations()
+            {
+                {
+                    // first call
+                    one(timeProvider).getCurrentTimeMillis();
+                    will(returnValue(nowMillis1));
+                    one(fileStore).lastChanged(ITEM, 0L);
+                    will(returnValue(pathLastChangedMillis));
+                    // second call
+                    one(timeProvider).getCurrentTimeMillis();
+                    will(returnValue(nowMillis2));
+                    one(fileStore).lastChanged(ITEM, pathLastChangedMillis);
+                    will(returnValue(pathLastChangedMillis));
+                }
+            });
+        assertFalse(filterUnderTest.accept(ITEM));
+        assertTrue(filterUnderTest.accept(ITEM));
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void testFrequentButUselessCallsUntilAccept()
+    {
+        final long nowMillis = 1000L;
+        final long pathLastChangedMillis = 500L;
+        context.checking(new Expectations()
+            {
+                {
+                    // first call
+                    one(timeProvider).getCurrentTimeMillis();
+                    will(returnValue(nowMillis));
+                    one(fileStore).lastChanged(ITEM, 0L);
+                    will(returnValue(pathLastChangedMillis));
+                    for (int i = 1; i <= 100; ++i)
+                    {
+                        one(timeProvider).getCurrentTimeMillis();
+                        will(returnValue(nowMillis + i));
+                    }
+                    // last call - will check only when last check is longer ago than the quiet period
+                    one(fileStore).lastChanged(ITEM, pathLastChangedMillis);
+                    will(returnValue(pathLastChangedMillis));
+                }
+            });
+        for (int i = 0; i < 100; ++i)
+        {
+            assertFalse(filterUnderTest.accept(ITEM));
+        }
+        assertTrue(filterUnderTest.accept(ITEM));
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void testThreeAcceptCallsUntilAccept()
+    {
+        final long nowMillis1 = 1000L;
+        final long nowMillis2 = 1100L;
+        final long nowMillis3 = 1200L;
+        final long pathLastChangedMillis1 = 1000L;
+        final long pathLastChangedMillis2 = 1001L;
+        context.checking(new Expectations()
+            {
+                {
+                    // first call
+                    one(timeProvider).getCurrentTimeMillis();
+                    will(returnValue(nowMillis1));
+                    one(fileStore).lastChanged(ITEM, 0L);
+                    will(returnValue(pathLastChangedMillis1));
+                    // second call
+                    one(timeProvider).getCurrentTimeMillis();
+                    will(returnValue(nowMillis2));
+                    one(fileStore).lastChanged(ITEM, pathLastChangedMillis1);
+                    will(returnValue(pathLastChangedMillis2));
+                    // third call
+                    one(timeProvider).getCurrentTimeMillis();
+                    will(returnValue(nowMillis3));
+                    one(fileStore).lastChanged(ITEM, pathLastChangedMillis2);
+                    will(returnValue(pathLastChangedMillis2));
+                }
+            });
+        assertFalse(filterUnderTest.accept(ITEM));
+        assertFalse(filterUnderTest.accept(ITEM));
+        assertTrue(filterUnderTest.accept(ITEM));
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void testThreeAcceptCallsUntilAcceptClockProblem()
+    {
+        final long nowMillis1 = 1000L;
+        final long nowMillis2 = 1100L;
+        final long nowMillis3 = 1200L;
+        final long pathLastChangedMillis1 = 1000L;
+        final long pathLastChangedMillis2 = 999L;
+        context.checking(new Expectations()
+            {
+                {
+                    // first call
+                    one(timeProvider).getCurrentTimeMillis();
+                    will(returnValue(nowMillis1));
+                    one(fileStore).lastChanged(ITEM, 0L);
+                    will(returnValue(pathLastChangedMillis1));
+                    // second call
+                    one(timeProvider).getCurrentTimeMillis();
+                    will(returnValue(nowMillis2));
+                    one(fileStore).lastChanged(ITEM, pathLastChangedMillis1);
+                    will(returnValue(pathLastChangedMillis2));
+                    // third call
+                    one(timeProvider).getCurrentTimeMillis();
+                    will(returnValue(nowMillis3));
+                    one(fileStore).lastChanged(ITEM, pathLastChangedMillis2);
+                    will(returnValue(pathLastChangedMillis2));
+                }
+            });
+        final LogMonitoringAppender appender =
+                LogMonitoringAppender.addAppender(LogCategory.MACHINE, Pattern
+                        .compile("Last modification time of path '.+' jumped back"));
+        assertFalse(filterUnderTest.accept(ITEM));
+        assertFalse(filterUnderTest.accept(ITEM));
+        assertTrue(filterUnderTest.accept(ITEM));
+        appender.verifyLogHasHappened();
+        LogMonitoringAppender.removeAppender(appender);
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void testCleanUpVanishedDirectory()
+    {
+        final long nowMillis1 = 1000L;
+        final long nowMillis2 = 10000L;
+        final long pathLastChangedMillis1 = 1000L;
+        final long pathLastChangedMillis2 = 1000L;
+        final String vanishingPathName = "vanished";
+        final StoreItem vanishingItem = new StoreItem(vanishingPathName);
+        context.checking(new Expectations()
+            {
+                {
+                    // first call for the vanishing file
+                    one(timeProvider).getCurrentTimeMillis();
+                    will(returnValue(nowMillis1));
+                    allowing(fileStore).lastChanged(vanishingItem, 0L);
+                    will(returnValue(pathLastChangedMillis1));
+                    // calls to get the required number of calls for clean up
+                    allowing(timeProvider).getCurrentTimeMillis();
+                    will(returnValue(nowMillis2));
+                    allowing(fileStore).lastChanged(with(same(ITEM)), with(greaterThanOrEqualTo(0L)));
+                    will(returnValue(pathLastChangedMillis2));
+                }
+            });
+        final LogMonitoringAppender appender =
+                LogMonitoringAppender.addAppender(LogCategory.OPERATION, "removing from path map");
+        assertFalse(filterUnderTest.accept(vanishingItem));
+        for (int i = 0; i < QuietPeriodFileFilter.MAX_CALLS_BEFORE_CLEANUP; ++i)
+        {
+            assertFalse(filterUnderTest.accept(ITEM));
+        }
+        appender.verifyLogHasHappened();
+        LogMonitoringAppender.removeAppender(appender);
+        context.assertIsSatisfied();
+    }
+
+}
