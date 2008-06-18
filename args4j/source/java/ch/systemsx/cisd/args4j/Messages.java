372820c4472dfc6c69c71922abdd49d514d86d57
ribeaudc <ribeaudc>
2008-06-18 13:43:08 +0000

    fix: - '*.properties' files of project 'args4j' not added in the openBIS client jar.
    SVN: 6697

--- args4j/source/java/ch/systemsx/cisd/args4j/Messages.java
+++ args4j/source/java/ch/systemsx/cisd/args4j/Messages.java
@@ -4,22 +4,31 @@ import java.text.MessageFormat;
 import java.util.ResourceBundle;
 
 /**
- * @author Kohsuke Kawaguchi
+ * The {@link ResourceBundle} handler.
+ * 
+ * @author Christian Ribeaud
  */
 enum Messages
 {
     MISSING_OPERAND, UNDEFINED_OPTION, NO_ARGUMENT_ALLOWED, ILLEGAL_METHOD_SIGNATURE,
     ILLEGAL_FIELD_SIGNATURE, REQUIRED_OPTION_MISSING;
 
-    private static ResourceBundle rb;
+    private static ResourceBundle resourceBundle;
 
-    public String format(Object... args)
+    /**
+     * For given {@link #name()}, returns the corresponding formatted (with given <var>args</var>)
+     * value.
+     */
+    public final String format(final Object... args)
     {
         synchronized (Messages.class)
         {
-            if (rb == null)
-                rb = ResourceBundle.getBundle(Messages.class.getName());
-            return MessageFormat.format(rb.getString(name()), args);
+            if (resourceBundle == null)
+            {
+                resourceBundle = ResourceBundle.getBundle(Messages.class.getName());
+            }
+            final String value = resourceBundle.getString(name());
+            return MessageFormat.format(value, args);
         }
     }
 }
