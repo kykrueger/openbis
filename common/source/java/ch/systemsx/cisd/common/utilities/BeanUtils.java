5b70bd92b8d1695e80fa25f01e7f3875dea8570b
ribeaudc <ribeaudc>
2008-06-27 11:53:45 +0000

    change: - No need to copy the beans if they are of same type (we so avoid a dead loop that used to occur before this change).
    SVN: 6896

--- common/source/java/ch/systemsx/cisd/common/utilities/BeanUtils.java
+++ common/source/java/ch/systemsx/cisd/common/utilities/BeanUtils.java
@@ -772,7 +772,7 @@ public final class BeanUtils
         }
         final Class<T> parameterType = (Class<T>) setter.getParameterTypes()[0];
         if (parameterType.isPrimitive() || immutableTypes.contains(parameterType)
-                || parameterType.isEnum())
+                || parameterType.isEnum() || oldBean.getClass().isAssignableFrom(parameterType))
         {
             return (T) oldBean;
         } else
@@ -936,7 +936,7 @@ public final class BeanUtils
             final List<PropertyDescriptor> descriptors =
                     new ArrayList<PropertyDescriptor>(Arrays.asList(Introspector.getBeanInfo(clazz)
                             .getPropertyDescriptors()));
-            for (final Iterator<PropertyDescriptor> iter = descriptors.iterator(); iter.hasNext(); )
+            for (final Iterator<PropertyDescriptor> iter = descriptors.iterator(); iter.hasNext();)
             {
                 final PropertyDescriptor descriptor = iter.next();
                 // If no write method, remove it. For instance 'class' property does not have any
