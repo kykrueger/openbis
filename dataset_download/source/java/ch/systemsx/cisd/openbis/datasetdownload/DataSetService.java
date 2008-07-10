bfbaab39c0022657a3c5625cd92c4ca1c0c8c8e4
ribeaudc <ribeaudc>
2008-07-10 13:20:30 +0000

    [LMS-486] add: - Package 'authorization' in 'lims' project. - 'AuthorizationAdvisor', 'IAccessController' and a first dummy implementation. change: - 'Roles' (renamed to 'RolesAllowed') and 'RoleSet' moved to this package. remove: - Package 'server' from 'lims_base' project.
    SVN: 7168

--- dataset_download/source/java/ch/systemsx/cisd/openbis/datasetdownload/DataSetService.java
+++ dataset_download/source/java/ch/systemsx/cisd/openbis/datasetdownload/DataSetService.java
@@ -23,37 +23,42 @@ import ch.systemsx.cisd.lims.base.ServiceRegistry;
 import ch.systemsx.cisd.lims.base.dto.ExternalData;
 
 /**
+ * A <code>IDataSetService</code> implementation.
  * 
- *
  * @author Franz-Josef Elmer
  */
-class DataSetService implements IDataSetService
+final class DataSetService implements IDataSetService
 {
     private IDataSetService service;
 
-    DataSetService(ConfigParameters configParameters)
+    DataSetService(final ConfigParameters configParameters)
     {
-        ILIMSServiceFactory factory = ServiceRegistry.getLIMSServiceFactory();
+        final ILIMSServiceFactory factory = ServiceRegistry.getLIMSServiceFactory();
         service = factory.createDataSetService(configParameters.getServerURL());
     }
-    
-    public ExternalData getDataSet(String sessionToken, String dataSetCode)
+
+    //
+    // IDataSetService
+    //
+
+    public final ExternalData getDataSet(final String sessionToken, final String dataSetCode)
             throws UserFailureException
     {
         return service.getDataSet(sessionToken, dataSetCode);
     }
 
-    public String authenticate(String user, String password) throws UserFailureException
+    public final String authenticate(final String user, final String password)
+            throws UserFailureException
     {
         return service.authenticate(user, password);
     }
 
-    public Void closeSession(String sessionToken) throws UserFailureException
+    public final void closeSession(final String sessionToken) throws UserFailureException
     {
-        return service.closeSession(sessionToken);
+        service.closeSession(sessionToken);
     }
 
-    public int getVersion()
+    public final int getVersion()
     {
         return service.getVersion();
     }
