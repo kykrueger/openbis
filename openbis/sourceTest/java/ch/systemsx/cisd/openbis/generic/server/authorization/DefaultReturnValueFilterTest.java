a60b5c50ff563b6fb787e387075065976ccb5f6b
felmer <felmer>
2008-09-22 12:31:10 +0000

    LMS-445 Some authorization classes moved from server.authorization -> shared.authorization
    SVN: 8417

--- openbis/sourceTest/java/ch/systemsx/cisd/openbis/generic/server/authorization/DefaultReturnValueFilterTest.java
+++ openbis/sourceTest/java/ch/systemsx/cisd/openbis/generic/server/authorization/DefaultReturnValueFilterTest.java
@@ -43,7 +43,7 @@ import ch.systemsx.cisd.common.logging.LogInitializer;
 import ch.systemsx.cisd.common.test.LogMonitoringAppender;
 import ch.systemsx.cisd.common.utilities.StringUtilities;
 import ch.systemsx.cisd.openbis.generic.server.authorization.DefaultReturnValueFilter;
-import ch.systemsx.cisd.openbis.generic.server.authorization.validator.IValidator;
+import ch.systemsx.cisd.openbis.generic.shared.authorization.validator.IValidator;
 import ch.systemsx.cisd.openbis.generic.shared.dto.PersonPE;
 
 /**
