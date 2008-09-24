95419724607e5d5b4f34b7e89310870bf35a010b
felmer <felmer>
2008-09-24 06:55:08 +0000

    LMS-445 refactor out GenericServerTestCase, more tests added
    SVN: 8445

---
+++ openbis/sourceTest/java/ch/systemsx/cisd/openbis/generic/server/GenericServerAuthenticationAuthorizationTest.java
@@ -0,0 +1,329 @@
+/*
+ * Copyright 2008 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.openbis.generic.server;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.jmock.Expectations;
+import org.testng.annotations.Test;
+
+import ch.systemsx.cisd.common.exceptions.UserFailureException;
+import ch.systemsx.cisd.openbis.generic.shared.dto.GroupPE;
+import ch.systemsx.cisd.openbis.generic.shared.dto.PersonPE;
+import ch.systemsx.cisd.openbis.generic.shared.dto.RoleAssignmentPE;
+import ch.systemsx.cisd.openbis.generic.shared.dto.RoleCode;
+import ch.systemsx.cisd.openbis.generic.shared.dto.Session;
+import ch.systemsx.cisd.openbis.generic.shared.dto.identifier.DatabaseInstanceIdentifier;
+
+/**
+ * @author Franz-Josef Elmer
+ */
+public class GenericServerAuthenticationAuthorizationTest extends GenericServerTestCase
+{
+    
+    @Test
+    public void testLogout()
+    {
+        context.checking(new Expectations()
+            {
+                {
+                    one(sessionManager).closeSession(SESSION_TOKEN);
+                }
+            });
+        createServer().logout(SESSION_TOKEN);
+        
+        context.assertIsSatisfied();
+    }
+    
+    @Test
+    public void testTryToAuthenticateWhichFailed()
+    {
+        final String user = "user";
+        final String password = "password";
+        context.checking(new Expectations()
+            {
+                {
+                    one(sessionManager).tryToOpenSession(user, password);
+                    will(returnValue(null));
+                }
+            });
+        
+        assertEquals(null, createServer().tryToAuthenticate(user, password));
+        
+        context.assertIsSatisfied();
+    }
+    
+    @Test
+    public void testFirstTryToAuthenticate()
+    {
+        final String user = "user";
+        final String password = "password";
+        final Session session = createExampleSession();
+        final PersonPE systemPerson = new PersonPE();
+        final PersonPE person = createPersonFromPrincipal(PRINCIPAL);
+        final RoleAssignmentPE roleAssignment = new RoleAssignmentPE();
+        roleAssignment.setPerson(person);
+        roleAssignment.setDatabaseInstance(homeDatabaseInstance);
+        roleAssignment.setRegistrator(systemPerson);
+        roleAssignment.setRole(RoleCode.ADMIN);
+        context.checking(new Expectations()
+            {
+                {
+                    one(sessionManager).tryToOpenSession(user, password);
+                    will(returnValue(SESSION_TOKEN));
+                    
+                    one(sessionManager).getSession(SESSION_TOKEN);
+                    will(returnValue(session));
+                    
+                    one(personDAO).listPersons();
+                    will(returnValue(Arrays.asList(systemPerson))); // only 'system' in database
+                    
+                    one(personDAO).tryFindPersonByUserId(user); // first login
+                    will(returnValue(null)); 
+                    
+                    one(personDAO).createPerson(person);
+                    one(roleAssignmentDAO).createRoleAssignment(roleAssignment);
+                }
+            });
+        
+        final Session s = createServer().tryToAuthenticate(user, password);
+        
+        assertEquals(person, s.tryGetPerson());
+        assertEquals(roleAssignment, s.tryGetPerson().getRoleAssignments().get(0));
+        
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void testFirstTryToAuthenticateButNotFirstUser()
+    {
+        final String user = "user";
+        final String password = "password";
+        final Session session = createExampleSession();
+        final PersonPE systemPerson = new PersonPE();
+        final PersonPE person = createPersonFromPrincipal(PRINCIPAL);
+        context.checking(new Expectations()
+            {
+                {
+                    one(sessionManager).tryToOpenSession(user, password);
+                    will(returnValue(SESSION_TOKEN));
+
+                    one(sessionManager).getSession(SESSION_TOKEN);
+                    will(returnValue(session));
+
+                    one(personDAO).listPersons();
+                    will(returnValue(Arrays.asList(systemPerson, person)));
+
+                    one(personDAO).tryFindPersonByUserId(user); // first login
+                    will(returnValue(null));
+
+                    one(personDAO).createPerson(person);
+                }
+            });
+
+        final Session s = createServer().tryToAuthenticate(user, password);
+
+        assertEquals(person, s.tryGetPerson());
+
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void testTryToAuthenticate()
+    {
+        final String user = "user";
+        final String password = "password";
+        final Session session = createExampleSession();
+        final PersonPE systemPerson = new PersonPE();
+        final PersonPE person = createPersonFromPrincipal(PRINCIPAL);
+        context.checking(new Expectations()
+            {
+                {
+                    one(sessionManager).tryToOpenSession(user, password);
+                    will(returnValue(SESSION_TOKEN));
+
+                    one(sessionManager).getSession(SESSION_TOKEN);
+                    will(returnValue(session));
+
+                    one(personDAO).listPersons();
+                    will(returnValue(Arrays.asList(systemPerson, person)));
+
+                    one(personDAO).tryFindPersonByUserId(user);
+                    will(returnValue(person));
+                }
+            });
+        assertEquals(null, session.tryGetPerson());
+
+        final Session s = createServer().tryToAuthenticate(user, password);
+
+        assertSame(session, s);
+        assertEquals(person, s.tryGetPerson());
+
+        context.assertIsSatisfied();
+    }
+    
+    @Test
+    public void testListGroups()
+    {
+        final PersonPE person = createPersonFromPrincipal(PRINCIPAL);
+        DatabaseInstanceIdentifier identifier = DatabaseInstanceIdentifier.createHome();
+        final GroupPE g1 = createGroup("g1", homeDatabaseInstance);
+        final GroupPE g2 = createGroup("g2", homeDatabaseInstance);
+        Session session = prepareGetSession();
+        session.setPerson(person);
+        person.setHomeGroup(g1);
+        g1.setId(42L);
+        context.checking(new Expectations()
+            {
+                {
+                    one(groupDAO).listGroups(homeDatabaseInstance);
+                    will(returnValue(Arrays.asList(g1, g2)));
+                }
+            });
+        
+        List<GroupPE> groups = createServer().listGroups(SESSION_TOKEN, identifier);
+        
+        assertSame(g1, groups.get(0));
+        assertSame(g2, groups.get(1));
+        assertEquals(2, groups.size());
+        assertEquals(true, g1.isHome().booleanValue());
+        assertEquals(false, g2.isHome().booleanValue());
+        
+        context.assertIsSatisfied();
+    }
+    
+    @Test
+    public void testRegisterGroup()
+    {
+        final Session session = prepareGetSession();
+        final String groupCode = "group";
+        final String description = "description";
+        final String leader = "leader";
+        context.checking(new Expectations()
+            {
+                {
+                    one(boFactory).createGroupBO(session);
+                    will(returnValue(groupBO));
+                    
+                    one(groupBO).define(groupCode, description, leader);
+                    one(groupBO).save();
+                }
+            });
+        
+        createServer().registerGroup(SESSION_TOKEN, groupCode, description, leader);
+        
+        context.assertIsSatisfied();
+    }
+    
+    @Test
+    public void testListPersons()
+    {
+        final PersonPE person = createPersonFromPrincipal(PRINCIPAL);
+        prepareGetSession();
+        context.checking(new Expectations()
+            {
+                {
+                    one(personDAO).listPersons();
+                    will(returnValue(Arrays.asList(person)));
+                }
+            });
+        
+        List<PersonPE> persons = createServer().listPersons(SESSION_TOKEN);
+        
+        assertSame(person, persons.get(0));
+        assertEquals(1, persons.size());
+        
+        context.assertIsSatisfied();
+    }
+    
+    @Test
+    public void testRegisterPerson()
+    {
+        prepareGetSession();
+        context.checking(new Expectations()
+            {
+                {
+                    one(personDAO).tryFindPersonByUserId(USER_ID);
+                    will(returnValue(null));
+                    
+                    String applicationToken = "application-token";
+                    one(authenticationService).authenticateApplication();
+                    will(returnValue(applicationToken));
+                    
+                    one(authenticationService).getPrincipal(applicationToken, USER_ID);
+                    will(returnValue(PRINCIPAL));
+                    
+                    PersonPE person = createPersonFromPrincipal(PRINCIPAL);
+                    one(personDAO).createPerson(person);
+                }
+            });
+        
+        createServer().registerPerson(SESSION_TOKEN, USER_ID);
+    }
+    
+    @Test
+    public void testRegisterExistingPerson()
+    {
+        prepareGetSession();
+        context.checking(new Expectations()
+        {
+            {
+                one(personDAO).tryFindPersonByUserId(USER_ID);
+                will(returnValue(createPersonFromPrincipal(PRINCIPAL)));
+            }
+        });
+        
+        try
+        {
+            createServer().registerPerson(SESSION_TOKEN, USER_ID);
+            fail("UserFailureException expected");
+        } catch (UserFailureException e)
+        {
+            assertEquals("Person '" + USER_ID + "' already exists.", e.getMessage());
+        }
+    }
+    
+    @Test
+    public void testRegisterUnknownPerson()
+    {
+        prepareGetSession();
+        context.checking(new Expectations()
+            {
+                {
+                    one(personDAO).tryFindPersonByUserId(USER_ID);
+                    will(returnValue(null));
+                    
+                    String applicationToken = "application-token";
+                    one(authenticationService).authenticateApplication();
+                    will(returnValue(applicationToken));
+                    
+                    one(authenticationService).getPrincipal(applicationToken, USER_ID);
+                    will(throwException(new IllegalArgumentException()));
+                }
+            });
+        
+        try
+        {
+            createServer().registerPerson(SESSION_TOKEN, USER_ID);
+            fail("UserFailureException expected");
+        } catch (UserFailureException e)
+        {
+            assertEquals("Person '" + USER_ID + "' unknown by the authentication service.", e.getMessage());
+        }
+    }
+}
---
+++ openbis/sourceTest/java/ch/systemsx/cisd/openbis/generic/server/GenericServerTestCase.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2008 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.openbis.generic.server;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import ch.systemsx.cisd.authentication.IAuthenticationService;
+import ch.systemsx.cisd.authentication.ISessionManager;
+import ch.systemsx.cisd.authentication.Principal;
+import ch.systemsx.cisd.openbis.generic.server.business.bo.IGenericBusinessObjectFactory;
+import ch.systemsx.cisd.openbis.generic.server.business.bo.IGroupBO;
+import ch.systemsx.cisd.openbis.generic.server.dataaccess.IDAOFactory;
+import ch.systemsx.cisd.openbis.generic.server.dataaccess.IDatabaseInstanceDAO;
+import ch.systemsx.cisd.openbis.generic.server.dataaccess.IGroupDAO;
+import ch.systemsx.cisd.openbis.generic.server.dataaccess.IPersonDAO;
+import ch.systemsx.cisd.openbis.generic.server.dataaccess.IRoleAssignmentDAO;
+import ch.systemsx.cisd.openbis.generic.shared.IGenericServer;
+import ch.systemsx.cisd.openbis.generic.shared.dto.DatabaseInstancePE;
+import ch.systemsx.cisd.openbis.generic.shared.dto.GroupPE;
+import ch.systemsx.cisd.openbis.generic.shared.dto.PersonPE;
+import ch.systemsx.cisd.openbis.generic.shared.dto.Session;
+
+/**
+ * 
+ *
+ * @author Franz-Josef Elmer
+ */
+public abstract class GenericServerTestCase extends AssertJUnit
+{
+    protected static final String HOME_DATABASE_INSTANCE_CODE = "HOME_DATABASE";
+
+    protected static final String USER_ID = "test";
+
+    protected static final Principal PRINCIPAL = new Principal(USER_ID, "john", "doe", "j@d");
+
+    protected static final String SESSION_TOKEN = "session-token";
+
+    protected Mockery context;
+
+    protected IDAOFactory daoFactory;
+
+    protected IAuthenticationService authenticationService;
+
+    protected ISessionManager<Session> sessionManager;
+
+    protected IGenericBusinessObjectFactory boFactory;
+
+    protected IDatabaseInstanceDAO databaseInstanceDAO;
+
+    protected IPersonDAO personDAO;
+
+    protected IRoleAssignmentDAO roleAssignmentDAO;
+
+    protected DatabaseInstancePE homeDatabaseInstance;
+
+    protected IGroupDAO groupDAO;
+
+    protected IGroupBO groupBO;
+
+    @BeforeMethod
+    @SuppressWarnings("unchecked")
+    public final void setUp()
+    {
+        context = new Mockery();
+        authenticationService = context.mock(IAuthenticationService.class);
+        sessionManager = context.mock(ISessionManager.class);
+        
+        daoFactory = context.mock(IDAOFactory.class);
+        databaseInstanceDAO = context.mock(IDatabaseInstanceDAO.class);
+        personDAO = context.mock(IPersonDAO.class);
+        groupDAO = context.mock(IGroupDAO.class);
+        roleAssignmentDAO = context.mock(IRoleAssignmentDAO.class);
+        
+        boFactory = context.mock(IGenericBusinessObjectFactory.class);
+        groupBO = context.mock(IGroupBO.class);
+        
+        homeDatabaseInstance = createDatabaseInstance(HOME_DATABASE_INSTANCE_CODE);
+        context.checking(new Expectations()
+            {
+                {
+                    allowing(daoFactory).getHomeDatabaseInstance();
+                    will(returnValue(homeDatabaseInstance));
+                    allowing(daoFactory).getDatabaseInstancesDAO();
+                    will(returnValue(databaseInstanceDAO));
+                    allowing(daoFactory).getPersonDAO();
+                    will(returnValue(personDAO));
+                    allowing(daoFactory).getGroupDAO();
+                    will(returnValue(groupDAO));
+                    allowing(daoFactory).getRoleAssignmentDAO();
+                    will(returnValue(roleAssignmentDAO));
+                }
+            });
+    }
+
+    @AfterMethod
+    public final void tearDown()
+    {
+        // To following line of code should also be called at the end of each test method.
+        // Otherwise one do not known which test failed.
+        context.assertIsSatisfied();
+    }
+
+    protected IGenericServer createServer()
+    {
+        return new GenericServer(authenticationService, sessionManager, daoFactory, boFactory);
+    }
+    
+    protected Session createExampleSession()
+    {
+        return new Session(USER_ID, SESSION_TOKEN, PRINCIPAL, "remote-host", 1);
+    }
+
+    protected DatabaseInstancePE createDatabaseInstance(String code)
+    {
+        final DatabaseInstancePE databaseInstance = new DatabaseInstancePE();
+        databaseInstance.setCode(code);
+        return databaseInstance;
+    }
+    
+    protected PersonPE createPersonFromPrincipal(Principal principal)
+    {
+        final PersonPE person = new PersonPE();
+        person.setUserId(principal.getUserId());
+        person.setFirstName(principal.getFirstName());
+        person.setLastName(principal.getLastName());
+        person.setEmail(principal.getEmail());
+        return person;
+    }
+    
+    protected GroupPE createGroup(String groupCode, DatabaseInstancePE databaseInstance)
+    {
+        GroupPE group = new GroupPE();
+        group.setCode(groupCode);
+        group.setDatabaseInstance(databaseInstance);
+        return group;
+    }
+    
+    protected Session prepareGetSession()
+    {
+        final Session session = createExampleSession();
+        context.checking(new Expectations()
+            {
+                {
+                    one(sessionManager).getSession(SESSION_TOKEN);
+                    will(returnValue(session));
+                }
+            });
+        return session;
+    }
+
+}
