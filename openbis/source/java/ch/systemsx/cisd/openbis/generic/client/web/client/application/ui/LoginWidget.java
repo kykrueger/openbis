adb7934f575a65a9c725c32b6a38cf1bd7533b0e
ribeaudc <ribeaudc>
2008-10-27 09:23:13 +0000

    fix: - If login successful, then reload the page as well.
    SVN: 8762

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/client/web/client/application/ui/LoginWidget.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/client/web/client/application/ui/LoginWidget.java
@@ -192,6 +192,9 @@ public class LoginWidget extends VerticalPanel
                             viewContext.getPageController().reload();
                         }
                     });
+            } else
+            {
+                viewContext.getPageController().reload();
             }
         }
     }
