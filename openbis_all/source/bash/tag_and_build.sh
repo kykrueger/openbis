c3c72cb8ea195acd441f22dd5ea7fa46b81fd8bc
tpylak <tpylak>
2009-03-04 12:55:19 +0000

    LMS-768 update build script
    SVN: 10092

--- openbis_all/source/bash/tag_and_build.sh
+++ openbis_all/source/bash/tag_and_build.sh
@@ -36,8 +36,7 @@ scp *.zip $CISD_SERVER:$SPRINT_DIR
 
 echo Copying new openBIS components to \'$SPRINT_SERVER\'...
 scp openBIS-server-*.zip $SPRINT_SERVER:.
-scp openbis-*.zip $SPRINT_SERVER:.
-scp download-server-*.zip $SPRINT_SERVER:.
+scp datastore_server-*.zip $SPRINT_SERVER:.
 
 # If sprint install script is present and executable, run it!
 if [ -x $SPRINT_INSTALL_SCRIPT ]; then
