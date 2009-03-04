c9fee3b92d2b6f2f8a82159db5fe4cdf9d430c0e
felmer <felmer>
2009-03-04 06:45:52 +0000

    LMS-766 fix bug in integration test runner
    SVN: 10084

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -216,7 +216,7 @@ function install_openbis_server {
 	copy_templates $OPENBIS_SERVER_NAME
     
         unzip -d $OPENBIS_SERVER $INSTALL/openBIS*.zip
-	$OPENBIS_SERVER/openbis/install.sh $PWD/$OPENBIS_SERVER $OPENBIS_SERVER/service.properties $OPENBIS_SERVER/openbis/log.xml
+	$OPENBIS_SERVER/openBIS-server/install.sh $PWD/$OPENBIS_SERVER $OPENBIS_SERVER/service.properties $OPENBIS_SERVER/openbis/log.xml
 	wait_for_server
     else
         copy_templates $OPENBIS_SERVER_NAME
