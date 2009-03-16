b3878f4c745e2495a8ac54466e80aebebe1d4a0e
izabel <izabel>
2009-03-16 13:03:54 +0000

    [LMS-782] add editable entities
    SVN: 10234

---
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/EditableMaterial.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2009 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.openbis.generic.shared.basic.dto;
+
+import java.util.Date;
+import java.util.List;
+
+import ch.systemsx.cisd.openbis.generic.client.web.client.dto.EditableEntity;
+
+/**
+ * @author Izabela Adamczyk
+ */
+public class EditableMaterial extends
+        EditableEntity<MaterialType, MaterialTypePropertyType, MaterialProperty>
+{
+
+    public EditableMaterial(List<MaterialTypePropertyType> etpts,
+            List<MaterialProperty> properties, MaterialType type, String identifier, Long id,
+            Date modificationDate)
+    {
+        super(EntityKind.MATERIAL, etpts, properties, type, identifier, id, modificationDate);
+    }
+
+}
