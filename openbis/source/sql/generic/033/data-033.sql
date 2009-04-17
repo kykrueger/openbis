e43e733500a1019f0907e91662cec5f20c55a9be
tpylak <tpylak>
2009-04-17 14:36:01 +0000

    LMS-849 add new file formats to data-XXX.sql script.
    SVN: 10706

--- openbis/source/sql/generic/033/data-033.sql
+++ openbis/source/sql/generic/033/data-033.sql
@@ -237,8 +237,68 @@ insert into file_format_types
 ,dbin_id)
 values 
 (nextval('FILE_FORMAT_TYPE_ID_SEQ')
+,'HDF5'
+,'Hierarchical Data Format File, version 5'
+,(select id from database_instances where code = 'SYSTEM_DEFAULT')
+);
+
+insert into file_format_types
+(id
+,code
+,description
+,dbin_id)
+values 
+(nextval('FILE_FORMAT_TYPE_ID_SEQ')
+,'PROPRIETARY'
+,'Proprietary Format File'
+,(select id from database_instances where code = 'SYSTEM_DEFAULT')
+);
+
+insert into file_format_types
+(id
+,code
+,description
+,dbin_id)
+values 
+(nextval('FILE_FORMAT_TYPE_ID_SEQ')
+,'SRF'
+,'SRF File'
+,(select id from database_instances where code = 'SYSTEM_DEFAULT')
+);
+
+insert into file_format_types
+(id
+,code
+,description
+,dbin_id)
+values 
+(nextval('FILE_FORMAT_TYPE_ID_SEQ')
 ,'TIFF'
 ,'TIFF File'
+,(select id from database_instances where code = 'SYSTEM_DEFAULT')
+);
+
+insert into file_format_types
+(id
+,code
+,description
+,dbin_id)
+values 
+(nextval('FILE_FORMAT_TYPE_ID_SEQ')
+,'TSV'
+,'Tab Separated Values File'
+,(select id from database_instances where code = 'SYSTEM_DEFAULT')
+);
+
+insert into file_format_types
+(id
+,code
+,description
+,dbin_id)
+values 
+(nextval('FILE_FORMAT_TYPE_ID_SEQ')
+,'XML'
+,'XML File'
 ,(select id from database_instances where code = 'SYSTEM_DEFAULT')
 );
 
