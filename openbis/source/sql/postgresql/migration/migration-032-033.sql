681832782d01a3f5e90cb8902ab9425d755b6d95
tpylak <tpylak>
2009-04-20 08:15:56 +0000

    LMS-849 add additional data types to db v33 migration
    SVN: 10715

--- openbis/source/sql/postgresql/migration/migration-032-033.sql
+++ openbis/source/sql/postgresql/migration/migration-032-033.sql
@@ -1,1 +1,63 @@
--- screening specific data types have been removed, but the migration script is not affected by this fact 
+-------------------------------------------------------------------------
+--  Purpose:  Insert additional initial data set into the table FILE_FORMAT_TYPES
+-------------------------------------------------------------------------
+
+insert into file_format_types
+(id
+,code
+,description
+,dbin_id)
+values 
+(nextval('FILE_FORMAT_TYPE_ID_SEQ')
+,'HDF5'
+,'Hierarchical Data Format File, version 5'
+,(select id from database_instances where code = 'SYSTEM_DEFAULT')
+);
+
+insert into file_format_types
+(id
+,code
+,description
+,dbin_id)
+values 
+(nextval('FILE_FORMAT_TYPE_ID_SEQ')
+,'PROPRIETARY'
+,'Proprietary Format File'
+,(select id from database_instances where code = 'SYSTEM_DEFAULT')
+);
+
+insert into file_format_types
+(id
+,code
+,description
+,dbin_id)
+values 
+(nextval('FILE_FORMAT_TYPE_ID_SEQ')
+,'SRF'
+,'Sequence Read Format File'
+,(select id from database_instances where code = 'SYSTEM_DEFAULT')
+);
+
+insert into file_format_types
+(id
+,code
+,description
+,dbin_id)
+values 
+(nextval('FILE_FORMAT_TYPE_ID_SEQ')
+,'TSV'
+,'Tab Separated Values File'
+,(select id from database_instances where code = 'SYSTEM_DEFAULT')
+);
+
+insert into file_format_types
+(id
+,code
+,description
+,dbin_id)
+values 
+(nextval('FILE_FORMAT_TYPE_ID_SEQ')
+,'XML'
+,'XML File'
+,(select id from database_instances where code = 'SYSTEM_DEFAULT')
+);
