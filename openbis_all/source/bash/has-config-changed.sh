be61879c6b32d1e6fd00a7b2b47eb068b8214bc4
tpylak <tpylak>
2009-05-08 14:34:12 +0000

    SE-105 Scripts which determines if the template configuration of the DSS and openBIS servers is the same as the current one.
    SVN: 10951

---
+++ openbis_all/source/bash/has-config-changed.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Scripts which determines if the template configuration of the DSS and openBIS servers 
+# is the same as the current one.
+# Returns 0 if configuration is the one stored in the template file, 1 otherwise.
+
+is_update_needed="false"
+
+function compare_configs {
+	local config_template=$1
+	local config_current=$2
+	
+	echo Comparing template config file $config_template with current config file $config_current
+	diff $config_template $config_current > .dss_changed
+	if [ "`cat .dss_changed`" != "" ]; then
+	   echo "Current configuration is not the same as the one stored in the template file. There are following differences:"
+	   cat .dss_changed
+		 echo 
+	   echo "If the current configuration is the right one, please update the template using the command:"
+	   echo "  cp $config_current $config_template"
+	   is_update_needed="true"
+	else
+	   echo "Configuration file $config_template is up to date."
+	   echo
+	fi
+}
+
+compare_configs datastore_server-service.properties sprint/datastore_server/etc/service.properties
+compare_configs service.properties sprint/openBIS-server/apache-tomcat/webapps/openbis/WEB-INF/classes/service.properties
+
+if [ "$is_update_needed" = "true" ]; then
+  exit 1
+else
+  exit 0
+fi
