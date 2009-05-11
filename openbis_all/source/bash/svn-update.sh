ab6cf3ffcde9c64d8e6f29b1bb566ee3a15c59ac
tpylak <tpylak>
2009-05-11 12:29:37 +0000

    SE-104 minor fix
    SVN: 10977

--- openbis_all/source/bash/svn-update.sh
+++ openbis_all/source/bash/svn-update.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
+# Author: Tomasz Pylak
 # this small script updates all scripts available in this directory from svn
 
 SVN=http://svncisd.ethz.ch/repos/cisd/openbis_all/trunk/source/bash
+TMP=svn-update-tmp
+mkdir $TMP
+echo Updating the scripts from $SVN
 for f in *.sh; do
-  if [ "$f" != $0 ]; then  
-  wget $SVN/$f
+  if [ "$f" != "$0" ]; then  
+    echo Synchronizing $f...
+    mv $f $TMP
+    wget $SVN/$f
+  fi
 done
+chmod 700 *.sh
+echo SVN update done.
