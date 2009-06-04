029c05a913a5e3791ebf3a6d641aefad26e67538
walshs <walshs>
2009-06-04 12:07:30 +0000

    added features to make the script safer and removed the automated restart of DSS since the 'user' needs to login first to openbis in order to gain INSTANCE_ADMIN role.
    SVN: 11243

--- openbis_all/source/bash/delete_openbis_repos.sh
+++ openbis_all/source/bash/delete_openbis_repos.sh
@@ -2,6 +2,35 @@
 
 # Delete (clear) an openBIS repository
 # 2009, Bernd Rinn, CISD
+# 2009, Sean Walsh, CISD - modified : 
+# 1) to ask for user confirm 
+# 2) removed automated restart of datastore server (see echos at end)
+# 3) to take a SnapShot of the deleted openBIS 
+
+echo "Do you really want to drop the database openbis_productive on this server?"
+echo ""
+echo "Server URL from datastore_server service.properties is :"
+echo ""
+grep server-url ~/sprint/datastore_server/etc/service.properties
+echo ""
+echo "Server DNS is :"
+host `uname -n`
+echo ""
+echo "Answer y to continue, followed by [ENTER]:"
+read ans
+
+if [ "$ans" = "y" ]; then
+  echo "Deleting openbis_productive" 
+else
+  echo "Answer is not y. Leaving database untouched."
+  exit 0
+fi
+
+# Take snapshot just in case
+pushd /tmp
+echo "Creating SnapShot in /tmp with the TAG DELETED_OPENBIS"
+~/bin/create_snapshot_openbis_repos.sh DELETED_OPENBIS
+popd
 
 # Shutdown servers
 ~/sprint/openBIS-server/apache-tomcat/bin/shutdown.sh
@@ -14,5 +43,10 @@ rm -fR ~/sprint/openBIS-server/apache-tomcat/indices/*
 
 # Startup servers (will rebuild the database)
 ~/sprint/openBIS-server/apache-tomcat/bin/startup.sh
-~/sprint/datastore_server/datastore_server.sh start
+
+echo ""
+echo "You need to login to openBIS now and to create the first user with system role"
+echo "The start datastore_server with "
+echo ""
+echo "~/sprint/datastore_server/datastore_server.sh start"
 
