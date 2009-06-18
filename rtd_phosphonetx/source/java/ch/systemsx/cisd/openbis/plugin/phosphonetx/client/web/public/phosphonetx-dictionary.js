6ce6e106f2476c1e874021b81a931c4d6af8aa62
felmer <felmer>
2009-06-18 06:48:08 +0000

    LMS-953 add column header 'protein_description'
    SVN: 11426

--- rtd_phosphonetx/source/java/ch/systemsx/cisd/openbis/plugin/phosphonetx/client/web/public/phosphonetx-dictionary.js
+++ rtd_phosphonetx/source/java/ch/systemsx/cisd/openbis/plugin/phosphonetx/client/web/public/phosphonetx-dictionary.js
@@ -4,6 +4,7 @@ var phosphonetx = {
   ALL_PROTEINS_OF_AN_EXPERIMENT: "All Proteins of an Experiment",
   query_all_proteins_by_experiment: "Proteins of an Experiment",
   selected_experiment_label: "Experiment",
+  protein_description: "Protein",
   
   // LAST LINE: KEEP IT AT THE END
   lastline: "" // we need a line without a comma
