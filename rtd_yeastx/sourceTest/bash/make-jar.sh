dbcf64c2328b09ebccdadd8fa43b25aee6bd3679
tpylak <tpylak>
2009-06-23 08:43:15 +0000

    LMS-940 minor yeastx: better manual tests support
    SVN: 11492

---
+++ rtd_yeastx/sourceTest/bash/make-jar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ----- compile the plugin and copy it to DSS dir	
+cd ../../build
+. ./antrun.sh jar
+cp ../targets/dist/datastore_server-plugins.jar ../../datastore_server/source/
--- rtd_yeastx/sourceTest/bash/prepare_tests.sh
+++ rtd_yeastx/sourceTest/bash/prepare_tests.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 ./clean.sh
-
-# ----- compile the plugin and copy it to DSS dir
-	
-cd ../../build
-. ./antrun.sh jar
-cp ../targets/dist/datastore_server-plugins.jar ../../datastore_server/source/
+./make-jar.sh
