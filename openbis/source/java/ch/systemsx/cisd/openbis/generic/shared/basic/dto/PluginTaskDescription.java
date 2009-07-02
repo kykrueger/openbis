2764451f4f0d7f639171628b01aa5951614ac5f9
tpylak <tpylak>
2009-07-02 10:58:26 +0000

    LMS-978 plugin description class
    SVN: 11631

---
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/PluginTaskDescription.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2009 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.openbis.generic.shared.basic.dto;
+
+/**
+ * Description of one datastore server plugin task: key, label, dataset type codes.
+ * 
+ * @author Tomasz Pylak
+ */
+public class PluginTaskDescription
+{
+    private final String key;
+
+    private final String label;
+
+    private final String[] datasetTypeCodes;
+
+    public PluginTaskDescription(String key, String label, String[] datasetTypeCodes)
+    {
+        this.key = key;
+        this.label = label;
+        this.datasetTypeCodes = datasetTypeCodes;
+    }
+
+    /** the unique key of the plugin */
+    public String getKey()
+    {
+        return key;
+    }
+
+    /** the user friendly name of the plugin */
+    public String getLabel()
+    {
+        return label;
+    }
+
+    /** codes of dataset types which are handled by this plugin */
+    public String[] getDatasetTypeCodes()
+    {
+        return datasetTypeCodes;
+    }
+}
