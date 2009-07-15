a450f56876a1a4f8570cdb773b38c3526a8d702b
brinn <brinn>
2009-07-15 18:03:04 +0000

    upgrade: to PostgreSQL 8.4
    SVN: 11769

--- dbmigration/source/java/ch/systemsx/cisd/dbmigration/postgresql/DumpPreparator.java
+++ dbmigration/source/java/ch/systemsx/cisd/dbmigration/postgresql/DumpPreparator.java
@@ -58,7 +58,7 @@ import ch.systemsx.cisd.dbmigration.SimpleTableMetaData;
  */
 public class DumpPreparator
 {
-    private static final String MAC_POSTGRESQL_PATH = "/opt/local/lib/postgresql83/bin/";
+    private static final String MAC_POSTGRESQL_PATH = "/opt/local/lib/postgresql84/bin/";
 
     private static final String DUMP_EXEC = "pg_dump";
     
