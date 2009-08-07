a5e9ffd2db6f0ce5b0c0cd8e484578dff84b83e6
tpylak <tpylak>
2009-08-07 09:56:21 +0000

    LMS-1088 yeastx post-install script
    SVN: 12038

--- openbis_all/source/bash/sprint_post_install_yeastx.sh
+++ openbis_all/source/bash/sprint_post_install_yeastx.sh
@@ -4,15 +4,17 @@
 function copy_or_die {
   SRC=$1
   DEST=$2
-if [ -f $SRC ]; then
-   cp $SRC $DEST
-else
-   echo Error: cannot copy $SRC to $DEST, because $SRC does not exist
-   exit 1
-fi
+	if [ -f $SRC ]; then
+		cp $SRC $DEST
+	else
+		echo Error: cannot copy $SRC to $DEST, because $SRC does not exist
+		exit 1
+	fi
+}
 
 DSS=sprint/datastore_server
 
 copy_or_die ~/eicmlDropboxProcessingPlugin.properties $DSS/etc/
 copy_or_die ~/fiamlDropboxProcessingPlugin.properties $DSS/etc/
 copy_or_die ~/datastore_server-plugins.jar $DSS/lib/
+
