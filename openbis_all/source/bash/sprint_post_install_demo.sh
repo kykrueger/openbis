7228b02dd2ee77773acf516af0727f2e63c7b14e
kohleman <kohleman>
2009-08-20 11:29:34 +0000

    Initial Release for the post install script
    SVN: 12211

---
+++ openbis_all/source/bash/sprint_post_install_demo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Finishes sprint installation of demo server 
+# Warning: all changes to this file should be transfered to SVN repository
+
+CIFEX_DIR=~/sprint/datastore_server/data/incoming-cifex
+
+[ -d $CIFEX_DIR ] || mkdir -p $CIFEX_DIR
