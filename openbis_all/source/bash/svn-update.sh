7ac5788f4f4d08de35666a619a05f76c68936c28
kohleman <kohleman>
2009-08-21 09:44:58 +0000

    using now wget features to get the shell scripts
    SVN: 12219

--- openbis_all/source/bash/svn-update.sh
+++ openbis_all/source/bash/svn-update.sh
@@ -2,20 +2,10 @@
 # Author: Tomasz Pylak
 # Updates all scripts available in the current directory to the version found in SVN.
 
-SVN=http://svncisd.ethz.ch/repos/cisd/openbis_all/trunk/source/bash
+SVN=svncisd.ethz.ch/repos/cisd/openbis_all/trunk/source/bash
 TMP=svn-update-tmp
 mkdir $TMP
-echo Updating the scripts from $SVN
-for f in *.sh; do
-  if [ "$f" != "$0" ]; then  
-    echo Synchronizing $f...
-    mv $f $TMP
-    wget $SVN/$f
-    if [ $? -ne 0 ]; then
-      mv $TMP/$f .
-    fi   
-fi
-done
+wget -r -l1 -P$TMP -A.sh http://$SVN/
+mv $TMP/$SVN/* .
 chmod 700 *.sh
 rm -fr $TMP
-echo SVN update done.
