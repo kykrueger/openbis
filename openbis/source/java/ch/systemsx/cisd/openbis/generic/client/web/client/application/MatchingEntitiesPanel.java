76519370291fa87f987bfc065a34e208eb31fa32
buczekp <buczekp>
2009-08-26 13:24:27 +0000

    [LMS-1135] refactoring: extracted dialog class for reusability; show related datasets to all displayed entities by default when no row is selected
    SVN: 12292

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/client/web/client/application/MatchingEntitiesPanel.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/client/web/client/application/MatchingEntitiesPanel.java
@@ -27,14 +27,10 @@ import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.extjs.gxt.ui.client.widget.button.Button;
-import com.extjs.gxt.ui.client.widget.form.Radio;
-import com.extjs.gxt.ui.client.widget.form.RadioGroup;
 
 import ch.systemsx.cisd.openbis.generic.client.web.client.ICommonClientServiceAsync;
-import ch.systemsx.cisd.openbis.generic.client.web.client.application.framework.DefaultTabItem;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.framework.DispatcherHelper;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.framework.DisplayTypeIDGenerator;
-import ch.systemsx.cisd.openbis.generic.client.web.client.application.framework.ITabItem;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.framework.ITabItemFactory;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.model.BaseEntityModel;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.model.MatchingEntityModel;
@@ -42,14 +38,11 @@ import ch.systemsx.cisd.openbis.generic.client.web.client.application.model.Matc
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.plugin.IClientPlugin;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.plugin.IClientPluginFactory;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.renderer.LinkRenderer;
-import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.data.RelatedDataSetGrid;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.grid.AbstractBrowserGrid;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.grid.ColumnDefsAndConfigs;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.grid.ICellListener;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.grid.IDisposableComponent;
-import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.widget.AbstractDataConfirmationDialog;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.widget.IDataRefreshCallback;
-import ch.systemsx.cisd.openbis.generic.client.web.client.application.util.WidgetUtils;
 import ch.systemsx.cisd.openbis.generic.client.web.client.dto.DefaultResultSetConfig;
 import ch.systemsx.cisd.openbis.generic.client.web.client.dto.ResultSet;
 import ch.systemsx.cisd.openbis.generic.client.web.client.dto.SearchableEntity;
@@ -113,118 +106,37 @@ final class MatchingEntitiesPanel extends AbstractBrowserGrid<MatchingEntity, Ma
         addEntityOperationsLabel();
 
         String showRelatedDatasetsTitle = viewContext.getMessage(Dict.BUTTON_SHOW_RELATED_DATASETS);
+
         Button showRelatedDatasetsButton =
-                createSelectedItemsButton(showRelatedDatasetsTitle,
-                        new SelectionListener<ButtonEvent>()
+                new Button(showRelatedDatasetsTitle, new SelectionListener<ButtonEvent>()
+                    {
+                        @Override
+                        public void componentSelected(ButtonEvent ce)
+                        {
+                            final List<MatchingEntity> selectedEntities = getSelectedBaseObjects();
+                            final TableExportCriteria<MatchingEntity> displayedEntities =
+                                    createTableExportCriteria();
+                            if (selectedEntities.isEmpty())
                             {
-                                @Override
-                                public void componentSelected(ButtonEvent ce)
-                                {
-                                    final List<MatchingEntity> entities = getSelectedBaseObjects();
-                                    new ShowRelatedDatasetsDialog(entities).show();
-                                }
-                            });
+                                // no entity selected - show datasets related to all displayed
+                                RelatedDataSetCriteria criteria =
+                                        RelatedDataSetCriteria
+                                                .createDisplayedEntities(displayedEntities);
+                                ShowRelatedDatasetsDialog.showRelatedDatasetsTab(viewContext,
+                                        criteria);
+                            } else
+                            {
+                                // > 0 entity selected - show dialog with all/selected radio
+                                new ShowRelatedDatasetsDialog(viewContext, selectedEntities,
+                                        displayedEntities).show();
+                            }
+                        }
+                    });
         showRelatedDatasetsButton.setId(SHOW_RELATED_DATASETS_BUTTON_ID);
         addButton(showRelatedDatasetsButton);
         allowMultipleSelection();
 
         addEntityOperationsSeparator();
-    }
-
-    private final class ShowRelatedDatasetsDialog extends
-            AbstractDataConfirmationDialog<List<MatchingEntity>>
-    {
-        private static final int FIELD_WIDTH = 200;
-
-        private static final int LABEL_WIDTH = 120;
-
-        private Radio allOrSelectedRadio;
-
-        public ShowRelatedDatasetsDialog(List<MatchingEntity> entities)
-        {
-            super(viewContext, entities, viewContext
-                    .getMessage(Dict.SHOW_RELATED_DATASETS_DIALOG_TITLE));
-            setWidth(LABEL_WIDTH + FIELD_WIDTH + 50);
-        }
-
-        @Override
-        protected String createMessage()
-        {
-            return viewContext.getMessage(Dict.SHOW_RELATED_DATASETS_DIALOG_MESSAGE, data.size());
-        }
-
-        @Override
-        protected final void extendForm()
-        {
-            formPanel.setLabelWidth(LABEL_WIDTH);
-            formPanel.setFieldWidth(FIELD_WIDTH);
-            formPanel.setBodyBorder(false);
-            formPanel.setHeaderVisible(false);
-
-            formPanel.add(createAllOrSelectedRadio());
-        }
-
-        private final RadioGroup createAllOrSelectedRadio()
-        {
-            final String radioGroupLabel =
-                    viewContext.getMessage(Dict.SHOW_RELATED_DATASETS_DIALOG_RADIO_LABEL);
-            final String selectedLabel =
-                    viewContext.getMessage(Dict.ONLY_SELECTED_RADIO, data.size());
-            final String allLabel = viewContext.getMessage(Dict.ALL_RADIO, data.size());
-
-            return WidgetUtils.createAllOrSelectedRadioGroup(allOrSelectedRadio =
-                    WidgetUtils.createRadio(selectedLabel), WidgetUtils.createRadio(allLabel),
-                    radioGroupLabel, data.size());
-        }
-
-        private boolean getSelected()
-        {
-            return WidgetUtils.isSelected(allOrSelectedRadio);
-        }
-
-        @Override
-        protected void executeConfirmedAction()
-        {
-            final boolean selected = getSelected();
-            RelatedDataSetCriteria criteria = createCriteria(selected);
-            showRelatedDatasetsTab(criteria);
-        }
-
-        private RelatedDataSetCriteria createCriteria(boolean selected)
-        {
-            if (getSelected())
-            {
-                return RelatedDataSetCriteria.createSelectedEntities(getSelectedBaseObjects());
-            } else
-            {
-                return RelatedDataSetCriteria.createDisplayedEntities(createTableExportCriteria());
-            }
-        }
-
-        private void showRelatedDatasetsTab(final RelatedDataSetCriteria criteria)
-        {
-            final ITabItemFactory tabFactory = new ITabItemFactory()
-                {
-                    public ITabItem create()
-                    {
-                        IDisposableComponent component =
-                                RelatedDataSetGrid.create(viewContext, criteria);
-                        return DefaultTabItem.create(getTabTitle(), component, viewContext);
-                    }
-
-                    public String getId()
-                    {
-                        return RelatedDataSetGrid.BROWSER_ID + XDOM.getUniqueId();
-                    }
-
-                    private String getTabTitle()
-                    {
-                        return "Related Data Sets";
-                    }
-                };
-            DispatcherHelper.dispatchNaviEvent(tabFactory);
-        }
-
     }
 
     private static String createId()
---
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/client/web/client/application/ShowRelatedDatasetsDialog.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2009 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.openbis.generic.client.web.client.application;
+
+import java.util.List;
+
+import com.extjs.gxt.ui.client.XDOM;
+import com.extjs.gxt.ui.client.widget.form.Radio;
+import com.extjs.gxt.ui.client.widget.form.RadioGroup;
+
+import ch.systemsx.cisd.openbis.generic.client.web.client.ICommonClientServiceAsync;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.framework.DefaultTabItem;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.framework.DispatcherHelper;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.framework.ITabItem;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.framework.ITabItemFactory;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.data.RelatedDataSetGrid;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.grid.IDisposableComponent;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.widget.AbstractDataConfirmationDialog;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.util.WidgetUtils;
+import ch.systemsx.cisd.openbis.generic.client.web.client.dto.TableExportCriteria;
+import ch.systemsx.cisd.openbis.generic.shared.basic.IEntityInformationHolder;
+import ch.systemsx.cisd.openbis.generic.shared.basic.dto.RelatedDataSetCriteria;
+
+/**
+ * Dialog used to show a tab with Data Sets related to entities like samples and experiments.
+ * 
+ * @author Piotr Buczek
+ */
+public final class ShowRelatedDatasetsDialog extends
+        AbstractDataConfirmationDialog<List<? extends IEntityInformationHolder>>
+{
+    public static void showRelatedDatasetsTab(
+            final IViewContext<ICommonClientServiceAsync> viewContext,
+            final RelatedDataSetCriteria criteria)
+    {
+        final ITabItemFactory tabFactory = new ITabItemFactory()
+            {
+                public ITabItem create()
+                {
+                    IDisposableComponent component =
+                            RelatedDataSetGrid.create(viewContext, criteria);
+                    return DefaultTabItem.create(getTabTitle(), component, viewContext);
+                }
+
+                public String getId()
+                {
+                    return RelatedDataSetGrid.BROWSER_ID + XDOM.getUniqueId();
+                }
+
+                private String getTabTitle()
+                {
+                    return "Related Data Sets";
+                }
+            };
+        DispatcherHelper.dispatchNaviEvent(tabFactory);
+    }
+
+    private static final int FIELD_WIDTH = 200;
+
+    private static final int LABEL_WIDTH = 120;
+
+    private final IViewContext<ICommonClientServiceAsync> viewContext;
+
+    private final TableExportCriteria<? extends IEntityInformationHolder> displayedEntities;
+
+    private Radio allOrSelectedRadio;
+
+    public ShowRelatedDatasetsDialog(IViewContext<ICommonClientServiceAsync> viewContext,
+            List<? extends IEntityInformationHolder> selectedEntities,
+            TableExportCriteria<? extends IEntityInformationHolder> displayedEntities)
+    {
+        super(viewContext, selectedEntities, viewContext
+                .getMessage(Dict.SHOW_RELATED_DATASETS_DIALOG_TITLE));
+        this.viewContext = viewContext;
+        this.displayedEntities = displayedEntities;
+        setWidth(LABEL_WIDTH + FIELD_WIDTH + 50);
+    }
+
+    @Override
+    protected String createMessage()
+    {
+        return viewContext.getMessage(Dict.SHOW_RELATED_DATASETS_DIALOG_MESSAGE, data.size());
+    }
+
+    @Override
+    protected final void extendForm()
+    {
+        formPanel.setLabelWidth(LABEL_WIDTH);
+        formPanel.setFieldWidth(FIELD_WIDTH);
+        formPanel.setBodyBorder(false);
+        formPanel.setHeaderVisible(false);
+
+        formPanel.add(createAllOrSelectedRadio());
+    }
+
+    private final RadioGroup createAllOrSelectedRadio()
+    {
+        final String radioGroupLabel =
+                viewContext.getMessage(Dict.SHOW_RELATED_DATASETS_DIALOG_RADIO_LABEL);
+        final String selectedLabel = viewContext.getMessage(Dict.ONLY_SELECTED_RADIO, data.size());
+        final String allLabel = viewContext.getMessage(Dict.ALL_RADIO, data.size());
+
+        return WidgetUtils.createAllOrSelectedRadioGroup(allOrSelectedRadio =
+                WidgetUtils.createRadio(selectedLabel), WidgetUtils.createRadio(allLabel),
+                radioGroupLabel, data.size());
+    }
+
+    private boolean getSelected()
+    {
+        return WidgetUtils.isSelected(allOrSelectedRadio);
+    }
+
+    @Override
+    protected void executeConfirmedAction()
+    {
+        final boolean selected = getSelected();
+        RelatedDataSetCriteria criteria = createCriteria(selected);
+        showRelatedDatasetsTab(viewContext, criteria);
+    }
+
+    private RelatedDataSetCriteria createCriteria(boolean selected)
+    {
+        if (getSelected())
+        {
+            return RelatedDataSetCriteria.createSelectedEntities(data);
+        } else
+        {
+            return RelatedDataSetCriteria.createDisplayedEntities(displayedEntities);
+        }
+    }
+
+}
