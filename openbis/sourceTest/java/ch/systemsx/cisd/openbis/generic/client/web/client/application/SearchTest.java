f5cbfa354cee2f3198328940451d0fb39fe748e1
buczekp <buczekp>
2009-08-27 06:02:05 +0000

    [LMS-1135] system tests
    SVN: 12299

--- openbis/sourceTest/java/ch/systemsx/cisd/openbis/generic/client/web/client/application/SearchTest.java
+++ openbis/sourceTest/java/ch/systemsx/cisd/openbis/generic/client/web/client/application/SearchTest.java
@@ -18,46 +18,31 @@ package ch.systemsx.cisd.openbis.generic.client.web.client.application;
 
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.model.MatchingEntityModel.MatchingEntityColumnKind;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.Login;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.columns.specific.data.CommonExternalDataColDefKind;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.data.RelatedDataSetGrid;
+import ch.systemsx.cisd.openbis.generic.client.web.client.testframework.AbstractDefaultTestCommand;
 import ch.systemsx.cisd.openbis.generic.client.web.client.testframework.AbstractGWTTestCase;
 import ch.systemsx.cisd.openbis.generic.client.web.client.testframework.CheckTableCommand;
+import ch.systemsx.cisd.openbis.generic.client.web.client.testframework.GWTTestUtil;
+import ch.systemsx.cisd.openbis.generic.client.web.client.testframework.PiggyBackCommand;
 import ch.systemsx.cisd.openbis.generic.client.web.client.testframework.Row;
 
 /**
  * A {@link AbstractGWTTestCase} extension to test searching.
  * 
  * @author Christian Ribeaud
+ * @author Piotr Buczek
  */
 public class SearchTest extends AbstractGWTTestCase
 {
-    private final static CheckTableCommand createCheckTableCommandForAll()
-    {
-        final CheckTableCommand checkTableCommand =
-                new CheckTableCommand(MatchingEntitiesPanel.GRID_ID).expectedSize(2);
-        checkTableCommand.expectedRow(createRow("CISD:/MP"));
-        checkTableCommand.expectedRow(createRow("CISD:/CISD/EMPTY-MP"));
-        return checkTableCommand;
-    }
-
-    private static Row createRow(String identifier)
-    {
-        return new Row().withCell(MatchingEntityColumnKind.IDENTIFIER.id(), identifier);
-    }
-
-    private final static CheckTableCommand createCheckTableCommandForExperiment()
-    {
-        final CheckTableCommand checkTableCommand =
-                new CheckTableCommand(MatchingEntitiesPanel.GRID_ID).expectedSize(8);
-        checkTableCommand.expectedRow(createRow("/CISD/NEMO/EXP10"));
-        checkTableCommand.expectedRow(createRow("/CISD/NEMO/EXP11"));
-        checkTableCommand.expectedRow(createRow("/CISD/NEMO/EXP1"));
-        return checkTableCommand;
-    }
-
     public final void testAllSearch()
     {
         remoteConsole.prepare(new Login("test", "a"));
         remoteConsole.prepare(new SearchCommand("MP"));
-        remoteConsole.prepare(createCheckTableCommandForAll());
+
+        final CheckTableCommand checkDatasetsTableCommand =
+                createCheckMatchingEntitiesTableCommand(2, "CISD:/MP", "CISD:/CISD/EMPTY-MP");
+        remoteConsole.prepare(checkDatasetsTableCommand);
 
         launchTest(20000);
     }
@@ -66,8 +51,128 @@ public class SearchTest extends AbstractGWTTestCase
     {
         remoteConsole.prepare(new Login("test", "a"));
         remoteConsole.prepare(new SearchCommand("Experiment", "John"));
-        remoteConsole.prepare(createCheckTableCommandForExperiment());
+
+        final CheckTableCommand checkDatasetsTableCommand =
+                createCheckMatchingEntitiesTableCommand(8, "/CISD/NEMO/EXP10", "/CISD/NEMO/EXP11",
+                        "/CISD/NEMO/EXP1");
+        remoteConsole.prepare(checkDatasetsTableCommand);
 
         launchTest(20000);
     }
+
+    private final static String SAMPLE_T1 = "CISD:/CISD/CP-TEST-1";
+
+    private final static String SAMPLE_T2 = "CISD:/CISD/CP-TEST-2";
+
+    private final static String SAMPLE_T3 = "CISD:/CISD/CP-TEST-3";
+
+    private final static String EXP_T1 = "/CISD/NEMO/EXP-TEST-1";
+
+    private final static String EXP_T2 = "/CISD/NEMO/EXP-TEST-2";
+
+    private final static String EXP_T3 = "/CISD/NOE/EXP-TEST-2";
+
+    private final static String DS_LOC1 = "a/1";
+
+    private final static String DS_LOC2 = "a/2";
+
+    private final static String DS_LOC3 = "a/3";
+
+    public final void testShowDatasetsRelatedToSamples()
+    {
+        remoteConsole.prepare(new Login("test", "a"));
+        remoteConsole.prepare(new SearchCommand("Sample", "cp-test"));
+
+        final CheckTableCommand checkDatasetsTableCommand =
+                createCheckMatchingEntitiesTableCommand(SAMPLE_T1, SAMPLE_T2, SAMPLE_T3);
+        remoteConsole.prepare(new PiggyBackCommand(checkDatasetsTableCommand,
+                showDatasetsRelatedToAllDisplayedEntities()));
+        remoteConsole.prepare(createCheckRelatedDatasetsTableCommand(DS_LOC1, DS_LOC2, DS_LOC3));
+
+        launchTest(20000);
+    }
+
+    public final void testShowDatasetsRelatedToExperiments()
+    {
+        remoteConsole.prepare(new Login("test", "a"));
+        remoteConsole.prepare(new SearchCommand("Experiment", "exp-test"));
+
+        final CheckTableCommand checkDatasetsTableCommand =
+                createCheckMatchingEntitiesTableCommand(EXP_T1, EXP_T2, EXP_T3);
+        remoteConsole.prepare(new PiggyBackCommand(checkDatasetsTableCommand,
+                showDatasetsRelatedToAllDisplayedEntities()));
+        remoteConsole.prepare(createCheckRelatedDatasetsTableCommand(DS_LOC1, DS_LOC2, DS_LOC3));
+
+        launchTest(20000);
+    }
+
+    public final void testShowDatasetsRelatedToAll()
+    {
+        remoteConsole.prepare(new Login("test", "a"));
+        remoteConsole.prepare(new SearchCommand("test"));
+
+        final CheckTableCommand checkDatasetsTableCommand =
+                createCheckMatchingEntitiesTableCommand(15, SAMPLE_T1, SAMPLE_T2, SAMPLE_T3,
+                        EXP_T1, EXP_T2, EXP_T3);
+        remoteConsole.prepare(new PiggyBackCommand(checkDatasetsTableCommand,
+                showDatasetsRelatedToAllDisplayedEntities()));
+        remoteConsole.prepare(createCheckRelatedDatasetsTableCommand(DS_LOC1, DS_LOC2, DS_LOC3));
+
+        launchTest(20000);
+    }
+
+    private CheckTableCommand createCheckMatchingEntitiesTableCommand(int size,
+            String... identifiers)
+    {
+        final CheckTableCommand checkTableCommand =
+                new CheckTableCommand(MatchingEntitiesPanel.GRID_ID);
+
+        checkTableCommand.expectedSize(size);
+        for (String identifier : identifiers)
+        {
+            checkTableCommand.expectedRow(createMatchingEntityRowWithIdentifier(identifier));
+        }
+        return checkTableCommand;
+    }
+
+    private CheckTableCommand createCheckMatchingEntitiesTableCommand(String... identifiers)
+    {
+        return createCheckMatchingEntitiesTableCommand(identifiers.length, identifiers);
+    }
+
+    private CheckTableCommand createCheckRelatedDatasetsTableCommand(String... locations)
+    {
+        final CheckTableCommand checkTableCommand =
+                new CheckTableCommand(RelatedDataSetGrid.GRID_ID);
+
+        checkTableCommand.expectedSize(locations.length);
+        for (String location : locations)
+        {
+            checkTableCommand.expectedRow(createRelatedDatasetRowWithLocation(location));
+        }
+        return checkTableCommand;
+    }
+
+    private static Row createMatchingEntityRowWithIdentifier(String identifier)
+    {
+        return new Row().withCell(MatchingEntityColumnKind.IDENTIFIER.id(), identifier);
+    }
+
+    private static Row createRelatedDatasetRowWithLocation(String location)
+    {
+        return new Row().withCell(CommonExternalDataColDefKind.LOCATION.id(), location);
+    }
+
+    private AbstractDefaultTestCommand showDatasetsRelatedToAllDisplayedEntities()
+    {
+        return new AbstractDefaultTestCommand()
+            {
+                public void execute()
+                {
+                    GWTTestUtil
+                            .clickButtonWithID(MatchingEntitiesPanel.SHOW_RELATED_DATASETS_BUTTON_ID);
+                }
+            };
+    }
+
 }
