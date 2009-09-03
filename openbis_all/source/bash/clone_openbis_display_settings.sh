dd12fc4d98c6256b075e4e45d2297e77f583aa5f
kohleman <kohleman>
2009-09-03 11:57:46 +0000

    initial checkin used for the cloning display setting to provide a pre-customized view for first time users. Should ease the first use.
    SVN: 12455

---
+++ openbis_all/source/bash/clone_openbis_display_settings.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+TARGET_USER="$1"
+SOURCE_USER="$2"
+
+
+if [ -z "$TARGET_USER" -o -z "SOURCE_USER" ];
+then
+   echo "Syntax: $0 TARGET_USER SOURCE_USER"
+   exit 1 
+fi
+
+psql -d openbis_productive -c "update persons set display_settings=(select display_settings from persons where user_id='${TARGET_USER}') where user_id='${SOURCE_USER}'"
