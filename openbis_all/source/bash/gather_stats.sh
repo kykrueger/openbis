64d0d46306d1a4d143736f4a507e93e5cbdd8c2d
kohleman <kohleman>
2009-09-09 12:42:33 +0000

    initial check-in
    SVN: 12533

---
+++ openbis_all/source/bash/gather_stats.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+# Find out several things of the openbis installation on all servers
+# This is a pattern that can editet for several (SQL) queries on several
+# openbis servers, was initially just for querying memory
+ 
+SERVERS=" sprint-openbis.ethz.ch
+          cisd-openbis.ethz.ch
+          imsb-us-openbis.ethz.ch
+          openbis-phosphonetx.ethz.ch
+          openbis-liverx.ethz.ch
+          agronomics.ethz.ch
+          openbis-bsse.ethz.ch
+          basysbio.ethz.ch"
+
+SQL="WITH RECURSIVE data_set_parents(id, parent_ids, cycle) AS (
+        SELECT  r.data_id_child AS data_id, 
+                ARRAY[CAST (r.data_id_parent AS bigint)] AS parent_ids, 
+                false AS cycle 
+        FROM data_set_relationships r
+    UNION ALL
+        SELECT  r.data_id_child,
+                CAST (r.data_id_parent AS bigint) || p.parent_ids,
+                r.data_id_child = ANY(p.parent_ids)
+        FROM data_set_relationships r, data_set_parents p
+        WHERE r.data_id_parent = p.id AND NOT cycle
+)
+SELECT count(*) AS cycles FROM data_set_parents WHERE cycle = true;"
+
+SQL_QUERY="psql -c \"$SQL\" openbis_productive"
+JAVA_MAX_HEAP_SIZE="echo -n JAVA_MAX_HEAP_SIZE: ;cat ~/sprint/openBIS-server/apache-tomcat/etc/openbis.conf | grep JAVA_MEM_OPTS | cut -d \- -f 2"
+JAVA_INIT_HEAP_SIZE="echo -n JAVA_INIT_HEAP_SIZE: ;cat ~/sprint/openBIS-server/apache-tomcat/etc/openbis.conf | grep JAVA_MEM_OPTS | cut -d \- -f 3"
+JAVA_VERSION="java -version"
+PSQL_VERSION="psql --version | grep PostgreSQL"
+
+STATEMENTS="$JAVA_MAX_HEAP_SIZE;
+            $JAVA_INIT_HEAP_SIZE;
+            $PSQL_VERSION;
+            $JAVA_VERSION;
+            echo"
+
+if [ -n "${1}" ]
+then
+  STATEMENTS="$STATEMENTS;$1" 
+fi
+
+echo Statistics for all openbis servers
+echo ====================================
+for j in $SERVERS; 
+  do
+    echo $j:  
+    ssh openbis@$j $STATEMENTS
+done
