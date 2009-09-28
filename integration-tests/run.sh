b357513c1e4c4c051993a2a4a50f1af02d630e7c
tpylak <tpylak>
2009-09-28 13:34:34 +0000

    SE-133 YeastX - integration tests: more verbose messages
    SVN: 12743

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -1,8 +1,22 @@
+#!/bin/bash
 
+function print_result {
+	local result=$1
+	if [ $result -ne 0 ]; then
+		echo ERROR: Test has failed.
+	fi
+}
+
+echo Testing Screening Workflow
 ./test-3v.sh $@
 result_3v=$?
+print_result $result_3v
+
+echo Testing YeastX Workflow
 ./test-yeastx.sh $@
 result_yeastx=$?
+print_result $result_yeastx
+
 if [ $result_3v -ne 0 -o $result_yeastx -ne 0 ]; then
 	exit 1;
 fi
