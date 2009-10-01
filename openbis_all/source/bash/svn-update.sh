0f596a1b6553b5a4e6e0d9fea921674ff32ca845
kohleman <kohleman>
2009-10-01 13:52:06 +0000

    add cd ~/bin always write to this dir
    SVN: 12817

--- openbis_all/source/bash/svn-update.sh
+++ openbis_all/source/bash/svn-update.sh
@@ -3,7 +3,8 @@
 # Updates all scripts available in the current directory to the version found in SVN.
 
 SVN=svncisd.ethz.ch/repos/cisd/openbis_all/trunk/source/bash
-wget -vn -r -l1 -A.sh http://$SVN/
+cd ~/bin
+wget -nv -r -l1 -A.sh http://$SVN/
 mv $SVN/* .
 chmod 700 *.sh
 RM=`echo $SVN | cut -d / -f1`
