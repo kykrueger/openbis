825b36b3291a291ceed2bb30f6ea029a5adb94cb
buczekp <buczekp>
2009-10-14 08:48:14 +0000

    increased service version
    SVN: 12962

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/ServiceVersionHolder.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/ServiceVersionHolder.java
@@ -24,5 +24,5 @@ package ch.systemsx.cisd.openbis.generic.shared.basic.dto;
 public final class ServiceVersionHolder
 {
 
-    public static final int VERSION = 6; // for S65
+    public static final int VERSION = 7; // for S66
 }
