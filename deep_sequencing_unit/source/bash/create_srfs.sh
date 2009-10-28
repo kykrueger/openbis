fd3a12641075247a81c1762304694f71e4d09ef9
kohleman <kohleman>
2009-10-28 11:15:13 +0000

    added the tile parameter needed for cif2txt
    SVN: 13109

--- deep_sequencing_unit/source/bash/create_srfs.sh
+++ deep_sequencing_unit/source/bash/create_srfs.sh
@@ -10,7 +10,17 @@ export CONFIG_FILE=$RUN_BASE/$LATEST_FOLDER/$INTENSITY_FOLDER/config.txt
 export CIF2TXT=/usr/local/dsu/bin/cif2txt.sh
 export LANES2SRF=/usr/local/dsu/bin/lanes2srf_RTA
 export NUMBER_OF_LANES=8
+export NUMBER_OF_TILES=120
 export NUMBER_OF_CYCLES=`ls -1tr $RUN_BASE/$LATEST_FOLDER/$INTENSITY_FOLDER/L00$NUMBER_OF_LANES/ | tail -1 | cut -d . -f1 | cut -d C -f2`
+
+export PRG=`basename $0`
+export USAGE="Usage: ${PRG} <Path_to_Run_Base> <Number_of_Lanes> \n\nEXAMPLE: ${PRG} /array0/Runs/ 8" 
+
+#if [ -z "${RUN_BASE}"  -o -z "${NUMBER_OF_LANES}" ]
+#then
+#   echo "${USAGE}"
+#   exit 1
+#fi
 
 cd $RUN_BASE/$LATEST_FOLDER/$INTENSITY_FOLDER
 
@@ -24,6 +34,6 @@ $BUSTARD --CIF . --GERALD=$CONFIG_FILE --control-lane=5 --make
 cd Bustard*
 make recursive -j 15 2>seq_make.err >seq_make.log
 
-$CIF2TXT $RUN_BASE/$LATEST_FOLDER/$INTENSITY_FOLDER $NUMBER_OF_CYCLES
+$CIF2TXT $RUN_BASE/$LATEST_FOLDER/$INTENSITY_FOLDER $NUMBER_OF_CYCLES $NUMBER_OF_TILES
 
-$LANES2SRF .
+$LANES2SRF . $NUMBER_OF_LANES
