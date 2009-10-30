cf6dceaa81ce372bdb05f44eaa6c8daeb95e883e
kohleman <kohleman>
2009-10-30 12:16:29 +0000

    changed the echo output to something easy readable
    SVN: 13152

--- openbis_all/source/bash/has-config-changed.sh
+++ openbis_all/source/bash/has-config-changed.sh
@@ -7,22 +7,22 @@ is_update_needed="false"
 CONFIG_DIR=~/config
 
 function compare_configs {
-	local config_template=$1
-	local config_current=$2
-	
-	echo Comparing template config file $config_template with current config file $config_current
-	diff $config_template $config_current > .dss_changed
-	if [ "`cat .dss_changed`" != "" ]; then
-	   echo "Current configuration is not the same as the one stored in the template file. There are following differences:"
-	   cat .dss_changed
-		 echo 
-	   echo "If the current configuration is the right one, please update the template using the command:"
-	   echo "  cp $config_current $config_template"
-	   is_update_needed="true"
-	else
-	   echo "Configuration file $config_template is up to date."
-	   echo
-	fi
+        local config_template=$1
+        local config_current=$2
+
+        #echo Comparing template config file $config_template with current config file $config_current
+        diff $config_template $config_current > .dss_changed
+        if [ "`cat .dss_changed`" != "" ]; then
+           echo "Current configuration is not the same as the one stored in the template file. There are following differences:"
+           cat .dss_changed
+                 echo 
+           echo "DIFF: Please update the template using the command:"
+           echo "  cp $config_current $config_template"
+           is_update_needed="true"
+        else
+           echo "OK: Configuration file $config_template is up to date."
+           echo
+        fi
 }
 
 compare_configs $CONFIG_DIR/datastore_server-service.properties ~/sprint/datastore_server/etc/service.properties
