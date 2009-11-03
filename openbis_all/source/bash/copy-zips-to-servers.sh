ca875adf178b49271f2573435e929ee787277560
kohleman <kohleman>
2009-11-03 13:54:02 +0000

    rewritten to cover the distinguation between different zips to different servers
    SVN: 13196

--- openbis_all/source/bash/copy-zips-to-servers.sh
+++ openbis_all/source/bash/copy-zips-to-servers.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 # Copies all zip files from the current directory to the home directory of the specified servers
 
-#addresses="cisd-openbis.ethz.ch 
-addresses="agronomics.ethz.ch 
-        openbis-liverx.ethz.ch
-        imsb-us-openbis.ethz.ch 
-        basysbio.ethz.ch 
-        openbis-dsu.ethz.ch
-	openbis-scu.ethz.ch"
-#      openbis-phosphonetx.ethz.ch"
+export SPRINT=cisd-bamus.ethz.ch
+export DEMO=cisd-tongariro.ethz.ch
+export YEASTX=imsb-us-openbis.ethz.ch
+export PHOSPHONETX=openbis-phosphonetx.ethz.ch
+export LIVERX=openbis-liverx.ethz.ch
+export AGRONOMICS=bs-dsvr11.ethz.ch
+export DSU=bs-dsvr28-openbis-dsu.ethz.ch
+export SCU=bs-dsvr28-openbis-scu.ethz.ch
+export BASYSBIO=bs-dsvr10.ethz.ch
 
-for address in $addresses; do
-  echo Updating $address
-  scp *.zip $address:.
+# Currently there are three different types of server specific zips we distinguish
+export ZIPS="openBIS-server-S*.zip  datastore_server-S*.zip" 
+export ZIPS_PHOSPHONETX=*phosphonetx*.zip
+export ZIPS_DSU="openBIS-server-S*.zip datastore_server-dsu*.zip"
+
+for i in $ZIPS; do
+	 scp $i $DEMO
+	 scp $i $YEASTX
+	 scp $i $LIVERX
+	 scp $i $AGRONOMICS
+	 scp $i $SCU
+	 scp $i $BASYSBIO
+done     
+
+for j in $ZIPS_PHOSPHONETX; do
+	scp $j $PHOSPHONETX
+done
+
+for k in $ZIPS_DSU; do
+	scp $k $DSU
 done
