f559042f03c1729335bd1d83ed43d03a853f287f
cramakri <cramakri>
2009-11-04 15:39:19 +0000

    LMS-1249 more extensive testing of ColumnDefinition.
    SVN: 13226

--- datastore_server/sourceTest/java/ch/systemsx/cisd/etlserver/validation/ColumnDefinitionTest.java
+++ datastore_server/sourceTest/java/ch/systemsx/cisd/etlserver/validation/ColumnDefinitionTest.java
@@ -21,9 +21,13 @@ import java.util.Properties;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
+import ch.systemsx.cisd.common.exceptions.ConfigurationFailureException;
+import ch.systemsx.cisd.common.exceptions.UserFailureException;
+
 /**
+ * A test that focuses on the creation of ColumnDefinitions to make sure that useful error messages are provided.
+ * More comprehensive tests of the functionality in the context of validation are found in the DataSetValidatorForTSVTest class.
  * 
- *
  * @author Franz-Josef Elmer
  */
 public class ColumnDefinitionTest extends AssertJUnit
@@ -32,20 +36,164 @@ public class ColumnDefinitionTest extends AssertJUnit
     public void testDefaultColumnDefinition()
     {
         ColumnDefinition definition = ColumnDefinition.create("col", new Properties());
+
+        IValidator validator = definition.createValidator();
+        validator.assertValid(null);
+        validator.assertValid("");
+        validator.assertValid("abc");
+
+        definition.assertValidHeader("blabla");
+        assertEquals(true, definition.isValidHeader("blabla"));
+        definition.assertValidHeader("");
+        assertEquals(true, definition.isValidHeader(""));
+
+        assertEquals("col", definition.getName());
+        assertEquals(false, definition.isMandatory());
+        assertEquals(null, definition.getOrderOrNull());
+    }
+
+    @Test
+    public void testInvalidOrderKeyColumnDefinition()
+    {
+        // Combine tests of 0 and negative order keys into one test method
+
+        // Test 0 case
+        Properties props = new Properties();
+        props.setProperty(ColumnDefinition.ORDER_KEY, "0");
+        try
+        {
+            ColumnDefinition.create("col", props);
+            fail("ConfigurationFailureException expected");
+        } catch (ConfigurationFailureException ex)
+        {
+            assertEquals("Order value has to be positive: " + "0", ex.getMessage());
+        }
+
+        // Test negative case
+        props = new Properties();
+        props.setProperty(ColumnDefinition.ORDER_KEY, "-1");
+        try
+        {
+            ColumnDefinition.create("col", props);
+            fail("ConfigurationFailureException expected");
+        } catch (ConfigurationFailureException ex)
+        {
+            assertEquals("Order value has to be positive: " + "-1", ex.getMessage());
+        }
+    }
+    
+    @Test
+    public void testValidOrderKeyColumnDefinition()
+    {
+        Properties props = new Properties();
+        props.setProperty(ColumnDefinition.ORDER_KEY, "1");
+        ColumnDefinition definition = ColumnDefinition.create("col", props);
         
         IValidator validator = definition.createValidator();
         validator.assertValid(null);
         validator.assertValid("");
         validator.assertValid("abc");
-        
+
         definition.assertValidHeader("blabla");
         assertEquals(true, definition.isValidHeader("blabla"));
         definition.assertValidHeader("");
         assertEquals(true, definition.isValidHeader(""));
-        
+
         assertEquals("col", definition.getName());
         assertEquals(false, definition.isMandatory());
-        assertEquals(null, definition.getOrderOrNull());
+        assertEquals(new Integer(1), definition.getOrderOrNull());
     }
     
+    // TODO 2009-11-04, Chandrasekhar: assertValidHeader throws a NullPointerException
+    @Test(groups="broken")
+    public void testValidSimpleHeaderPatternColumnDefinition()
+    {
+        Properties props = new Properties();
+        props.setProperty(ColumnDefinition.HEADER_PATTERN_KEY, "ID");
+        ColumnDefinition definition = ColumnDefinition.create("col", props);
+        
+        // We did not specify anything about the value -- these should all pass
+        IValidator valueValidator = definition.createValidator();
+        valueValidator.assertValid(null);
+        valueValidator.assertValid("");
+        valueValidator.assertValid("abc");
+        
+        try
+        {
+            definition.assertValidHeader(null);
+            fail("null header should raise UserFailureException");
+        } catch (UserFailureException ex)
+        {
+            assertEquals("According to column definition 'col' the following header is invalid: null", ex.getMessage());
+        }
+        
+        try
+        {
+            definition.assertValidHeader("");
+            fail("Empty string header should raise UserFailureException");
+        } catch (UserFailureException ex)
+        {
+            assertEquals("According to column definition 'col' the following header is invalid: ", ex.getMessage());
+        }
+        assertEquals(false, definition.isValidHeader(""));
+        
+        try
+        {
+            definition.assertValidHeader("abc");
+            fail("A header of 'abc' should raise UserFailureException");
+        } catch (UserFailureException ex)
+        {
+            assertEquals("According to column definition 'col' the following header is invalid: abc", ex.getMessage());
+        }
+        assertEquals(false, definition.isValidHeader("abc"));
+        
+        definition.assertValidHeader("ID");
+        assertEquals(true, definition.isValidHeader("ID"));
+    }
+    
+    // TODO 2009-11-04, Chandrasekhar: assertValidHeader throws a NullPointerException
+    @Test(groups="broken")
+    public void testValidSimpleMandatoryHeaderPatternColumnDefinition()
+    {
+        Properties props = new Properties();
+        props.setProperty(ColumnDefinition.HEADER_PATTERN_KEY, "ID");
+        props.setProperty(ColumnDefinition.MANDATORY_KEY, "true");
+        ColumnDefinition definition = ColumnDefinition.create("col", props);
+        
+        // We did not specify anything about the value -- these should all pass
+        IValidator valueValidator = definition.createValidator();
+        valueValidator.assertValid(null);
+        valueValidator.assertValid("");
+        valueValidator.assertValid("abc");
+        
+        try
+        {
+            definition.assertValidHeader(null);
+            fail("null header should raise UserFailureException");
+        } catch (UserFailureException ex)
+        {
+            assertEquals("According to column definition 'col' the following header is invalid: null", ex.getMessage());
+        }
+        
+        try
+        {
+            definition.assertValidHeader("");
+            fail("Empty string header should raise UserFailureException");
+        } catch (UserFailureException ex)
+        {
+            assertEquals("According to column definition 'col' the following header is invalid: ", ex.getMessage());
+        }
+        
+        try
+        {
+            definition.assertValidHeader("abc");
+            fail("A header of 'abc' should raise UserFailureException");
+        } catch (UserFailureException ex)
+        {
+            assertEquals("According to column definition 'col' the following header is invalid: abc", ex.getMessage());
+        }
+
+        definition.assertValidHeader("ID");
+        assertEquals(true, definition.isValidHeader("ID"));
+    }
 }
