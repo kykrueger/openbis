b9ca6a4bd04adbcbf11a9e9863c7b1a44b35f34e
kohleman <kohleman>
2009-11-04 14:58:31 +0000

    cleaned up werners old scripts, useless with new pipeline and openbis
    SVN: 13225

--- deep_sequencing_unit/source/bash/convert_template.sh
+++ deep_sequencing_unit/source/bash/convert_template.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-Eland2Wig 1 drosmel_r5 Kc0.5 36
-Eland2Wig 2 musmus_ncbi37 TNK27me3 145
-Eland2Wig 3 musmus_ncbi37 TNK27me3 145
-Eland2Wig 4 musmus_ncbi37 ESCTCF 380
-Eland2Wig 8 homsap_ncbi36.50 HEK7b 36
+#Usage: eland2wig <chromosomes> <trackname> <fragmentsize> <windowsize> <strand>
+eland2wig drosmel.genome Lane5 160 10 0 <s_5_export.txt | gzip - -9 >s_5.wig.gz
