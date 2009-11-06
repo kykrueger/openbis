0e88ee8c5954f42ad5ca2c5c641cbd83a159d017
kohleman <kohleman>
2009-11-06 10:50:45 +0000

    added dss logs and changed the log destination folder name
    SVN: 13243

--- openbis_all/source/bash/rsync_logs.sh
+++ openbis_all/source/bash/rsync_logs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# saves the auth and usage logs of openBIS
+# saves the auth and usage logs of openBIS and DSS logs
 
-export TOMCAT_LOGS=~/sprint/openBIS-server/apache-tomcat/logs
+export TOMCAT_LOGS=~openbis/sprint/openBIS-server/apache-tomcat/logs
+export DSS_LOGS=~openbis/sprint/datastore_server/log
 export RSYNC=/usr/bin/rsync
-export DESTINATION=/local0/home/openbis/tomcat_logs
+export DESTINATION=/local0/home/openbis/logs
 
 [ -d $DESTINATION ] || mkdir -p $DESTINATION
 
 $RSYNC -av $TOMCAT_LOGS/*auth* $DESTINATION
 $RSYNC -av $TOMCAT_LOGS/*usage* $DESTINATION
+$RSYNC -av $DSS_LOGS/* $DESTINATION
