1ff288845d20a1f4c8033de68acfceec21ebe1e2
buczekp <buczekp>
2009-11-09 09:58:42 +0000

    [LMS-1258] reverted change
    SVN: 13270

--- openbis/sourceTest/java/ch/systemsx/cisd/openbis/generic/SystemTestSuite.java
+++ openbis/sourceTest/java/ch/systemsx/cisd/openbis/generic/SystemTestSuite.java
@@ -21,7 +21,30 @@ import junit.framework.TestSuite;
 
 import com.google.gwt.junit.tools.GWTTestSuite;
 
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.AuthenticationTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.AuthorizationGroupsTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.AuthorizationManagementConsolTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.DataSetSearchTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.EntityTypeBrowserTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.EntityTypePropertyTypeAssignmentTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.ExperimentBrowserTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.FileFormatTypeGridTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.MaterialBrowserTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.ProjectRegistrationTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.PropertyTypeAssignmentBrowserTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.PropertyTypeBrowserTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.PropertyTypeRegistrationTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.SampleBrowserTest;
 import ch.systemsx.cisd.openbis.generic.client.web.client.application.SearchTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.VocabularyBrowserTest;
+import ch.systemsx.cisd.openbis.generic.client.web.client.application.VocabularyRegistrationTest;
+import ch.systemsx.cisd.openbis.plugin.generic.client.web.client.application.dataset.GenericDataSetEditorTest;
+import ch.systemsx.cisd.openbis.plugin.generic.client.web.client.application.dataset.GenericDataSetViewerTest;
+import ch.systemsx.cisd.openbis.plugin.generic.client.web.client.application.experiment.GenericExperimentAttachmentDownloadTest;
+import ch.systemsx.cisd.openbis.plugin.generic.client.web.client.application.experiment.GenericExperimentEditorTest;
+import ch.systemsx.cisd.openbis.plugin.generic.client.web.client.application.experiment.GenericExperimentViewerTest;
+import ch.systemsx.cisd.openbis.plugin.generic.client.web.client.application.sample.GenericSampleRegistrationTest;
+import ch.systemsx.cisd.openbis.plugin.generic.client.web.client.application.sample.GenericSampleViewerTest;
 
 /**
  * @author Franz-Josef Elmer
@@ -31,35 +54,35 @@ public class SystemTestSuite extends GWTTestSuite
     public static Test suite()
     {
         final TestSuite testSuite = new TestSuite("System Tests");
-        // testSuite.addTestSuite(AuthenticationTest.class);
-        // testSuite.addTestSuite(AuthorizationGroupsTest.class);
-        // testSuite.addTestSuite(VocabularyBrowserTest.class);
-        // testSuite.addTestSuite(FileFormatTypeGridTest.class);
-        // testSuite.addTestSuite(DataSetSearchTest.class);
-        // testSuite.addTestSuite(GenericDataSetViewerTest.class);
-        // testSuite.addTestSuite(GenericDataSetEditorTest.class);
-        // testSuite.addTestSuite(AuthorizationManagementConsolTest.class);
-        // testSuite.addTestSuite(SampleBrowserTest.class);
-        // testSuite.addTestSuite(GenericSampleViewerTest.class);
+        testSuite.addTestSuite(AuthenticationTest.class);
+        testSuite.addTestSuite(AuthorizationGroupsTest.class);
+        testSuite.addTestSuite(VocabularyBrowserTest.class);
+        testSuite.addTestSuite(FileFormatTypeGridTest.class);
+        testSuite.addTestSuite(DataSetSearchTest.class);
+        testSuite.addTestSuite(GenericDataSetViewerTest.class);
+        testSuite.addTestSuite(GenericDataSetEditorTest.class);
+        testSuite.addTestSuite(AuthorizationManagementConsolTest.class);
+        testSuite.addTestSuite(SampleBrowserTest.class);
+        testSuite.addTestSuite(GenericSampleViewerTest.class);
         testSuite.addTestSuite(SearchTest.class);
-        // testSuite.addTestSuite(GenericSampleRegistrationTest.class);
-        // testSuite.addTestSuite(ExperimentBrowserTest.class);
-        // testSuite.addTestSuite(MaterialBrowserTest.class);
-        //
-        // // TODO 2009-01-21, IA: Uncomment after it's clear how to deal with
-        // // (NS_ERROR_DOM_SECURITY_ERR): Security error on CC
-        // // testSuite.addTestSuite(GenericExperimentRegistrationTest.class);
-        //
-        // testSuite.addTestSuite(GenericExperimentViewerTest.class);
-        // testSuite.addTestSuite(GenericExperimentEditorTest.class);
-        // testSuite.addTestSuite(PropertyTypeBrowserTest.class);
-        // testSuite.addTestSuite(PropertyTypeAssignmentBrowserTest.class);
-        // testSuite.addTestSuite(EntityTypePropertyTypeAssignmentTest.class);
-        // testSuite.addTestSuite(VocabularyRegistrationTest.class);
-        // testSuite.addTestSuite(PropertyTypeRegistrationTest.class);
-        // testSuite.addTestSuite(GenericExperimentAttachmentDownloadTest.class);
-        // testSuite.addTestSuite(ProjectRegistrationTest.class);
-        // testSuite.addTestSuite(EntityTypeBrowserTest.class);
+        testSuite.addTestSuite(GenericSampleRegistrationTest.class);
+        testSuite.addTestSuite(ExperimentBrowserTest.class);
+        testSuite.addTestSuite(MaterialBrowserTest.class);
+
+        // TODO 2009-01-21, IA: Uncomment after it's clear how to deal with
+        // (NS_ERROR_DOM_SECURITY_ERR): Security error on CC
+        // testSuite.addTestSuite(GenericExperimentRegistrationTest.class);
+
+        testSuite.addTestSuite(GenericExperimentViewerTest.class);
+        testSuite.addTestSuite(GenericExperimentEditorTest.class);
+        testSuite.addTestSuite(PropertyTypeBrowserTest.class);
+        testSuite.addTestSuite(PropertyTypeAssignmentBrowserTest.class);
+        testSuite.addTestSuite(EntityTypePropertyTypeAssignmentTest.class);
+        testSuite.addTestSuite(VocabularyRegistrationTest.class);
+        testSuite.addTestSuite(PropertyTypeRegistrationTest.class);
+        testSuite.addTestSuite(GenericExperimentAttachmentDownloadTest.class);
+        testSuite.addTestSuite(ProjectRegistrationTest.class);
+        testSuite.addTestSuite(EntityTypeBrowserTest.class);
         return testSuite;
     }
 }
