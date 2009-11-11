518d28f57cd2888d8e54320b808a7c759a7f4095
felmer <felmer>
2009-11-11 07:34:35 +0000

    LMS-1244 Extended version of the DefaultDataSetInfoExtractor introduced and tested.
    SVN: 13299

---
+++ deep_sequencing_unit/source/java/ch/ethz/bsse/cisd/dsu/dss/DataSetInfoExtractor.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2009 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.ethz.bsse.cisd.dsu.dss;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
+
+import ch.systemsx.cisd.common.exceptions.EnvironmentFailureException;
+import ch.systemsx.cisd.common.exceptions.UserFailureException;
+import ch.systemsx.cisd.common.filesystem.FileUtilities;
+import ch.systemsx.cisd.common.logging.LogCategory;
+import ch.systemsx.cisd.common.logging.LogFactory;
+import ch.systemsx.cisd.etlserver.DefaultDataSetInfoExtractor;
+import ch.systemsx.cisd.etlserver.IDataSetInfoExtractor;
+import ch.systemsx.cisd.openbis.dss.generic.shared.IEncapsulatedOpenBISService;
+import ch.systemsx.cisd.openbis.dss.generic.shared.dto.DataSetInformation;
+import ch.systemsx.cisd.openbis.generic.shared.dto.NewProperty;
+
+/**
+ * Extension of {@link DefaultDataSetInfoExtractor} for DSU which tries to get version information
+ * from the content and store it into the data set property <code>ILLUMINA_GA_OUTPUT</code>. The
+ * path to the configuration file inside a flow cell folder is specified by the property
+ * <code>path-to-config-file</code>. Its default value is <code>Data/Intensities/config.xml</code>.
+ * <p>
+ * If version information couldn't be fetched a warning is logged.
+ * 
+ * @author Franz-Josef Elmer
+ */
+public class DataSetInfoExtractor implements IDataSetInfoExtractor
+{
+    static final String VERSION_KEY = "ILLUMINA_GA_OUTPUT";
+    static final String PATH_TO_CONFIG_FILE_KEY = "path-to-config-file";
+    static final String DEFAULT_PATH_TO_CONFIG_FILE = "Data/Intensities/config.xml";
+    
+    private final static Logger operationLog =
+        LogFactory.getLogger(LogCategory.OPERATION, DataSetInfoExtractor.class);
+
+    private final IDataSetInfoExtractor dataSetInfoExtractor;
+    private final String pathToConfigFile;
+    private final Pattern pattern;
+
+    public DataSetInfoExtractor(Properties properties)
+    {
+        dataSetInfoExtractor = new DefaultDataSetInfoExtractor(properties);
+        pathToConfigFile = properties.getProperty(PATH_TO_CONFIG_FILE_KEY, DEFAULT_PATH_TO_CONFIG_FILE);
+        pattern = Pattern.compile(".*<Software.*Version=('|\")(.*)('|\").*");
+    }
+
+    public DataSetInformation getDataSetInformation(File incomingDataSetPath,
+            IEncapsulatedOpenBISService openbisService) throws UserFailureException,
+            EnvironmentFailureException
+    {
+        DataSetInformation info =
+                dataSetInfoExtractor.getDataSetInformation(incomingDataSetPath, openbisService);
+        File configFile = new File(incomingDataSetPath, pathToConfigFile);
+        if (configFile.isFile())
+        {
+            String version = tryToExtractVersion(configFile);
+            if (version != null)
+            {
+                NewProperty property = new NewProperty();
+                property.setPropertyCode(VERSION_KEY);
+                property.setValue(version);
+                info.setDataSetProperties(Arrays.asList(property));
+            } else
+            {
+                operationLog.warn("No version found in config file '" + pathToConfigFile + "'.");
+            }
+        } else
+        {
+            operationLog.warn("Config file '" + pathToConfigFile
+                    + "' does not exists or is a directory.");
+        }
+        return info;
+    }
+    
+    private String tryToExtractVersion(File configFile)
+    {
+        List<String> configFileContent = FileUtilities.loadToStringList(configFile);
+        for (String line : configFileContent)
+        {
+            Matcher matcher = pattern.matcher(line);
+            if (matcher.matches())
+            {
+                return matcher.group(2);
+            }
+        }
+        return null;
+    }
+
+}
---
+++ deep_sequencing_unit/sourceTest/java/ch/ethz/bsse/cisd/dsu/dss/DataSetInfoExtractorTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2009 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.ethz.bsse.cisd.dsu.dss;
+
+import java.io.File;
+import java.util.List;
+import java.util.Properties;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import ch.systemsx.cisd.base.tests.AbstractFileSystemTestCase;
+import ch.systemsx.cisd.common.filesystem.FileUtilities;
+import ch.systemsx.cisd.common.logging.BufferedAppender;
+import ch.systemsx.cisd.openbis.dss.generic.shared.dto.DataSetInformation;
+import ch.systemsx.cisd.openbis.generic.shared.dto.NewProperty;
+
+/**
+ * 
+ *
+ * @author Franz-Josef Elmer
+ */
+public class DataSetInfoExtractorTest extends AbstractFileSystemTestCase
+{
+    private BufferedAppender appender;
+
+    @Override
+    @BeforeMethod
+    public final void setUp()
+    {
+        appender = new BufferedAppender();
+        appender.resetLogContent();
+    }
+
+    @Test
+    public void testWithSingleQuotes()
+    {
+        DataSetInfoExtractor extractor = new DataSetInfoExtractor(new Properties());
+        File configFile = new File(workingDirectory, DataSetInfoExtractor.DEFAULT_PATH_TO_CONFIG_FILE);
+        configFile.getParentFile().mkdirs();
+        FileUtilities.writeToFile(configFile, "<abc>\n  <Software Name='RTA' Version='1.4.15.0'/>\n</abc>\n");
+        
+        DataSetInformation info = extractor.getDataSetInformation(workingDirectory, null);
+        
+        List<NewProperty> properties = info.getDataSetProperties();
+        assertEquals(1, properties.size());
+        assertEquals(DataSetInfoExtractor.VERSION_KEY, properties.get(0).getPropertyCode());
+        assertEquals("1.4.15.0", properties.get(0).getValue());
+    }
+    
+    @Test
+    public void testWithDoubleQuotes()
+    {
+        DataSetInfoExtractor extractor = new DataSetInfoExtractor(new Properties());
+        File configFile = new File(workingDirectory, DataSetInfoExtractor.DEFAULT_PATH_TO_CONFIG_FILE);
+        configFile.getParentFile().mkdirs();
+        FileUtilities.writeToFile(configFile, "<abc>\n  <Software Name=\"RTA\" Version=\"1.4.15.0\"/>\n</abc>\n");
+        
+        DataSetInformation info = extractor.getDataSetInformation(workingDirectory, null);
+        
+        List<NewProperty> properties = info.getDataSetProperties();
+        assertEquals(1, properties.size());
+        assertEquals(DataSetInfoExtractor.VERSION_KEY, properties.get(0).getPropertyCode());
+        assertEquals("1.4.15.0", properties.get(0).getValue());
+    }
+    
+    @Test
+    public void testWithMissingVersion()
+    {
+        DataSetInfoExtractor extractor = new DataSetInfoExtractor(new Properties());
+        File configFile = new File(workingDirectory, DataSetInfoExtractor.DEFAULT_PATH_TO_CONFIG_FILE);
+        configFile.getParentFile().mkdirs();
+        FileUtilities.writeToFile(configFile, "<abc>\n  </abc>\n");
+        
+        DataSetInformation info = extractor.getDataSetInformation(workingDirectory, null);
+        
+        List<NewProperty> properties = info.getDataSetProperties();
+        assertEquals(0, properties.size());
+        String logContent = appender.getLogContent();
+        assertEquals("No version found in config file 'Data/Intensities/config.xml'.", logContent);
+    }
+    
+    @Test
+    public void testWithMissingConfigFile()
+    {
+        DataSetInfoExtractor extractor = new DataSetInfoExtractor(new Properties());
+        
+        DataSetInformation info = extractor.getDataSetInformation(workingDirectory, null);
+        
+        List<NewProperty> properties = info.getDataSetProperties();
+        assertEquals(0, properties.size());
+        String logContent = appender.getLogContent();
+        assertEquals("No version found in config file 'Data/Intensities/config.xml'.", logContent);
+    }
+}
