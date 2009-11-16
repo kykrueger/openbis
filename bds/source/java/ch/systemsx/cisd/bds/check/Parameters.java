b142ffe7a41d1990f2ed7a9fb4a78431db69423b
felmer <felmer>
2009-11-16 09:04:58 +0000

    add BuildAndEnvironmentInfo class in order to avoid shadowing by using application specific BUILD.INFO files
    SVN: 13402

--- bds/source/java/ch/systemsx/cisd/bds/check/Parameters.java
+++ bds/source/java/ch/systemsx/cisd/bds/check/Parameters.java
@@ -26,7 +26,7 @@ import ch.systemsx.cisd.args4j.CmdLineException;
 import ch.systemsx.cisd.args4j.CmdLineParser;
 import ch.systemsx.cisd.args4j.ExampleMode;
 import ch.systemsx.cisd.args4j.Option;
-import ch.systemsx.cisd.base.utilities.BuildAndEnvironmentInfo;
+import ch.systemsx.cisd.bds.BuildAndEnvironmentInfo;
 import ch.systemsx.cisd.bds.StringUtils;
 import ch.systemsx.cisd.common.exceptions.UserFailureException;
 import ch.systemsx.cisd.common.utilities.IExitHandler;
