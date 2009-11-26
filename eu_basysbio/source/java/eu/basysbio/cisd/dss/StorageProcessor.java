87260b773f7c3f422b5fd0ba1450792809b582fc
felmer <felmer>
2009-11-26 16:27:33 +0000

    SE-169 StorageProcessor stub
    SVN: 13573

---
+++ eu_basysbio/source/java/eu/basysbio/cisd/dss/StorageProcessor.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2009 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.basysbio.cisd.dss;
+
+import java.io.File;
+import java.util.Properties;
+
+import ch.systemsx.cisd.etlserver.DelegatingStorageProcessorWithDropbox;
+import ch.systemsx.cisd.openbis.dss.generic.shared.IPostRegistrationDatasetHandler;
+import ch.systemsx.cisd.openbis.dss.generic.shared.dto.DataSetInformation;
+
+/**
+ * 
+ *
+ * @author Franz-Josef Elmer
+ */
+public class StorageProcessor extends DelegatingStorageProcessorWithDropbox
+{
+
+    public StorageProcessor(Properties properties)
+    {
+        super(properties, new IPostRegistrationDatasetHandler()
+            {
+                
+                public void undoLastOperation()
+                {
+                    System.out.println("undoLastOperation()");
+                    
+                }
+                
+                public void handle(File originalData, DataSetInformation dataSetInformation)
+                {
+                    System.out
+                            .println(".handle(" + originalData + ", " + dataSetInformation + ")");
+                }
+            });
+    }
+
+}
