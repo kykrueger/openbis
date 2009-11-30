bf610244987a710f9cd4812c887d5624a077d4f3
kohleman <kohleman>
2009-11-30 12:07:43 +0000

    changed home dir to ~openbis
    SVN: 13605

--- openbis_all/source/bash/svn-update.sh
+++ openbis_all/source/bash/svn-update.sh
@@ -3,7 +3,7 @@
 # Updates all scripts available in the current directory to the version found in SVN.
 
 SVN=svncisd.ethz.ch/repos/cisd/openbis_all/trunk/source/bash
-cd ~/bin
+cd ~openbis/bin
 wget -nv -r -l1 -A.sh http://$SVN/
 mv $SVN/* .
 chmod 700 *.sh
