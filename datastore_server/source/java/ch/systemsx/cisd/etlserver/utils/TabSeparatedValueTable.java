f7188b70dc610e154d7fce43bd940613791c4bfc
tpylak <tpylak>
2009-12-15 14:27:38 +0000

    minor: remove unused import
    SVN: 13948

--- datastore_server/source/java/ch/systemsx/cisd/etlserver/utils/TabSeparatedValueTable.java
+++ datastore_server/source/java/ch/systemsx/cisd/etlserver/utils/TabSeparatedValueTable.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.LineIterator;
