23b09e802d258e4bc555f540c1bdc115157e60bf
cramakri <cramakri>
2010-01-25 09:57:24 +0000

    SE-186 Code to generate images from ChromatogramDTO objects with associated test.
    SVN: 14421

---
+++ rtd_yeastx/source/java/ch/systemsx/cisd/yeastx/eicml/EICMLChromatogramImageGenerator.java
@@ -0,0 +1,247 @@
+/*
+ * Copyright 2010 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.yeastx.eicml;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYSplineRenderer;
+import org.jfree.data.xy.IntervalXYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+/**
+ * Generates a chromatogram from EICML data. Can be configured to generate the chromatogram in
+ * several different styles -- Line (the standard), Area (filled in contour), Spline (Line using
+ * spline curves), and Bar (3-D bar graph).
+ * 
+ * @author Chandrasekhar Ramakrishnan
+ */
+public class EICMLChromatogramImageGenerator
+{
+    public static enum ChartType
+    {
+        AREA, BAR, LINE, SPLINE
+    }
+
+    public final static ChartType DEFAULT_CHART_TYPE = ChartType.LINE;
+
+    private final ChromatogramDTO chromatogram;
+
+    private final OutputStream out;
+
+    private final int imageWidth;
+
+    private final int imageHeight;
+
+    private final ChartType chartType;
+
+    /**
+     * Initialize a chromatogram generator. The chart type is defaulted to DEFAULT_CHART_TYPE.
+     * 
+     * @param chromatogram The data to generate a chromatogram image from
+     * @param out The stream to write the image to
+     * @param imageWidth The width in pixels of the generated image
+     * @param imageHeight The height in pixels of the generated image
+     */
+    public EICMLChromatogramImageGenerator(ChromatogramDTO chromatogram, OutputStream out,
+            int imageWidth, int imageHeight)
+    {
+        this(chromatogram, out, imageWidth, imageHeight, DEFAULT_CHART_TYPE);
+    }
+
+    /**
+     * Initialize a chromatogram generator.
+     * 
+     * @param chromatogram The data to generate a chromatogram image from.
+     * @param out The stream to write the image to.
+     * @param imageWidth The width in pixels of the generated image
+     * @param imageHeight The height in pixels of the generated image
+     * @param chartType One of AREA, BAR, LINE, or SPLINE
+     */
+    public EICMLChromatogramImageGenerator(ChromatogramDTO chromatogram, OutputStream out,
+            int imageWidth, int imageHeight, ChartType chartType)
+    {
+        this.chromatogram = chromatogram;
+        this.out = out;
+        this.imageWidth = imageWidth;
+        this.imageHeight = imageHeight;
+        this.chartType = chartType;
+    }
+
+    /**
+     * Create an image from the chromatogram and write it to the output stream.
+     * 
+     * @throws IOException
+     */
+    public void generateImage() throws IOException
+    {
+        JFreeChart chart = createChart(createDataset());
+        ChartUtilities.writeChartAsPNG(out, chart, imageWidth, imageHeight);
+    }
+
+    private JFreeChart createChart(IntervalXYDataset dataset)
+    {
+        JFreeChart chart = null;
+        switch (chartType)
+        {
+            case AREA:
+                chart = createAreaChart(dataset);
+                break;
+            case BAR:
+                chart = createBarChart(dataset);
+                break;
+            case LINE:
+                chart = createLineChart(dataset);
+                break;
+            case SPLINE:
+                chart = createSplineChart(dataset);
+                break;
+        }
+
+        return chart;
+    }
+
+    private JFreeChart createBarChart(IntervalXYDataset dataset)
+    {
+        JFreeChart chart = ChartFactory.createXYBarChart(chromatogram.getLabel(), // title
+                "Run Time", // x-axis label
+                false, // use date axis?
+                "Intensity", // y-axis label
+                dataset, // data
+                PlotOrientation.VERTICAL, // plot orientation
+                false, // create legend?
+                false, // generate tooltips?
+                false // generate URLs?
+                );
+
+        XYPlot plot = configureChart(chart);
+
+        XYItemRenderer r = plot.getRenderer();
+        if (r instanceof XYBarRenderer)
+        {
+            XYBarRenderer renderer = (XYBarRenderer) r;
+            renderer.setShadowVisible(false);
+        }
+
+        return chart;
+    }
+
+    private JFreeChart createLineChart(IntervalXYDataset dataset)
+    {
+        JFreeChart chart = ChartFactory.createXYLineChart(chromatogram.getLabel(), // title
+                "run time", // x-axis label
+                "intensity", // y-axis label
+                dataset, // data
+                PlotOrientation.VERTICAL, // plot orientation
+                false, // create legend?
+                false, // generate tooltips?
+                false // generate URLs?
+                );
+
+        configureChart(chart);
+
+        return chart;
+    }
+
+    private JFreeChart createSplineChart(IntervalXYDataset dataset)
+    {
+        JFreeChart chart = ChartFactory.createXYLineChart(chromatogram.getLabel(), // title
+                "run time", // x-axis label
+                "intensity", // y-axis label
+                dataset, // data
+                PlotOrientation.VERTICAL, // plot orientation
+                false, // create legend?
+                false, // generate tooltips?
+                false // generate URLs?
+                );
+
+        XYPlot plot = configureChart(chart);
+
+        XYSplineRenderer renderer = new XYSplineRenderer();
+        renderer.setSeriesShapesVisible(0, false);
+        plot.setRenderer(renderer);
+
+        return chart;
+    }
+
+    private JFreeChart createAreaChart(IntervalXYDataset dataset)
+    {
+        JFreeChart chart = ChartFactory.createXYAreaChart(chromatogram.getLabel(), // title
+                "run time", // x-axis label
+                "intensity", // y-axis label
+                dataset, // data
+                PlotOrientation.VERTICAL, // plot orientation
+                false, // create legend?
+                false, // generate tooltips?
+                false // generate URLs?
+                );
+
+        configureChart(chart);
+
+        return chart;
+    }
+
+    /**
+     * Create a JFreeChart dataset from the chromatogram data.
+     */
+    private IntervalXYDataset createDataset()
+    {
+        XYSeries s1 = new XYSeries(chromatogram.getLabel());
+        float[] runTimes = chromatogram.getRunTimes();
+        float[] intensities = chromatogram.getIntensities();
+        for (int i = 0; i < intensities.length; ++i)
+        {
+            s1.add(runTimes[i], intensities[i]);
+        }
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(s1);
+
+        return dataset;
+    }
+
+    /**
+     * Apply the visual preferences to the chart.
+     */
+    private XYPlot configureChart(JFreeChart chart)
+    {
+        chart.setBackgroundPaint(Color.WHITE);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.WHITE);
+        plot.setDomainGridlinesVisible(false);
+        plot.setRangeGridlinesVisible(true);
+        plot.setRangeGridlinePaint(Color.LIGHT_GRAY);
+        plot.setDomainCrosshairVisible(false);
+        plot.setRangeCrosshairVisible(false);
+
+        ValueAxis axis = plot.getDomainAxis();
+        axis.setAutoRange(true);
+
+        return plot;
+    }
+}
---
+++ rtd_yeastx/sourceTest/java/ch/systemsx/cisd/yeastx/eicml/EICMLChromatogramImageGeneratorTest.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2010 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.yeastx.eicml;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.jmock.Mockery;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.xml.sax.SAXException;
+
+import ch.systemsx.cisd.yeastx.eicml.EICMLParser.IChromatogramObserver;
+
+/**
+ * @author Chandrasekhar Ramakrishnan
+ */
+public class EICMLChromatogramImageGeneratorTest extends AssertJUnit implements
+        IChromatogramObserver
+{
+    Mockery context;
+
+    ArrayList<ChromatogramDTO> chromatograms = new ArrayList<ChromatogramDTO>();
+
+    @BeforeClass
+    public final void initializeChromatogram()
+    {
+        // Get a chromatogram to work with.
+        try
+        {
+            new EICMLParser("resource/examples/example.eicML", null, this);
+        } catch (ParserConfigurationException ex)
+        {
+            fail(ex.getMessage());
+        } catch (SAXException ex)
+        {
+            fail(ex.getMessage());
+        } catch (IOException ex)
+        {
+            fail(ex.getMessage());
+        }
+
+        if (chromatograms.size() < 1)
+        {
+            fail("Could not create ChromatogramDTO instance for test.");
+        }
+    }
+
+    @BeforeMethod
+    public final void setUp()
+    {
+        context = new Mockery();
+    }
+
+    @AfterMethod
+    public final void tearDown()
+    {
+        // To following line of code should also be called at the end of each test method.
+        // Otherwise one do not known which test failed.
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void testImageGeneration()
+    {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        EICMLChromatogramImageGenerator imageGenerator =
+                new EICMLChromatogramImageGenerator(chromatograms.get(0), out, 300, 200);
+        try
+        {
+            imageGenerator.generateImage();
+        } catch (IOException ex)
+        {
+            fail(ex.getMessage());
+        }
+        if (out.size() < 1)
+            fail("Image generation created an empty image");
+    }
+
+    @Test
+    public void testImageStyles()
+    {
+        for (EICMLChromatogramImageGenerator.ChartType type : EICMLChromatogramImageGenerator.ChartType
+                .values())
+        {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            EICMLChromatogramImageGenerator imageGenerator =
+                    new EICMLChromatogramImageGenerator(chromatograms.get(0), out, 300, 200, type);
+            try
+            {
+                imageGenerator.generateImage();
+            } catch (IOException ex)
+            {
+                fail(ex.getMessage());
+            }
+            if (out.size() < 1)
+                fail("Image generation created an empty image");
+        }
+    }
+
+    // CR : Not a real test, but it is useful to looking at the generated images, so I'm leaving it,
+    // but commenting it out
+    // @Test
+    // public void testImageGenerationFile()
+    // {
+    // java.io.FileOutputStream out = null;
+    // try
+    // {
+    // // Generate images with the default style for each of the datasets
+    // int i = 0;
+    // for (ChromatogramDTO chromatogram : chromatograms)
+    // {
+    // out = new java.io.FileOutputStream("chromatogram" + i + ".png");
+    // EICMLChromatogramImageGenerator imageGenerator =
+    // new EICMLChromatogramImageGenerator(chromatogram, out, 800, 600);
+    // imageGenerator.generateImage();
+    // ++i;
+    // }
+    // } catch (java.io.FileNotFoundException ex1)
+    // {
+    // ex1.printStackTrace();
+    // } catch (IOException ex)
+    // {
+    // fail(ex.getMessage());
+    // }
+    //
+    // try
+    // {
+    // // Generate each style for the first image
+    // ChromatogramDTO chromatogram = chromatograms.get(0);
+    // for (EICMLChromatogramImageGenerator.ChartType type :
+    // EICMLChromatogramImageGenerator.ChartType
+    // .values())
+    // {
+    // out = new java.io.FileOutputStream("chromatogram" + "-" + type + ".png");
+    // EICMLChromatogramImageGenerator imageGenerator =
+    // new EICMLChromatogramImageGenerator(chromatogram, out, 800, 600, type);
+    // imageGenerator.generateImage();
+    // }
+    // } catch (java.io.FileNotFoundException ex1)
+    // {
+    // ex1.printStackTrace();
+    // } catch (IOException ex)
+    // {
+    // fail(ex.getMessage());
+    // }
+    // }
+
+    public void observe(ChromatogramDTO chromatogram)
+    {
+        chromatograms.add(chromatogram);
+    }
+
+}
