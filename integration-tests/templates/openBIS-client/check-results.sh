ccfd41f245bae1831b1fb2123c81af305f3961a2
tpylak <tpylak>
2010-01-26 10:20:15 +0000

    minor: remove unused integration tests code
    SVN: 14445

--- integration-tests/templates/openBIS-client/check-results.sh
+++
@@ -1,5 +0,0 @@
-CL=./bis.sh
-$CL login -u test -p unimportant
-$CL list-processing-instructions NEMO/EXP1
-$CL list-experiment-datasets NEMO/EXP1
-$CL logout
--- integration-tests/templates/openBIS-client/load-lims-data.sh
+++
@@ -1,9 +0,0 @@
-CL=./bis.sh
-CMDS_FILE=commands.txt
-PASSWORD=unimportant
-
-echo "=========== Loading data from client ================="
-while read cmd; do
-    cmd=${cmd/<password>/$PASSWORD}
-    $CL $cmd
-done < $CMDS_FILE
