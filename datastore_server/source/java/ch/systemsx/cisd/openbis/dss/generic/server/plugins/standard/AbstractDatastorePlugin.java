cac94a78298acfb0fde1f807bd0be6cdbb4ec067
tpylak <tpylak>
2010-02-17 13:35:31 +0000

    SE-206 minor
    SVN: 14795

--- datastore_server/source/java/ch/systemsx/cisd/openbis/dss/generic/server/plugins/standard/AbstractDatastorePlugin.java
+++ datastore_server/source/java/ch/systemsx/cisd/openbis/dss/generic/server/plugins/standard/AbstractDatastorePlugin.java
@@ -17,11 +17,13 @@
 package ch.systemsx.cisd.openbis.dss.generic.server.plugins.standard;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Properties;
 
 import org.apache.commons.lang.StringUtils;
 
+import ch.systemsx.cisd.common.exceptions.EnvironmentFailureException;
 import ch.systemsx.cisd.openbis.generic.shared.dto.DatasetDescription;
 
 /**
@@ -65,4 +67,33 @@ public abstract class AbstractDatastorePlugin implements Serializable
         location = location.replace("\\", File.separator);
         return new File(storeRoot, location);
     }
+
+    /** returns a path relative to the store directory */
+    protected String getStoreRelativePath(String absolutePath)
+    {
+        return getStoreRelativePath(absolutePath, storeRoot);
+    }
+
+    private static String getStoreRelativePath(String absolutePath, File storeRoot)
+    {
+        String storePath;
+        String filePath;
+        try
+        {
+            storePath = storeRoot.getCanonicalPath();
+            filePath = new File(absolutePath).getCanonicalPath();
+        } catch (IOException ex)
+        {
+            throw EnvironmentFailureException
+                    .fromTemplate("Error when checking the canonical path: " + ex.getMessage());
+        }
+        int index = absolutePath.indexOf(storePath);
+        if (index == -1)
+        {
+            throw EnvironmentFailureException.fromTemplate(
+                    "File path '%s' does not contain the store path '%s'.", filePath, storePath);
+        }
+        return filePath.substring(index + storePath.length());
+    }
+
 }
