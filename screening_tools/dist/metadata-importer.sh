c1cbf38e9bafb0889babba3c9fd1ce448473ad3f
tpylak <tpylak>
2010-02-24 08:36:25 +0000

    minor: metadata importer - bash version
    SVN: 14925

---
+++ screening_tools/dist/metadata-importer.sh
@@ -0,0 +1,3 @@
+LIB=:lib/
+CP=.${LIB}openbis-metadata-transformer.jar${LIB}screening.jar${LIB}commons-io.jar${LIB}commons-lang.jar${LIB}csv.jar
+java -cp $CP ch.systemsx.cisd.openbis.metadata.ScreeningLibraryTransformer $*
