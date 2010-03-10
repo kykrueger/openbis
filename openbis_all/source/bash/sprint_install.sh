0d9e2fef65d85625c5e12d23b5711816d37ed033
tpylak <tpylak>
2010-03-10 15:58:53 +0000

    minor: installation message
    SVN: 15128

--- openbis_all/source/bash/sprint_install.sh
+++ openbis_all/source/bash/sprint_install.sh
@@ -97,4 +97,4 @@ alias 'cp=cp -ipR'
 echo Running ~openbis/bin/sprint_post_install.sh
 ~openbis/bin/sprint_post_install.sh
 
-echo Done, now start the servers (all-up.sh)!
+echo "Done, now start the servers using all-up.sh"
