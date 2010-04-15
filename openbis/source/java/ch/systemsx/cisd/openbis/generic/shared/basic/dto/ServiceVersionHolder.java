c8f2b3259628b4e21caa0e74dbc141394ab14d4b
tpylak <tpylak>
2010-04-15 15:19:36 +0000

    minor: version number
    SVN: 15488

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/ServiceVersionHolder.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/ServiceVersionHolder.java
@@ -24,5 +24,5 @@ package ch.systemsx.cisd.openbis.generic.shared.basic.dto;
 public final class ServiceVersionHolder
 {
 
-    public static final int VERSION = 12; // for S77
+    public static final int VERSION = 13; // for S79
 }
