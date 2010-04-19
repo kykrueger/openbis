4c887c20a7d99922dffbd92323ec491200b6d336
felmer <felmer>
2010-04-19 06:29:02 +0000

    LMS-1485 restrict of property lists increased to 200 characters
    SVN: 15503

--- eu_basysbio/source/java/eu/basysbio/cisd/dss/HeaderUtils.java
+++ eu_basysbio/source/java/eu/basysbio/cisd/dss/HeaderUtils.java
@@ -262,7 +262,7 @@ class HeaderUtils
 
     static String join(List<String> list)
     {
-        return join(list, HeaderUtils.LIST_SEPARATOR, 100);
+        return join(list, HeaderUtils.LIST_SEPARATOR, 200);
     }
     
     @Private static String join(List<String> list, String separator, int maxSize)
