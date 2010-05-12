0d08a5f884fc15d7cf3513a245dc25a2114953ae
cramakri <cramakri>
2010-05-12 16:58:13 +0000

    LMS-1527 Automatically detect openbis url.
    SVN: 15973

--- openbis/source/java/ch/systemsx/cisd/openbis/plugin/query/client/api/v1/FacadeFactory.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/plugin/query/client/api/v1/FacadeFactory.java
@@ -17,6 +17,8 @@
 package ch.systemsx.cisd.openbis.plugin.query.client.api.v1;
 
 import ch.systemsx.cisd.common.spring.HttpInvokerUtils;
+import ch.systemsx.cisd.openbis.generic.shared.DefaultLimsServiceStubFactory;
+import ch.systemsx.cisd.openbis.generic.shared.OpenBisServiceFactory;
 import ch.systemsx.cisd.openbis.plugin.query.server.api.v1.ResourceNames;
 import ch.systemsx.cisd.openbis.plugin.query.shared.api.v1.IQueryApiServer;
 
@@ -29,14 +31,24 @@ public class FacadeFactory
 {
     private static final int SERVER_TIMEOUT_MIN = 5;
 
+    // Trick for discovering the server Url
+    private static String getServiceUrl(String serverUrl)
+    {
+        OpenBisServiceFactory openBisServiceFactory =
+                new OpenBisServiceFactory(serverUrl, new DefaultLimsServiceStubFactory());
+        openBisServiceFactory.createService();
+        return openBisServiceFactory.getUsedServerUrl() + ResourceNames.QUERY_PLUGIN_SERVER_URL;
+    }
+
     /**
      * Creates a facade for specified server URL, user Id, and password.
      */
     public static IQueryApiFacade create(String serverURL, String userID, String password)
     {
+        String serviceUrl = getServiceUrl(serverURL);
         IQueryApiServer service =
-                HttpInvokerUtils.createServiceStub(IQueryApiServer.class, serverURL
-                        + ResourceNames.QUERY_PLUGIN_SERVER_URL, SERVER_TIMEOUT_MIN);
+                HttpInvokerUtils.createServiceStub(IQueryApiServer.class, serviceUrl,
+                        SERVER_TIMEOUT_MIN);
         String sessionToken = service.tryToAuthenticateAtQueryServer(userID, password);
         if (sessionToken == null)
         {
