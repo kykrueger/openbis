8728c1707a961cba65c151776964bfcbb85a7469
tpylak <tpylak>
2010-05-27 09:27:49 +0000

    minor: formatting
    SVN: 16147

--- eu_basysbio/source/java/eu/basysbio/cisd/dss/TimeSeriesPropertiesUpdateProcessingPlugin.java
+++ eu_basysbio/source/java/eu/basysbio/cisd/dss/TimeSeriesPropertiesUpdateProcessingPlugin.java
@@ -64,8 +64,7 @@ public class TimeSeriesPropertiesUpdateProcessingPlugin extends AbstractDatastor
         try
         {
             File file = getDataSubDir(dataset);
-            List<NewProperty> newProperties =
-                    HeaderUtils.extractHeaderProperties(file, true);
+            List<NewProperty> newProperties = HeaderUtils.extractHeaderProperties(file, true);
             SpaceIdentifier space =
                     new SpaceIdentifier(dataset.getDatabaseInstanceCode(), dataset.getGroupCode());
             ServiceProvider.getOpenBISService().updateDataSet(dataset.getDatasetCode(),
