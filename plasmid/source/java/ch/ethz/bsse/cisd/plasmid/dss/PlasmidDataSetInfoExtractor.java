59d1d5e9bf565111f7a639276e9c0329f348749f
buczekp <buczekp>
2010-05-31 08:02:58 +0000

    SE-248 file name property type
    SVN: 16205

--- plasmid/source/java/ch/ethz/bsse/cisd/plasmid/dss/PlasmidDataSetInfoExtractor.java
+++ plasmid/source/java/ch/ethz/bsse/cisd/plasmid/dss/PlasmidDataSetInfoExtractor.java
@@ -25,17 +25,24 @@ import ch.systemsx.cisd.etlserver.DefaultDataSetInfoExtractor;
 import ch.systemsx.cisd.etlserver.IDataSetInfoExtractor;
 import ch.systemsx.cisd.openbis.dss.generic.shared.IEncapsulatedOpenBISService;
 import ch.systemsx.cisd.openbis.dss.generic.shared.dto.DataSetInformation;
+import ch.systemsx.cisd.openbis.generic.shared.dto.NewProperty;
 
 /**
  * CSB uses DSS to register data sets attached to an existing sample plasmid. This extractor will
  * receive a single file that is inside a directory from which {@link DefaultDataSetInfoExtractor}
  * will extract information about the sample. All properties will be passed to
  * {@link DefaultDataSetInfoExtractor}.
+ * <p>
+ * Registered data sets will have one property of type {@code FILE_NAME} holding name of the file
+ * stored for the data set. Registration will fail if this property type doesn't exist in openBIS DB
+ * or is not attached to the data set type extracted by {@link PlasmidTypeExtractor}.
  * 
  * @author Piotr Buczek
  */
 public class PlasmidDataSetInfoExtractor implements IDataSetInfoExtractor
 {
+    private final static String FILE_NAME_PROPERTY = "FILE_NAME";
+
     private final IDataSetInfoExtractor delegator;
 
     public PlasmidDataSetInfoExtractor(final Properties globalProperties)
@@ -47,7 +54,11 @@ public class PlasmidDataSetInfoExtractor implements IDataSetInfoExtractor
             IEncapsulatedOpenBISService openbisService) throws UserFailureException,
             EnvironmentFailureException
     {
-        return delegator.getDataSetInformation(incomingDataSetFile.getParentFile(), openbisService);
+        final DataSetInformation result =
+                delegator
+                        .getDataSetInformation(incomingDataSetFile.getParentFile(), openbisService);
+        result.getDataSetProperties().add(
+                new NewProperty(FILE_NAME_PROPERTY, incomingDataSetFile.getName()));
+        return result;
     }
-
 }
