d195e32139d5e8804a17a115e8ef920193dd6e17
buczekp <buczekp>
2010-06-11 07:38:32 +0000

    usage statistics (script author: tpylak)
    SVN: 16408

---
+++ openbis_all/source/bash/usage_stats.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Shows openBIS server usage statistics done by users outside CISD
+
+cat ~openbis/sprint/openBIS-server/apache-tomcat/logs/*usage_log.txt* | egrep -v "tpylak|brinn|etlserver|kohleman|izabelaa|buczekp|felmer|hclaus|cramakri|baucha|ryanj" | cut -d" " -f1,8,12 | sort | uniq -c
