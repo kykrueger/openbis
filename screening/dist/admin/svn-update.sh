f24f6c8a5a00b93c19bab5c7f75bf6a45cb6b545
tpylak <tpylak>
2010-08-18 13:36:51 +0000

    admin scripts for screening
    SVN: 17507

---
+++ screening/dist/admin/svn-update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Author: Tomasz Pylak
+# Updates all the screening admin scripts to the version found in SVN.
+
+BASE=`dirname "$0"`
+if [ ${BASE#/} == ${BASE} ]; then
+    BASE="`pwd`/${BASE}"
+fi
+
+SVN=svncisd.ethz.ch/repos/cisd/screening/trunk/dist/admin
+cd $BASE
+wget -nv -r -l1 -A.sh http://$SVN/
+mv $SVN/* .
+chmod 700 *.sh
+RM=`echo $SVN | cut -d / -f1`
+rm -rf $RM
