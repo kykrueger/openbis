8d16eae7993262be8006a1fe30991be4a39afe49
felmer <felmer>
2010-08-19 07:22:18 +0000

    LMS-1707 extend CodeAndTitle
    SVN: 17523

--- datastore_server/source/java/ch/systemsx/cisd/openbis/dss/generic/shared/utils/CodeAndTitle.java
+++ datastore_server/source/java/ch/systemsx/cisd/openbis/dss/generic/shared/utils/CodeAndTitle.java
@@ -17,31 +17,20 @@
 package ch.systemsx.cisd.openbis.dss.generic.shared.utils;
 
 /**
- * Extracts from a string two other strings called 'title' and 'code'. The string has the form
- * <tt>&lt;code&gt; title</tt>. The code part is optional. In this case the code is given by the
- * title. Note: In any case the code will be normalized. That is, it is turn to upper case and any
- * symbol which isn't from A-Z or 0-9 is replaced by an underscore character.
+ * Value object which has a title and a normalized code. Normalized means that the original code
+ * arguments turn to upper case and any symbol which isn't from A-Z or 0-9 is replaced by an
+ * underscore character.
  * 
  * @author Franz-Josef Elmer
  */
 public class CodeAndTitle
 {
-    private String title;
-    private String code;
-
-    public CodeAndTitle(String titleWithOptionalCode)
+    /**
+     * Normalizes the specified code. That is lower-case characters are turned to upper case and any
+     * symbol which isn't from A-Z or 0-9 is replaced by an underscore character.
+     */
+    public static String normalize(String code)
     {
-        title = titleWithOptionalCode;
-        code = titleWithOptionalCode;
-        if (titleWithOptionalCode.startsWith("<"))
-        {
-            int indexOfClosing = titleWithOptionalCode.indexOf('>');
-            if (indexOfClosing > 0)
-            {
-                code = titleWithOptionalCode.substring(1, indexOfClosing).trim();
-                title = titleWithOptionalCode.substring(indexOfClosing + 1).trim();
-            }
-        }
         StringBuilder builder = new StringBuilder(code.toUpperCase());
         for (int i = 0, n = builder.length(); i < n; i++)
         {
@@ -50,17 +39,82 @@ public class CodeAndTitle
                 builder.setCharAt(i, '_');
             }
         }
-        code = builder.toString();
-
+        return builder.toString();
+    }
+    
+    private final String title;
+    private final String code;
+    
+    /**
+     * Creates an instance for specified code and title. The code will be normalized.
+     */
+    public CodeAndTitle(String code, String title)
+    {
+        this.code = normalize(code);
+        this.title = title;
     }
 
+    /**
+     * Creates an instance from specified title with optional code prefix in form of
+     * <code>&lt;code&gt;</code>. The code will be normalized.
+     */
+    public CodeAndTitle(String titleWithOptionalCode)
+    {
+        String t = titleWithOptionalCode;
+        String c = titleWithOptionalCode;
+        if (titleWithOptionalCode.startsWith("<"))
+        {
+            int indexOfClosing = titleWithOptionalCode.indexOf('>');
+            if (indexOfClosing > 0)
+            {
+                c = titleWithOptionalCode.substring(1, indexOfClosing).trim();
+                t = titleWithOptionalCode.substring(indexOfClosing + 1).trim();
+            }
+        }
+        code = normalize(c);
+        title = t;
+    }
+
+    /**
+     * Returns the title.
+     */
     public final String getTitle()
     {
         return title;
     }
 
+    /**
+     * Returns the attribute.
+     */
     public final String getCode()
     {
         return code;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj instanceof CodeAndTitle == false)
+        {
+            return false;
+        }
+        CodeAndTitle codeAndTitle = (CodeAndTitle) obj;
+        return codeAndTitle.code.equals(code) && codeAndTitle.title.equals(title);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return code.hashCode() * 37 + title.hashCode();
+    }
+
+    @Override
+    public String toString()
+    {
+        return "<" + code + "> " + title;
     }
 }
---
+++ datastore_server/sourceTest/java/ch/systemsx/cisd/openbis/dss/generic/shared/utils/CodeAndTitleTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2010 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.openbis.dss.generic.shared.utils;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+/**
+ * 
+ *
+ * @author Franz-Josef Elmer
+ */
+public class CodeAndTitleTest extends AssertJUnit
+{
+    @Test
+    public void testNormalize()
+    {
+        assertEquals("", CodeAndTitle.normalize(""));
+        assertEquals("ABC_123", CodeAndTitle.normalize("Abc=123"));
+    }
+    
+    @Test
+    public void testConstructor()
+    {
+        assertCodeAndTitle("ABC_1_2_3_4", "abc", new CodeAndTitle("<abc?1=2-3+4> abc"));
+        assertCodeAndTitle("ABC_123_", "abc<123>", new CodeAndTitle("abc<123>"));
+    }
+
+    private void assertCodeAndTitle(String expectedCode, String expectedTitle,
+            CodeAndTitle codeAndTitle)
+    {
+        assertEquals(expectedCode, codeAndTitle.getCode());
+        assertEquals(expectedTitle, codeAndTitle.getTitle());
+    }
+}
