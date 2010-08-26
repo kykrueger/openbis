a3ecfb1ff4e30adf994095f3f0130edbee7bdef5
cramakri <cramakri>
2010-08-26 13:59:58 +0000

    MINOR: Refactored tag and build script.
    SVN: 17641

--- openbis_all/source/bash/tag_and_build.sh
+++ openbis_all/source/bash/tag_and_build.sh
@@ -21,30 +21,104 @@ SPRINT_SERVER=sprint-openbis.ethz.ch
 CISD_SERVER=cisd-vesuvio.ethz.ch
 SPRINT_INSTALL_SCRIPT=sprint_install.sh
 
-svn checkout svn+ssh://svncisd.ethz.ch/repos/cisd/build_resources/trunk build_resources
-cd build_resources
-echo Tagging openBIS to $FULL_VER...
-./tag_sprint.sh openbis_all $FULL_VER
-echo Building openBIS...
-./build.sh --private openbis_all $FULL_VER
+# Unset this to do a dry-run (like rsync -n) and set it to actually execute the commands
+# unset EXECUTE_COMMANDS
+EXECUTE_COMMANDS=1
 
-echo Copying new openBIS components to \'$CISD_SERVER\'...
-OPENBIS_PATH=/localhome/cisd/sprint_builds/openBIS
-SPRINT_DIR=$OPENBIS_PATH/$TODAY-$FULL_VER
-echo "mkdir -p $SPRINT_DIR"  | ssh -T $CISD_SERVER
-scp *.zip $CISD_SERVER:$SPRINT_DIR
-echo "chmod g+w -R $SPRINT_DIR" | ssh -T $CISD_SERVER
+function state_start {
+	echo "----------------------------------------------------------------------"
+	echo -n "| "
+	echo $1
+	echo ""
+}
 
-echo Copying new openBIS components to \'$SPRINT_SERVER\'...
-scp openBIS-server-S*.zip $SPRINT_SERVER:.
-scp datastore_server-S*.zip $SPRINT_SERVER:.
-scp *.zip $SPRINT_SERVER:~/sprint_builds
-rm -f *.zip
+function state_end {
+	echo "\ -----------------------------------"
+	echo ""	
+}
 
-# If sprint install script is present and executable, run it!
-if [ -x $SPRINT_INSTALL_SCRIPT ]; then
-    echo Installing server remotely...
-    cat $SPRINT_INSTALL_SCRIPT | ssh -T $SPRINT_SERVER "cat > ~/$SPRINT_INSTALL_SCRIPT ; chmod 755 ~/$SPRINT_INSTALL_SCRIPT ; ~/$SPRINT_INSTALL_SCRIPT $VER ; rm -f ~/$SPRINT_INSTALL_SCRIPT"
+function setup {
+	state_start Setup
+  	echo "svn checkout svn+ssh://svncisd.ethz.ch/repos/cisd/build_resources/trunk build_resources"
+	if [ $EXECUTE_COMMANDS ]; then
+  		svn checkout svn+ssh://svncisd.ethz.ch/repos/cisd/build_resources/trunk build_resources
+	fi
+  
+  	echo "cd build_resources"
+	if [ $EXECUTE_COMMANDS ]; then
+  		cd build_resources
+	fi
+	state_end
+}
+
+function tag {
+	state_start "Tagging openBIS to $FULL_VER..."
+	
+	echo "./tag_sprint.sh openbis_all $FULL_VER"
+	if [ $EXECUTE_COMMANDS ]; then
+		./tag_sprint.sh openbis_all $FULL_VER
+	fi
+	state_end
+}
+
+function build {
+	state_start "Building openBIS..."
+	
+	echo "./build.sh --private openbis_all $FULL_VER"
+	if [ $EXECUTE_COMMANDS ]; then
+		./build.sh --private openbis_all $FULL_VER
+	fi
+	state_end
+}
+
+function copy_to_cisd_server {
+	state_start "Copying new openBIS components to '$CISD_SERVER'..."
+	
+	if [ $EXECUTE_COMMANDS ]; then
+		OPENBIS_PATH=/localhome/cisd/sprint_builds/openBIS
+		SPRINT_DIR=$OPENBIS_PATH/$TODAY-$FULL_VER
+		echo "mkdir -p $SPRINT_DIR"  | ssh -T $CISD_SERVER
+		scp *.zip $CISD_SERVER:$SPRINT_DIR
+		echo "chmod g+w -R $SPRINT_DIR" | ssh -T $CISD_SERVER
+	fi
+	state_end
+}
+
+function copy_to_sprint_server {
+	state_start "Copying new openBIS components to '$SPRINT_SERVER'..."
+	
+	if [ $EXECUTE_COMMANDS ]; then
+		scp openBIS-server-S*.zip $SPRINT_SERVER:.
+		scp datastore_server-S*.zip $SPRINT_SERVER:.
+		scp *.zip $SPRINT_SERVER:~/sprint_builds
+		rm -f *.zip
+	fi
+	state_end
+}
+
+function install_sprint {
+	# If sprint install script is present and executable, run it!
+	if [ $EXECUTE_COMMANDS ]; then
+		if [ -x $SPRINT_INSTALL_SCRIPT ]; then
+			state_start "Installing new sprint builds on '$SPRINT_SERVER'..."
+		    echo Installing server remotely...
+		    cat $SPRINT_INSTALL_SCRIPT | ssh -T $SPRINT_SERVER "cat > ~/$SPRINT_INSTALL_SCRIPT ; chmod 755 ~/$SPRINT_INSTALL_SCRIPT ; ~/$SPRINT_INSTALL_SCRIPT $VER ; rm -f ~/$SPRINT_INSTALL_SCRIPT"
+			state_end
+		fi
+	fi
+}
+
+if [ $EXECUTE_COMMANDS ]; then
+	echo -n
+else
+	state_start "RUNNING DRY RUN"
 fi
 
-echo Done!
+setup
+tag
+build
+copy_to_cisd_server
+copy_to_sprint_server
+install_sprint
+
+state_start Done!
