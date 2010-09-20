7246a88e4cd9a0e16142c7a6ae86410ae3f9ee26
felmer <felmer>
2010-09-20 07:24:44 +0000

    LMS-1610 try to fix test
    SVN: 17905

--- integration-tests/run.sh
+++ integration-tests/run.sh
@@ -12,15 +12,15 @@ echo Testing Screening Workflow
 result_hcs=$?
 print_result $result_hcs
 
-echo Testing YeastX Workflow
-./test-yeastx.sh $@
-result_yeastx=$?
-print_result $result_yeastx
-
 echo Testing 3V Screening Workflow
 ./test-3v.sh $@
 result_3v=$?
 print_result $result_3v
+
+echo Testing YeastX Workflow
+./test-yeastx.sh $@
+result_yeastx=$?
+print_result $result_yeastx
 
 if [ $result_3v -ne 0 -o $result_yeastx -ne 0 -o $result_hcs -ne 0 ]; then
 	exit 1;
