2aa855a3cf308a707f08f34176be8c7e62d8bb3a
cramakri <cramakri>
2010-10-06 13:09:13 +0000

    LMS-1765 Fixed the sprint_post_install.sh script to work with the new DSS distribution structure.
    SVN: 18184

--- openbis_all/source/bash/sprint_post_install_sprint.sh
+++ openbis_all/source/bash/sprint_post_install_sprint.sh
@@ -6,16 +6,8 @@ CIFEX_DIR=~/sprint/datastore_server/data/incoming-cifex
 INCOMING_DIR=~/sprint/datastore_server/data/incoming-batch
 DROPBOX_FIA_DIR=~/sprint/datastore_server/data/dropbox-fiaML
 DROPBOX_EIC_DIR=~/sprint/datastore_server/data/dropbox-eicML
-DS_LOCATION=~/datastore_server-plugins.jar
-DS_DESTINATION=~/sprint/datastore_server/lib
 
 [ -d $CIFEX_DIR ] || mkdir -p $CIFEX_DIR
 [ -d $INCOMING_DIR ] || mkdir -p $INCOMING_DIR
 [ -d $DROPBOX_FIA_DIR ] || mkdir -p $DROPBOX_FIA_DIR 
 [ -d $DROPBOX_EIC_DIR ] || mkdir -p $DROPBOX_EIC_DIR 
-
-if [ -f $DS_LOCATION ]; then
-  cp $DS_LOCATION $DS_DESTINATION 
-else
-  echo $DS_LOCATION not present! 
-fi
