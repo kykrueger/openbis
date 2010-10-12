ae7ddb500de93d5c86192ebd3423d2e1c4e72d28
buczekp <buczekp>
2010-10-12 11:24:51 +0000

    [LMS-1832] removed old function
    SVN: 18262

--- openbis/source/sql/postgresql/060/function-060.sql
+++ openbis/source/sql/postgresql/060/function-060.sql
@@ -272,36 +272,3 @@ $$ LANGUAGE 'plpgsql';
 
 CREATE TRIGGER DATA_SET_PROPERTY_WITH_MATERIAL_DATA_TYPE_CHECK BEFORE INSERT OR UPDATE ON data_set_properties
     FOR EACH ROW EXECUTE PROCEDURE DATA_SET_PROPERTY_WITH_MATERIAL_DATA_TYPE_CHECK();
-    
-    
-------------------------------------------------------------------------------------
---  Purpose:  Create trigger that checks if entity_type_code specified in a query
---            matches a code of an entity of type specified in query_type.
-------------------------------------------------------------------------------------
-
-CREATE OR REPLACE FUNCTION query_entity_type_code_check() RETURNS trigger AS $$
-DECLARE
-   counter  INTEGER;
-BEGIN
-   if (NEW.entity_type_code IS NOT NULL) then
-     if (NEW.query_type = 'GENERIC') then
-       RAISE EXCEPTION 'Insert/Update of Query (Name: %) failed because entity_type has to be null for GENERIC queries.', NEW.name;
-     elsif (NEW.query_type = 'EXPERIMENT') then
-       SELECT count(*) INTO counter FROM experiment_types WHERE code = NEW.entity_type_code;
-		 elsif (NEW.query_type = 'DATA_SET') then
-       SELECT count(*) INTO counter FROM data_set_types WHERE code = NEW.entity_type_code;
-		 elsif (NEW.query_type = 'MATERIAL') then
-       SELECT count(*) INTO counter FROM material_types WHERE code = NEW.entity_type_code;
-		 elsif (NEW.query_type = 'SAMPLE') then
-       SELECT count(*) INTO counter FROM sample_types WHERE code = NEW.entity_type_code;
-		 end if;
-		 if (counter = 0) then
-			 RAISE EXCEPTION 'Insert/Update of Query (Name: %) failed because % Type (Code: %) does not exist.', NEW.name, NEW.query_type, NEW.entity_type_code;
-		 end if;			
-   end if;
-   RETURN NEW;
-END;
-$$ LANGUAGE 'plpgsql';
-
-CREATE TRIGGER query_entity_type_code_check BEFORE INSERT OR UPDATE ON queries
-    FOR EACH ROW EXECUTE PROCEDURE query_entity_type_code_check();
