4854641528a9251c39fad743fe45fa76797fad24
felmer <felmer>
2010-10-19 14:03:00 +0000

    LMS-1717 headless system tests fixed
    SVN: 18359

--- openbis/sourceTest/java/ch/systemsx/cisd/openbis/systemtest/BatchMaterialRegistrationAndUpdate.java
+++ openbis/sourceTest/java/ch/systemsx/cisd/openbis/systemtest/BatchMaterialRegistrationAndUpdate.java
@@ -18,9 +18,13 @@ package ch.systemsx.cisd.openbis.systemtest;
 
 import static org.testng.AssertJUnit.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.http.HttpSession;
 
@@ -28,8 +32,13 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.testng.annotations.Test;
 
 import ch.systemsx.cisd.openbis.generic.client.web.client.dto.BatchRegistrationResult;
+import ch.systemsx.cisd.openbis.generic.client.web.client.dto.DisplayedOrSelectedIdHolderCriteria;
+import ch.systemsx.cisd.openbis.generic.client.web.client.dto.GridRowModels;
+import ch.systemsx.cisd.openbis.generic.client.web.client.dto.ListMaterialDisplayCriteria;
+import ch.systemsx.cisd.openbis.generic.client.web.client.dto.ResultSet;
 import ch.systemsx.cisd.openbis.generic.client.web.client.exception.UserFailureException;
 import ch.systemsx.cisd.openbis.generic.client.web.server.UploadedFilesBean;
+import ch.systemsx.cisd.openbis.generic.shared.basic.GridRowModel;
 import ch.systemsx.cisd.openbis.generic.shared.basic.IEntityInformationHolderWithPermId;
 import ch.systemsx.cisd.openbis.generic.shared.basic.TechId;
 import ch.systemsx.cisd.openbis.generic.shared.basic.dto.IEntityProperty;
@@ -46,11 +55,13 @@ public class BatchMaterialRegistrationAndUpdate extends SystemTestCase
 {
     private static final String MATERIAL_TYPE = "CONTROL";
     private static final String SESSION_KEY = "session-key";
+    private static final Set<String> CODES = new HashSet<String>(Arrays.asList("C1", "C2"));
 
     @Test
     public void testRegistration()
     {
         logIntoCommonClientService();
+        deleteTestMaterials();
         String materialBatchData = "code\tdescription\tsize\nc1\tcompound 1\t42\nc2\tcompound 2\t43";
         
         List<BatchRegistrationResult> result = registerMaterials(materialBatchData);
@@ -61,17 +72,15 @@ public class BatchMaterialRegistrationAndUpdate extends SystemTestCase
         assertProperties("[DESCRIPTION: compound 1, SIZE: 42]", "C1");
         assertProperties("[DESCRIPTION: compound 2, SIZE: 43]", "C2");
     }
-    
+
     @Test
     public void testUpdate()
     {
         logIntoCommonClientService();
-        if (getMaterialOrNull("C1") == null)
-        {
-            String materialBatchData =
+        deleteTestMaterials();
+        String materialBatchData =
                 "code\tdescription\tsize\n" + "c1\tcompound 1\t42\n" + "c2\tcompound 2\t43";
-            registerMaterials(materialBatchData);
-        }
+        registerMaterials(materialBatchData);
 
         List<BatchRegistrationResult> result =
                 updateMaterials("code\tdescription\n" + "c1\tnew description\n" + "c2\t--DELETE--",
@@ -82,6 +91,58 @@ public class BatchMaterialRegistrationAndUpdate extends SystemTestCase
         
         assertProperties("[DESCRIPTION: new description, SIZE: 42]", "C1");
         assertProperties("[SIZE: 43]", "C2");
+    }
+
+    @Test
+    public void testUpdateIgnoreUnregistered()
+    {
+        logIntoCommonClientService();
+        deleteTestMaterials();
+        String materialBatchData =
+                "code\tdescription\tsize\n" + "c1\tcompound 1\t42\n" + "c2\tcompound 2\t43";
+        registerMaterials(materialBatchData);
+
+        List<BatchRegistrationResult> result =
+                updateMaterials("code\tdescription\tsize\n" + "c1\tcompound one\t\n"
+                        + "c2\tcompound two\t4711\n" + "c3\t3\t\n", true);
+
+        assertEquals("2 material(s) updated, 1 ignored.", result.get(0).getMessage());
+        assertEquals(1, result.size());
+
+        assertProperties("[DESCRIPTION: compound one, SIZE: 42]", "C1");
+        assertProperties("[DESCRIPTION: compound two, SIZE: 4711]", "C2");
+    }
+    
+    private void deleteTestMaterials()
+    {
+        MaterialType materialType = getMaterialType(MATERIAL_TYPE);
+        ResultSet<Material> materials =
+                commonClientService.listMaterials(ListMaterialDisplayCriteria
+                        .createForMaterialType(materialType));
+        GridRowModels<Material> list = materials.getList();
+        List<Material> materialsToBeDeleted = new ArrayList<Material>();
+        for (GridRowModel<Material> gridRowModel : list)
+        {
+            if (CODES.contains(gridRowModel.getOriginalObject().getCode()))
+            {
+                materialsToBeDeleted.add(gridRowModel.getOriginalObject());
+            }
+        }
+        commonClientService.deleteMaterials(DisplayedOrSelectedIdHolderCriteria
+                .<Material> createSelectedItems(materialsToBeDeleted), "?");
+    }
+    
+    private MaterialType getMaterialType(String code)
+    {
+        List<MaterialType> materialTypes = commonClientService.listMaterialTypes();
+        for (MaterialType materialType : materialTypes)
+        {
+            if (materialType.getCode().equalsIgnoreCase(code))
+            {
+                return materialType;
+            }
+        }
+        throw new IllegalArgumentException("Unknown material type: " + code);
     }
     
     private void assertProperties(String expectedProperties, String materialCode)
