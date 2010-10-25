0dec8c5a5b3913ce9db51b172f917d029e959248
kohleman <kohleman>
2010-10-25 10:27:45 +0000

    initial checkin
    SVN: 18426

---
+++ openbis_all/source/bash/sprint_post_install_phosphonetx.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Finishes sprint installation of PhoshponetX
+# Warning: all changes to this file should be transfered to SVN repository
+
+DSS=~/sprint/datastore_server
+CONFIG_DIR=~/config
+
+
+function copy_or_die {
+  SRC=$1
+  DEST=$2
+	if [ -f $SRC ]; then
+		echo Copy $SRC to $DEST
+		cp $SRC $DEST
+	else
+			echo -e "\n[ERROR]: cannot copy $SRC to $DEST, because $SRC does not exist"
+		exit 1
+	fi
+}
+
+copy_or_die ~/config/openbis-as/genericCommonContext.xml ~/sprint/openBIS-server/jetty/webapps/openbis/WEB-INF/classes
