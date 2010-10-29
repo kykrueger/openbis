4de285e2d343ed2f7a32bd6cbf5c3ffba2d67128
tpylak <tpylak>
2010-10-29 11:36:54 +0000

    screening: administration script
    SVN: 18495

--- screening/dist/admin/boot.sh
+++ screening/dist/admin/boot.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # Fetches "openBIS for HCS" administration scripts from source repository to the bin folder in current directory.
 
-BASE=`dirname "$0"`
-if [ ${BASE#/} == ${BASE} ]; then
-    BASE="`pwd`/${BASE}"
-fi
-
 SVN_DIR=svncisd.ethz.ch/repos/cisd/screening/trunk/dist/admin
 
 mkdir bin
