4ab206a0587b1721a38da4de5fc42084fdd7b26f
kohleman <kohleman>
2010-11-04 10:43:13 +0000

    added LIMB and modified CINA
    SVN: 18555

--- openbis_all/source/bash/copy-zips-to-servers.sh
+++ openbis_all/source/bash/copy-zips-to-servers.sh
@@ -5,23 +5,26 @@ export SPRINT=cisd-bamus.ethz.ch
 export DEMO=cisd-tongariro.ethz.ch
 export YEASTX=obis.ethz.ch
 export PHOSPHONETX=openbis-phosphonetx.ethz.ch
-export AGRONOMICS=bs-agronomics01.ethz.ch
+export AGRONOMICS=bs-dsvr11.ethz.ch
 export DSU=bs-dsvr28-openbis-dsu.ethz.ch
 export SCU=bs-dsvr28-openbis-scu.ethz.ch
 export BASYSBIO=basysbio.ethz.ch
 export BASYSBIO_TEST=bs-dsvr28-openbis-test.ethz.ch
 export CINA=bs-openbis01.ethz.ch
 export PLASMIDS=bs-openbis02.ethz.ch
+export LIMB=bs-openbis03.ethz.ch
 
 
 # Different types of server specific zips we distinguish
 export ZIPS="openBIS-server-S*.zip  datastore_server-S*.zip"
-export ZIPS_YEASTX="openBIS-server-S*.zip datastore_server-S*.zip *yeastx*.zip"
-export ZIPS_PHOSPHONETX="datastore_server-S*.zip *phosphonetx*.zip"
-export ZIPS_DSU="openBIS-server-S*.zip datastore_server-S*.zip datastore_server_plugin-dsu*.zip openbis-tracking-client*.zip"
-export ZIPS_BASYSBIO="datastore_server-S*.zip *basysbio*.zip openBIS-server-S*.zip"
-export ZIPS_CINA="openBIS-server-S*.zip datastore_server-S*.zip datastore_server_plugin-cina-*.zip"
-export ZIPS_PLASMIDS="datastore_server-S*.zip *plasmid* openBIS-server-S*.zip"
+export ZIPS_PHOSPHONETX="*phosphonetx*.zip"
+export ZIPS_DSU="openBIS-server-S*.zip datastore_server-dsu*.zip openbis-tracking-client*.zip"
+export ZIPS_BASYSBIO="*basysbio*.zip openBIS-server-S*.zip"
+export ZIPS_CINA=" openBIS-server-S*.zip *cina-*.zip datastore_server-S*"
+export ZIPS_PLASMIDS="*plasmid* openBIS-server-S*.zip"
+
+# Special plugin
+export DATASTORE_PLUGIN="datastore_server_plugin*.zip"
 
 echo -e "\nCopying default openBIS/DSS to servers...\n"
 for i in $ZIPS; do
@@ -30,12 +33,8 @@ for i in $ZIPS; do
          echo $LIVERX; scp -p $i $LIVERX:~openbis
          echo $AGRONOMICS; scp -p $i $AGRONOMICS:~openbis
          echo $SCU; scp -p $i $SCU:~openbis
+         echo $LIMB; scp -p $i $LIMB:~openbis
  
-done
-
-echo -e "\nCopying to $YEASTX...\n"
-for j in $ZIPS_YEASTX; do
-        echo $YEASTX; scp -p $j $YEASTX:~openbis
 done
 
 echo -e "\nCopying to $PHOSPHONETX...\n"
@@ -60,4 +59,15 @@ for y in $ZIPS_PLASMIDS; do
                 echo $PLASMIDS; scp -p $y $PLASMIDS:~openbis
 done
 
+echo -e "\n Copying to $CINA...\n"
+for z in $ZIPS_CINA; do
+                echo $CINA; scp -p $z $CINA:~openbis
+done
 
+echo -e "\nCopying to default dss...\n"
+for l in $DATASTORE_PLUGIN; do
+        echo $AGRONOMICS; scp -p $l $AGRONOMICS:~openbis/config
+        echo $YEASTX; scp -p $l $YEASTX:~openbis/config
+        echo $BASYSBIO; scp -p $l $BASYSBIO:~openbis/config
+        echo $BASYSBIO_TEST; scp -p $l $BASYSBIO_TEST:~openbis/config
+done
