7a316ae90bf4eab1ad17fab32b3be774b912854a
brinn <brinn>
2010-11-13 09:39:57 +0000

    fix: startup file for Windows
    SVN: 18667

--- datamover/dist/datamover.bat
+++ datamover/dist/datamover.bat
@@ -1,3 +1,3 @@
 @echo off
 
-java -cp lib\datamover.jar:lib\log4j.jar:lib\cisd-base.jar:lib\cisd-args4j.jar:lib\commons-lang.jar:lib\commons-io.jar:lib\activation.jar:lib\mail.jar ch.systemsx.cisd.datamover.Main --rsync-executable=bin\rsync.exe --ssh-executable=bin\ssh.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -cp lib\datamover.jar;lib\log4j.jar;lib\cisd-base.jar;lib\cisd-args4j.jar;lib\commons-lang.jar;lib\commons-io.jar;lib\activation.jar;lib\mail.jar ch.systemsx.cisd.datamover.Main --rsync-executable=bin\rsync.exe --ssh-executable=bin\ssh.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
