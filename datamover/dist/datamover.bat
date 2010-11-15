fedfa3bbc41b0fe512dd3b74e4dbbdfa6154d816
brinn <brinn>
2010-11-15 14:33:44 +0000

    let datamover.bat create the .ssh directory automatically
    SVN: 18694

--- datamover/dist/datamover.bat
+++ datamover/dist/datamover.bat
@@ -1,3 +1,7 @@
 @echo off
 
+IF EXIST home\%username%\.ssh GOTO START
+mkdir home\%username%\.ssh
+
+:START
 java -cp lib\datamover.jar;lib\log4j.jar;lib\cisd-base.jar;lib\cisd-args4j.jar;lib\commons-lang.jar;lib\commons-io.jar;lib\activation.jar;lib\mail.jar ch.systemsx.cisd.datamover.Main --rsync-executable=bin\rsync.exe --ssh-executable=bin\ssh.exe --ln-executable=bin\ln.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
