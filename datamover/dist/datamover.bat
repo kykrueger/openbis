19a877964f21da55fdfb522e97d35398b5d03231
brinn <brinn>
2010-11-15 20:26:07 +0000

    add: binaries for the lastchanged utility on the most common platforms move: Windows Cygwin binaries and libraries from bin to bin/win
    SVN: 18711

--- datamover/dist/datamover.bat
+++ datamover/dist/datamover.bat
@@ -4,4 +4,4 @@ IF EXIST home\%username%\.ssh GOTO START
 mkdir home\%username%\.ssh
 
 :START
-java -cp lib\datamover.jar;lib\log4j.jar;lib\cisd-base.jar;lib\cisd-args4j.jar;lib\commons-lang.jar;lib\commons-io.jar;lib\activation.jar;lib\mail.jar ch.systemsx.cisd.datamover.Main --rsync-executable=bin\rsync.exe --ssh-executable=bin\ssh.exe --ln-executable=bin\ln.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -cp lib\datamover.jar;lib\log4j.jar;lib\cisd-base.jar;lib\cisd-args4j.jar;lib\commons-lang.jar;lib\commons-io.jar;lib\activation.jar;lib\mail.jar ch.systemsx.cisd.datamover.Main --rsync-executable=bin\win\rsync.exe --ssh-executable=bin\win\ssh.exe --ln-executable=bin\win\ln.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
