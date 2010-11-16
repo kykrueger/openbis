838b2ab568b0c656701a85fc2f82769c60c5aef0
felmer <felmer>
2010-11-16 13:55:41 +0000

    back to normal log mode for Jetty
    SVN: 18737

--- openbis/dist/server/startup.sh
+++ openbis/dist/server/startup.sh
@@ -3,6 +3,5 @@
 source `dirname "$0"`/setup-env
 $JVM -DSTOP.PORT=$JETTY_STOP_PORT \
      -DSTOP.KEY=$JETTY_STOP_KEY \
-     -Dorg.eclipse.jetty.util.log.DEBUG=true \
      $JAVA_OPTS $JAVA_MEM_OPTS \
      -jar start.jar etc/jetty.xml >> logs/jetty.out 2>&1 &
