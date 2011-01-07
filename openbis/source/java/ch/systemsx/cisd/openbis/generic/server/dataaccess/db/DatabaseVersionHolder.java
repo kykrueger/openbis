b6ad40f63add18a9061e4da7883a67e8bc09f726
kaloyane <kaloyane>
2011-01-07 14:11:05 +0000

    [LMS-1949] database migration for a new field - SCRIPT_TYPE
    SVN: 19319

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
@@ -24,7 +24,7 @@ package ch.systemsx.cisd.openbis.generic.server.dataaccess.db;
 public final class DatabaseVersionHolder
 {
     /** Current version of the database. */
-    private static final String DATABASE_VERSION = "063";// S96
+    private static final String DATABASE_VERSION = "064";// S98
 
     private DatabaseVersionHolder()
     {
