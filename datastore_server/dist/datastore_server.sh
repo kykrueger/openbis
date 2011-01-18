7779b602ef2c42e4c119587f9ae3bdfec8c7fc0f
tpylak <tpylak>
2011-01-18 14:21:06 +0000

    LMS-1966 allow jython scripts to use standard library in DSS
    SVN: 19481

--- datastore_server/dist/datastore_server.sh
+++ datastore_server/dist/datastore_server.sh
@@ -131,7 +131,7 @@ else
 fi
 
 command=$1
-ALL_JAVA_OPTS="-Djavax.net.ssl.trustStore=etc/openBIS.keystore $JAVA_OPTS"
+ALL_JAVA_OPTS="-Djavax.net.ssl.trustStore=etc/openBIS.keystore $JAVA_OPTS -Dpython.path=lib/jython-lib"
 
 # Build classpath from $LIB_FOLDER content. First JAR is datastore_server.jar because it has to appear before cifex.jar
 CP=`echo $LIB_FOLDER/datastore_server.jar $LIB_FOLDER/*.jar | sed 's/ /:/g'`
