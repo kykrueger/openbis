282721e64edc9bb5322591fc1656b5fc69dacbb7
felmer <felmer>
2011-01-24 15:29:23 +0000

    TIMEOUT increase to 1000
    SVN: 19555

--- common/sourceTest/java/ch/systemsx/cisd/common/concurrent/MonitoringProxyTest.java
+++ common/sourceTest/java/ch/systemsx/cisd/common/concurrent/MonitoringProxyTest.java
@@ -61,7 +61,7 @@ public class MonitoringProxyTest
 
     private static final Status THE_STATUS = Status.TWO;
 
-    private static final long TIMEOUT_MILLIS = 300L;
+    private static final long TIMEOUT_MILLIS = 1000L;
 
     private volatile Thread threadToStop;
 
