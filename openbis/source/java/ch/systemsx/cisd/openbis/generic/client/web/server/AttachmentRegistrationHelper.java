7a642b12a2fabc1fb214674943ff775da8ddacb3
kaloyane <kaloyane>
2011-01-27 15:57:23 +0000

    [LMS-1935] limit attachment size re-implemented as agreed on the S98.0 demo meeting
    SVN: 19627

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/client/web/server/AttachmentRegistrationHelper.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/client/web/server/AttachmentRegistrationHelper.java
@@ -7,13 +7,17 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.FileUtils;
+
 import ch.systemsx.cisd.common.exceptions.UserFailureException;
+import ch.systemsx.cisd.common.filesystem.FileUtilities;
 import ch.systemsx.cisd.common.spring.IUncheckedMultipartFile;
 import ch.systemsx.cisd.openbis.generic.client.web.server.translator.UserFailureExceptionTranslator;
 import ch.systemsx.cisd.openbis.generic.shared.basic.dto.NewAttachment;
 
 public abstract class AttachmentRegistrationHelper
 {
+    private static final long MAX_ATTACHMENT_SIZE = 256 * FileUtils.ONE_MB;
 
     abstract public void register(Collection<NewAttachment> attachmentPEs);
 
@@ -24,6 +28,7 @@ public abstract class AttachmentRegistrationHelper
         try
         {
             uploadedFiles = (UploadedFilesBean) httpSession.getAttribute(sessionKey);
+            abortIfMaxSizeExceeded(uploadedFiles);
             final Map<String, NewAttachment> attachmentMap = createAttachmentMap(attachmentsOrNull);
             fillContent(uploadedFiles, attachmentMap);
             register(attachmentMap.values());
@@ -58,6 +63,30 @@ public abstract class AttachmentRegistrationHelper
         return result;
     }
 
+    private void abortIfMaxSizeExceeded(UploadedFilesBean uploadedFiles)
+    {
+        if (uploadedFiles != null)
+        {
+            for (final IUncheckedMultipartFile multipartFile : uploadedFiles.iterable())
+            {
+                long fileSize = multipartFile.getSize();
+                if (fileSize > MAX_ATTACHMENT_SIZE)
+                {
+                    String maxSizeString =
+                            FileUtilities.byteCountToDisplaySize(MAX_ATTACHMENT_SIZE);
+                    String fileSizeString = FileUtilities.byteCountToDisplaySize(fileSize);
+                    String errorMessage =
+                            String.format(
+                                    "The file %s(%s) is larger than the configured maximum (%s).",
+                                    multipartFile.getOriginalFilename(), fileSizeString,
+                                    maxSizeString);
+                    throw new UserFailureException(errorMessage);
+                }
+            }
+        }
+
+    }
+
     private void fillContent(UploadedFilesBean uploadedFiles, Map<String, NewAttachment> attachments)
     {
         if (uploadedFiles != null)
@@ -65,6 +94,7 @@ public abstract class AttachmentRegistrationHelper
             for (final IUncheckedMultipartFile multipartFile : uploadedFiles.iterable())
             {
                 final String fileName = multipartFile.getOriginalFilename();
+                // NOTE: this will load the entire attachments in memory
                 final byte[] content = multipartFile.getBytes();
                 final NewAttachment attachmentOrNull = attachments.get(fileName);
                 if (attachmentOrNull != null)
