e4aba14a43d0f8be71bfbf7661f1676d5c5f04f8
kaloyane <kaloyane>
2011-02-02 07:57:23 +0000

    minor: test script readability improvement
    SVN: 19689

--- openbis/sourceTest/java/ch/systemsx/cisd/openbis/generic/shared/managed_property/structured/structured-property-test.py
+++ openbis/sourceTest/java/ch/systemsx/cisd/openbis/generic/shared/managed_property/structured/structured-property-test.py
@@ -1,3 +1,6 @@
+factory = createElementFactory()
+converter = createPropertyConverter()
+
 #
 # we use a function named "configureUI" to test something completely unrelated,
 # because this is the easiest way to have the entire managed properties infrastrcture
@@ -8,7 +11,6 @@ def configureUI():
   # 
   # test creating the element structure
   #
-  factory = createElementFactory()
   
   elements = [
       factory.createSampleLink("samplePermId"),
@@ -16,7 +18,6 @@ def configureUI():
       factory.createElement("testElement").addAttribute("key1", "value1").addAttribute("key2", "value2")
   ]
   
-  converter = createPropertyConverter()
   property.value = converter.convertToString(elements)
 
 
