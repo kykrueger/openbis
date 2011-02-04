d6ebc83392426ae8fbb70dcff4f25a9dd3bdcc55
kaloyane <kaloyane>
2011-02-04 17:15:40 +0000

    [LMS-2018] added more logging information to help us with the analysis of the issue in the future
    SVN: 19756

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/GenericHttpSessionListener.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/GenericHttpSessionListener.java
@@ -16,10 +16,19 @@
 
 package ch.systemsx.cisd.openbis.generic.server;
 
+import java.io.Serializable;
+import java.util.Enumeration;
+
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionActivationListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
+import org.apache.log4j.Logger;
+
+import ch.systemsx.cisd.common.logging.LogCategory;
+import ch.systemsx.cisd.common.logging.LogFactory;
+import ch.systemsx.cisd.openbis.generic.client.web.server.AbstractClientService;
 import ch.systemsx.cisd.openbis.generic.shared.IServer;
 import ch.systemsx.cisd.openbis.generic.shared.dto.Session;
 
@@ -35,6 +44,29 @@ import ch.systemsx.cisd.openbis.generic.shared.dto.Session;
  */
 public final class GenericHttpSessionListener implements HttpSessionListener
 {
+    protected static final Logger operationLog = LogFactory.getLogger(LogCategory.OPERATION,
+            AbstractClientService.class);
+
+    private static final String LOGGING_ACTIVATION_PARAM = "opennis.log.activation.parameter";
+
+    private final HttpSessionActivationListener loggingActivationListener =
+            new LoggingActivationListener();
+        
+    private class LoggingActivationListener implements HttpSessionActivationListener, Serializable
+    {
+
+        private static final long serialVersionUID = 1L;
+
+        public void sessionWillPassivate(HttpSessionEvent arg0)
+        {
+            logSessionEvent("sessionWillPassivate", arg0);
+        }
+
+        public void sessionDidActivate(HttpSessionEvent arg0)
+        {
+            logSessionEvent("sessionDidActivate", arg0);
+        }
+    }
 
     //
     // HttpSessionListener
@@ -42,6 +74,8 @@ public final class GenericHttpSessionListener implements HttpSessionListener
 
     public final void sessionCreated(final HttpSessionEvent sessionEvent)
     {
+        logSessionEvent("sessionCreated", sessionEvent);
+        sessionEvent.getSession().setAttribute(LOGGING_ACTIVATION_PARAM, loggingActivationListener);
     }
 
     public final void sessionDestroyed(final HttpSessionEvent sessionEvent)
@@ -50,15 +84,49 @@ public final class GenericHttpSessionListener implements HttpSessionListener
         final String sessionToken =
                 (String) httpSession
                         .getAttribute(SessionConstants.OPENBIS_SESSION_TOKEN_ATTRIBUTE_KEY);
+
+        httpSession.removeAttribute(LOGGING_ACTIVATION_PARAM);
+        logSessionEvent("sessionDestroyed", sessionEvent);
+
         if (sessionToken != null)
         {
             final IServer server =
                     (IServer) httpSession
                             .getAttribute(SessionConstants.OPENBIS_SERVER_ATTRIBUTE_KEY);
+
             if (server != null)
             {
                 server.logout(sessionToken);
             }
         }
     }
+
+    private void logSessionEvent(String methodName, HttpSessionEvent event)
+    {
+        HttpSession session = event.getSession();
+        StringBuilder message = new StringBuilder();
+        message.append(methodName).append(": ");
+
+        message.append("id=");
+        message.append(session.getId());
+        message.append(", lastAccessedTime=");
+        message.append(session.getLastAccessedTime());
+        message.append(", maxInactiveInterval=");
+        message.append(session.getMaxInactiveInterval());
+
+        message.append(", attributes=[");
+
+        Enumeration<?> attrNames = session.getAttributeNames();
+
+        while (attrNames.hasMoreElements())
+        {
+            String attrName = (String) attrNames.nextElement();
+            Object formattedPropValue =
+                    String.format("'%s'='%s', ", attrName, session.getAttribute(attrName));
+            message.append(formattedPropValue);
+        }
+        message.append("]");
+        operationLog.info(message.toString());
+    }
+
 }
