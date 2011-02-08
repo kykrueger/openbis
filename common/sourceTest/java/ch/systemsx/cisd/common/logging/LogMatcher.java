aed821d45d18dc587435d7e42da63ef5fd109a30
cramakri <cramakri>
2011-02-08 12:35:05 +0000

    LMS-2033 Added utility class to match lines from the logger.
    SVN: 19809

---
+++ common/sourceTest/java/ch/systemsx/cisd/common/logging/LogMatcher.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2011 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.common.logging;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * @author Chandrasekhar Ramakrishnan
+ */
+public class LogMatcher
+{
+    private final BufferedAppender logRecorder;
+
+    private final String[] expectedLines;
+
+    public LogMatcher(BufferedAppender logRecorder, String... expectedLines)
+    {
+        this.logRecorder = logRecorder;
+        this.expectedLines = expectedLines;
+    }
+
+    public void assertMatches()
+    {
+        try
+        {
+            BufferedReader reader =
+                    new BufferedReader(new StringReader(logRecorder.getLogContent()));
+
+            int lineNumber = 0;
+            for (String expectedLine : expectedLines)
+            {
+                String actualLine;
+                actualLine = reader.readLine();
+                if (false == actualLine.matches(expectedLine))
+                {
+                    throw new AssertionError(format("Line " + lineNumber, expectedLine, actualLine));
+                }
+                ++lineNumber;
+            }
+        } catch (IOException ex)
+        {
+            // Ignore, we are reading from a string
+        }
+    }
+
+    private String format(String message, Object expected, Object actual)
+    {
+        String formatted = "";
+        if (message != null)
+        {
+            formatted = message + " ";
+        }
+
+        return formatted + "expected:<" + expected + "> but was:<" + actual + ">";
+    }
+}
