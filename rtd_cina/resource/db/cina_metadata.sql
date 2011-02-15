1a81faca235cd47ba6796b47fbf54aaf3c1fad11
cramakri <cramakri>
2011-02-15 10:51:24 +0000

    LMS-1993 Fixed DB initialization scripts.
    SVN: 19961

--- rtd_cina/resource/db/cina_metadata.sql
+++ rtd_cina/resource/db/cina_metadata.sql
@@ -16,118 +16,21 @@ SET escape_string_warning = off;
 -- Name: plpgsql; Type: PROCEDURAL LANGUAGE; Schema: -; Owner: cramakri
 --
 
-CREATE PROCEDURAL LANGUAGE plpgsql;
+-- CREATE PROCEDURAL LANGUAGE plpgsql;
 
 
-ALTER PROCEDURAL LANGUAGE plpgsql OWNER TO cramakri;
+-- ALTER PROCEDURAL LANGUAGE plpgsql OWNER TO cramakri;
 
 SET search_path = public, pg_catalog;
-
---
--- TOC entry 2566 (class 0 OID 1366480)
--- Dependencies: 1725
--- Data for Name: data_set_type_property_types; Type: TABLE DATA; Schema: public; Owner: cramakri
---
-
-COPY data_set_type_property_types (id, dsty_id, prty_id, is_mandatory, is_managed_internally, pers_id_registerer, registration_timestamp, ordinal, section, is_dynamic, script_id) FROM stdin;
-1	2	2	f	f	2	2010-11-23 16:41:20.476646+01	1	\N	f	\N
-5	2	16	f	f	2	2010-11-23 16:42:42.31869+01	2	\N	f	\N
-2	2	7	f	f	2	2010-11-23 16:41:55.598405+01	4	Dimension	f	\N
-3	2	8	f	f	2	2010-11-23 16:42:03.75754+01	5	Dimension	f	\N
-4	2	9	f	f	2	2010-11-23 16:42:13.6773+01	6	Dimension	f	\N
-6	2	17	f	f	2	2010-11-23 16:42:52.425723+01	3	\N	f	\N
-7	2	18	f	f	2	2010-11-23 16:43:21.420024+01	7	Size	f	\N
-8	2	19	f	f	2	2010-11-23 16:43:36.39704+01	8	Size	f	\N
-9	2	20	f	f	2	2010-11-23 16:43:46.571047+01	9	Size	f	\N
-10	2	15	f	f	2	2010-11-23 16:44:48.993521+01	10	\N	f	\N
-11	3	15	f	f	2	2010-11-23 16:45:08.535134+01	1	\N	f	\N
-12	4	15	f	f	2	2010-11-23 16:45:17.44938+01	1	\N	f	\N
-13	5	15	f	f	2	2010-11-23 16:45:25.349048+01	1	\N	f	\N
-\.
-
-
---
--- TOC entry 2567 (class 0 OID 1366491)
--- Dependencies: 1726
--- Data for Name: data_set_types; Type: TABLE DATA; Schema: public; Owner: cramakri
---
-
-COPY data_set_types (id, code, description, dbin_id, modification_timestamp, main_ds_pattern, main_ds_path) FROM stdin;
-1	UNKNOWN	Unknown	1	2010-11-23 15:35:26.619042+01	\N	\N
-2	IMAGE	\N	1	2010-11-23 16:35:48.274825+01	\N	\N
-3	BUNDLE	\N	1	2010-11-23 16:39:16.522518+01	\N	\N
-4	METADATA	\N	1	2010-11-23 16:39:23.890373+01	\N	\N
-5	RAW_IMAGES	\N	1	2010-11-23 16:39:28.89983+01	\N	\N
-\.
-
-select setval('data_set_type_id_seq', 5);
-
-
---
--- TOC entry 2575 (class 0 OID 1366560)
--- Dependencies: 1742
--- Data for Name: experiment_properties; Type: TABLE DATA; Schema: public; Owner: cramakri
---
-
-COPY experiment_properties (id, expe_id, etpt_id, value, cvte_id, mate_prop_id, pers_id_registerer, registration_timestamp, modification_timestamp) FROM stdin;
-1	1	1	Generic Experiment	\N	\N	2	2010-11-25 08:31:33.818753+01	2010-11-25 08:31:33.997+01
-\.
-
-select setval('experiment_property_id_seq', 1);
-
---
--- TOC entry 2576 (class 0 OID 1366573)
--- Dependencies: 1745
--- Data for Name: experiment_type_property_types; Type: TABLE DATA; Schema: public; Owner: cramakri
---
-
-COPY experiment_type_property_types (id, exty_id, prty_id, is_mandatory, is_managed_internally, pers_id_registerer, registration_timestamp, ordinal, section, is_dynamic, script_id) FROM stdin;
-1	1	1	f	f	2	2010-11-23 16:46:31.838797+01	1	\N	f	\N
-\.
-
-
---
--- TOC entry 2577 (class 0 OID 1366584)
--- Dependencies: 1746
--- Data for Name: experiment_types; Type: TABLE DATA; Schema: public; Owner: cramakri
---
-
-COPY experiment_types (id, code, description, dbin_id, modification_timestamp) FROM stdin;
-1	CINA_EXP_TYPE	\N	1	2010-11-23 16:40:21.980445+01
-\.
-
-select setval('experiment_type_id_seq', 1);
-
---
--- TOC entry 2578 (class 0 OID 1366591)
--- Dependencies: 1747
--- Data for Name: experiments; Type: TABLE DATA; Schema: public; Owner: cramakri
---
-
-COPY experiments (id, perm_id, code, exty_id, mate_id_study_object, pers_id_registerer, registration_timestamp, modification_timestamp, proj_id, inva_id, is_public) FROM stdin;
-1	20101125083133963-1	E1	1	\N	2	2010-11-25 08:31:33.818753+01	2010-11-25 10:38:05.38+01	1	\N	f
-\.
-
---
--- TOC entry 2591 (class 0 OID 1366718)
--- Dependencies: 1773
--- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: cramakri
---
-
-COPY projects (id, code, space_id, pers_id_leader, description, pers_id_registerer, registration_timestamp, modification_timestamp) FROM stdin;
-1	GENERIC	1	\N	\N	2	2010-11-23 15:39:17.549496+01	2010-11-25 08:31:33.998+01
-\.
-
-select setval('project_id_seq', 1);
 
 --
 -- TOC entry 2592 (class 0 OID 1366728)
 -- Dependencies: 1775
 -- Data for Name: property_types; Type: TABLE DATA; Schema: public; Owner: cramakri
 --
+-- 1	DESCRIPTION	A Description	Description	1	2010-11-23 15:35:26.619042+01	1	\N	f	f	1	\N	\N	\N
 
 COPY property_types (id, code, description, label, daty_id, registration_timestamp, pers_id_registerer, covo_id, is_managed_internally, is_internal_namespace, dbin_id, maty_prop_id, schema, transformation) FROM stdin;
-1	DESCRIPTION	A Description	Description	1	2010-11-23 15:35:26.619042+01	1	\N	f	f	1	\N	\N	\N
 2	ANNOTATION	A user annotation of the image.	Annotation	2	2010-11-23 16:04:23.696762+01	2	\N	f	f	1	\N	\N	\N
 3	COLORFLAG	Is the image a color image?	Is Color?	5	2010-11-23 16:05:14.828992+01	2	\N	f	f	1	\N	\N	\N
 4	CREATION_DATE	Creation date of this sample.	Creation Date	6	2010-11-23 16:05:42.995123+01	2	\N	f	f	1	\N	\N	\N
@@ -151,17 +54,67 @@ COPY property_types (id, code, description, label, daty_id, registration_timesta
 
 select setval('property_type_id_seq', 21);
 
---
--- TOC entry 2598 (class 0 OID 1366790)
--- Dependencies: 1788
--- Data for Name: sample_type_property_types; Type: TABLE DATA; Schema: public; Owner: cramakri
---
 
-COPY sample_type_property_types (id, saty_id, prty_id, is_mandatory, is_managed_internally, pers_id_registerer, registration_timestamp, is_displayed, ordinal, section, is_dynamic, script_id) FROM stdin;
-1	1	1	f	f	2	2010-11-23 16:45:50.000057+01	t	1	\N	f	\N
-3	1	6	f	f	2	2010-11-23 16:46:14.461392+01	t	2	\N	f	\N
+--
+-- TOC entry 2567 (class 0 OID 1366491)
+-- Dependencies: 1726
+-- Data for Name: data_set_types; Type: TABLE DATA; Schema: public; Owner: cramakri
+--
+-- 1	UNKNOWN	Unknown	1	2010-11-23 15:35:26.619042+01	\N	\N
+
+COPY data_set_types (id, code, description, dbin_id, modification_timestamp, main_ds_pattern, main_ds_path) FROM stdin;
+2	IMAGE	\N	1	2010-11-23 16:35:48.274825+01	\N	\N
+3	BUNDLE	\N	1	2010-11-23 16:39:16.522518+01	\N	\N
+4	METADATA	\N	1	2010-11-23 16:39:23.890373+01	\N	\N
+5	RAW_IMAGES	\N	1	2010-11-23 16:39:28.89983+01	\N	\N
 \.
 
+select setval('data_set_type_id_seq', 5);
+
+--
+-- TOC entry 2566 (class 0 OID 1366480)
+-- Dependencies: 1725
+-- Data for Name: data_set_type_property_types; Type: TABLE DATA; Schema: public; Owner: cramakri
+--
+
+COPY data_set_type_property_types (id, dsty_id, prty_id, is_mandatory, is_managed_internally, pers_id_registerer, registration_timestamp, ordinal, section, script_id) FROM stdin;
+1	2	2	f	f	2	2010-11-23 16:41:20.476646+01	1	\N	\N
+5	2	16	f	f	2	2010-11-23 16:42:42.31869+01	2	\N	\N
+2	2	7	f	f	2	2010-11-23 16:41:55.598405+01	4	Dimension	\N
+3	2	8	f	f	2	2010-11-23 16:42:03.75754+01	5	Dimension	\N
+4	2	9	f	f	2	2010-11-23 16:42:13.6773+01	6	Dimension	\N
+6	2	17	f	f	2	2010-11-23 16:42:52.425723+01	3	\N	\N
+7	2	18	f	f	2	2010-11-23 16:43:21.420024+01	7	Size	\N
+8	2	19	f	f	2	2010-11-23 16:43:36.39704+01	8	Size	\N
+9	2	20	f	f	2	2010-11-23 16:43:46.571047+01	9	Size	\N
+10	2	15	f	f	2	2010-11-23 16:44:48.993521+01	10	\N	\N
+11	3	15	f	f	2	2010-11-23 16:45:08.535134+01	1	\N	\N
+12	4	15	f	f	2	2010-11-23 16:45:17.44938+01	1	\N	\N
+13	5	15	f	f	2	2010-11-23 16:45:25.349048+01	1	\N	\N
+\.
+
+--
+-- TOC entry 2577 (class 0 OID 1366584)
+-- Dependencies: 1746
+-- Data for Name: experiment_types; Type: TABLE DATA; Schema: public; Owner: cramakri
+--
+
+COPY experiment_types (id, code, description, dbin_id, modification_timestamp) FROM stdin;
+1	CINA_EXP_TYPE	\N	1	2010-11-23 16:40:21.980445+01
+\.
+
+--
+-- TOC entry 2576 (class 0 OID 1366573)
+-- Dependencies: 1745
+-- Data for Name: experiment_type_property_types; Type: TABLE DATA; Schema: public; Owner: cramakri
+--
+
+COPY experiment_type_property_types (id, exty_id, prty_id, is_mandatory, is_managed_internally, pers_id_registerer, registration_timestamp, ordinal, section, script_id) FROM stdin;
+1	1	1	f	f	2	2010-11-23 16:46:31.838797+01	1	\N	\N
+\.
+
+
+select setval('experiment_type_id_seq', 1);
 
 --
 -- TOC entry 2599 (class 0 OID 1366802)
@@ -177,6 +130,18 @@ COPY sample_types (id, code, description, dbin_id, is_listable, generated_from_d
 select setval('sample_type_id_seq', 3);
 
 --
+-- TOC entry 2598 (class 0 OID 1366790)
+-- Dependencies: 1788
+-- Data for Name: sample_type_property_types; Type: TABLE DATA; Schema: public; Owner: cramakri
+--
+
+COPY sample_type_property_types (id, saty_id, prty_id, is_mandatory, is_managed_internally, pers_id_registerer, registration_timestamp, is_displayed, ordinal, section, script_id) FROM stdin;
+1	1	1	f	f	2	2010-11-23 16:45:50.000057+01	t	1	\N	\N
+3	1	6	f	f	2	2010-11-23 16:46:14.461392+01	t	2	\N	\N
+\.
+
+
+--
 -- TOC entry 2583 (class 0 OID 1366638)
 -- Dependencies: 1756
 -- Data for Name: spaces; Type: TABLE DATA; Schema: public; Owner: cramakri
@@ -187,6 +152,42 @@ COPY spaces (id, code, dbin_id, description, registration_timestamp, pers_id_reg
 \.
 
 select setval('space_id_seq', 1);
+
+--
+-- TOC entry 2591 (class 0 OID 1366718)
+-- Dependencies: 1773
+-- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: cramakri
+--
+
+COPY projects (id, code, space_id, pers_id_leader, description, pers_id_registerer, registration_timestamp, modification_timestamp) FROM stdin;
+1	GENERIC	1	\N	\N	2	2010-11-23 15:39:17.549496+01	2010-11-25 08:31:33.998+01
+\.
+
+select setval('project_id_seq', 1);
+
+
+--
+-- TOC entry 2578 (class 0 OID 1366591)
+-- Dependencies: 1747
+-- Data for Name: experiments; Type: TABLE DATA; Schema: public; Owner: cramakri
+--
+
+COPY experiments (id, perm_id, code, exty_id, mate_id_study_object, pers_id_registerer, registration_timestamp, modification_timestamp, proj_id, inva_id, is_public) FROM stdin;
+1	20101125083133963-1	E1	1	\N	2	2010-11-25 08:31:33.818753+01	2010-11-25 10:38:05.38+01	1	\N	f
+\.
+
+--
+-- TOC entry 2575 (class 0 OID 1366560)
+-- Dependencies: 1742
+-- Data for Name: experiment_properties; Type: TABLE DATA; Schema: public; Owner: cramakri
+--
+
+COPY experiment_properties (id, expe_id, etpt_id, value, cvte_id, mate_prop_id, pers_id_registerer, registration_timestamp, modification_timestamp) FROM stdin;
+1	1	1	Generic Experiment	\N	\N	2	2010-11-25 08:31:33.818753+01	2010-11-25 08:31:33.997+01
+\.
+
+select setval('experiment_property_id_seq', 1);
+
 
 --
 -- PostgreSQL database dump complete
--- rtd_cina/resource/db/db_initialize_cina.sh
+++ rtd_cina/resource/db/db_initialize_cina.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-DB=openbis_cina
+DB=openbis_screening_cina
 USER=cramakri
 psql -U $USER -h localhost -p 5432 -d $DB -f cina_metadata.sql
