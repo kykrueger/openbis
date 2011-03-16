fbf74cf5513b04b70e8d1eafc156c1f81cce38c8
tpylak <tpylak>
2011-03-16 09:30:59 +0000

    LMS-2069 iBrain2 dropboxes: image quality dropbox
    SVN: 20355

--- integration-tests/templates/data-ibrain2/scripts/hcs_analysis_well_quality.py
+++ integration-tests/templates/data-ibrain2/scripts/hcs_analysis_well_quality.py
@@ -2,6 +2,7 @@
 
 import commonImageDropbox
 import commonDropbox
+from java.util import Properties
 
 reload(commonImageDropbox)
 reload(commonDropbox)
@@ -10,17 +11,32 @@ reload(commonDropbox)
 # so that the rollback can use it as well.
 iBrain2DatasetId = None
 
+def rollback_transaction(service, transaction, algorithmRunner, throwable):
+    commonDropbox.createFailureStatus(iBrain2DatasetId, throwable, incoming)
+
+def getConfigurationProperties():
+    config = Properties()
+    config.setProperty("separator", ",")
+    config.setProperty("well-name-row", "File_Name")
+    config.setProperty("well-name-col", "File_Name")
+    config.setProperty("well-name-col-is-alphanum", "true")
+    return config
+    
 def register(incomingPath):
     metadataParser = commonDropbox.DerivedDatasetMetadataParser(incomingPath)
+    global iBrain2DatasetId
     iBrain2DatasetId = metadataParser.getIBrain2DatasetId()
-    openbisDatasetParent = metadataParser.getParentDatasetPermId()
 
-    (space, plate) = commonDropbox.tryGetConnectedPlate(state, openbisDatasetParent, iBrain2DatasetId, incomingPath)
-    if plate == None:
-        return
-    
-    #TODO: finish me !!!!
+    transaction = service.transaction(incoming, factory)
+    configProps = getConfigurationProperties()
+    incomingCsvFile = commonDropbox.findCSVFile(incomingPath)
+    analysisRegistrationDetails = factory.createFeatureVectorRegistrationDetails(incomingCsvFile, configProps)
 
+    dataset = transaction.createNewDataSet(analysisRegistrationDetails)
+    dataset.setDataSetType('HCS_ANALYSIS_WELL_QUALITY')
+    dataset.setFileFormatType('CSV')
+    commonDropbox.registerDerivedDataset(state, transaction, dataset, incoming, metadataParser)
+ 
 def rollback_transaction(service, transaction, algorithmRunner, throwable):
     commonDropbox.createFailureStatus(iBrain2DatasetId, throwable, incoming)
             
