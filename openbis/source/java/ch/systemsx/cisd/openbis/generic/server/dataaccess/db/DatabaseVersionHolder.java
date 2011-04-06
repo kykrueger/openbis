0ceb793549a9d7dc9ceccf7fb460166515723ee3
tpylak <tpylak>
2011-04-06 12:48:37 +0000

    LMS-2162 change db version
    SVN: 20673

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
@@ -24,7 +24,7 @@ package ch.systemsx.cisd.openbis.generic.server.dataaccess.db;
 public final class DatabaseVersionHolder
 {
     /** Current version of the database. */
-    private static final String DATABASE_VERSION = "068";// S103
+    private static final String DATABASE_VERSION = "069";// S104
 
     private DatabaseVersionHolder()
     {
