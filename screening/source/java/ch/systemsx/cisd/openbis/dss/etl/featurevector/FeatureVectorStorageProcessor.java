cca81a17487972a934de7246117727693b8516a8
tpylak <tpylak>
2011-05-31 09:46:37 +0000

    LMS-2271 fix: do not rollback twice
    SVN: 21531

--- screening/source/java/ch/systemsx/cisd/openbis/dss/etl/featurevector/FeatureVectorStorageProcessor.java
+++ screening/source/java/ch/systemsx/cisd/openbis/dss/etl/featurevector/FeatureVectorStorageProcessor.java
@@ -103,23 +103,15 @@ public class FeatureVectorStorageProcessor extends AbstractDelegatingStorageProc
             nestedTransaction.storeData(dataSetInformation, typeExtractor, mailClient,
                     incomingDataSetDirectory, rootDirectory);
 
+            dataAccessObject = createDAO();
+            File parent = new File(nestedTransaction.getStoredDataDirectory(), ORIGINAL_DIR);
+            File dataSet = new File(parent, incomingDataSetDirectory.getName());
             try
             {
-                dataAccessObject = createDAO();
-                File parent = new File(nestedTransaction.getStoredDataDirectory(), ORIGINAL_DIR);
-                File dataSet = new File(parent, incomingDataSetDirectory.getName());
-                try
-                {
-                    loadDataSetIntoDatabase(dataAccessObject, dataSet, dataSetInformation);
-                } catch (IOException ex)
-                {
-                    throw new IOExceptionUnchecked(ex);
-                }
-            } catch (RuntimeException ex)
+                loadDataSetIntoDatabase(dataAccessObject, dataSet, dataSetInformation);
+            } catch (IOException ex)
             {
-                // NOTE: the framework will not call the rollback by itself
-                executeRollback(ex);
-                throw ex;
+                throw new IOExceptionUnchecked(ex);
             }
             return nestedTransaction.getStoredDataDirectory();
         }
