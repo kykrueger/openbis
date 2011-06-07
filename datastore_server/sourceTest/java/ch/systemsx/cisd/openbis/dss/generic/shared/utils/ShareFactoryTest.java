52cbb937242047829924632702ba1c989923f31c
kaloyane <kaloyane>
2011-06-07 13:37:25 +0000

    [LMS-2238] added ShareFactoryTest with resources
    SVN: 21615

---
+++ datastore_server/sourceTest/java/ch/systemsx/cisd/openbis/dss/generic/shared/utils/ShareFactoryTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2011 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.openbis.dss.generic.shared.utils;
+
+import java.io.File;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import ch.systemsx.cisd.openbis.dss.generic.shared.utils.Share.ShufflePriority;
+
+/**
+ * @author Kaloyan Enimanev
+ */
+public class ShareFactoryTest extends AssertJUnit
+{
+    private final static File DATA_DIRECTORY = new File("./resource/test-data/"
+            + ShareFactoryTest.class.getSimpleName());
+
+    @Test
+    public void testSharePropertiesOverridesSpeedFile()
+    {
+        Share share = readShare("share-1");
+        assertEquals(70, share.getSpeed());
+        assertEquals(ShufflePriority.SPEED, share.getShufflePriority());
+    }
+
+    @Test
+    public void testSpeedFileRespected()
+    {
+        Share share = readShare("share-2");
+        assertEquals(12, share.getSpeed());
+        assertEquals(ShufflePriority.MOVE_TO_EXTENSION, share.getShufflePriority());
+    }
+
+    @Test
+    public void testOnlySharePropertiesAvailable()
+    {
+        Share share = readShare("share-3");
+        assertEquals(70, share.getSpeed());
+        assertEquals(ShufflePriority.SPEED, share.getShufflePriority());
+    }
+
+    @Test
+    public void testOnlySpeedFileAvailable()
+    {
+        Share share = readShare("share-4");
+        assertEquals(12, share.getSpeed());
+        assertEquals(ShufflePriority.SPEED, share.getShufflePriority());
+    }
+
+    private Share readShare(String shareName)
+    {
+        File shareRoot = new File(DATA_DIRECTORY, shareName);
+        return new ShareFactory().createShare(shareRoot, null, null);
+    }
+}
