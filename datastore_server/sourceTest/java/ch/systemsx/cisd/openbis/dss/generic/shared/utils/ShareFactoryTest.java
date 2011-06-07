e4bfde7573d95a66b843463f269a69672ea3849e
kaloyane <kaloyane>
2011-06-07 13:55:42 +0000

    minor: make the test run from within screening
    SVN: 21616

--- datastore_server/sourceTest/java/ch/systemsx/cisd/openbis/dss/generic/shared/utils/ShareFactoryTest.java
+++ datastore_server/sourceTest/java/ch/systemsx/cisd/openbis/dss/generic/shared/utils/ShareFactoryTest.java
@@ -28,7 +28,7 @@ import ch.systemsx.cisd.openbis.dss.generic.shared.utils.Share.ShufflePriority;
  */
 public class ShareFactoryTest extends AssertJUnit
 {
-    private final static File DATA_DIRECTORY = new File("./resource/test-data/"
+    private final static File DATA_DIRECTORY = new File("../datastore_server/resource/test-data/"
             + ShareFactoryTest.class.getSimpleName());
 
     @Test
