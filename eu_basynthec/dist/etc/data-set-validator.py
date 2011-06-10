445556157fa97a067340e9bbf9e74d52101e7586
cramakri <cramakri>
2011-06-10 20:02:55 +0000

    LMS-2323 Fixes to handlers for BaSynthec.
    SVN: 21679

--- eu_basynthec/dist/etc/data-set-validator.py
+++ eu_basynthec/dist/etc/data-set-validator.py
@@ -2,8 +2,5 @@ import os
 import re
  
 def validate_data_set_file(file):
-    errors = []
-    if False:
-        errors.append(createFileValidationError(file.getName() + " is not a valid data set. The file name cannot contain 'invalid'."))
- 
-    return errors
+	errors = []
+	return errors
