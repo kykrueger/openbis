b3c2aee8e311adf26804628e90222db8e8be2202
kaloyane <kaloyane>
2011-07-04 15:30:31 +0000

    [LMS-2350] almost fully functional Sanofi dropbox
    SVN: 21966

--- sanofi/dist/etc/sanofi-dropbox/sanofi-dropbox.py
+++ sanofi/dist/etc/sanofi-dropbox/sanofi-dropbox.py
@@ -1,27 +1,133 @@
 import re
+import os
 
+from java.io import File
+
+from ch.systemsx.cisd.common.geometry import Point, ConversionUtils
 from ch.systemsx.cisd.common.mail import From
+from ch.systemsx.cisd.common.fileconverter import FileConverter, Tiff2PngConversionStrategy
 
 from ch.systemsx.cisd.openbis.generic.shared.api.v1.dto import SearchCriteria 
 from ch.systemsx.cisd.openbis.generic.shared.api.v1.dto.SearchCriteria import MatchClause, MatchClauseAttribute
 from ch.systemsx.cisd.openbis.dss.generic.shared.api.internal.v1 import MaterialIdentifierCollection
- 
+from ch.systemsx.cisd.openbis.dss.etl.dto.api.v1 import SimpleImageDataConfig, ImageMetadata, OriginalDataStorageFormat, Location 
+from ch.systemsx.cisd.openbis.dss.etl.custom.geexplorer import GEExplorerImageAnalysisResultParser
 from ch.systemsx.cisd.openbis.plugin.screening.shared.basic.dto import ScreeningConstants
+from ch.systemsx.cisd.openbis.plugin.screening.shared.api.v1.dto import Geometry
 
-# Switch to False for the Sanofi production environment
+""" Switch to False for the Sanofi production environment """
 TEST_MODE=True
 
-PLATE_TYPE = "PLATE"
-
-DATA_SET_TYPE = "HCS_IMAGE_RAW"
-DATA_SET_BATCH_PROPNAME = "ACQUISITION_BATCH"
-
+""" the url of the Sanofi's openBIS installation """
 OPENBIS_URL = "https://bwl27.sanofi-aventis.com:8443/openbis"
 
-EXPERIMENT_RECIPIENTS_PROPNAME = "OBSERVER_EMAILS"
+EXPERIMENT_RECIPIENTS_PROPCODE = "OBSERVER_EMAILS"
 
 # TODO KE: get all instance admin emails here
 DEFAULT_RECIPIENT_LIST = "Matthew.Smicker@sanofi-aventis.com"
+
+""" the sample type identifying plates """
+PLATE_TYPE = "PLATE"
+
+""" file format code of files in a new image dataset """
+IMAGE_DATASET_FILE_FORMAT = "TIFF"
+IMAGE_DATASET_BATCH_PROPCODE = "ACQUISITION_BATCH"
+
+"""
+Allows to recognize that the subdirectory of the incoming dataset directory contains overlay images.
+This text has to appear in the subdirectory name.
+"""
+OVERLAYS_DIR_PATTERN = "_ROITiff"
+""" file format of the image overlay dataset """
+OVERLAY_IMAGE_FILE_FORMAT = "PNG"
+""" name of the color which should be treated as transparent in overlays """
+OVERLAYS_TRANSPARENT_COLOR = "black"
+
+""" file format of the analysis dataset """
+ANALYSIS_FILE_FORMAT = "CSV"
+ANALYSIS_RUN_PROPCODE = "ANALYSIS_RUN"
+    
+""" should thumbnails be generated? """
+GENERATE_THUMBNAILS = True
+
+""" the maximal width and height of the generated thumbnails """
+MAX_THUMNAIL_WIDTH_AND_HEIGHT = 256
+
+"""
+number of threads that are used for thumbnail generation will be equal to:
+   this constant * number of processor cores
+"""
+ALLOWED_MACHINE_LOAD_DURING_THUMBNAIL_GENERATION = 1.0
+
+""" should all dataset in one experiment use the same channels? """
+STORE_CHANNELS_ON_EXPERIMENT_LEVEL = False
+
+""" should the original data be stored in the original form or should we pack them into one container? """
+ORIGINAL_DATA_STORAGE_FORMAT = OriginalDataStorageFormat.UNCHANGED
+
+# =================================
+#  Generic utility functions
+# =================================
+
+""" 
+Finds first occurence of the patter from the right.
+Throws exception if the pattern cannot be found.
+"""
+def rfind(text, pattern):
+    ix = text.rfind(pattern)
+    ensurePatternFound(ix, text, pattern)
+    return ix
+
+""" 
+Finds first occurence of the patter from the left. 
+Throws exception if the pattern cannot be found.
+"""
+def find(text, pattern):
+    ix = text.find(pattern)
+    ensurePatternFound(ix, text, pattern)
+    return ix
+
+def ensurePatternFound(ix, file, pattern):
+    if ix == -1:
+        raise Exception("Cannot find '" + pattern + "' pattern in file name '" + file + "'")    
+
+""" Returns: name of the file without the extension """
+def extractFileBasename(filename):
+    lastDot = filename.rfind(".")
+    if lastDot != -1:
+        return filename[0:lastDot]
+    else:
+        return filename
+
+""" Returns: extension of the file """
+def getFileExt(file):
+    return os.path.splitext(file)[1][1:].lower()
+
+""" Returns: java.io.File - first file with the specified extension or None if no file matches """
+def findFileByExt(incomingFile, expectedExt):
+    if not incomingFile.isDirectory():
+        return None
+    incomingPath = incomingFile.getPath()
+    for file in os.listdir(incomingPath):
+        ext = getFileExt(file)
+        if ext.upper() == expectedExt.upper():
+            return File(incomingPath, file)
+    return None
+
+""" Returns: java.io.File - subdirectory which contains the specified marker in the name """
+def findDir(incomingFile, dirNameMarker):
+    if not incomingFile.isDirectory():
+        return None
+    incomingPath = incomingFile.getPath()
+    for file in os.listdir(incomingPath):
+        if dirNameMarker.upper() in file.upper():
+            return File(incomingPath, file)
+    return None
+
+
+# ======================================
+# end generic utility functions 
+# ======================================
 
 def rollback_transaction(service, transaction, runner, ex):
     plateLink = createPlateLink(OPENBIS_URL, plate.getCode())
@@ -50,7 +156,7 @@ def commit_transaction(service, transaction):
 
 def sendEmail(title, content):
     if experiment is not None:
-       recipientsProp = experiment.getPropertyValue(EXPERIMENT_RECIPIENTS_PROPNAME)
+       recipientsProp = experiment.getPropertyValue(EXPERIMENT_RECIPIENTS_PROPCODE)
         
     if not recipientsProp:
         recipientsProp = DEFAULT_RECIPIENT_LIST
@@ -148,16 +254,9 @@ class PlateInitializer:
         self.plate = plate
         self.plateCode = plate.getCode()
         self.experimentId = plate.getExperiment().getExperimentIdentifier()
-    
-    def wellColumn(self, x):
-        numLetters = 26
-        if x < numLetters:
-            return chr(ord('A') + x)
-        else:
-            return self.wellColumn((x / numLetters) - 1) + self.wellColumn( x % numLetters) 
         
     def getWellCode(self, x, y):
-        return self.wellColumn(x) + str(y + 1)
+        return ConversionUtils.convertToSpreadsheetLocation(Point(x,y))
     
     def getPlateDimensions(self):
         """
@@ -165,9 +264,8 @@ class PlateInitializer:
           to a tuple of integers (plateHeight, plateWidth) 
         """
         plateGeometryString = self.plate.getPropertyValue(ScreeningConstants.PLATE_GEOMETRY)
-        widthByHeight = plateGeometryString.split("_")[-1]
-        dimensions = map(int, widthByHeight.split("X"))
-        return (dimensions[0], dimensions[1])
+        geometry = Geometry.createFromPlateGeometryString(plateGeometryString)
+        return (geometry.height, geometry.width)
     
     def validateLibraryDimensions(self, tsvLines):
         (plateHeight, plateWidth) = self.getPlateDimensions()
@@ -229,11 +327,15 @@ class PlateInitializer:
         sanofiMaterials = []
         for resultMap in list(queryResult):
             materialMap = self.upperCaseKeys(resultMap)
-            wellCode = str(materialMap['WELL_CODE'])
-            materialCode = str(materialMap['MATERIAL_CODE'])
-            sanofiId = str(materialMap['ABASE_COMPOUND_ID'])
-            sanofiBatchId = str(materialMap['ABASE_COMPOUND_BATCH_ID'])
-            material = SanofiMaterial(wellCode, materialCode, sanofiId, sanofiBatchId)
+            def val(code):
+                if code in materialMap:
+                    return str(materialMap[code])
+                else:
+                    raise RuntimeError("No column '%s' in the query results from the ABASE Database" % (code))
+                
+            material = SanofiMaterial(val('WELL_CODE'), val('MATERIAL_CODE'), \
+                                      val('ABASE_COMPOUND_ID'), val('ABASE_COMPOUND_BATCH_ID'))
+                
             sanofiMaterials.append(material)
             
         queryResult.close()
@@ -324,22 +426,136 @@ class PlateInitializer:
         # TODO KE: validate that library and sanofiMaterials data agrees
         openbisMaterials = self.getOrCreateMaterials(library, sanofiMaterials)
         self.createWells(library, sanofiMaterials, openbisMaterials)
- 
 
-transaction = service.transaction()
-dataSet = transaction.createNewDataSet(DATA_SET_TYPE)
+# ------------
+# Image dataset registration
+# ------------
+
+def convertToPng(dir, transparentColor):
+    delete_original_files = True
+    strategy = Tiff2PngConversionStrategy(transparentColor, 0, delete_original_files)
+    # Uses cores * machineLoad threads for the conversion, but not more than maxThreads
+    machineLoad = ALLOWED_MACHINE_LOAD_DURING_THUMBNAIL_GENERATION
+    maxThreads = 100
+    errorMsg = FileConverter.performConversion(File(dir), strategy, machineLoad, maxThreads)
+    if errorMsg != None:
+        raise Exception("Error", errorMsg)
+
+# ---------------------
+
+class MyImageDataSetConfig(SimpleImageDataConfig):
+    
+    def __init__(self, incomingDir, imageRootDir):
+        self.incomingDir = incomingDir
+        self.imageRootDir = imageRootDir
+        self.setStorageConfiguration()
+        
+    def setStorageConfiguration(self):
+        self.setStoreChannelsOnExperimentLevel(STORE_CHANNELS_ON_EXPERIMENT_LEVEL)
+        self.setOriginalDataStorageFormat(ORIGINAL_DATA_STORAGE_FORMAT)
+        if GENERATE_THUMBNAILS:
+            self.setAllowedMachineLoadDuringThumbnailsGeneration(ALLOWED_MACHINE_LOAD_DURING_THUMBNAIL_GENERATION)
+            self.setMaxThumbnailWidthAndHeight(MAX_THUMNAIL_WIDTH_AND_HEIGHT)
+    
+    """
+    Creates ImageFileInfo for a given ImageTokens.
+    Converts tile number to coordinates on the 'well matrix'.
+    Example file name: A - 1(fld 1 wv Cy5 - Cy5).tif
+    Returns:
+        ImageTokens
+    """
+    def extractImageMetadata(self, path):
+        imageTokens = ImageMetadata()
+        
+        imageFile = File(self.incomingDir, path)
+        if not self.imageRootDir.equals(imageFile.getParentFile()):
+            return None
+    
+        basename = os.path.splitext(imageFile.name)[0]
+        wellText = basename[0:find(basename, "(")] # A - 1
+        imageTokens.well = wellText.replace(" - ", "")
+        
+        if " wv " in basename:
+            fieldText = basename[find(basename, "fld ") + 4 : find(basename, " wv")]
+            imageTokens.channelCode = basename[rfind(basename, " - ") + 3 :-1]
+        else:
+            fieldText = basename[find(basename, "fld ") + 4 : find(basename, ")")]
+            imageTokens.channelCode = "DEFAULT"
+        
+        try:
+            imageTokens.tileNumber = int(fieldText)
+        except ValueError:
+            raise Exception("Cannot parse field number from '" + fieldText + "' in '" + basename + "' file name.")
+    
+        return imageTokens
+    
+    def getTileCoordinates(self, tileNumber, tileGeometry):
+        columns = tileGeometry.getWidth()
+        row = ((tileNumber - 1) / columns) + 1
+        col = ((tileNumber - 1) % columns) + 1
+        return Location(row, col)
+    
+    def getTileGeometry(self, imageMetadataList, maxTile):
+        if maxTile % 4 == 0 and maxTile != 4:
+            (cols, rows) = (maxTile / 4, 4)
+        elif maxTile % 3 == 0:
+            (cols, rows) = (maxTile / 3, 3)
+        elif maxTile % 2 == 0:
+            (cols, rows) = (maxTile / 2, 2)
+        else:
+            (cols, rows) = (maxTile, 1)
+        
+        return Geometry.createFromRowColDimensions(cols, rows);
+
+
+
+transaction = service.transaction(incoming, factory)
 
 (batchName, barCode) = parseIncomingDirname(incoming.getName())
-dataSet.setPropertyValue(DATA_SET_BATCH_PROPNAME, batchName)
 plate = findPlateByCode(barCode)
 if not plate.getExperiment():
     raise RuntimeError("Plate with code '%(barCode)s' is not associated with experiment" % vars())
+
 experimentId = plate.getExperiment().getExperimentIdentifier()
 experiment = transaction.getExperiment(experimentId)
 
+# reload the sample with all contained samples
+plate = transaction.getSample(plate.getSampleIdentifier())
 if len(plate.getContainedSamples()) == 0:
     plateInitializer = PlateInitializer(transaction, plate)
     plateInitializer.createWellsAndMaterials()
     
-dataSet.setSample(plate)
-transaction.moveFile(incoming.getAbsolutePath(), dataSet)
+imageDatasetConfig = MyImageDataSetConfig(incoming, incoming)
+imageDatasetConfig.setRawImageDatasetType()
+imageDatasetConfig.setFileFormatType(IMAGE_DATASET_FILE_FORMAT)
+imageDatasetDetails = factory.createImageRegistrationDetails(imageDatasetConfig, incoming)
+imageDataSet = transaction.createNewDataSet(imageDatasetDetails)
+imageDataSet.setPropertyValue(IMAGE_DATASET_BATCH_PROPCODE, batchName)
+imageDataSet.setSample(plate)
+
+# check for overlays folder
+overlaysDir = findDir(incoming, OVERLAYS_DIR_PATTERN)
+if overlaysDir is not None:
+    convertToPng(overlaysDir.getPath(), OVERLAYS_TRANSPARENT_COLOR)
+    overlayDatasetConfig = MyImageDataSetConfig(incoming, overlaysDir)
+    overlayDatasetConfig.setSegmentationImageDatasetType()
+    overlayDatasetConfig.setFileFormatType(OVERLAY_IMAGE_FILE_FORMAT)
+    overlayDatasetDetails = factory.createImageRegistrationDetails(overlayDatasetConfig, incoming)
+    overlayDataset = transaction.createNewDataSet(overlayDatasetDetails)
+    overlayDataset.setSample(imageDataSet.getSample())
+    overlayDataset.setParentDatasets([ imageDataSet.getDataSetCode() ])
+    transaction.moveFile(overlaysDir.getPath(), overlayDataset, "overlays")
+
+# transform and move analysis file
+analysisFile = findFileByExt(incoming, "xml")
+if analysisFile is not None:
+    analysisDataSet = transaction.createNewDataSet(ScreeningConstants.DEFAULT_ANALYSIS_WELL_DATASET_TYPE)
+    analysisDataSet.setSample(imageDataSet.getSample())
+    analysisDataSet.setParentDatasets([ imageDataSet.getDataSetCode() ])
+    analysisDataSet.setFileFormatType(ANALYSIS_FILE_FORMAT)
+    analysisDataSet.setMeasuredData(False)
+    analysisDataSet.setPropertyValue(ANALYSIS_RUN_PROPCODE, extractFileBasename(analysisFile.getName()))
+    analysisDataSetFile = transaction.createNewFile(analysisDataSet, analysisFile.getName())
+    GEExplorerImageAnalysisResultParser(analysisFile.getPath()).writeCSV(File(analysisDataSetFile))
+
+imageDataSetFolder = transaction.moveFile(incoming.getPath(), imageDataSet)
