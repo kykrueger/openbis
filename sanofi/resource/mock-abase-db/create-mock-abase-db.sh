9a3a6b9c6915009abe15cf081b7dbeaf7d646803
kaloyane <kaloyane>
2011-07-05 08:33:02 +0000

    minor: script to create abase_mock_db automatically
    SVN: 21972

--- sanofi/resource/mock-abase-db/create-mock-abase-db.sh
+++ sanofi/resource/mock-abase-db/create-mock-abase-db.sh
@@ -4,4 +4,4 @@ ABASE_DB="abase_mock_db"
 
 psql -U postgres -c "create database $ABASE_DB with owner $USER template = template0 encoding = 'UNICODE'"
 
-#psql -U $USER -d $ABASE_DB -f mock-abase-database.sql
+psql -U $USER -d $ABASE_DB -f db.sql
