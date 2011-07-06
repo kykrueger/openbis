483d68bce228d2edd2ed08cfd93fbfcefdaab18f
tpylak <tpylak>
2011-07-06 09:07:45 +0000

    LMS-2349 minor: add test data
    SVN: 22009

--- sanofi/resource/mock-abase-db/create-mock-abase-db.sh
+++ sanofi/resource/mock-abase-db/create-mock-abase-db.sh
@@ -2,13 +2,13 @@
 USER=`whoami`
 ABASE_DB="abase_mock_db"
 
-#psql -U postgres -c "create database $ABASE_DB with owner $USER template = template0 encoding = 'UNICODE'"
+psql -U postgres -c "create database $ABASE_DB with owner $USER template = template0 encoding = 'UNICODE'"
 
-#psql -U $USER -d $ABASE_DB -f db.sql
+psql -U $USER -d $ABASE_DB -f db.sql
 
 # Alternative version for tests on sprint server:
-##psql -U postgres -c "create database abase_mock_db with owner openbis template = template0 encoding = 'UNICODE'"
-##psql -U openbis -d abase_mock_db -c "CREATE TABLE plates (WELL_CODE VARCHAR NOT NULL, MATERIAL_CODE VARCHAR NOT NULL, ABASE_COMPOUND_ID VARCHAR NOT NULL, ABASE_COMPOUND_BATCH_ID VARCHAR NOT NULL, ABASE_PLATE_CODE VARCHAR NOT NULL)"
+#psql -U postgres -c "create database abase_mock_db with owner openbis template = template0 encoding = 'UNICODE'"
+#psql -U openbis -d abase_mock_db -c "CREATE TABLE plates (WELL_CODE VARCHAR NOT NULL, MATERIAL_CODE VARCHAR NOT NULL, ABASE_COMPOUND_ID VARCHAR NOT NULL, ABASE_COMPOUND_BATCH_ID VARCHAR NOT NULL, ABASE_PLATE_CODE VARCHAR NOT NULL)"
 
 #psql -U openbis -d abase_mock_db -c "#INSERT INTO plates (well_code, material_code, abase_compound_id, abase_compound_batch_id, abase_plate_code) values ('A001', 'COMPOUND-1', 'ABASE Compound (1)', 'ABASE Batch Compound (1)', 'PLATE1')"
 #psql -U openbis -d abase_mock_db -c "#INSERT INTO plates (well_code, material_code, abase_compound_id, abase_compound_batch_id, abase_plate_code) values ('A002', 'COMPOUND-2', 'ABASE Compound (1)', 'ABASE Batch Compound (1)', 'PLATE1')"
