bfc1bb49d67a5956cb5387c223f9f3cd9f3f5c2c
buczekp <buczekp>
2011-07-19 12:15:16 +0000

    increase service version for snapshot
    SVN: 22213

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/ServiceVersionHolder.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/ServiceVersionHolder.java
@@ -23,5 +23,5 @@ package ch.systemsx.cisd.openbis.generic.shared.basic.dto;
  */
 public final class ServiceVersionHolder
 {
-    public static final int VERSION = 29; // for S110
+    public static final int VERSION = 30; // for S111
 }
