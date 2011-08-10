6d649790e459f34b6b376da2139e9212a31e793b
kaloyane <kaloyane>
2011-08-10 21:03:41 +0000

    [LMS-2427] working "getting-started" instructions for the generic installation
    SVN: 22465

--- screening/dist/tarball/installer/data/dropboxes/simple-dropbox.py
+++ screening/dist/tarball/installer/data/dropboxes/simple-dropbox.py
@@ -5,7 +5,8 @@ expid = "/TEST/TEST-PROJECT/DEMO-EXP-SIMPLE"
 exp = transaction.getExperiment(expid)
  
 if None == exp:
-    exp = transaction.createNewExperiment(expid, "TEST-EXPERIMENT-TYPE")
+    exp = transaction.createNewExperiment(expid, "SIRNA_HCS")
+    exp.setPropertyValue("DESCRIPTION", "Description of the demo experiment")
  
 dataSet = transaction.createNewDataSet()
         
