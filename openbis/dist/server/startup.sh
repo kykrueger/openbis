99b7f3cd5af71829e5d38489d691a12835d01fa8
felmer <felmer>
2011-08-17 06:00:58 +0000

    LMS-2329 implemented by using org.eclipse.jetty.util.component.LifeCycle.Listener
    SVN: 22514

--- openbis/dist/server/startup.sh
+++ openbis/dist/server/startup.sh
@@ -5,4 +5,4 @@ $JVM -DSTOP.PORT=$JETTY_STOP_PORT \
      -DSTOP.KEY=$JETTY_STOP_KEY \
      $JAVA_OPTS $JAVA_MEM_OPTS \
      -Dpython.path=$JETTY_LIB_PATH \
-     -jar start.jar etc/jetty.xml >> logs/jetty.out 2>&1 &
+     -jar start.jar etc/jetty.xml lib=webapps/openbis/WEB-INF/lib >> logs/jetty.out 2>&1 &
---
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/util/LifeCycleListener.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2011 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.openbis.generic.server.util;
+
+import org.eclipse.jetty.util.component.LifeCycle;
+
+/**
+ * Stops a component (e.g. Web Server) if it goes into state "Failure".
+ *
+ * @author Franz-Josef Elmer
+ */
+public class LifeCycleListener implements LifeCycle.Listener
+{
+
+    public void lifeCycleFailure(LifeCycle lifeCycle, Throwable throwable)
+    {
+        System.err.println("Failed component " + lifeCycle + ": " + throwable);
+        try
+        {
+            lifeCycle.stop();
+        } catch (Exception ex)
+        {
+            System.err.println("Couldn't stop component " + lifeCycle);
+            ex.printStackTrace(System.err);
+        }
+    }
+
+    public void lifeCycleStarted(LifeCycle lifeCycle)
+    {
+    }
+
+    public void lifeCycleStarting(LifeCycle lifeCycle)
+    {
+    }
+
+    public void lifeCycleStopped(LifeCycle lifeCycle)
+    {
+    }
+
+    public void lifeCycleStopping(LifeCycle lifeCycle)
+    {
+    }
+
+}
