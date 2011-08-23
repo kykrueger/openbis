277aa6845b1e64b0dba202c6ad58e145b691c364
felmer <felmer>
2011-08-23 14:33:41 +0000

    LMS-2471 nightly-upgrade-generic-openbis.sh does dummy things to test cronjob
    SVN: 22617

--- openbis_all/source/bash/nightly-upgrade-generic-openbis.sh
+++ openbis_all/source/bash/nightly-upgrade-generic-openbis.sh
@@ -5,9 +5,12 @@
 # - Restore store and databases from latest snapshot
 # - Restart AS and DSS
 # 
+echo "home directory: $PWD"
+
+
 fetch-ci-artifacts.sh openbis
-fetch-ci-artifacts.sh -p server datastore_server
 fetch-ci-artifacts.sh rtd_yeastx
-sprint_install.sh
-servers-startup-from-latest-snapshot.sh snapshots
+fetch-ci-artifacts.sh -p server datastore_server
+#sprint_install.sh
+#servers-startup-from-latest-snapshot.sh snapshots
 
