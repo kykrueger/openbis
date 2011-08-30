e590c94c2aed15d2c8ef3d1696390478f56c81e3
tpylak <tpylak>
2011-08-30 13:15:54 +0000

    minor: better user doc
    SVN: 22717

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/NewSample.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/NewSample.java
@@ -41,7 +41,7 @@ public class NewSample extends Identifier<NewSample> implements Comparable<NewSa
             "# The \"experiment\" column is optional, cannot be specified for shared samples and should contain experiment identifier, e.g. /SPACE/PROJECT/EXP_1\n";
 
     public static String WITH_SPACE_COMMENT =
-            "# The \"space\" column is optional, will override space home value for the row\n";
+            "# The \"space\" column is optional, it can be used to override home space for the row\n";
 
     public static final String CONTAINER = "container";
 
