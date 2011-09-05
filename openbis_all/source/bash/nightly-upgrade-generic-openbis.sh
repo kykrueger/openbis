03a1d31b83849264899d5abecd3136c7a1019912
felmer <felmer>
2011-09-05 08:31:47 +0000

    LMS-2471 improved check whether sprint server is from this week or not
    SVN: 22779

--- openbis_all/source/bash/nightly-upgrade-generic-openbis.sh
+++ openbis_all/source/bash/nightly-upgrade-generic-openbis.sh
@@ -10,35 +10,15 @@
 # Crontab entry:
 # 30 3  *  *  *  [ -f $HOME/.profile ] && . $HOME/.profile && [ -f $HOME/.bash_profile ] && . $HOME/.bash_profile && nightly-upgrade-generic-openbis.sh &>> openbis-cronjob.txt
 # 
+# Dependencies:
+# - check-and-exit-if-new-sprint-server.sh
 
 BIN_DIR=`dirname "$0"`
 SERVERS=sprint
 
 echo ":::::::::::::::::::: Nightly Upgrade Generic openBIS Servers [`date`] :::::::::::::::::::::"
 
-##################################################
-#
-# Check whether upgrade should be done or not
-#
-LOG_FILE=$SERVERS/openBIS-server/jetty/logs/openbis_log.txt
-CURRENT_VERSION=UNKNOWN
-if [ -f $LOG_FILE ]; then
-    CURRENT_VERSION=`awk '/OPERATION.CISDContextLoaderListener - Version/ {print $1" "$2" "$8" "$9}' $LOG_FILE | tail -1`
-    echo "Current openBIS Application Server: $CURRENT_VERSION"
-    if [ "SNAPSHOT" != "`echo $CURRENT_VERSION|awk '{print $3}'`" ]; then
-        TIME_STAMP="`echo $CURRENT_VERSION|awk '{print $1, $2}'`"
-        WEEK=`date --date="$TIME_STAMP" "+%W"`
-        if [ $? -ne 0 ]; then
-            # Mac OSX has a different flavor of date command
-            WEEK=`date -j -f "%Y-%m-%d %H:%M:%S" "$TIME_STAMP" "+%W"`
-        fi
-        CURRENT_WEEK=`date "+%W"`
-        if [ "$WEEK" = "$CURRENT_WEEK" ]; then
-            echo "Sprint server not replaced until next week."
-            exit
-        fi
-    fi 
-fi
+"$BIN_DIR/check-and-exit-if-new-sprint-server.sh" "$SERVERS"
 
 ##################################################
 #
