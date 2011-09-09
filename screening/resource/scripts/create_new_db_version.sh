4cc4c52fa14f9cc88e53cb31cc8c25d7c8825d11
tpylak <tpylak>
2011-09-09 12:18:35 +0000

    LMS-2502 fix the script which generated new db version
    SVN: 22849

--- screening/resource/scripts/create_new_db_version.sh
+++ screening/resource/scripts/create_new_db_version.sh
@@ -3,6 +3,7 @@
 # Creates new database version (all sql files) which is identical to the previous version.
 # Assumes that it is called from the directory where it is located.
 
+SQL_DIR=../../source/sql/imaging
 source ../../../openbis/resource/scripts/common.sh
 
 copy_db_folder postgresql
