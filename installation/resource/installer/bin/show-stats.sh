d7c19e6253173c57a66c4a45d907e5f30dfc5d06
tpylak <tpylak>
2011-09-13 11:26:30 +0000

    minor: addapt show-stats to the new log format (from previous sprint)
    SVN: 22910

--- installation/resource/installer/bin/show-stats.sh
+++ installation/resource/installer/bin/show-stats.sh
@@ -9,4 +9,4 @@ fi
 # change it if you e.g. do not want to see when admin users access openBIS
 IGNORED_USERS="tpylak|brinn|kohleman|izabelaa|buczekp|felmer|hclaus|cramakri|baucha|ryanj|etlserver"
 
-cat $BASE/../servers/openBIS-server/jetty/logs/*usage_log.txt* | egrep -v "$IGNORED_USERS" | cut -d" " -f1,8,12 | sort | uniq -c
+cat $BASE/../servers/openBIS-server/jetty/logs/*usage_log.txt* | egrep -v "$IGNORED_USERS" | cut -d" " -f1,10,14 | sort | uniq -c
