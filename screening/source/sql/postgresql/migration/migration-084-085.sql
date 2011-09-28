5cab4dbda4e53b803c78ccdf0b48e9b3637cf23f
kaloyane <kaloyane>
2011-09-28 15:17:34 +0000

    minor
    SVN: 23141

--- screening/source/sql/postgresql/migration/migration-084-085.sql
+++ screening/source/sql/postgresql/migration/migration-084-085.sql
@@ -1,4 +1,4 @@
 -- mark screening core plugin as initialized for existing installation
 --
-insert into CORE_PLUGINS (ID, NAME, VERSION) values (1, "screening", 1);
+insert into CORE_PLUGINS (ID, NAME, VERSION) values (1, 'screening', 1);
 
