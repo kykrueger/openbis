7a8c6ffa166e568ea914ac7b9f4d19bd20f5ffdb
felmer <felmer>
2011-10-20 11:08:11 +0000

    LMS-2587 to have at least one test
    SVN: 23363

---
+++ openbis_knime/sourceTest/java/ch/systemsx/cisd/openbis/knime/query/UtilTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2011 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.openbis.knime.query;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import ch.systemsx.cisd.openbis.plugin.query.shared.api.v1.dto.QueryDescription;
+
+
+/**
+ * 
+ *
+ * @author Franz-Josef Elmer
+ */
+public class UtilTest extends AssertJUnit
+{
+
+    @Test
+    public void testSerializationDeserializationOfQueryDescription()
+    {
+        QueryDescription queryDescription = new QueryDescription();
+        queryDescription.setDescription("description");
+        
+        byte[] serializedQueryDescription = Util.serializeQueryDescription(queryDescription);
+        QueryDescription deserializedQueryDescription = Util.deserializeQueryDescription(serializedQueryDescription);
+        
+        assertEquals(queryDescription.getDescription(), deserializedQueryDescription.getDescription());
+    }
+
+}
