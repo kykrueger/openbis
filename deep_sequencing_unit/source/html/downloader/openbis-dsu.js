593f64e0acbdf74a2cb354ae8811470e6e2de05c
cramakri <cramakri>
2011-11-08 15:37:48 +0000

    LMS-2613 Prototype DSU application
    SVN: 23596

---
+++ deep_sequencing_unit/source/html/downloader/openbis-dsu.js
@@ -0,0 +1,162 @@
+/**
+ * openbis-dsu.js
+ * OpenBIS-DSU API
+ *
+ * A DSU-specific API for accessing openBIS. Depends on openbis.js.
+ * @author Chandrasekhar Ramakrishnan
+ */
+
+/**
+ * The openbis_dsu object provides a dsu-specific interface to openbis. 
+ * 
+ * It creates objects for projects, experiments, samples, and datasets. These objects
+ * are designed to be passed on to GUI libraries (like d3). The openbis version of each
+ * object is stored in the bis variable.
+ * @constructor
+ */
+function openbis_dsu(url, dssUrl) {
+	this.server = new openbis(url, dssUrl);
+	// Initial projects
+	var projects = [{code: "No Projects"}];
+}
+
+/**
+ * Request the experiments for the project
+ */
+openbis_dsu.prototype.retrieveExperimentsForProject =  function(project, action)
+{
+	// Initialize the experiments
+	this.server.listExperiments([project.bis], null, action);
+}
+
+/**
+ * Request the sequencing samples for a project
+ */
+openbis_dsu.prototype.retrieveSequencingSamplesForProject = function(project, action)
+{	
+	// To serach for samples by project, we need to go through the experiment
+	var experimentCriteria = 
+	{
+		targetEntityKind : "EXPERIMENT",
+		criteria : { 
+			matchClauses : 
+			[ {"@type":"AttributeMatchClause",
+				"attribute":"PROJECT",
+				"fieldType":"ATTRIBUTE",
+				"desiredValue": project.bis.code
+			} ]
+		}
+	};
+	
+	var sampleCriteria = 
+	{
+		subCriterias : [ experimentCriteria ],
+		matchClauses : 
+			[ {"@type":"AttributeMatchClause",
+				attribute : "TYPE",
+				fieldType : "ATTRIBUTE",
+				desiredValue : "ILLUMINA_SEQUENCING" 
+			} ],
+		operator : "MATCH_ALL_CLAUSES"
+	};
+
+	this.server.searchForSamples(sampleCriteria, action)
+}
+
+/**
+ * Request the sequencing samples for an experiment
+ */
+openbis_dsu.prototype.retrieveSequencingSamplesForExperiment = function(experiment, action)
+{	
+	var experimentCriteria = 
+	{
+		targetEntityKind : "EXPERIMENT",
+		criteria : { 
+			matchClauses : 
+			[ {"@type":"AttributeMatchClause",
+				"attribute":"CODE",
+				"fieldType":"ATTRIBUTE",
+				"desiredValue": experiment.bis.code 
+			},
+			{"@type":"AttributeMatchClause",
+				"attribute":"PROJECT",
+				"fieldType":"ATTRIBUTE",
+				"desiredValue": experiment.project.bis.code
+			} ]
+		}
+	};
+	
+	var sampleCriteria = 
+	{
+		subCriterias : [ experimentCriteria ],
+		matchClauses : 
+			[ {"@type":"AttributeMatchClause",
+				attribute : "TYPE",
+				fieldType : "ATTRIBUTE",
+				desiredValue : "ILLUMINA_SEQUENCING" 
+			} ],
+		operator : "MATCH_ALL_CLAUSES"
+	};
+
+	this.server.searchForSamples(sampleCriteria, action);
+}
+
+/**
+ * Request the replicas for the grid
+ */
+openbis_dsu.prototype.retrieveFlowLanesForSequencingSample = function(sample, action)
+{
+	var parentCriteria = 
+	{
+		targetEntityKind : "SAMPLE_PARENT",
+		criteria : { 
+			matchClauses : 
+			[ {"@type":"AttributeMatchClause",
+				"attribute":"CODE",
+				"fieldType":"ATTRIBUTE",
+				"desiredValue": sample.bis.code 
+			} ]
+		}
+	};
+	
+	var sampleCriteria = 
+	{
+		subCriterias : [ parentCriteria ],
+		matchClauses : 
+			[ {"@type":"AttributeMatchClause",
+				attribute : "TYPE",
+				fieldType : "ATTRIBUTE",
+				desiredValue : "ILLUMINA_FLOW_LANE" 
+			} ],
+		operator : "MATCH_ALL_CLAUSES"
+	};
+
+	this.server.searchForSamples(sampleCriteria, action)
+}
+
+/**
+ * Request the replicas for the grid
+ */
+openbis_dsu.prototype.retrieveDataSetsForFlowLane = function(flowlane, action)
+{
+	var flowLaneCriteria = 
+	{
+		targetEntityKind : "SAMPLE",
+		criteria : { 
+			matchClauses : 
+			[ {"@type":"AttributeMatchClause",
+				"attribute":"CODE",
+				"fieldType":"ATTRIBUTE",
+				"desiredValue": flowlane.code 
+			} ]
+		}
+	};
+	
+	var dataSetCriteria = 
+	{
+		subCriterias : [ flowLaneCriteria ],
+		operator : "MATCH_ALL_CLAUSES"
+	};
+
+	this.server.searchForDataSets(dataSetCriteria, action);
+}
---
+++ deep_sequencing_unit/source/html/downloader/openbis.js
@@ -0,0 +1,200 @@
+/*!
+ * OpenBIS API
+ *
+ * An API for accessing openBIS. Depends on jQuery.
+ */
+ 
+var jsonRequestData = function(params) {
+	// KE: generate unique ids ? Hardcoded "id" seems to work too for now
+	params["id"] = "1"
+	params["jsonrpc"] = "2.0"
+	return JSON.stringify(params)
+}
+ 
+var ajaxRequest = function(settings) {
+	settings.type = "POST";
+	settings.processData = false;
+	settings.dataType = "json";
+	settings.crossDomain = true;
+	settings.data = jsonRequestData(settings.data);
+	settings.headers = { "Access-Control-Allow-Origin" : "*" };
+	$.ajax(settings);
+}
+
+/* 
+ * Functions for working with cookies.
+ *
+ * These are from http://www.quirksmode.org/js/cookies.html
+ */
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
+function openbis(url, dssUrl) {
+	this.generalInfoServiceUrl = url + "/rmi-general-information-v1.json"
+	this.queryServiceUrl = url + "/rmi-query-v1.json"
+	this.dssUrl = dssUrl + "/rmi-dss-api-v1.json"
+}
+
+
+openbis.prototype.login = function(username, password, action) {
+	openbisObj = this
+	ajaxRequest({
+		url: this.generalInfoServiceUrl,
+		data: { "method" : "tryToAuthenticateForAllServices",
+				"params" : [ username, password ] 
+			  },
+		success: 
+			function(data) {
+				openbisObj.sessionToken = data.result;
+				openbisObj.rememberSession();
+				action(data)
+			},
+		error: function() {
+		  alert("Login failed")
+		}
+	 });
+}
+
+openbis.prototype.rememberSession = function() {
+	// Store the result in a cookie, so the user doesn't need to log in every time.
+	createCookie('openbis', this.sessionToken, 1);
+}
+
+openbis.prototype.restoreSession = function() {
+	this.sessionToken = readCookie('openbis');
+}
+
+openbis.prototype.isSessionActive = function(action) {	
+	ajaxRequest({
+		url: this.generalInfoServiceUrl,
+		data: { "method" : "isSessionActive",
+				"params" : [ this.sessionToken ] 
+			  },
+		success: action
+	 });
+}
+
+openbis.prototype.ifRestoredSessionActive = function(action) {	
+	this.restoreSession();
+	this.isSessionActive(function(data) { if (data.result) action(data) });
+}
+
+openbis.prototype.logout = function(action) {
+	ajaxRequest({
+		url: this.generalInfoServiceUrl,
+		data: { "method" : "logout",
+				"params" : [ this.sessionToken ] 
+			  },
+		success: action
+	 });
+}
+
+openbis.prototype.listProjects = function(action) {
+	 ajaxRequest({
+		url: this.generalInfoServiceUrl,
+		data: { "method" : "listProjects",
+				"params" : [ this.sessionToken ] 
+			  },
+		success: action
+	 });
+}
+
+openbis.prototype.listExperiments = function(projects, experimentType, action) {
+	 ajaxRequest({
+		url: this.generalInfoServiceUrl,
+		data: { "method" : "listExperiments",
+				"params" : [ this.sessionToken, projects, experimentType ] 
+			  },
+		success: action
+	 });
+}
+ 
+openbis.prototype.searchForSamples = function(searchCriteria, action) {
+	 ajaxRequest({
+		url: this.generalInfoServiceUrl,
+		data: { "method" : "searchForSamples",
+				"params" : [ this.sessionToken,
+							 searchCriteria ] },
+		success: action
+	 });
+}
+
+openbis.prototype.searchForSamples = function(searchCriteria, action) {
+	 ajaxRequest({
+		url: this.generalInfoServiceUrl,
+		data: { "method" : "searchForSamples",
+				"params" : [ this.sessionToken,
+							 searchCriteria ] },
+		success: action
+	 });
+}
+
+openbis.prototype.searchForDataSets = function(searchCriteria, action) {
+	 ajaxRequest({
+		url: this.generalInfoServiceUrl,
+		data: { "method" : "searchForDataSets",
+				"params" : [ this.sessionToken,
+							 searchCriteria ] },
+		success: action
+	 });
+}
+
+ openbis.prototype.listFilesForDataSet = function(dataSetCode, path, recursive, action) {
+     ajaxRequest({
+        url: this.dssUrl,
+        data: { "method" : "listFilesForDataSet",
+                "params" : [ this.sessionToken, dataSetCode, path, recursive ]
+               },
+        success: action
+     });
+}
+
+openbis.prototype.getDownloadUrlForFileForDataSet = function(dataSetCode, filePath, action) {
+    ajaxRequest({
+        url: openbis.dssUrl,
+        data: { "method" : "getDownloadUrlForFileForDataSet",
+                "params" : [ this.sessionToken, dataSetCode, filePath ]
+               },
+        success: action
+    });
+}
+
+openbis.prototype.listQueries = function(action) {
+	 ajaxRequest({
+		url: this.queryServiceUrl,
+		data: { "method" : "listQueries",
+				"params" : [ this.sessionToken ] },
+		success: action
+	 });
+}
+
+openbis.prototype.executeQuery = function(queryId, parameterBindings, action) {
+	 ajaxRequest({
+		url: this.queryServiceUrl,
+		data: { "method" : "executeQuery",
+				"params" : [ this.sessionToken, queryId, parameterBindings ] },
+		success: action
+	 });
+}
