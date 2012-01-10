fd4c1288c0595118f6c03a72a387e1e2cc81398f
felmer <felmer>
2012-01-10 09:15:23 +0000

    LMS-2735 bug in migration script fixed
    SVN: 24134

--- screening/source/sql/imaging/postgresql/migration/migration-020-021.sql
+++ screening/source/sql/imaging/postgresql/migration/migration-020-021.sql
@@ -1,5 +1,6 @@
-alter table image_zoom_levels add column ds text not null;
+alter table image_zoom_levels add column ds text;
 update image_zoom_levels set ds = physical_dataset_perm_id;
+alter table image_zoom_levels alter column ds set not null;
 alter table image_zoom_levels drop column physical_dataset_perm_id;
 alter table image_zoom_levels rename ds to physical_dataset_perm_id;
 create index image_zoom_levels_phys_ds_idx on image_zoom_levels (physical_dataset_perm_id);
