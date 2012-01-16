0bdb9cbc9a11cffe2299a7fd4839220efeb1798f
felmer <felmer>
2012-01-16 13:07:51 +0000

    LMS-2737 database extend from 094 to 095: column storage_confirmation added.
    SVN: 24202

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
@@ -24,7 +24,7 @@ package ch.systemsx.cisd.openbis.generic.server.dataaccess.db;
 public final class DatabaseVersionHolder
 {
     /** Current version of the database. */
-    private static final String DATABASE_VERSION = "094"; // S123
+    private static final String DATABASE_VERSION = "095"; // S123
 
     private DatabaseVersionHolder()
     {
