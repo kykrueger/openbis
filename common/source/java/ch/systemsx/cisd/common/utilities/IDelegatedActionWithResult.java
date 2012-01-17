fa788173407c2fa1a8f7e2534c51f6f998abaa7c
cramakri <cramakri>
2012-01-17 21:41:01 +0000

    LMS-2739 Use a pre-stage folder with hardlink copies for processing files.
    SVN: 24216

--- common/source/java/ch/systemsx/cisd/common/utilities/IDelegatedActionWithResult.java
+++ common/source/java/ch/systemsx/cisd/common/utilities/IDelegatedActionWithResult.java
@@ -24,9 +24,6 @@ package ch.systemsx.cisd.common.utilities;
  */
 public interface IDelegatedActionWithResult<T>
 {
-    // Execute the action regardless of whether or not the action succeeded
-    T execute();
-
     // Execute the action, allowing the implementation to branch on whether or not the action
     // succeeded
     T execute(boolean didOperationSucceed);
