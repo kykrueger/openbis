98148d2ed98d2aac80954a968aaaba70b5dbb540
felmer <felmer>
2012-01-18 09:05:35 +0000

    LMS-2737 getDssInternalTempDir() and getDssRegistrationLogDir() refactored. Tests written. Check added that these directories are on local file system.
    SVN: 24217

--- datastore_server/source/java/ch/systemsx/cisd/openbis/dss/generic/shared/utils/DssPropertyParametersUtil.java
+++ datastore_server/source/java/ch/systemsx/cisd/openbis/dss/generic/shared/utils/DssPropertyParametersUtil.java
@@ -21,10 +21,14 @@ import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.Set;
 
+import ch.rinn.restrictions.Private;
 import ch.systemsx.cisd.common.exceptions.ConfigurationFailureException;
+import ch.systemsx.cisd.common.filesystem.FileOperations;
 import ch.systemsx.cisd.common.filesystem.FileUtilities;
+import ch.systemsx.cisd.common.filesystem.IFileOperations;
 import ch.systemsx.cisd.common.utilities.ExtendedProperties;
 import ch.systemsx.cisd.common.utilities.PropertyUtils;
+import ch.systemsx.cisd.common.utilities.Template;
 
 /**
  * Utility class to load properties.
@@ -33,6 +37,7 @@ import ch.systemsx.cisd.common.utilities.PropertyUtils;
  */
 public class DssPropertyParametersUtil
 {
+
     /** Prefix of system properties which may override service.properties. */
     public static final String OPENBIS_DSS_SYSTEM_PROPERTIES_PREFIX = "openbis.dss.";
 
@@ -49,6 +54,8 @@ public class DssPropertyParametersUtil
      */
     static final String DSS_TEMP_DIR_PATH = "dss-temp-dir";
 
+    @Private static final File EMPTY_TEST_FILE = new File("an-empty-test-file");
+    
     /**
      * Directory for registration log files.
      */
@@ -56,6 +63,17 @@ public class DssPropertyParametersUtil
 
     /** Location of service properties file. */
     public static final String SERVICE_PROPERTIES_FILE = "etc/service.properties";
+
+    private static final String EXPLANATION =
+            "Please make sure this directory exists on the local file system and is writable "
+                    + "by the data store server or provide such a directory "
+                    + "by the configuration parameter '${path-key}'.";
+
+    private static final Template NON_EXISTING_DIR_TEMPLATE = new Template(
+            "Could not create ${dir-description} at path: ${path}. " + EXPLANATION);
+
+    private static final Template NON_LOCAL_DIR_TEMPLATE = new Template(
+            "Directory at path '${path}' is not on the local file system. " + EXPLANATION);
 
     /** loads server configuration */
     public static ExtendedProperties loadServiceProperties()
@@ -98,43 +116,83 @@ public class DssPropertyParametersUtil
     {
         return PropertyUtils.getMandatoryProperty(serviceProperties, DOWNLOAD_URL_KEY);
     }
-
+    
     public static File getDssInternalTempDir(final Properties properties)
     {
-        String defaultTempDirPath =
-                new File(System.getProperty("user.dir"), "dss-tmp").getAbsolutePath();
-        String tempDirPath =
-                PropertyUtils.getProperty(properties, DSS_TEMP_DIR_PATH, defaultTempDirPath);
-        File tempDir = new File(tempDirPath);
-        tempDir.mkdirs();
-        if (false == tempDir.exists())
-        {
-            throw new ConfigurationFailureException(
-                    "Could not create an internal temp directory for the data store server at path: "
-                            + tempDir.getPath()
-                            + ". Please make sure this directory exists and is writable by the data store server or provide an alternate using directory for the "
-                            + DSS_TEMP_DIR_PATH + " configuration setting");
-        }
-        return tempDir;
+        return getDssInternalTempDir(FileOperations.getInstance(), properties);
+    }
+
+    @Private
+    static File getDssInternalTempDir(IFileOperations fileOperations, final Properties properties)
+    {
+        return getDir(fileOperations, properties, "dss-tmp",
+                "an internal temp directory for the data store server", DSS_TEMP_DIR_PATH);
     }
 
     public static File getDssRegistrationLogDir(final Properties properties)
     {
+        return getDssRegistrationLogDir(FileOperations.getInstance(), properties);
+    }
+
+    @Private
+    static File getDssRegistrationLogDir(IFileOperations fileOperations, final Properties properties)
+    {
+        return getDir(fileOperations, properties, "log-registrations",
+                "a directory for storing registration logs", DSS_REGISTRATION_LOG_DIR_PATH);
+    }
+
+    private static File getDir(IFileOperations fileOperations, final Properties properties,
+            String defaultDirName, String dirDescription, String pathKey)
+    {
         String defaultRegistrationLogDirPath =
-                new File(System.getProperty("user.dir"), "log-registrations").getAbsolutePath();
+                new File(System.getProperty("user.dir"), defaultDirName).getAbsolutePath();
         String registrationLogDirPath =
-                PropertyUtils.getProperty(properties, DSS_REGISTRATION_LOG_DIR_PATH,
+                PropertyUtils.getProperty(properties, pathKey,
                         defaultRegistrationLogDirPath);
         File registrationLogDir = new File(registrationLogDirPath);
-        registrationLogDir.mkdirs();
-        if (false == registrationLogDir.exists())
-        {
-            throw new ConfigurationFailureException(
-                    "Could not create a directory for storing for registration logs at path: "
-                            + registrationLogDir.getPath()
-                            + ". Please make sure this directory exists and is writable by the data store server or provide an alternate using directory for the "
-                            + DSS_REGISTRATION_LOG_DIR_PATH + " configuration setting");
-        }
+        fileOperations.mkdirs(registrationLogDir);
+        assertDirExistsAndIsLocal(fileOperations, registrationLogDir,
+                dirDescription, pathKey);
         return registrationLogDir;
+    }
+
+    private static void assertDirExistsAndIsLocal(IFileOperations fileOperations, File dir,
+            String dirDescription, String pathKey)
+    {
+        assertDirExists(fileOperations, dir, dirDescription, pathKey);
+        File emptyTestFileInDir = new File(dir, EMPTY_TEST_FILE.getName());
+        try
+        {
+            fileOperations.createNewFile(EMPTY_TEST_FILE);
+            if (fileOperations.rename(EMPTY_TEST_FILE, emptyTestFileInDir) == false)
+            {
+                throw createException(NON_LOCAL_DIR_TEMPLATE.createFreshCopy(), dir, dirDescription,
+                        pathKey);
+            }
+        } finally
+        {
+            fileOperations.delete(EMPTY_TEST_FILE);
+            fileOperations.delete(emptyTestFileInDir);
+        }
+    }
+
+    private static void assertDirExists(IFileOperations fileOperations, File dir, String dirDescription,
+            String pathKey)
+    {
+        if (fileOperations.exists(dir) == false)
+        {
+            throw createException(NON_EXISTING_DIR_TEMPLATE.createFreshCopy(), dir, dirDescription,
+                    pathKey);
+        }
+    }
+
+    private static ConfigurationFailureException createException(Template template, File dir,
+            String dirName, String pathKey)
+    {
+        template.attemptToBind("dir-description", dirName);
+        template.bind("path", dir.getPath());
+        template.bind("path-key", pathKey);
+        ConfigurationFailureException e = new ConfigurationFailureException(template.createText());
+        return e;
     }
 }
