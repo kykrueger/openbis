de6c7eae0faa4cc366a77e99ea11794c95d7284c
jakubs <jakubs>
2012-02-21 10:08:33 +0000

    LMS-2806: add the test to dssComponentTest
    SVN: 24514

--- datastore_server/sourceTest/java/ch/systemsx/cisd/openbis/datastoreserver/systemtests/data-set-handler.py
+++ datastore_server/sourceTest/java/ch/systemsx/cisd/openbis/datastoreserver/systemtests/data-set-handler.py
@@ -5,6 +5,6 @@ transaction = service.transaction(incoming)
 dataSet = transaction.createNewDataSet()
 exp = dataSet.getExperiment()
 samp = dataSet.getSample()
-if exp is None and samp is None:
-  raise IllegalArgumentException("No Experiment or Sample specified")
+if exp is None or samp is None:
+  raise IllegalArgumentException("data-set-handler.py: No Experiment or Sample specified")
 transaction.moveFile(incoming.getAbsolutePath(), dataSet)
