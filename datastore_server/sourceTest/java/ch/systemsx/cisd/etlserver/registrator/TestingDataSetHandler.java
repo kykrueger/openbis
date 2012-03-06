a6f0ec96e8271aa323f1e11e58d2019cb41a90d6
jakubs <jakubs>
2012-03-06 14:08:18 +0000

    LMS-2820: refactor a class
    SVN: 24631

--- datastore_server/sourceTest/java/ch/systemsx/cisd/etlserver/registrator/TestingDataSetHandler.java
+++ datastore_server/sourceTest/java/ch/systemsx/cisd/etlserver/registrator/TestingDataSetHandler.java
@@ -1,108 +1,16 @@
 package ch.systemsx.cisd.etlserver.registrator;
 
-import java.util.List;
-
 import org.python.core.PyFunction;
-import org.python.util.PythonInterpreter;
 
-import ch.systemsx.cisd.base.exceptions.CheckedExceptionTunnel;
 import ch.systemsx.cisd.common.exceptions.UserFailureException;
 import ch.systemsx.cisd.etlserver.TopLevelDataSetRegistratorGlobalState;
-import ch.systemsx.cisd.etlserver.registrator.JythonTopLevelDataSetHandler.JythonDataSetRegistrationService;
-import ch.systemsx.cisd.etlserver.registrator.api.v1.SecondaryTransactionFailure;
 import ch.systemsx.cisd.etlserver.registrator.api.v1.impl.DataSetRegistrationTransaction;
 import ch.systemsx.cisd.openbis.dss.generic.shared.dto.DataSetInformation;
 import ch.systemsx.cisd.openbis.generic.shared.dto.NewExternalData;
 
-class TestingDataSetHandlerExpectationsAndRealizations
-{
-    /*
-     * shouldas
-     */
-    protected final boolean shouldRegistrationFail;
-
-    protected final boolean shouldReThrowRollbackException;
-
-    /*
-     * happends
-     */
-    protected boolean didRollbackServiceFunctionRun = false;
-
-    protected boolean didTransactionRollbackHappen = false;
-
-    protected boolean didRollbackTransactionFunctionRunHappen = false;
-
-    protected boolean didCommitTransactionFunctionRunHappen = false;
-
-    protected boolean didSecondaryTransactionErrorNotificationHappen = false;
-
-    protected boolean didPostRegistrationFunctionRunHappen = false;
-
-    protected boolean didPreRegistrationFunctionRunHappen = false;
-
-    protected boolean didPostStorageFunctionRunHappen = false;
-
-    protected boolean didServiceRollbackHappen = false;
-
-    protected String registrationContextError;
-
-    public TestingDataSetHandlerExpectationsAndRealizations(boolean shouldRegistrationFail,
-            boolean shouldReThrowRollbackException)
-    {
-        super();
-        this.shouldRegistrationFail = shouldRegistrationFail;
-        this.shouldReThrowRollbackException = shouldReThrowRollbackException;
-    }
-
-    /**
-     * reads boolean or false if null from interpreter
-     */
-    private static boolean readBoolean(PythonInterpreter interpreter, String variable)
-    {
-        Boolean retVal = interpreter.get(variable, Boolean.class);
-        if (retVal == null)
-            return false;
-        return retVal;
-    }
-
-    public void checkPythonInterpreterVariables(
-            DataSetRegistrationService<DataSetInformation> service)
-    {
-        PythonInterpreter interpreter =
-                ((JythonDataSetRegistrationService<DataSetInformation>) service).getInterpreter();
-
-        didRollbackServiceFunctionRun = readBoolean(interpreter, "didRollbackServiceFunctionRun");
-        didRollbackTransactionFunctionRunHappen =
-                readBoolean(interpreter, "didTransactionRollbackHappen");
-        didCommitTransactionFunctionRunHappen =
-                readBoolean(interpreter, "didTransactionCommitHappen");
-        didPreRegistrationFunctionRunHappen =
-                readBoolean(interpreter, "didPreRegistrationFunctionRunHappen");
-        didPostRegistrationFunctionRunHappen =
-                readBoolean(interpreter, "didPostRegistrationFunctionRunHappen");
-        didPostStorageFunctionRunHappen =
-                readBoolean(interpreter, "didPostStorageFunctionRunHappen");
-        didSecondaryTransactionErrorNotificationHappen =
-                readBoolean(interpreter, "didSecondaryTransactionErrorNotificationHappen");
-
-        registrationContextError = interpreter.get("contextTestFailed", String.class);
-    }
-
-    public void handleRollbackException(Throwable throwable)
-    {
-        if (shouldReThrowRollbackException)
-        {
-            throw CheckedExceptionTunnel.wrapIfNecessary(throwable);
-        } else
-        {
-            throwable.printStackTrace();
-        }
-    }
-}
-
 public class TestingDataSetHandler extends JythonTopLevelDataSetHandler<DataSetInformation>
 {
-    protected final TestingDataSetHandlerExpectationsAndRealizations expectations;
+    protected final TestingDataSetHandlerExpectations expectations;
 
     public TestingDataSetHandler(TopLevelDataSetRegistratorGlobalState globalState,
             boolean shouldRegistrationFail, boolean shouldReThrowRollbackException)
@@ -110,7 +18,7 @@ public class TestingDataSetHandler extends JythonTopLevelDataSetHandler<DataSetI
         super(globalState);
 
         this.expectations =
-                new TestingDataSetHandlerExpectationsAndRealizations(shouldRegistrationFail,
+                new TestingDataSetHandlerExpectations(shouldRegistrationFail,
                         shouldReThrowRollbackException);
     }
 
---
+++ datastore_server/sourceTest/java/ch/systemsx/cisd/etlserver/registrator/TestingDataSetHandlerExpectations.java
@@ -0,0 +1,93 @@
+package ch.systemsx.cisd.etlserver.registrator;
+
+import org.python.util.PythonInterpreter;
+
+import ch.systemsx.cisd.base.exceptions.CheckedExceptionTunnel;
+import ch.systemsx.cisd.etlserver.registrator.JythonTopLevelDataSetHandler.JythonDataSetRegistrationService;
+import ch.systemsx.cisd.openbis.dss.generic.shared.dto.DataSetInformation;
+
+public class TestingDataSetHandlerExpectations
+{
+    /*
+     * shouldas
+     */
+    protected final boolean shouldRegistrationFail;
+
+    protected final boolean shouldReThrowRollbackException;
+
+    /*
+     * happends
+     */
+    protected boolean didRollbackServiceFunctionRun = false;
+
+    protected boolean didTransactionRollbackHappen = false;
+
+    protected boolean didRollbackTransactionFunctionRunHappen = false;
+
+    protected boolean didCommitTransactionFunctionRunHappen = false;
+
+    protected boolean didSecondaryTransactionErrorNotificationHappen = false;
+
+    protected boolean didPostRegistrationFunctionRunHappen = false;
+
+    protected boolean didPreRegistrationFunctionRunHappen = false;
+
+    protected boolean didPostStorageFunctionRunHappen = false;
+
+    protected boolean didServiceRollbackHappen = false;
+
+    protected String registrationContextError;
+
+    public TestingDataSetHandlerExpectations(boolean shouldRegistrationFail,
+            boolean shouldReThrowRollbackException)
+    {
+        super();
+        this.shouldRegistrationFail = shouldRegistrationFail;
+        this.shouldReThrowRollbackException = shouldReThrowRollbackException;
+    }
+
+    /**
+     * reads boolean or false if null from interpreter
+     */
+    private static boolean readBoolean(PythonInterpreter interpreter, String variable)
+    {
+        Boolean retVal = interpreter.get(variable, Boolean.class);
+        if (retVal == null)
+            return false;
+        return retVal;
+    }
+
+    public void checkPythonInterpreterVariables(
+            DataSetRegistrationService<DataSetInformation> service)
+    {
+        PythonInterpreter interpreter =
+                ((JythonDataSetRegistrationService<DataSetInformation>) service).getInterpreter();
+
+        didRollbackServiceFunctionRun = readBoolean(interpreter, "didRollbackServiceFunctionRun");
+        didRollbackTransactionFunctionRunHappen =
+                readBoolean(interpreter, "didTransactionRollbackHappen");
+        didCommitTransactionFunctionRunHappen =
+                readBoolean(interpreter, "didTransactionCommitHappen");
+        didPreRegistrationFunctionRunHappen =
+                readBoolean(interpreter, "didPreRegistrationFunctionRunHappen");
+        didPostRegistrationFunctionRunHappen =
+                readBoolean(interpreter, "didPostRegistrationFunctionRunHappen");
+        didPostStorageFunctionRunHappen =
+                readBoolean(interpreter, "didPostStorageFunctionRunHappen");
+        didSecondaryTransactionErrorNotificationHappen =
+                readBoolean(interpreter, "didSecondaryTransactionErrorNotificationHappen");
+
+        registrationContextError = interpreter.get("contextTestFailed", String.class);
+    }
+
+    public void handleRollbackException(Throwable throwable)
+    {
+        if (shouldReThrowRollbackException)
+        {
+            throw CheckedExceptionTunnel.wrapIfNecessary(throwable);
+        } else
+        {
+            throwable.printStackTrace();
+        }
+    }
+}
