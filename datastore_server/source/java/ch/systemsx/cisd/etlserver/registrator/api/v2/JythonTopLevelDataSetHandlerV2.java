d633e820738c74dea4cae7a5fefceb0d1b89ca99
jakubs <jakubs>
2012-03-06 14:08:19 +0000

    LMS-2820: add v2 of jython top level data set handler
    SVN: 24632

---
+++ datastore_server/source/java/ch/systemsx/cisd/etlserver/registrator/api/v2/JythonTopLevelDataSetHandlerV2.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2011 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.etlserver.registrator.api.v2;
+
+import java.io.File;
+import java.util.List;
+
+import org.python.core.Py;
+import org.python.core.PyBaseCode;
+import org.python.core.PyException;
+import org.python.core.PyFunction;
+import org.python.util.PythonInterpreter;
+
+import ch.systemsx.cisd.common.exceptions.ConfigurationFailureException;
+import ch.systemsx.cisd.common.filesystem.FileUtilities;
+import ch.systemsx.cisd.common.utilities.IDelegatedActionWithResult;
+import ch.systemsx.cisd.common.utilities.PropertyUtils;
+import ch.systemsx.cisd.etlserver.ITopLevelDataSetRegistratorDelegate;
+import ch.systemsx.cisd.etlserver.TopLevelDataSetRegistratorGlobalState;
+import ch.systemsx.cisd.etlserver.registrator.AbstractDataSetRegistrationDetailsFactory;
+import ch.systemsx.cisd.etlserver.registrator.AbstractOmniscientTopLevelDataSetRegistrator;
+import ch.systemsx.cisd.etlserver.registrator.DataSetFile;
+import ch.systemsx.cisd.etlserver.registrator.DataSetRegistrationDetails;
+import ch.systemsx.cisd.etlserver.registrator.DataSetRegistrationService;
+import ch.systemsx.cisd.etlserver.registrator.DataSetStorageAlgorithmRunner;
+import ch.systemsx.cisd.etlserver.registrator.IDataSetRegistrationDetailsFactory;
+import ch.systemsx.cisd.etlserver.registrator.api.v1.IDataSetRegistrationTransaction;
+import ch.systemsx.cisd.etlserver.registrator.api.v1.SecondaryTransactionFailure;
+import ch.systemsx.cisd.etlserver.registrator.api.v1.impl.DataSetRegistrationTransaction;
+import ch.systemsx.cisd.openbis.dss.generic.shared.dto.DataSetInformation;
+
+/**
+ * A top-level data set handler that runs a python (jython) script to register data sets.
+ * 
+ * @author Chandrasekhar Ramakrishnan
+ */
+public class JythonTopLevelDataSetHandlerV2<T extends DataSetInformation> extends
+        ch.systemsx.cisd.etlserver.registrator.JythonTopLevelDataSetHandler<T>
+{
+
+    /**
+     * Constructor.
+     * 
+     * @param globalState
+     */
+    public JythonTopLevelDataSetHandlerV2(TopLevelDataSetRegistratorGlobalState globalState)
+    {
+        super(globalState);
+    }
+
+    /**
+     * Create a registration service that includes a python interpreter (we need the interpreter in
+     * the service so we can use it in error handling).
+     */
+    @Override
+    protected DataSetRegistrationService<T> createDataSetRegistrationService(
+            DataSetFile incomingDataSetFile, DataSetInformation callerDataSetInformationOrNull,
+            IDelegatedActionWithResult<Boolean> cleanAfterwardsAction,
+            ITopLevelDataSetRegistratorDelegate delegate)
+    {
+        return createJythonDataSetRegistrationServiceV2(incomingDataSetFile,
+                callerDataSetInformationOrNull, cleanAfterwardsAction, delegate,
+                new PythonInterpreter(), getGlobalState());
+    }
+
+    /**
+     * Create a Jython registration service that includes access to the interpreter.
+     * 
+     * @param pythonInterpreter
+     */
+    protected DataSetRegistrationService<T> createJythonDataSetRegistrationServiceV2(
+            DataSetFile incomingDataSetFile,
+            DataSetInformation userProvidedDataSetInformationOrNull,
+            IDelegatedActionWithResult<Boolean> cleanAfterwardsAction,
+            ITopLevelDataSetRegistratorDelegate delegate, PythonInterpreter pythonInterpreter,
+            TopLevelDataSetRegistratorGlobalState globalState)
+    {
+        return new JythonDataSetRegistrationService<T>(this, incomingDataSetFile,
+                userProvidedDataSetInformationOrNull, cleanAfterwardsAction, delegate,
+                pythonInterpreter, globalState);
+    }
+
+    public static class JythonDataSetRegistrationServiceV2<T extends DataSetInformation>
+            extends
+            ch.systemsx.cisd.etlserver.registrator.JythonTopLevelDataSetHandler.JythonDataSetRegistrationService<T>
+    {
+
+        public JythonDataSetRegistrationServiceV2(JythonTopLevelDataSetHandlerV2<T> registrator,
+                DataSetFile incomingDataSetFile,
+                DataSetInformation userProvidedDataSetInformationOrNull,
+                IDelegatedActionWithResult<Boolean> globalCleanAfterwardsAction,
+                ITopLevelDataSetRegistratorDelegate delegate, PythonInterpreter interpreter,
+                TopLevelDataSetRegistratorGlobalState globalState)
+        {
+            super(registrator, incomingDataSetFile, userProvidedDataSetInformationOrNull,
+                    globalCleanAfterwardsAction, delegate, interpreter, globalState);
+        }
+
+        /** Creates the transaction object. Can be overriden in subclasses. */
+        protected DataSetRegistrationTransaction<T> createTransaction(
+                File rollBackStackParentFolder, File workingDir, File stagingDir,
+                IDataSetRegistrationDetailsFactory<T> registrationDetailsFactory)
+        {
+            if (transactions.isEmpty())
+            {
+                return new DataSetRegistrationTransaction<T>(rollBackStackParentFolder, workingDir,
+                        stagingDir, this, registrationDetailsFactory);
+            } else
+            {
+                throw new IllegalStateException(
+                        "Failed to create transaction. Transaction has already been created before.");
+            }
+        }
+
+        private DataSetRegistrationTransaction<T> getTransaction()
+        {
+            if (transactions.isEmpty())
+            {
+                return null;
+            } else if (transactions.size() > 1)
+            {
+                throw new IllegalStateException(
+                        "This version of DataSetRegistator doesn't allow multiple transactions, but there are some.");
+            } else
+            {
+                return transactions.get(0);
+            }
+        }
+
+        /**
+         * Commit any scheduled changes.
+         */
+        public void commit()
+        {
+            DataSetRegistrationTransaction<T> transaction = getTransaction();
+
+            transaction.commit();
+
+            logDssRegistrationResult();
+
+            // Execute the clean afterwards action as successful only if no errors occurred and we
+            // registered data sets
+            executeGlobalCleanAfterwardsAction(false == (didErrorsArise() || transaction.isRolledback()));
+        }
+    }
+
+}
---
+++ datastore_server/sourceTest/java/ch/systemsx/cisd/etlserver/registrator/TestingDataSetHandlerV2.java
@@ -0,0 +1,68 @@
+package ch.systemsx.cisd.etlserver.registrator;
+
+import org.python.core.PyFunction;
+
+import ch.systemsx.cisd.common.exceptions.UserFailureException;
+import ch.systemsx.cisd.etlserver.TopLevelDataSetRegistratorGlobalState;
+import ch.systemsx.cisd.etlserver.registrator.TestingDataSetHandlerExpectations;
+import ch.systemsx.cisd.etlserver.registrator.api.v1.impl.DataSetRegistrationTransaction;
+import ch.systemsx.cisd.etlserver.registrator.api.v2.JythonTopLevelDataSetHandlerV2;
+import ch.systemsx.cisd.openbis.dss.generic.shared.dto.DataSetInformation;
+import ch.systemsx.cisd.openbis.generic.shared.dto.NewExternalData;
+
+public class TestingDataSetHandlerV2 extends JythonTopLevelDataSetHandlerV2<DataSetInformation>
+{
+    protected final TestingDataSetHandlerExpectations expectations;
+
+    public TestingDataSetHandlerV2(TopLevelDataSetRegistratorGlobalState globalState,
+            boolean shouldRegistrationFail, boolean shouldReThrowRollbackException)
+    {
+        super(globalState);
+
+        this.expectations =
+                new TestingDataSetHandlerExpectations(shouldRegistrationFail,
+                        shouldReThrowRollbackException);
+    }
+
+    @Override
+    public void registerDataSetInApplicationServer(DataSetInformation dataSetInformation,
+            NewExternalData data) throws Throwable
+    {
+        if (expectations.shouldRegistrationFail)
+        {
+            throw new UserFailureException("Didn't work.");
+        } else
+        {
+            super.registerDataSetInApplicationServer(dataSetInformation, data);
+        }
+    }
+
+    @Override
+    public void rollback(DataSetRegistrationService<DataSetInformation> service, Throwable throwable)
+    {
+        super.rollback(service, throwable);
+        expectations.didServiceRollbackHappen = true;
+        expectations.handleRollbackException(throwable);
+    }
+
+    @Override
+    public void didRollbackTransaction(DataSetRegistrationService<DataSetInformation> service,
+            DataSetRegistrationTransaction<DataSetInformation> transaction,
+            DataSetStorageAlgorithmRunner<DataSetInformation> algorithmRunner, Throwable throwable)
+    {
+        super.didRollbackTransaction(service, transaction, algorithmRunner, throwable);
+
+        expectations.didTransactionRollbackHappen = true;
+        expectations.handleRollbackException(throwable);
+    }
+
+    @Override
+    protected void invokeFuncion(
+            ch.systemsx.cisd.etlserver.registrator.DataSetRegistrationService<DataSetInformation> service,
+            PyFunction function, Object... args)
+    {
+        super.invokeFuncion(service, function, args);
+        expectations.checkPythonInterpreterVariables(service);
+    }
+
+}
