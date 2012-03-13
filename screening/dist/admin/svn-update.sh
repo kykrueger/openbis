f77a1f34bf1ae586598c3a7a763042a3a6c74bce
felmer <felmer>
2012-03-13 14:45:28 +0000

    also add backup-database.sh from installtion project
    SVN: 24694

--- screening/dist/admin/svn-update.sh
+++ screening/dist/admin/svn-update.sh
@@ -20,7 +20,7 @@ WGET_HOME=`which wget`
 
 # download the scripts
 $WGET_HOME -nH --cut-dirs=6 --directory-prefix $OPENBIS_HOME --no-verbose -r -l2 -A.{sh,sql} http://$SVN/
-$WGET_HOME -nH --cut-dirs=6 --directory-prefix $OPENBIS_HOME --no-verbose -r -l2 -A.{sh,sql} http://$SVN2/
+$WGET_HOME -nH  --directory-prefix $OPENBIS_HOME --no-verbose http://$SVN2
 
 # set permissions
 find $OPENBIS_HOME -type f -name "*.sh" -exec chmod 700 {} \;
