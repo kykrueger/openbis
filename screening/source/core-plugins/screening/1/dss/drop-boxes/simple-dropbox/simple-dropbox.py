ceed364a2ee4102f122c5cb3db80fd7d24dba4cf
kohleman <kohleman>
2012-05-02 08:00:14 +0000

    - forgotten by eclipse for Illumina NGS
    SVN: 25128

--- screening/source/core-plugins/screening/1/dss/drop-boxes/simple-dropbox/simple-dropbox.py
+++ screening/source/core-plugins/screening/1/dss/drop-boxes/simple-dropbox/simple-dropbox.py
@@ -5,7 +5,7 @@ expid = "/TEST/TEST-PROJECT/DEMO-EXP-SIMPLE"
 exp = transaction.getExperiment(expid)
  
 if None == exp:
-    exp = transaction.createNewExperiment(expid, "SIRNA_HCS")
+    exp = transaction.createNewExperiment(expid, "NGS-Experiment")
     exp.setPropertyValue("DESCRIPTION", "Description of the demo experiment")
  
 dataSet = transaction.createNewDataSet()
