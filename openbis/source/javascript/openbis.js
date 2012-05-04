897bf78e7ee5149cfc7e1bc81ca440a13870bc84
cramakri <cramakri>
2012-05-04 08:24:04 +0000

    MINOR Moved actionDeferred to the openbis.js file for compatability with existing apps
    SVN: 25165

--- openbis/source/javascript/openbis.js
+++ openbis/source/javascript/openbis.js
@@ -309,3 +309,35 @@ openbis.prototype.executeQuery = function(queryId, parameterBindings, action) {
 		success: action
 	});
 }
+
+/**
+ * A utility class for deferring an action until all of some kind of action has completed
+ *
+ * @argument dependencies An array of the keys for the dependencies.
+ */
+function actionDeferrer(pendingAction, dependencies) {
+	this.pendingAction = pendingAction;
+	this.dependencies = {};
+	var newme = this;
+	dependencies.forEach(function(key) {
+		newme.dependencies[key] = false;
+	});
+}
+
+/**
+ * Note that a dependency completed. Execute the pending action if appropriate.
+ */
+actionDeferrer.prototype.dependencyCompleted = function(key) {
+	this.dependencies[key] = true;
+	var shouldExecute = true;
+	for (prop in this.dependencies) {
+		if (false == this.dependencies[prop]) {
+			shouldExecute = false;
+			break;
+		}
+	}
+	if (shouldExecute) {
+		this.pendingAction();
+	}
+}
+
