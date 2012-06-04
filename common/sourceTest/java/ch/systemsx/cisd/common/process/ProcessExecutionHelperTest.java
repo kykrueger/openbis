a570860304537e8284099fa3f852f484997e2361
cramakri <cramakri>
2012-06-04 11:26:07 +0000

    MINOR Fixing up warnings
    SVN: 25529

--- common/sourceTest/java/ch/systemsx/cisd/common/process/ProcessExecutionHelperTest.java
+++ common/sourceTest/java/ch/systemsx/cisd/common/process/ProcessExecutionHelperTest.java
@@ -502,7 +502,6 @@ public class ProcessExecutionHelperTest
                 result.getStartupFailureMessage().indexOf("some_non_existent_executable") >= 0);
     }
 
-    @SuppressWarnings("null")
     @Test(groups =
         { "requires_unix", "slow", "flaky" })
     public void testSleepyExecutionWithTermination() throws Exception
