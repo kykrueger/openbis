5226c051331156b71242ac3edc1706295faf3606
gpawel <gpawel>
2012-06-06 13:12:58 +0000

    fixing warnings
    SVN: 25593

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/client/web/client/application/ui/project/ProjectListDeletionConfirmationDialog.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/client/web/client/application/ui/project/ProjectListDeletionConfirmationDialog.java
@@ -33,6 +33,7 @@ public final class ProjectListDeletionConfirmationDialog extends
         AbstractDataListPermanentDeletionConfirmationDialog<Project>
 {
 
+    @SuppressWarnings("hiding")
     private final IViewContext<ICommonClientServiceAsync> viewContext;
 
     public ProjectListDeletionConfirmationDialog(
