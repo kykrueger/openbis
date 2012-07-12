f21392be37d9b77bd3be9289ce07c219d321085e
cramakri <cramakri>
2012-07-12 14:44:23 +0000

    MINOR : Fixing warnings
    SVN: 26096

--- common/sourceTest/java/ch/systemsx/cisd/common/utilities/AnnotationUtilsTest.java
+++ common/sourceTest/java/ch/systemsx/cisd/common/utilities/AnnotationUtilsTest.java
@@ -128,6 +128,7 @@ public final class AnnotationUtilsTest
         @SuppressWarnings("unused")
         protected Object b;
 
+        @SuppressWarnings("unused")
         @BeanProperty
         protected void setA(final Object a)
         {
