962626e1e73c021b9075c7ebb4a795de7c23ea45
cramakri <cramakri>
2012-07-18 04:54:06 +0000

    BIS-62 SP-92 : Added ScreeningJythonBasedDbModifyingAggregationServiceReportingPlugin.
    SVN: 26137

---
+++ screening/source/java/ch/systemsx/cisd/openbis/dss/screening/server/plugins/jython/ScreeningJythonBasedDbModifyingAggregationServiceReportingPlugin.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2012 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.openbis.dss.screening.server.plugins.jython;
+
+import java.io.File;
+import java.util.Properties;
+
+import ch.systemsx.cisd.openbis.dss.generic.server.plugins.jython.IPluginScriptRunnerFactory;
+import ch.systemsx.cisd.openbis.dss.generic.server.plugins.jython.JythonBasedDbModifyingAggregationServiceReportingPlugin;
+
+/**
+ * A version of the {@link JythonBasedDbModifyingAggregationServiceReportingPlugin} with extra
+ * support for screening.
+ * 
+ * @author Chandrasekhar Ramakrishnan
+ */
+public class ScreeningJythonBasedDbModifyingAggregationServiceReportingPlugin extends
+        JythonBasedDbModifyingAggregationServiceReportingPlugin
+{
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Public constructor.
+     */
+    public ScreeningJythonBasedDbModifyingAggregationServiceReportingPlugin(Properties properties,
+            File storeRoot)
+    {
+        this(properties, storeRoot, new ScreeningPluginScriptRunnerFactory(
+                getScriptPathProperty(properties)));
+    }
+
+    /**
+     * Constructor used in tests.
+     */
+    protected ScreeningJythonBasedDbModifyingAggregationServiceReportingPlugin(
+            Properties properties, File storeRoot, IPluginScriptRunnerFactory scriptRunnerFactory)
+    {
+        super(properties, storeRoot, scriptRunnerFactory);
+    }
+}
