653a5afd1996b0271b24f34e34e360fcd00d55fa
cramakri <cramakri>
2012-07-25 11:34:17 +0000

    BIS-131 SP-214 : Use the screening transaction in the screening db-modifying aggregation service
    SVN: 26203

--- screening/source/java/ch/systemsx/cisd/openbis/dss/screening/server/plugins/jython/ScreeningJythonBasedDbModifyingAggregationServiceReportingPlugin.java
+++ screening/source/java/ch/systemsx/cisd/openbis/dss/screening/server/plugins/jython/ScreeningJythonBasedDbModifyingAggregationServiceReportingPlugin.java
@@ -19,8 +19,19 @@ package ch.systemsx.cisd.openbis.dss.screening.server.plugins.jython;
 import java.io.File;
 import java.util.Properties;
 
+import ch.systemsx.cisd.common.utilities.IDelegatedActionWithResult;
+import ch.systemsx.cisd.etlserver.registrator.AbstractOmniscientTopLevelDataSetRegistrator.NoOpDelegate;
+import ch.systemsx.cisd.etlserver.registrator.api.v1.impl.DataSetRegistrationTransaction;
+import ch.systemsx.cisd.etlserver.registrator.recovery.AutoRecoverySettings;
+import ch.systemsx.cisd.etlserver.registrator.DataSetFile;
+import ch.systemsx.cisd.etlserver.registrator.DataSetRegistrationService;
+import ch.systemsx.cisd.etlserver.registrator.IDataSetRegistrationDetailsFactory;
+import ch.systemsx.cisd.openbis.dss.etl.jython.ImagingDataSetRegistrationTransaction;
+import ch.systemsx.cisd.openbis.dss.etl.jython.JythonPlateDataSetHandlerUtils;
+import ch.systemsx.cisd.openbis.dss.etl.jython.JythonPlateDatasetFactory;
 import ch.systemsx.cisd.openbis.dss.generic.server.plugins.jython.IPluginScriptRunnerFactory;
 import ch.systemsx.cisd.openbis.dss.generic.server.plugins.jython.JythonBasedDbModifyingAggregationServiceReportingPlugin;
+import ch.systemsx.cisd.openbis.dss.generic.shared.dto.DataSetInformation;
 
 /**
  * A version of the {@link JythonBasedDbModifyingAggregationServiceReportingPlugin} with extra
@@ -51,5 +62,36 @@ public class ScreeningJythonBasedDbModifyingAggregationServiceReportingPlugin ex
             Properties properties, File storeRoot, IPluginScriptRunnerFactory scriptRunnerFactory)
     {
         super(properties, storeRoot, scriptRunnerFactory);
+    }
+
+    @Override
+    protected DataSetRegistrationService<DataSetInformation> createRegistrationService(
+            DataSetFile incoming, IDelegatedActionWithResult<Boolean> cleanUpAction,
+            NoOpDelegate delegate)
+    {
+        IDataSetRegistrationDetailsFactory<DataSetInformation> registrationDetailsFactory =
+                (IDataSetRegistrationDetailsFactory<DataSetInformation>) new JythonPlateDatasetFactory(
+                        getRegistratorState(), null);
+
+        DataSetRegistrationService<DataSetInformation> service =
+                new DataSetRegistrationService<DataSetInformation>(this, incoming,
+                        registrationDetailsFactory, cleanUpAction, delegate)
+                    {
+                        @SuppressWarnings("unchecked")
+                        @Override
+                        protected DataSetRegistrationTransaction<DataSetInformation> createTransaction(
+                                File rollBackStackParentFolder,
+                                File workingDirectory,
+                                File stagingDirectory,
+                                IDataSetRegistrationDetailsFactory<DataSetInformation> factory)
+                        {
+                            return new ImagingDataSetRegistrationTransaction(
+                                    rollBackStackParentFolder, workingDirectory, stagingDirectory,
+                                    this, factory,
+                                    JythonPlateDataSetHandlerUtils.parseOriginalDir(properties),
+                                    AutoRecoverySettings.DO_NOT_USE_AUTO_RECOVERY);
+                        }
+                    };
+        return service;
     }
 }
