0984719aa24bbd8e6a717c0e8ed78f3055d1a91c
brinn <brinn>
2012-07-29 20:09:21 +0000

    Expose method checkSession() to IServer.
    SVN: 26264

--- openbis_api/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/ServiceVersionHolder.java
+++ openbis_api/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/ServiceVersionHolder.java
@@ -23,5 +23,5 @@ package ch.systemsx.cisd.openbis.generic.shared.basic.dto;
  */
 public final class ServiceVersionHolder
 {
-    public static final int VERSION = 32; // for S119
+    public static final int VERSION = 33; // for S137
 }
