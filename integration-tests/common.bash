ec2691242c15376f4ed71c408afe1e229ab618a8
felmer <felmer>
2012-09-18 12:26:01 +0000

    integration tests: wrong host not lascar but bs-ci01.ethz.ch
    SVN: 26662

--- integration-tests/common.bash
+++ integration-tests/common.bash
@@ -17,7 +17,7 @@ LOCAL_PROJECTS=..
 
 OPENBIS_SERVER=$WORK/openBIS-server
 
-CI_HOST=cisd-lascar.ethz.ch
+CI_HOST=bs-ci01.ethz.ch
 CI_HOME=/localhome/ci
 SSH_CRUISE_CONTROL_NAME=ci@$CI_HOST
 HUDSON_ARTIFACTS=hudson/jobs
