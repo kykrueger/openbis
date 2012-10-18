7e66796cdb3b7e1a7430208840313451fa7b7ecd
anttil <anttil>
2012-10-18 13:16:56 +0000

    BIS-217 / SP-326: Split screening master data script to mandatory and optional parts
    SVN: 27241

--- screening/source/core-plugins/screening/4/as/initialize-master-data.py
+++ screening/source/core-plugins/screening/4/as/initialize-master-data.py
@@ -2,40 +2,6 @@ import ch.systemsx.cisd.openbis.generic.server.jython.api.v1.DataType as DataTyp
 
 tr = service.transaction()
 
-#   
-# File types existing by default
-#
-#file_type_HDF5 = tr.getOrCreateNewFileFormatType('HDF5')
-#file_type_HDF5.setDescription('Hierarchical Data Format File, version 5')
-
-#file_type_PROPRIETARY = tr.getOrCreateNewFileFormatType('PROPRIETARY')
-#file_type_PROPRIETARY.setDescription('Proprietary Format File')
-
-#file_type_SRF = tr.getOrCreateNewFileFormatType('SRF')
-#file_type_SRF.setDescription('Sequence Read Format File')
-
-#file_type_TIFF = tr.getOrCreateNewFileFormatType('TIFF')
-#file_type_TIFF.setDescription('TIFF File')
-
-#file_type_TSV = tr.getOrCreateNewFileFormatType('TSV')
-#file_type_TSV.setDescription('Tab Separated Values File')
-
-#file_type_XML = tr.getOrCreateNewFileFormatType('XML')
-#file_type_XML.setDescription('XML File')
-
-file_type_CSV = tr.getOrCreateNewFileFormatType('CSV')
-file_type_CSV.setDescription('files with values separated by comma or semicolon')
-
-file_type_JPG = tr.getOrCreateNewFileFormatType('JPG')
-file_type_JPG.setDescription(None)
-
-file_type_PNG = tr.getOrCreateNewFileFormatType('PNG')
-file_type_PNG.setDescription(None)
-
-file_type_UNKNOWN = tr.getOrCreateNewFileFormatType('UNKNOWN')
-file_type_UNKNOWN.setDescription('Unknown file format')
-
-
 vocabulary_PLATE_GEOMETRY = tr.getOrCreateNewVocabulary('PLATE_GEOMETRY')
 vocabulary_PLATE_GEOMETRY.setDescription('The geometry or dimensions of a plate')
 vocabulary_PLATE_GEOMETRY.setUrlTemplate(None)
@@ -73,61 +39,6 @@ vocabulary_term_PLATE_GEOMETRY_384_WELLS_16X24.setLabel('384 Wells, 16x24')
 vocabulary_term_PLATE_GEOMETRY_384_WELLS_16X24.setOrdinal(1)
 vocabulary_PLATE_GEOMETRY.addTerm(vocabulary_term_PLATE_GEOMETRY_384_WELLS_16X24)
 
-#
-# Exists in the database by default
-#
-#vocabulary_STORAGE_FORMAT = tr.getOrCreateNewVocabulary('STORAGE_FORMAT')
-#vocabulary_STORAGE_FORMAT.setDescription('The on-disk storage format of a data set')
-#vocabulary_STORAGE_FORMAT.setUrlTemplate(None)
-#vocabulary_STORAGE_FORMAT.setManagedInternally(True)
-#vocabulary_STORAGE_FORMAT.setInternalNamespace(True)
-#vocabulary_STORAGE_FORMAT.setChosenFromList(True)
-
-#vocabulary_term_STORAGE_FORMAT_PROPRIETARY = tr.createNewVocabularyTerm('PROPRIETARY')
-#vocabulary_term_STORAGE_FORMAT_PROPRIETARY.setDescription(None)
-#vocabulary_term_STORAGE_FORMAT_PROPRIETARY.setUrl(None)
-#vocabulary_term_STORAGE_FORMAT_PROPRIETARY.setLabel(None)
-#vocabulary_term_STORAGE_FORMAT_PROPRIETARY.setOrdinal(1)
-#vocabulary_STORAGE_FORMAT.addTerm(vocabulary_term_STORAGE_FORMAT_PROPRIETARY)
-#
-#vocabulary_term_STORAGE_FORMAT_BDS_DIRECTORY = tr.createNewVocabularyTerm('BDS_DIRECTORY')
-#vocabulary_term_STORAGE_FORMAT_BDS_DIRECTORY.setDescription(None)
-#vocabulary_term_STORAGE_FORMAT_BDS_DIRECTORY.setUrl(None)
-#vocabulary_term_STORAGE_FORMAT_BDS_DIRECTORY.setLabel(None)
-#vocabulary_term_STORAGE_FORMAT_BDS_DIRECTORY.setOrdinal(2)
-#vocabulary_STORAGE_FORMAT.addTerm(vocabulary_term_STORAGE_FORMAT_BDS_DIRECTORY)
-
-vocabulary_MICROSCOPE = tr.getOrCreateNewVocabulary('MICROSCOPE')
-vocabulary_MICROSCOPE.setDescription('Microscope used in an experiment.')
-vocabulary_MICROSCOPE.setUrlTemplate(None)
-vocabulary_MICROSCOPE.setManagedInternally(False)
-vocabulary_MICROSCOPE.setInternalNamespace(False)
-vocabulary_MICROSCOPE.setChosenFromList(True)
-
-vocabulary_term_MICROSCOPE_MD_IMAGEXPRESS_MICRO_2 = tr.createNewVocabularyTerm('MD_IMAGEXPRESS_MICRO_2')
-vocabulary_term_MICROSCOPE_MD_IMAGEXPRESS_MICRO_2.setDescription(None)
-vocabulary_term_MICROSCOPE_MD_IMAGEXPRESS_MICRO_2.setLabel(None)
-vocabulary_term_MICROSCOPE_MD_IMAGEXPRESS_MICRO_2.setOrdinal(3)
-vocabulary_MICROSCOPE.addTerm(vocabulary_term_MICROSCOPE_MD_IMAGEXPRESS_MICRO_2)
-
-vocabulary_term_MICROSCOPE_MD_IMAGEXPRESS_MICROLIVE = tr.createNewVocabularyTerm('MD_IMAGEXPRESS_MICROLIVE')
-vocabulary_term_MICROSCOPE_MD_IMAGEXPRESS_MICROLIVE.setDescription(None)
-vocabulary_term_MICROSCOPE_MD_IMAGEXPRESS_MICROLIVE.setLabel(None)
-vocabulary_term_MICROSCOPE_MD_IMAGEXPRESS_MICROLIVE.setOrdinal(2)
-vocabulary_MICROSCOPE.addTerm(vocabulary_term_MICROSCOPE_MD_IMAGEXPRESS_MICROLIVE)
-
-vocabulary_term_MICROSCOPE_BD_PATHWAY_855 = tr.createNewVocabularyTerm('BD_PATHWAY_855')
-vocabulary_term_MICROSCOPE_BD_PATHWAY_855.setDescription(None)
-vocabulary_term_MICROSCOPE_BD_PATHWAY_855.setLabel(None)
-vocabulary_term_MICROSCOPE_BD_PATHWAY_855.setOrdinal(1)
-vocabulary_MICROSCOPE.addTerm(vocabulary_term_MICROSCOPE_BD_PATHWAY_855)
-
-exp_type_COMPOUND_HCS = tr.getOrCreateNewExperimentType('COMPOUND_HCS')
-exp_type_COMPOUND_HCS.setDescription('Compound High Content Screening')
-
-exp_type_SIRNA_HCS = tr.getOrCreateNewExperimentType('SIRNA_HCS')
-exp_type_SIRNA_HCS.setDescription('Small Interfering RNA High Content Screening')
-
 samp_type_CONTROL_WELL = tr.getOrCreateNewSampleType('CONTROL_WELL')
 samp_type_CONTROL_WELL.setDescription(None)
 samp_type_CONTROL_WELL.setListable(False)
@@ -156,18 +67,6 @@ samp_type_SIRNA_WELL.setSubcodeUnique(False)
 samp_type_SIRNA_WELL.setAutoGeneratedCode(False)
 samp_type_SIRNA_WELL.setGeneratedCodePrefix('O')
 
-data_set_type_HCS_ANALYSIS_CELL_CLASS = tr.getOrCreateNewDataSetType('HCS_ANALYSIS_CELL_CLASS')
-data_set_type_HCS_ANALYSIS_CELL_CLASS.setDescription('HCS image analysis cell classification')
-data_set_type_HCS_ANALYSIS_CELL_CLASS.setContainerType(False)
-
-data_set_type_HCS_ANALYSIS_CELL_FEATURES = tr.getOrCreateNewDataSetType('HCS_ANALYSIS_CELL_FEATURES')
-data_set_type_HCS_ANALYSIS_CELL_FEATURES.setDescription('HCS image analysis cell feature vectors')
-data_set_type_HCS_ANALYSIS_CELL_FEATURES.setContainerType(False)
-
-data_set_type_HCS_ANALYSIS_CELL_SEGMENTATION = tr.getOrCreateNewDataSetType('HCS_ANALYSIS_CELL_SEGMENTATION')
-data_set_type_HCS_ANALYSIS_CELL_SEGMENTATION.setDescription('HCS image analysis cell segmentation')
-data_set_type_HCS_ANALYSIS_CELL_SEGMENTATION.setContainerType(False)
-
 data_set_type_HCS_ANALYSIS_WELL_FEATURES = tr.getOrCreateNewDataSetType('HCS_ANALYSIS_WELL_FEATURES')
 data_set_type_HCS_ANALYSIS_WELL_FEATURES.setDescription('HCS image analysis well feature vectors.')
 data_set_type_HCS_ANALYSIS_WELL_FEATURES.setContainerType(False)
@@ -175,10 +74,6 @@ data_set_type_HCS_ANALYSIS_WELL_FEATURES.setContainerType(False)
 data_set_type_HCS_IMAGE_OVERVIEW = tr.getOrCreateNewDataSetType('HCS_IMAGE_OVERVIEW')
 data_set_type_HCS_IMAGE_OVERVIEW.setDescription('Overview High Content Screening Images. Generated from raw images.')
 data_set_type_HCS_IMAGE_OVERVIEW.setContainerType(False)
-
-data_set_type_MICROSCOPY_IMG_OVERVIEW = tr.getOrCreateNewDataSetType('MICROSCOPY_IMG_OVERVIEW')
-data_set_type_MICROSCOPY_IMG_OVERVIEW.setDescription('Overview Microscopy Images. Generated from raw images.')
-data_set_type_MICROSCOPY_IMG_OVERVIEW.setContainerType(False)
 
 data_set_type_HCS_IMAGE_RAW = tr.getOrCreateNewDataSetType('HCS_IMAGE_RAW')
 data_set_type_HCS_IMAGE_RAW.setDescription('Raw High Content Screening Images')
@@ -192,32 +87,9 @@ data_set_type_HCS_IMAGE_CONTAINER_RAW = tr.getOrCreateNewDataSetType('HCS_IMAGE_
 data_set_type_HCS_IMAGE_CONTAINER_RAW.setDescription('Container for HCS images of different resolutions (raw, overviews, thumbnails).')
 data_set_type_HCS_IMAGE_CONTAINER_RAW.setContainerType(True)
 
-data_set_type_HCS_IMAGE_CONTAINER_SEGMENTATION = tr.getOrCreateNewDataSetType('HCS_IMAGE_CONTAINER_SEGMENTATION')
-data_set_type_HCS_IMAGE_CONTAINER_SEGMENTATION.setDescription('Container for HCS segmentation (a.k.a. overlays) images of different resolutions (original, overviews, thumbnails).')
-data_set_type_HCS_IMAGE_CONTAINER_SEGMENTATION.setContainerType(True)
-
-data_set_type_HCS_ANALYSIS_CONTAINER_CELL_FEATURES = tr.getOrCreateNewDataSetType('HCS_ANALYSIS_CONTAINER_CELL_FEATURES')
-data_set_type_HCS_ANALYSIS_CONTAINER_CELL_FEATURES.setDescription('Container for HCS image analysis cell feature vectors.')
-data_set_type_HCS_ANALYSIS_CONTAINER_CELL_FEATURES.setContainerType(True)
-
-data_set_type_HCS_ANALYSIS_CONTAINER_WELL_FEATURES = tr.getOrCreateNewDataSetType('HCS_ANALYSIS_CONTAINER_WELL_FEATURES')
-data_set_type_HCS_ANALYSIS_CONTAINER_WELL_FEATURES.setDescription('Container for HCS image analysis well feature vectors.')
-data_set_type_HCS_ANALYSIS_CONTAINER_WELL_FEATURES.setContainerType(True)
-
 data_set_type_HCS_ANALYSIS_FEATURES_LIST = tr.getOrCreateNewDataSetType('HCS_ANALYSIS_FEATURES_LIST')
 data_set_type_HCS_ANALYSIS_FEATURES_LIST.setDescription('The list (or group) of features. The subset of features from feature vectors.')
 data_set_type_HCS_ANALYSIS_FEATURES_LIST.setContainerType(False)
-
-# Exists in the database by default
-#data_set_type_UNKNOWN = tr.getOrCreateNewDataSetType('UNKNOWN')
-#data_set_type_UNKNOWN.setDescription('Unknown')
-#data_set_type_UNKNOWN.setContainerType(False)
-
-material_type_BACTERIUM = tr.getOrCreateNewMaterialType('BACTERIUM')
-material_type_BACTERIUM.setDescription('Bacterium')
-
-material_type_CELL_LINE = tr.getOrCreateNewMaterialType('CELL_LINE')
-material_type_CELL_LINE.setDescription('Cell Line or Cell Culture. The growing of cells under controlled conditions.')
 
 material_type_COMPOUND = tr.getOrCreateNewMaterialType('COMPOUND')
 material_type_COMPOUND.setDescription('Compound')
@@ -230,9 +102,6 @@ material_type_GENE.setDescription('Gene')
 
 material_type_SIRNA = tr.getOrCreateNewMaterialType('SIRNA')
 material_type_SIRNA.setDescription('Oligo nucleotide')
-
-material_type_VIRUS = tr.getOrCreateNewMaterialType('VIRUS')
-material_type_VIRUS.setDescription('Virus')
 
 prop_type_ANALYSIS_PROCEDURE = tr.getOrCreateNewPropertyType('ANALYSIS_PROCEDURE', DataType.VARCHAR)
 prop_type_ANALYSIS_PROCEDURE.setLabel('Analysis procedure')
@@ -252,7 +121,7 @@ prop_type_PLATE_GEOMETRY.setVocabulary(vocabulary_PLATE_GEOMETRY)
 
 prop_type_CONTROL = tr.getOrCreateNewPropertyType('CONTROL', DataType.MATERIAL)
 prop_type_CONTROL.setLabel('Control')
-prop_type_CONTROL.setManagedInternally(False)
+prop_type_CONTROL.setManagedInternally(True)
 prop_type_CONTROL.setInternalNamespace(False)
 prop_type_CONTROL.setMaterialType(material_type_CONTROL)
 
@@ -261,73 +130,41 @@ prop_type_DESCRIPTION = tr.getPropertyType('DESCRIPTION')
 
 prop_type_GENE = tr.getOrCreateNewPropertyType('GENE', DataType.MATERIAL)
 prop_type_GENE.setLabel('Gene')
-prop_type_GENE.setManagedInternally(False)
+prop_type_GENE.setManagedInternally(True)
 prop_type_GENE.setInternalNamespace(False)
 prop_type_GENE.setMaterialType(material_type_GENE)
 
 prop_type_GENE_SYMBOLS = tr.getOrCreateNewPropertyType('GENE_SYMBOLS', DataType.VARCHAR)
 prop_type_GENE_SYMBOLS.setLabel('Gene symbols')
-prop_type_GENE_SYMBOLS.setManagedInternally(False)
+prop_type_GENE_SYMBOLS.setManagedInternally(True)
 prop_type_GENE_SYMBOLS.setInternalNamespace(False)
-
 
 prop_type_INHIBITOR_OF = tr.getOrCreateNewPropertyType('INHIBITOR_OF', DataType.MATERIAL)
 prop_type_INHIBITOR_OF.setLabel('Inhibitor Of')
-prop_type_INHIBITOR_OF.setManagedInternally(False)
+prop_type_INHIBITOR_OF.setManagedInternally(True)
 prop_type_INHIBITOR_OF.setInternalNamespace(False)
 prop_type_INHIBITOR_OF.setMaterialType(material_type_GENE)
 
 prop_type_LIBRARY_ID = tr.getOrCreateNewPropertyType('LIBRARY_ID', DataType.VARCHAR)
 prop_type_LIBRARY_ID.setLabel('Library ID')
-prop_type_LIBRARY_ID.setManagedInternally(False)
+prop_type_LIBRARY_ID.setManagedInternally(True)
 prop_type_LIBRARY_ID.setInternalNamespace(False)
-
-
-prop_type_MICROSCOPE = tr.getOrCreateNewPropertyType('MICROSCOPE', DataType.CONTROLLEDVOCABULARY)
-prop_type_MICROSCOPE.setLabel('Microscope')
-prop_type_MICROSCOPE.setManagedInternally(False)
-prop_type_MICROSCOPE.setInternalNamespace(False)
-
-prop_type_MICROSCOPE.setVocabulary(vocabulary_MICROSCOPE)
 
 prop_type_NUCLEOTIDE_SEQUENCE = tr.getOrCreateNewPropertyType('NUCLEOTIDE_SEQUENCE', DataType.VARCHAR)
 prop_type_NUCLEOTIDE_SEQUENCE.setLabel('Nucleotide Sequence')
-prop_type_NUCLEOTIDE_SEQUENCE.setManagedInternally(False)
+prop_type_NUCLEOTIDE_SEQUENCE.setManagedInternally(True)
 prop_type_NUCLEOTIDE_SEQUENCE.setInternalNamespace(False)
-
-
-prop_type_NUMBER_OF_CHANNEL = tr.getOrCreateNewPropertyType('NUMBER_OF_CHANNEL', DataType.INTEGER)
-prop_type_NUMBER_OF_CHANNEL.setLabel('Channels')
-prop_type_NUMBER_OF_CHANNEL.setManagedInternally(False)
-prop_type_NUMBER_OF_CHANNEL.setInternalNamespace(False)
-
-
-prop_type_REFSEQ = tr.getOrCreateNewPropertyType('REFSEQ', DataType.VARCHAR)
-prop_type_REFSEQ.setLabel('RefSeq')
-prop_type_REFSEQ.setManagedInternally(False)
-prop_type_REFSEQ.setInternalNamespace(False)
-
 
 prop_type_SIRNA = tr.getOrCreateNewPropertyType('SIRNA', DataType.MATERIAL)
 prop_type_SIRNA.setLabel('siRNA')
-prop_type_SIRNA.setManagedInternally(False)
+prop_type_SIRNA.setManagedInternally(True)
 prop_type_SIRNA.setInternalNamespace(False)
 prop_type_SIRNA.setMaterialType(material_type_SIRNA)
-
-assignment_MATERIAL_BACTERIUM_DESCRIPTION = tr.assignPropertyType(material_type_BACTERIUM, prop_type_DESCRIPTION)
-assignment_MATERIAL_BACTERIUM_DESCRIPTION.setMandatory(False)
-assignment_MATERIAL_BACTERIUM_DESCRIPTION.setSection(None)
-assignment_MATERIAL_BACTERIUM_DESCRIPTION.setPositionInForms(1)
 
 assignment_MATERIAL_COMPOUND_DESCRIPTION = tr.assignPropertyType(material_type_COMPOUND, prop_type_DESCRIPTION)
 assignment_MATERIAL_COMPOUND_DESCRIPTION.setMandatory(False)
 assignment_MATERIAL_COMPOUND_DESCRIPTION.setSection(None)
 assignment_MATERIAL_COMPOUND_DESCRIPTION.setPositionInForms(1)
-
-assignment_EXPERIMENT_COMPOUND_HCS_DESCRIPTION = tr.assignPropertyType(exp_type_COMPOUND_HCS, prop_type_DESCRIPTION)
-assignment_EXPERIMENT_COMPOUND_HCS_DESCRIPTION.setMandatory(True)
-assignment_EXPERIMENT_COMPOUND_HCS_DESCRIPTION.setSection(None)
-assignment_EXPERIMENT_COMPOUND_HCS_DESCRIPTION.setPositionInForms(1)
 
 assignment_MATERIAL_CONTROL_DESCRIPTION = tr.assignPropertyType(material_type_CONTROL, prop_type_DESCRIPTION)
 assignment_MATERIAL_CONTROL_DESCRIPTION.setMandatory(False)
@@ -349,25 +186,10 @@ assignment_MATERIAL_GENE_GENE_SYMBOLS.setMandatory(False)
 assignment_MATERIAL_GENE_GENE_SYMBOLS.setSection(None)
 assignment_MATERIAL_GENE_GENE_SYMBOLS.setPositionInForms(4)
 
-assignment_DATA_SET_HCS_ANALYSIS_WELL_FEATURES_ANALYSIS_PROCEDURE = tr.assignPropertyType(data_set_type_HCS_ANALYSIS_CONTAINER_WELL_FEATURES, prop_type_ANALYSIS_PROCEDURE)
-assignment_DATA_SET_HCS_ANALYSIS_WELL_FEATURES_ANALYSIS_PROCEDURE.setMandatory(False)
-assignment_DATA_SET_HCS_ANALYSIS_WELL_FEATURES_ANALYSIS_PROCEDURE.setSection(None)
-assignment_DATA_SET_HCS_ANALYSIS_WELL_FEATURES_ANALYSIS_PROCEDURE.setPositionInForms(1)
-
-assignment_DATA_SET_HCS_IMAGE_SEGMENTATION_ANALYSIS_PROCEDURE = tr.assignPropertyType(data_set_type_HCS_IMAGE_CONTAINER_SEGMENTATION, prop_type_ANALYSIS_PROCEDURE)
-assignment_DATA_SET_HCS_IMAGE_SEGMENTATION_ANALYSIS_PROCEDURE.setMandatory(False)
-assignment_DATA_SET_HCS_IMAGE_SEGMENTATION_ANALYSIS_PROCEDURE.setSection(None)
-assignment_DATA_SET_HCS_IMAGE_SEGMENTATION_ANALYSIS_PROCEDURE.setPositionInForms(1)
-
 assignment_DATA_SET_HCS_IMAGE_OVERVIEW_RESOLUTION = tr.assignPropertyType(data_set_type_HCS_IMAGE_OVERVIEW, prop_type_RESOLUTION)
 assignment_DATA_SET_HCS_IMAGE_OVERVIEW_RESOLUTION.setMandatory(False)
 assignment_DATA_SET_HCS_IMAGE_OVERVIEW_RESOLUTION.setSection(None)
 assignment_DATA_SET_HCS_IMAGE_OVERVIEW_RESOLUTION.setPositionInForms(1)
-
-assignment_DATA_SET_MICROSCOPY_IMG_OVERVIEW_RESOLUTION = tr.assignPropertyType(data_set_type_MICROSCOPY_IMG_OVERVIEW, prop_type_RESOLUTION)
-assignment_DATA_SET_MICROSCOPY_IMG_OVERVIEW_RESOLUTION.setMandatory(False)
-assignment_DATA_SET_MICROSCOPY_IMG_OVERVIEW_RESOLUTION.setSection(None)
-assignment_DATA_SET_MICROSCOPY_IMG_OVERVIEW_RESOLUTION.setPositionInForms(1)
 
 assignment_SAMPLE_PLATE_PLATE_GEOMETRY = tr.assignPropertyType(samp_type_PLATE, prop_type_PLATE_GEOMETRY)
 assignment_SAMPLE_PLATE_PLATE_GEOMETRY.setMandatory(True)
@@ -394,16 +216,6 @@ assignment_MATERIAL_SIRNA_LIBRARY_ID.setMandatory(False)
 assignment_MATERIAL_SIRNA_LIBRARY_ID.setSection(None)
 assignment_MATERIAL_SIRNA_LIBRARY_ID.setPositionInForms(5)
 
-assignment_EXPERIMENT_SIRNA_HCS_DESCRIPTION = tr.assignPropertyType(exp_type_SIRNA_HCS, prop_type_DESCRIPTION)
-assignment_EXPERIMENT_SIRNA_HCS_DESCRIPTION.setMandatory(True)
-assignment_EXPERIMENT_SIRNA_HCS_DESCRIPTION.setSection(None)
-assignment_EXPERIMENT_SIRNA_HCS_DESCRIPTION.setPositionInForms(1)
-
-assignment_EXPERIMENT_SIRNA_HCS_MICROSCOPE = tr.assignPropertyType(exp_type_SIRNA_HCS, prop_type_MICROSCOPE)
-assignment_EXPERIMENT_SIRNA_HCS_MICROSCOPE.setMandatory(False)
-assignment_EXPERIMENT_SIRNA_HCS_MICROSCOPE.setSection(None)
-assignment_EXPERIMENT_SIRNA_HCS_MICROSCOPE.setPositionInForms(2)
-
 assignment_SAMPLE_SIRNA_WELL_SIRNA = tr.assignPropertyType(samp_type_SIRNA_WELL, prop_type_SIRNA)
 assignment_SAMPLE_SIRNA_WELL_SIRNA.setMandatory(False)
 assignment_SAMPLE_SIRNA_WELL_SIRNA.setSection(None)
@@ -413,8 +225,3 @@ assignment_SAMPLE_SIRNA_WELL_GENE = tr.assignPropertyType(samp_type_SIRNA_WELL, 
 assignment_SAMPLE_SIRNA_WELL_GENE.setMandatory(False)
 assignment_SAMPLE_SIRNA_WELL_GENE.setSection(None)
 assignment_SAMPLE_SIRNA_WELL_GENE.setPositionInForms(2)
-
-assignment_MATERIAL_VIRUS_DESCRIPTION = tr.assignPropertyType(material_type_VIRUS, prop_type_DESCRIPTION)
-assignment_MATERIAL_VIRUS_DESCRIPTION.setMandatory(False)
-assignment_MATERIAL_VIRUS_DESCRIPTION.setSection(None)
-assignment_MATERIAL_VIRUS_DESCRIPTION.setPositionInForms(1)
