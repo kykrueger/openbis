6111d08aa255ce1737346afc2e40b8058ad2e13f
jakubs <jakubs>
2012-11-05 09:24:24 +0000

    SP-295 BIS-190 - add hashcode and equals to objectId classes
    SVN: 27486

--- openbis_api/source/java/ch/systemsx/cisd/openbis/generic/shared/api/v1/dto/id/dataset/DataSetCodeId.java
+++ openbis_api/source/java/ch/systemsx/cisd/openbis/generic/shared/api/v1/dto/id/dataset/DataSetCodeId.java
@@ -69,4 +69,39 @@ public class DataSetCodeId implements IDataSetId
         return getCode();
     }
 
+    @Override
+    public int hashCode()
+    {
+        return ((code == null) ? 0 : code.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        DataSetCodeId other = (DataSetCodeId) obj;
+        if (code == null)
+        {
+            if (other.code != null)
+            {
+                return false;
+            }
+        } else if (!code.equals(other.code))
+        {
+            return false;
+        }
+        return true;
+    }
+
 }
