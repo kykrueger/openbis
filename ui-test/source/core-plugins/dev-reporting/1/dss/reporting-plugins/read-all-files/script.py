14545ca0314ee4eb174d37517be90b9582f8284f
anttil <anttil>
2012-11-05 09:39:37 +0000

    BIS-228 / SP-329: enable second dss
    SVN: 27488

--- ui-test/source/core-plugins/dev-reporting/1/dss/reporting-plugins/read-all-files/script.py
+++ ui-test/source/core-plugins/dev-reporting/1/dss/reporting-plugins/read-all-files/script.py
@@ -2,11 +2,12 @@ DATA_SET = "Data Set"
 DATA_SET_TYPE = "Data Set Type"
 
 def describe(dataSets, tableBuilder):
-    print "AT THE SCRIPT"
-    tableBuilder.addHeader(DATA_SET)
-    tableBuilder.addHeader(DATA_SET_TYPE)
-    for dataSet in dataSets:
-    		print "LOOPING " + str(dataSet)
-        row = tableBuilder.addRow()
-        row.setCell(DATA_SET, dataSet.dataSetCode)
-        row.setCell(DATA_SET_TYPE, dataSet.dataSetTypeCode)
+	print "AT THE SCRIPT"
+	tableBuilder.addHeader(DATA_SET)
+	tableBuilder.addHeader(DATA_SET_TYPE)
+	for dataSet in dataSets:
+		print "LOOPING " + str(dataSet)
+		row = tableBuilder.addRow()
+		row.setCell(DATA_SET, dataSet.dataSetCode)
+		row.setCell(DATA_SET_TYPE, dataSet.dataSetTypeCode)
+
