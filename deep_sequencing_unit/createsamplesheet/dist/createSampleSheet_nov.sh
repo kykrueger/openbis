cdb6f936c4116123d34e5a21e9c9de6052a8de9e
kohleman <kohleman>
2012-11-08 15:33:42 +0000

    -remove the echo to make it less verbose
    SVN: 27557

--- deep_sequencing_unit/createsamplesheet/dist/createSampleSheet_nov.sh
+++ deep_sequencing_unit/createsamplesheet/dist/createSampleSheet_nov.sh
@@ -26,5 +26,5 @@ fi
 ALL_JAVA_OPTS="-Djavax.net.ssl.trustStore=etc/openBIS.keystore $JAVA_OPTS"
 
 LIB="lib"
-echo "Starting Sample Sheet Creator for openBIS Illumina NGS"
+# echo "Starting Sample Sheet Creator for openBIS Illumina NGS"
 ${JAVA_BIN} ${ALL_JAVA_OPTS} -cp ${LIB}/commons-codec.jar:${LIB}/commons-httpclient.jar:${LIB}/commons-logging.jar:${LIB}/dss_client.jar:${LIB}/spring.jar:${LIB}/stream-supporting-httpinvoker.jar:${LIB}/jython.jar org.python.util.jython $JYTHON_FILE "$@"
