b06b34344478b6b41b325479186a0d4d142a19a5
brinn <brinn>
2013-01-18 10:01:39 +0000

    Fix warning.
    SVN: 28129

--- datastore_server/source/java/ch/systemsx/cisd/etlserver/registrator/v1/IOmniscientEntityRegistrator.java
+++ datastore_server/source/java/ch/systemsx/cisd/etlserver/registrator/v1/IOmniscientEntityRegistrator.java
@@ -21,7 +21,6 @@ import java.util.List;
 
 import ch.systemsx.cisd.etlserver.TopLevelDataSetRegistratorGlobalState;
 import ch.systemsx.cisd.etlserver.registrator.DataSetRegistrationContext;
-import ch.systemsx.cisd.etlserver.registrator.DataSetRegistrationContext.IHolder;
 import ch.systemsx.cisd.etlserver.registrator.api.impl.SecondaryTransactionFailure;
 import ch.systemsx.cisd.etlserver.registrator.api.v1.impl.DataSetRegistrationTransaction;
 import ch.systemsx.cisd.etlserver.registrator.v1.AbstractOmniscientTopLevelDataSetRegistrator.OmniscientTopLevelDataSetRegistratorState;
