5cf5f521ff0aaacd4aeda4bacd908f56db6f6768
gpawel <gpawel>
2013-02-26 13:15:05 +0000

    SP-515 BIS-336: Java Plugins Usability Improvements (fixing imports)
    SVN: 28452

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/hotdeploy_plugins/AbstractCommonPropertyBasedHotDeployPluginFactory.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/hotdeploy_plugins/AbstractCommonPropertyBasedHotDeployPluginFactory.java
@@ -26,7 +26,7 @@ import ch.ethz.cisd.hotdeploy.PluginEventListener;
 import ch.ethz.cisd.hotdeploy.PluginMapHolder;
 import ch.systemsx.cisd.common.exceptions.UserFailureException;
 import ch.systemsx.cisd.common.shared.basic.string.StringUtils;
-import ch.systemsx.cisd.openbis.generic.shared.IHotDeploymentController;
+import ch.systemsx.cisd.openbis.generic.server.IHotDeploymentController;
 import ch.systemsx.cisd.openbis.generic.shared.basic.dto.ScriptType;
 import ch.systemsx.cisd.openbis.generic.shared.hotdeploy_plugins.api.ICommonPropertyBasedHotDeployPlugin;
 
