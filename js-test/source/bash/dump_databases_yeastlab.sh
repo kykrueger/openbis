ceab7ee4ae641d26fe69b1913a081fb3856fd716
pkupczyk <pkupczyk>
2013-05-07 09:00:11 +0000

    SP-555 / BIS-366 : openbis.js - delete utilities.js files and use openbis-action-deferrer.js - remove store folders from svn - fix script-folders for AS
    SVN: 29014

--- js-test/source/bash/dump_databases_yeastlab.sh
+++ js-test/source/bash/dump_databases_yeastlab.sh
@@ -3,5 +3,5 @@
 SCRIPT_PATH="`dirname \"$0\"`"
 SCRIPT_PATH="`( cd \"$SCRIPT_PATH\" && pwd )`"
 
-pg_dump -U postgres -O openbis_yeastlab > $SCRIPT_PATH/../../servers/yeastlab/openBIS-server/db/openbis_test_js_yeastlab.sql
-pg_dump -U postgres -O pathinfo_yeastlab > $SCRIPT_PATH/../../servers/yeastlab/datastore_server/db/pathinfo_test_js_yeastlab.sql
+pg_dump -U postgres -O openbis_test_js_yeastlab > $SCRIPT_PATH/../../servers/yeastlab/openBIS-server/db/openbis_test_js_yeastlab.sql
+pg_dump -U postgres -O pathinfo_test_js_yeastlab > $SCRIPT_PATH/../../servers/yeastlab/datastore_server/db/pathinfo_test_js_yeastlab.sql
