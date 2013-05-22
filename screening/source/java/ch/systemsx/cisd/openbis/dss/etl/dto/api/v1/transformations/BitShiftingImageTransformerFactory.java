33aa6d80be32825d6356ad91d23173b666631d72
jakubs <jakubs>
2013-05-22 13:43:05 +0000

    BIS-415 SP-647 fix problem with deserialization of transformations created in the old openbis version
    SVN: 29198

---
+++ screening/source/java/ch/systemsx/cisd/openbis/dss/etl/dto/api/v1/transformations/BitShiftingImageTransformerFactory.java
@@ -0,0 +1,45 @@
+package ch.systemsx.cisd.openbis.dss.etl.dto.api.v1.transformations;
+
+import java.awt.image.BufferedImage;
+
+import ch.systemsx.cisd.base.annotation.JsonObject;
+import ch.systemsx.cisd.base.image.IImageTransformer;
+import ch.systemsx.cisd.base.image.IImageTransformerFactory;
+import ch.systemsx.cisd.common.image.IntensityRescaling;
+
+/**
+ * This class is obsolete, and should not be used. Use {@link ch.systemsx.cisd.openbis.dss.etl.dto.api.transformations.ConvertToolImageTransformer}
+ * instead Warning: The serialized version of this class can be stored in the database for each image. Moving this class to a different package or
+ * changing it in a backward incompatible way would make all the saved transformations invalid.
+ * 
+ * @author Jakub Straszewski
+ */
+@JsonObject("BitShiftingImageTransformerFactory")
+final class BitShiftingImageTransformerFactory implements IImageTransformerFactory
+{
+    private static final long serialVersionUID = 1L;
+
+    private final int shiftBits;
+
+    public BitShiftingImageTransformerFactory(int shiftBits)
+    {
+        this.shiftBits = shiftBits;
+    }
+
+    @Override
+    public IImageTransformer createTransformer()
+    {
+        return new IImageTransformer()
+            {
+                @Override
+                public BufferedImage transform(BufferedImage image)
+                {
+                    if (IntensityRescaling.isNotGrayscale(image))
+                    {
+                        return image;
+                    }
+                    return IntensityRescaling.rescaleIntensityBitShiftTo8Bits(image, shiftBits);
+                }
+            };
+    }
+}
