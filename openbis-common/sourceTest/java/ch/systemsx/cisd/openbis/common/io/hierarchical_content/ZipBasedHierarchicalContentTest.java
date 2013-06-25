92aed7e524915cde6fa640d1209639787d826ced
felmer <felmer>
2013-06-25 13:28:10 +0000

    SP-724, BIS-428: clean up imports
    SVN: 29455

--- openbis-common/sourceTest/java/ch/systemsx/cisd/openbis/common/io/hierarchical_content/ZipBasedHierarchicalContentTest.java
+++ openbis-common/sourceTest/java/ch/systemsx/cisd/openbis/common/io/hierarchical_content/ZipBasedHierarchicalContentTest.java
@@ -25,8 +25,6 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.zip.CRC32;
 import java.util.zip.CheckedInputStream;
