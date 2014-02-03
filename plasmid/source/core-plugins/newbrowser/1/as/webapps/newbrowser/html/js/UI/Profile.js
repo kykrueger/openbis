10479ca2c6033de5f197d525c91d3bc2e05968be
barillac <barillac>
2014-02-03 15:46:07 +0000

    bodenmiller lab profile added
    SVN: 30470

--- plasmid/source/core-plugins/newbrowser/1/as/webapps/newbrowser/html/js/UI/Profile.js
+++ plasmid/source/core-plugins/newbrowser/1/as/webapps/newbrowser/html/js/UI/Profile.js
@@ -719,3 +719,566 @@ $.extend(ExampleProfile.prototype, DefaultProfile.prototype, {
 		};
 }
 });
+
+
+//LSILAB PROFILE
+function LSILabProfile(serverFacade) {
+	this.init(serverFacade);
+}
+
+$.extend(LSILabProfile.prototype, DefaultProfile.prototype, {
+	init: function(serverFacade){
+		DefaultProfile.prototype.init.call(this, serverFacade);
+		
+		this.ELNExperiments = ["SYSTEM_EXPERIMENT"];
+
+		this.notShowTypes = ["SYSTEM_EXPERIMENT", "ILLUMINA_FLOW_CELL", "ILLUMINA_FLOW_LANE", "LIBRARY", "LIBRARY_POOL", "MASTER_SAMPLE","MS_INJECTION","RAW_SAMPLE","TEMPLATE_SAMPLE", "SEARCH"];
+	
+		this.typeGroups = {
+			"SEQUENCING" : {
+				"TYPE" : "SEQUENCING",
+				"DISPLAY_NAME" : "Sequencing",
+				"LIST" : ["SEQUENCING"]
+			},
+			"CHEMBIO" : {
+				"TYPE" : "CHEMICALS_AND_BIOLOGICALS",
+				"DISPLAY_NAME" : "Chemicals and Biologicals",
+				"LIST" : ["PRIMARY_ANTIBODIES", "BACTERIA", "ENZYMES", "MICE", "OLIGOS", "PLASMIDS", "PRIMERS"]
+			},
+			"EQUIPMENT" : {
+				"TYPE" : "EQUIPMENT",
+				"DISPLAY_NAME" : "Equipment",
+				"LIST" : ["EQUIPMENT"]
+			},
+			"OTHERS" : {
+				"TYPE" : "OTHERS",
+				"DISPLAY_NAME" : "Others",
+				"LIST" : [] 
+			}
+		};
+	
+		this.typePropertiesForTable = {
+			"SYSTEM_EXPERIMENT" : ["NAME", "GOALS", "RESULT_INTERPRETATION"],
+			"GENERAL_PROTOCOL" : ["NAME", "FOR_WHAT", "PROTOCOL_TYPE"],
+			"PCR" : ["NAME", "FOR_WHAT", "TEMPLATE", "PUBLICATION"],
+			"WESTERN_BLOTTING" : ["NAME", "FOR_WHAT", "STORAGE"],
+			"CHEMICAL" : ["NAME", "SUPPLIER", "ARTICLE_NUMBER", "LOCAL_ID", "STORAGE"],
+			"ANTIBODY" : ["NAME", "STORAGE", "HOST", "FOR_WHAT"],
+			"MEDIA" : ["NAME", "STORAGE", "FOR_WHAT", "ORGANISM"],
+			"SOLUTIONS_BUFFERS" : ["NAME", "STORAGE", "FOR_WHAT"],
+			"ENZYME" : ["NAME", "SUPPLIER", "ARTICLE_NUMBER", "KIT"],
+			"OLIGO" : ["TARGET", "DIRECTION", "RESTRICTION_ENZYME", "PROJECT"],
+			"PLASMID" : ["OWNER", "OWNER_NUMBER", "PLASMID_NAME", "BACTERIAL_ANTIBIOTIC_RESISTANCE", "YEAST_MARKER"],
+			"YEAST" : ["OWNER", "OWNER_NUMBER", "YEAST_STRAIN_NAME", "PROJECT", "GENETIC_BACKGROUND", "MATING_TYPE", "FREEZER_NAME", "ROW", "COLUMN", "BOX_NUMBER"],
+			"BACTERIA" : ["BACTERIA_STRAIN_NAME", "BACTERIA_GENOTYPE", "FOR_WHAT", "SUPPLIER", "ARTICLE_NUMBER", "COMMENTS"]
+		}
+	
+		this.colorForInspectors = {
+			"GENERAL_PROTOCOL" : "#CCFFCC",
+			"PCR" : "#CCFFCC",
+			"WESTERN_BLOTTING" : "#CCFFCC",
+			"CHEMICAL" : "#E3E3E3",
+			"ANTIBODY" : "#E3E3E3",
+			"MEDIA" : "#E3E3E3",
+			"SOLUTIONS_BUFFERS" : "#E3E3E3",
+			"ENZYME" : "#E3E3E3",
+			"BACTERIA" : "#E3E3E3",
+			"OLIGO" : "#ACE8FC",
+			"PLASMID" : "#FCDEC0",
+			"YEAST" : "#CCCC99",
+			"SAMPLE_PROPERTY_TEST" : "#000000"
+		};
+	
+		this.storagesConfiguration = {
+			"isEnabled" : true,
+			/*
+			 * Should be the same across all storages, if not correct behaviour is not guaranteed.
+			*/
+			"STORAGE_PROPERTIES": {
+						"NAME_PROPERTY" : "FREEZER_NAME", //Should be a Vocabulary.
+						"ROW_PROPERTY" : "ROW", //Vocabulary on LSILab, can be (Vocabulary, text and integer).
+						"COLUMN_PROPERTY" : "COLUMN", //Integer on LSILab, can be (Vocabulary, text and integer).
+						"BOX_PROPERTY" : "BOX_NUMBER" //Should be text.
+			},
+			/*
+			 * Where the storage will be painted.
+			*/
+			"STORAGE_PROPERTY_GROUP" : "Storage information",
+			/*
+			 * Storages map, can hold configurations for several storages.
+			*/
+			"STORAGE_CONFIGS": {
+				"-80DEGREES" : { //Freezer name given by the NAME_PROPERTY
+								"ROW_NUM" : 9, //Number of rows
+								"COLUMN_NUM" : 9, //Number of columns
+								"BOX_NUM" : 999 //Boxes on each rack, used for validation, to avoid validation increase the number to 9999 for example
+							}
+			}
+		};
+	
+		/*
+		 * Used by Sample Form
+		 */
+		this.getSpaceForSampleType = function(type) {
+			if(type === "SEQUENCING") {
+				return "LSI";
+			} else if(type === "PRIMARY_ANTIBODIES") {
+				return "LSI";
+			} else if(type === "BACTERIA") {
+				return "LSI";
+			} else if(type === "ENZYME") {
+				return "LSI";
+			} else if(type === "CHEMICAL") {
+				return "LSI";
+			} else if(type === "ANTIBODY") {
+				return "LSI";
+			} else if(type === "OLIGO") {
+				return "LSI";
+			} else if(type === "PLASMID") {
+				return "LSI";
+			} else if(type === "WESTERN_BLOTTING") {
+				return "LSI";
+			} else if(type === "READOUT") {
+				return "LSI";
+			} else if(type === "PCR") {
+				return "LSI";
+			} else if(type === "GENERAL_PROTOCOL") {
+				return "LSI";
+			} else if(type === "YEAST") {
+				return "LSI";
+			} else if(type === "POMBE") {
+				return "LSI";
+			} else {
+				return null;
+			}
+		}
+		
+		this.getExperimentIdentifierForSample = function(type, code, properties) {
+			if(type === "BACTERIA") {
+				return "/LSI/BACTERIA/LAB_BENCH_BACTERIA";
+			} else if(type === "SOLUTIONS_BUFFERS") {
+				return "/LSI/CHEMICALS/SOLUTIONS_BUFFERS";
+			} else if(type === "MEDIA") {
+				return "/LSI/CHEMICALS/MEDIA";
+			} else if(type === "ENZYME") {
+				return "/LSI/CHEMICALS/ENZYMES";
+			} else {
+				return null;
+			}
+		}
+		
+		this.getHTMLTableFromXML = function(xmlDocument) {
+			var table_head = null;
+			var table_body = "";
+			var dom;
+	
+			if (window.DOMParser) {
+			  parser = new DOMParser();
+			  dom = parser.parseFromString(xmlDocument,"text/xml");
+			} else {// Internet Explorer
+			  dom = new ActiveXObject("Microsoft.XMLDOM");
+			  dom.async = false;
+			  dom.loadXML(xmlDocument); 
+			} 
+	
+			var html = null;
+			var root = dom.childNodes[0];
+			var children = root.childNodes;
+			for(var i = 0; i < children.length; i++) {
+				var child = children[i];
+				if (child.localName != null) {
+					var keys = child.attributes;
+					if (table_head == null) {
+						table_head = "<tr>";
+						var key = null;
+						for (var j = 0; j < keys.length; j++) {
+							key = keys[j];
+							if(key.localName != "permId") {
+								table_head += "<th style='text-align:left; width:" + (100/(keys.length-1)) + "%;'>"+ key.localName + "</th>";
+							}
+						}
+						table_head += "</tr>";
+					}
+					table_body += "<tr>";
+					for (var j = 0; j < keys.length; j++) {
+						key = keys[j];
+						if(key.localName != "permId") {
+							if(key.localName == "date") {
+								table_body += "<td style='text-align:left; width:" + (100/(keys.length-1)) + "%;'>" + key.value + "</td>";
+							} else {
+								table_body += "<td style='text-align:left; width:" + (100/(keys.length-1)) + "%;'>" + key.value + "</td>";
+							}
+						}
+					}
+					table_body += "</tr>";
+				}
+			}
+			html = "<table style='font-family:helvetica; font-size:90%; width: 100%;'><thead>" + table_head + "</thead><tbody>"+ table_body + "</tbody></table>";
+			return html;
+		}
+	
+		this.inspectorContentTransformer = function(sample, propertyCode, propertyContent) {
+		
+			if(propertyContent.indexOf("<root>") != -1) {
+				return {
+					"isSingleColumn" : true,
+					"content" : this.getHTMLTableFromXML(propertyContent)
+				};
+			} else {
+				if(propertyContent === "<root/>\n") { //To clean empty XMLs and don't show them.
+					propertyContent = "";
+				}
+				return {
+					"isSingleColumn" : false,
+					"content" : propertyContent
+				};
+			}
+		}
+	
+		this.searchSorter = function(searchResults) {
+		
+			var getChars = function(code) {
+				var theChars = code.replace(/[0-9]/g, '')
+				return theChars;
+			}
+		
+			var getNums = function(code) {
+				var thenum = code.replace( /^\D+/g, '');
+				if(thenum.length > 0) {
+					return parseInt(thenum);
+				} else {
+					return 0;
+				}
+			}
+		
+			var customSort = function(sampleA, sampleB){
+				var aCode = getChars(sampleA.code);
+				var bCode = getChars(sampleB.code);
+			
+				var returnValue = null;
+				if(aCode < bCode) {
+					returnValue = -1;
+				} else if(aCode > bCode) {
+					returnValue = 1;
+				} else {
+					var aNum = getNums(sampleA.code);
+					var bNum = getNums(sampleB.code);
+					returnValue = aNum - bNum;
+				}
+				return -1 * returnValue;
+			}
+		
+			var sortedResults = searchResults.sort(customSort);
+		
+			return sortedResults;
+		}
+	
+		this.inspectorContentExtra = function(extraContainerId, sample) {
+			// When requesting information about the sample, we don't need parents and children, so send a copy of the saple without that information.
+			var sampleToSend = $.extend({}, sample);
+			delete sampleToSend.parents;
+			delete sampleToSend.children; 
+		
+			var localReference = this;
+			this.serverFacade.listDataSetsForSample(sampleToSend, true, function(datasets) {
+				for(var i = 0; i < datasets.result.length; i++) {
+					var dataset = datasets.result[i];
+					if(dataset.dataSetTypeCode === "SEQ_FILE") {
+						var listFilesForDataSetWithDataset = function(dataset) {
+							localReference.serverFacade.listFilesForDataSet(dataset.code, "/", true, function(files) {
+								for(var i = 0; i < files.result.length; i++) {
+										var isDirectory = files.result[i].isDirectory;
+										var pathInDataSet = files.result[i].pathInDataSet;
+										if (/\.svg$/.test(pathInDataSet) && !isDirectory) {
+											var downloadUrl = localReference.allDataStores[0].downloadUrl + '/' + dataset.code + "/" + pathInDataSet + "?sessionID=" + localReference.serverFacade.getSession();
+											d3.xml(downloadUrl, "image/svg+xml", 
+												function(xml) {
+													var importedNode = document.importNode(xml.documentElement, true);
+													d3.select(importedNode)
+														.attr("width", 400 - 20)
+														.attr("height", 400 - 20)
+														.attr("viewBox", "200 200 650 650");
+													var inspectorNode = d3.select("#"+extraContainerId).node();
+													if(inspectorNode) { //Sometimes the user hides the sticker very clicky and the node doesn't exist anymore
+														inspectorNode.appendChild(importedNode);
+													}
+												});
+										}
+								}
+							});
+						}
+						listFilesForDataSetWithDataset(dataset);
+					}
+				}
+			}
+			);
+			return "";
+		}
+
+		this.initMenuStructure = function() {
+			for(typeGroupCode in this.typeGroups) {
+				groupOfMenuItems = new GroupOfMenuItems(typeGroupCode,this.typeGroups[typeGroupCode]["DISPLAY_NAME"],[]);
+			
+				for(var i = 0; i < this.typeGroups[typeGroupCode]["LIST"].length; i++) {
+					var sampleType = this.getTypeForTypeCode(this.typeGroups[typeGroupCode]["LIST"][i]);
+				
+					if(sampleType !== null) {
+						var description = sampleType.code;
+						var menuItem = new MenuItem("./images/notebook-icon.png", "showSamplesPage", sampleType.code, description);
+						groupOfMenuItems.menuItems.push(menuItem);
+					}
+				}
+				if(groupOfMenuItems.menuItems.length > 0) {
+					this.menuStructure.push(groupOfMenuItems);
+				}
+			}
+		}
+	}
+});
+
+
+//BODENMILLER LAB PROFILE
+function BodenmillerLabProfile(serverFacade) {
+	this.init(serverFacade);
+}
+
+$.extend(BodenmillerLabProfile.prototype, DefaultProfile.prototype, {
+	init: function(serverFacade){
+		DefaultProfile.prototype.init.call(this, serverFacade);
+		
+		this.ELNExperiments = ["SYSTEM_EXPERIMENT"];
+
+		
+	
+		this.typeGroups = {
+			"ANTIBODIES" : {
+				"TYPE" : "ANTIBODIES",
+				"DISPLAY_NAME" : "Antibodies",
+				"LIST" : ["PROTEIN", "CLONE", "LOT", "CONJUGATED_CLONE"]
+			},
+			"OTHERS" : {
+				"TYPE" : "OTHERS",
+				"DISPLAY_NAME" : "Others",
+				"LIST" : [] 
+			}
+		};
+	
+		//this.typePropertiesForTable = {
+		//	"SYSTEM_EXPERIMENT" : ["NAME", "GOALS", "RESULT_INTERPRETATION"],
+		//}
+	
+		this.colorForInspectors = {
+			"PROTEIN" : "#CCFFCC",
+			"CLONE" : "#E3E3E3",
+			"CONJUGATED_CLONE" : "#ACE8FC",
+			"LOT" : "#CCCC99",
+		};
+	
+		this.storagesConfiguration = {
+			"isEnabled" : true,
+			/*
+			 * Should be the same across all storages, if not correct behaviour is not guaranteed.
+			*/
+			"STORAGE_PROPERTIES": {
+						"NAME_PROPERTY" : "FREEZER_NAME", //Should be a Vocabulary.
+						"ROW_PROPERTY" : "ROW", //Vocabulary on BodenmillerLab, can be (Vocabulary, text and integer).
+						"COLUMN_PROPERTY" : "COLUMN", //Integer on BodenmillerLab, can be (Vocabulary, text and integer).
+						"BOX_PROPERTY" : "BOX_NUMBER" //Should be text.
+			},
+			/*
+			 * Where the storage will be painted.
+			*/
+			"STORAGE_PROPERTY_GROUP" : "Storage information",
+			/*
+			 * Storages map, can hold configurations for several storages.
+			*/
+			"STORAGE_CONFIGS": {
+				"-80DEGREES" : { //Freezer name given by the NAME_PROPERTY
+								"ROW_NUM" : 9, //Number of rows
+								"COLUMN_NUM" : 9, //Number of columns
+								"BOX_NUM" : 999 //Boxes on each rack, used for validation, to avoid validation increase the number to 9999 for example
+							}
+			}
+		};
+	
+		/*
+		 * Used by Sample Form
+		 */
+		this.getSpaceForSampleType = function(type) {
+			if(type === "PROTEIN") {
+				return "BODENMILLER_LAB";
+			} else if(type === "CLONE") {
+				return "BODENMILLER_LAB";
+			} else if(type === "CONJUGATED_CLONE") {
+				return "BODENMILLER_LAB";
+			} else if(type === "LOT") {
+				return "BODENMILLER_LAB";
+			} else {
+				return null;
+			}
+		}
+		
+		this.getExperimentIdentifierForSample = function(type, code, properties) {
+			if(type === "PROTEIN") {
+				return "/BODENMILLER_LAB/ANTIBODIES/ANTIBODIES";
+			} else if(type === "CLONE") {
+				return "/BODENMILLER_LAB/ANTIBODIES/ANTIBODIES";
+			} else if(type === "CONJUGATED_CLONE") {
+				return "/BODENMILLER_LAB/ANTIBODIES/ANTIBODIES";
+			} else if(type === "LOT") {
+				return "/BODENMILLER_LAB/ANTIBODIES/ANTIBODIES";
+			} else {
+				return null;
+			}
+		}
+		
+		this.getHTMLTableFromXML = function(xmlDocument) {
+			var table_head = null;
+			var table_body = "";
+			var dom;
+	
+			if (window.DOMParser) {
+			  parser = new DOMParser();
+			  dom = parser.parseFromString(xmlDocument,"text/xml");
+			} else {// Internet Explorer
+			  dom = new ActiveXObject("Microsoft.XMLDOM");
+			  dom.async = false;
+			  dom.loadXML(xmlDocument); 
+			} 
+	
+			var html = null;
+			var root = dom.childNodes[0];
+			var children = root.childNodes;
+			for(var i = 0; i < children.length; i++) {
+				var child = children[i];
+				if (child.localName != null) {
+					var keys = child.attributes;
+					if (table_head == null) {
+						table_head = "<tr>";
+						var key = null;
+						for (var j = 0; j < keys.length; j++) {
+							key = keys[j];
+							if(key.localName != "permId") {
+								table_head += "<th style='text-align:left; width:" + (100/(keys.length-1)) + "%;'>"+ key.localName + "</th>";
+							}
+						}
+						table_head += "</tr>";
+					}
+					table_body += "<tr>";
+					for (var j = 0; j < keys.length; j++) {
+						key = keys[j];
+						if(key.localName != "permId") {
+							if(key.localName == "date") {
+								table_body += "<td style='text-align:left; width:" + (100/(keys.length-1)) + "%;'>" + key.value + "</td>";
+							} else {
+								table_body += "<td style='text-align:left; width:" + (100/(keys.length-1)) + "%;'>" + key.value + "</td>";
+							}
+						}
+					}
+					table_body += "</tr>";
+				}
+			}
+			html = "<table style='font-family:helvetica; font-size:90%; width: 100%;'><thead>" + table_head + "</thead><tbody>"+ table_body + "</tbody></table>";
+			return html;
+		}
+	
+		this.inspectorContentTransformer = function(sample, propertyCode, propertyContent) {
+		
+			if(propertyContent.indexOf("<root>") != -1) {
+				return {
+					"isSingleColumn" : true,
+					"content" : this.getHTMLTableFromXML(propertyContent)
+				};
+			} else {
+				if(propertyContent === "<root/>\n") { //To clean empty XMLs and don't show them.
+					propertyContent = "";
+				}
+				return {
+					"isSingleColumn" : false,
+					"content" : propertyContent
+				};
+			}
+		}
+	
+		this.searchSorter = function(searchResults) {
+		
+			var getChars = function(code) {
+				var theChars = code.replace(/[0-9]/g, '')
+				return theChars;
+			}
+		
+			var getNums = function(code) {
+				var thenum = code.replace( /^\D+/g, '');
+				if(thenum.length > 0) {
+					return parseInt(thenum);
+				} else {
+					return 0;
+				}
+			}
+		
+			var customSort = function(sampleA, sampleB){
+				var aCode = getChars(sampleA.code);
+				var bCode = getChars(sampleB.code);
+			
+				var returnValue = null;
+				if(aCode < bCode) {
+					returnValue = 1;
+				} else if(aCode > bCode) {
+					returnValue = -1;
+				} else {
+					var aNum = getNums(sampleA.code);
+					var bNum = getNums(sampleB.code);
+					returnValue = bNum - aNum;
+				}
+				return -1 * returnValue;
+			}
+		
+			var sortedResults = searchResults.sort(customSort);
+		
+			return sortedResults;
+		}
+	
+		this.inspectorContentExtra = function(extraContainerId, sample) {
+			// When requesting information about the sample, we don't need parents and children, so send a copy of the saple without that information.
+			var sampleToSend = $.extend({}, sample);
+			delete sampleToSend.parents;
+			delete sampleToSend.children; 
+		
+			var localReference = this;
+			this.serverFacade.listDataSetsForSample(sampleToSend, true, function(datasets) {
+				for(var i = 0; i < datasets.result.length; i++) {
+					var dataset = datasets.result[i];
+					if(dataset.dataSetTypeCode === "SEQ_FILE") {
+						var listFilesForDataSetWithDataset = function(dataset) {
+							localReference.serverFacade.listFilesForDataSet(dataset.code, "/", true, function(files) {
+								for(var i = 0; i < files.result.length; i++) {
+										var isDirectory = files.result[i].isDirectory;
+										var pathInDataSet = files.result[i].pathInDataSet;
+										if (/\.svg$/.test(pathInDataSet) && !isDirectory) {
+											var downloadUrl = localReference.allDataStores[0].downloadUrl + '/' + dataset.code + "/" + pathInDataSet + "?sessionID=" + localReference.serverFacade.getSession();
+											d3.xml(downloadUrl, "image/svg+xml", 
+												function(xml) {
+													var importedNode = document.importNode(xml.documentElement, true);
+													d3.select(importedNode)
+														.attr("width", 400 - 20)
+														.attr("height", 400 - 20)
+														.attr("viewBox", "200 200 650 650");
+													var inspectorNode = d3.select("#"+extraContainerId).node();
+													if(inspectorNode) { //Sometimes the user hides the sticker very clicky and the node doesn't exist anymore
+														inspectorNode.appendChild(importedNode);
+													}
+												});
+										}
+								}
+							});
+						}
+						listFilesForDataSetWithDataset(dataset);
+					}
+				}
+			}
+			);
+			return "";
+		}
+	}
+});
+
