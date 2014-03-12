84ccf3c14bb36dcafe7086e2ed6aaaf4aaa5ea04
felmer <felmer>
2014-03-12 12:13:52 +0000

    SP-656, SWE-22: svn-update.sh modified to get also scripts in sub-folders
    SVN: 31107

--- openbis_all/source/bash/svn-update.sh
+++ openbis_all/source/bash/svn-update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Author: Tomasz Pylak
-# Updates all scripts available in the current directory to the version found in SVN.
+# Updates all scripts available in the current directory and its subdirectories to the version found in SVN.
 
 SVN=svncisd.ethz.ch/repos/cisd/openbis_all/trunk/source/bash
 cd ~openbis/bin
-wget -nv -r -l1 -A.{sh,txt} http://$SVN/
+wget -nv -r -l2 -A.{sh,txt} http://$SVN/
 mv $SVN/* .
 chmod 700 *.sh
 RM=`echo $SVN | cut -d / -f1`
