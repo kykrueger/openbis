4c24a889bafb5437fa063a44dc1de491ea9217fc
felmer <felmer>
2014-03-26 13:14:35 +0000

    gradle based tag & build: Copy artifacts to file server.
    SVN: 31197

--- openbis_all/source/bash/build/build.sh
+++ openbis_all/source/bash/build/build.sh
@@ -8,10 +8,23 @@ usage()
   exit 1
 }
 
+function move_to_file_server {
+  echo "Moving new openBIS components to file server"
+  
+  OPENBIS_PATH=~openbis/fileserver/sprint_builds/openBIS
+  SPRINT_DIR=$OPENBIS_PATH/$TODAY-$FULL_VER
+  mkdir -p $SPRINT_DIR
+  mv *$FULL_VER*.{zip,gz,jar} $SPRINT_DIR/
+  chmod g+w -R $SPRINT_DIR
+}
+
 if [ $# -ne 2 ]
 then
 	usage
 fi
+
+TODAY=`date "+%Y-%m-%d"`
+FULL_VER=$2
 
 rm -rf tmp
 mkdir -p tmp
@@ -27,3 +40,6 @@ cd tmp/gradle
 cd ../..
 mv tmp/openbis_standard_technologies/targets/gradle/distributions/openBIS-clients-and-APIs*.zip .
 mv tmp/installation/targets/gradle/distributions/openBIS-installation-standard-technologies*.tar.gz .
+
+move_to_file_server
+
