b3e5da5fb735bbdcdfb12f9d0c9fb5805a2987ef
pkupczyk <pkupczyk>
2014-04-03 10:19:17 +0000

    SSDM-12 : Control the destination of the archived file based on file size
    SVN: 31278

--- datastore_server/source/java/ch/systemsx/cisd/openbis/dss/generic/shared/ArchiveFolders.java
+++ datastore_server/source/java/ch/systemsx/cisd/openbis/dss/generic/shared/ArchiveFolders.java
@@ -56,7 +56,7 @@ public abstract class ArchiveFolders
         {
             if (false == StringUtils.isBlank(folderPath))
             {
-                notBlankPaths.add(folderPath);
+                notBlankPaths.add(folderPath.trim());
             }
         }
 
