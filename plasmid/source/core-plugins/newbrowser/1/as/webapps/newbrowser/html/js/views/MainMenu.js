e35c650be62c1a853ef0f85228e72ba9e9bfa252
juanf <juanf>
2014-05-02 14:43:37 +0000

    SSDM-232: ELN UI - Navigation widget from spaces to experiments (unfinished)
    SVN: 31449

--- plasmid/source/core-plugins/newbrowser/1/as/webapps/newbrowser/html/js/views/MainMenu.js
+++ plasmid/source/core-plugins/newbrowser/1/as/webapps/newbrowser/html/js/views/MainMenu.js
@@ -36,15 +36,25 @@ function MainMenu(mainController, containerId, menuStructure, mainMenuContentExt
 	this.repaint = function() {
 		var mainMenuWrapper = $("#"+this.containerId);
 		mainMenuWrapper.empty();
+		
+		//Browser Widget Title/Container
 		mainMenuWrapper.append($("<h1>", {"style" : "clear: both;"}).text("Spaces > Projects > Experiments"));
 		mainMenuWrapper.append($("<div>", { "id" : "browserWidgetContainer", "style" : "clear: both;"}));
+		
+		//Inventory Widget Title/Container
 		mainMenuWrapper.append($("<h1>", {"style" : "clear: both;"}).text("Inventory"));
 		mainMenuWrapper.append($("<div>", { "id" : "inventoryWidgetContainer", "style" : "clear: both;"}));
+		
+		//Extra content form the profile
 		var $mainMenuExtra = $("<div>", { id: "mainMenuExtra", style: "clear:both;" });
 		$mainMenuExtra.append(this.mainMenuContentExtra);
 		mainMenuWrapper.append($mainMenuExtra);
+		
+		//Browser Widget
 		this.browserWidget = new BrowserWidget("browserWidgetContainer", this.mainController, this.mainController.serverFacade);
 		this.browserWidget.init();
+		
+		//Inventory Widget
 		this.inventoryWidget = new InventoryWidget(this.mainController, "inventoryWidgetContainer", this.menuStructure);
 		this.inventoryWidget.init();
 	}
