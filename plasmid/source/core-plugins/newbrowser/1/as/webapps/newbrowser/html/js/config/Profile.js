4824ba8bb33a4c8e5df24992659636106c4b7ef0
barillac <barillac>
2014-05-16 13:00:27 +0000

    changes in TestProfile function
    SVN: 31536

--- plasmid/source/core-plugins/newbrowser/1/as/webapps/newbrowser/html/js/config/Profile.js
+++ plasmid/source/core-plugins/newbrowser/1/as/webapps/newbrowser/html/js/config/Profile.js
@@ -1401,7 +1401,7 @@ $.extend(TestProfile.prototype, DefaultProfile.prototype, {
 		//Put on this list all experiment types, ELN experiments need to have both an experiment type and a sample type with the same CODE.
 		this.ELNExperiments = ["PLASMID_EXPRESSION", "WESTERN_BLOTTING"];
 
-		//Black list, put on this list all types that you donÂ´t want to appear on the menu and the ELN experiments.
+		//Black list, put on this list all types that you don«t want to appear on the menu and the ELN experiments.
 		this.notShowTypes = ["SYSTEM_EXPERIMENT", "PLASMID_EXPRESSION", "WESTERN_BLOTTING"];
 	
 		//Use this with all known types to create groups, if a type is not specified by default will be added to the OTHERS group.
@@ -1429,40 +1429,48 @@ $.extend(TestProfile.prototype, DefaultProfile.prototype, {
 					"SAMPLE_PARENTS_HINT" : [
 					                             	{
 														"LABEL" : "Protocol",
-														"TYPE": "PROTOCOL",
-														"MIN_COUNT" : 1,
-														"ANNOTATION_PROPERTIES" : []
+														"TYPE": "PROTOCOLS",
+														"MIN_COUNT" : 1
 													}
 													,
 													{
 														"LABEL" : "Plasmid",
 														"TYPE": "PLASMIDS",
-														"MIN_COUNT" : 1,
-														"ANNOTATION_PROPERTIES" : []
+														"MIN_COUNT" : 1
 													}
 													,
 													{
 														"LABEL" : "Inhibitor",
 														"TYPE": "INHIBITORS",
-														"MIN_COUNT" : 1,
-														"ANNOTATION_PROPERTIES" : []
+														"MIN_COUNT" : 1
 													}
 													,
 													{
 														"LABEL" : "Cell Line",
 														"TYPE": "CELL_LINE",
-														"MIN_COUNT" : 1,
-														"ANNOTATION_PROPERTIES" : []
+														"MIN_COUNT" : 1
+													}
+												],
+				},
+			
+				"WESTERN_BLOTTING" : {
+					"SAMPLE_PARENTS_HINT" : [
+					                             	{
+														"LABEL" : "Antibodies",
+														"TYPE": "ANTIBODIES",
+														"MIN_COUNT" : 1
 													}
 												],
 				}
+
 		}
 		
 		
-		//The properties you want to appear on the tables, if you donÂ´t specify the list, all of them will appear by default.
+		
+		//The properties you want to appear on the tables, if you don«t specify the list, all of them will appear by default.
 		this.typePropertiesForTable = {};
 		
-		//The colors for the notes, if you donÂ´t specify the color, light yellow will be used by default.
+		//The colors for the notes, if you don«t specify the color, light yellow will be used by default.
 		this.colorForInspectors = {};
 		
 		//The configuration for the visual storages.
