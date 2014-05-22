221762346ca68385416fe15e0541609bcda0e578
pkupczyk <pkupczyk>
2014-05-22 14:41:34 +0000

    SSDM-359 : JS Movie Player - detect openBIS context path in RequireJS config.js
    SVN: 31573

--- openbis/source/java/ch/systemsx/cisd/openbis/public/resources/config.js
+++ openbis/source/java/ch/systemsx/cisd/openbis/public/resources/config.js
@@ -1,20 +1,31 @@
-var require = {
-	baseUrl : "/openbis/resources",
-	paths : {
-		"jquery" : "js/jquery",
-		"openbis" : "js/openbis",
-		"openbis-screening" : "js/openbis-screening",
-		"bootstrap" : "lib/bootstrap/js/bootstrap.min",
-		"bootstrap-slider" : "lib/bootstrap-slider/js/bootstrap-slider.min"
-	},
-	shim : {
-		"openbis" : {
-			deps : [ "jquery" ],
-			exports : "openbis"
+var require = (function() {
+
+	var getBaseUrl = function() {
+		var path = window.location.pathname;
+		var index = path.substr(1).indexOf("/") + 1;
+		var baseUrl = path.substring(0, index) + "/resources";
+		return baseUrl;
+	}
+
+	return {
+		baseUrl : getBaseUrl(),
+		paths : {
+			"jquery" : "js/jquery",
+			"openbis" : "js/openbis",
+			"openbis-screening" : "js/openbis-screening",
+			"bootstrap" : "lib/bootstrap/js/bootstrap.min",
+			"bootstrap-slider" : "lib/bootstrap-slider/js/bootstrap-slider.min"
 		},
-		"openbis-screening" : {
-			deps : [ "openbis" ],
-			exports : "openbis"
+		shim : {
+			"openbis" : {
+				deps : [ "jquery" ],
+				exports : "openbis"
+			},
+			"openbis-screening" : {
+				deps : [ "openbis" ],
+				exports : "openbis"
+			}
 		}
 	}
-}
+
+})();
