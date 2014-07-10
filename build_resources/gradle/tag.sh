a4e745c6aa43e6f889b9e294ba7cf7f92bdba9d9
felmer <felmer>
2014-07-10 08:03:20 +0000

    SSDM-291 gradle/tag.sh for tagging any project as a sprint tag finished.
    SVN: 31986

--- build_resources/gradle/tag.sh
+++ build_resources/gradle/tag.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then
+  echo
+  echo "Usage: $0 <project name> <branch> [<version>[.<patch number>]]"
+  echo
+  echo "Creates a tagged version of the specified project."
+  echo "If necessary a branch is created first. It will contain the project itself, all its sub-projects"
+  echo "as defined in file 'settings.gradle' of the project as well as 'building_resources' and 'gradle'."
+  echo
+  echo "Currently only the sprint branch is supported."
+  echo "If <patch number> is undefined the next available patch number is used."
+  echo "If <version> is undefined the next available version number is used."
+  echo
+  echo "NOTE: Sprint versions have to start with an 'S'."
+  echo
+  echo "Examples:"
+  echo "$0 datamover sprint"
+  echo "$0 datamover sprint S184"
+  echo "$0 datamover sprint S184.2"
+  exit 1
+fi
+
 PROJECT=$1
 BRANCH=$2
 VERSION_TAG=$3
@@ -10,10 +31,14 @@ if [ ${BASE#/} == ${BASE} ]; then
 fi
 source $BASE/utilities.sh
 
+assert_valid_project $PROJECT
 if [[ "$BRANCH" == "sprint" ]]; then
-  version=$(get_sprint_version $PROJECT $VERSION_TAG)
-  echo "sprint version:$version"
+  assert_valid_sprint_version_tag $VERSION_TAG
+  version=$(get_sprint_version $PROJECT ${BASH_REMATCH[2]})
+  patch=$(get_sprint_patch $PROJECT $version ${BASH_REMATCH[4]})
+  echo "sprint version number:$version, patch number:$patch"
   create_sprint_branch_if_necessary $PROJECT $version
+  copy_sprint_branch_to_tag $PROJECT $version $patch
 else
   echo "Branch '$BRANCH' unknown or not supported."
   exit 1
--- build_resources/gradle/utilities.sh
+++ build_resources/gradle/utilities.sh
@@ -5,17 +5,50 @@ REPOSITORY_URL="svn+ssh://svncisd.ethz.ch/repos/cisd"
 SPRINT_VERSION_REGEX="^(S([0-9]+)(\.([0-9]+))?)?$"
 
 #
-# Extracts the sprint version from specified version/tag. If undefined the sprint version of the specified
-# project is read from the repository. This version number increased by one will be returned. 
+# Asserts specified project exists in the repository. Otherwise the script is terminated.
+#
+assert_valid_project()
+{
+  local project=$1
+  
+  if [  $(_path_exists $project) == FALSE ]; then
+    _error "Unknown project: $project"
+    exit 1
+  fi
+}
+
+#
+# Asserts specified sprint version/tag is valid. Otherwise the script is terminated.
+#
+# As a side effect BASH_REMATCH[2] and BASH_REMATCH[4] are set with version and patch number, respectively.
+#
+assert_valid_sprint_version_tag()
+{
+  local version_tag=$1
+  
+  if [[ ! $version_tag =~ $SPRINT_VERSION_REGEX ]]; then
+    _error "Sprint version/tag is not of form S<version number>[.<patch number>]: $version_tag"
+    _error "Valid examples: S23.12, S108, S156.8"
+    exit 1
+  fi
+}
+
+#
+# Returns specified version number if it is not an empty string.
+# If undefined the latest sprint version of the specified project is read from the repository.
+# This version number increased by one will be returned.
 #
 # This function should be used as follows:
-# sprint_version=$(get_sprint_version $project $version_tag)
+# sprint_version=$(get_sprint_version $project $version_number)
+#
+# Note: assert_valid_sprint_version_tag has to be invoked before invoking this function.
 #
 get_sprint_version()
 {
-  project=$1
-  version_tag=$2
-  if [ "$version_tag" == "" ]; then
+  local project=$1
+  local version_number=$2
+
+  if [ "$version_number" == "" ]; then
     latest=`_list_branches $project sprint|awk -F. '{print substr($1,2)}'|sort -nr|head -1`
     if [ "$latest" == "" ]; then
       echo 1
@@ -23,31 +56,40 @@ get_sprint_version()
       echo $(($latest + 1))
     fi
   else
-    if [[ $version_tag =~ $SPRINT_VERSION_REGEX ]]; then
-      echo "${BASH_REMATCH[2]}"
-    else
-      echo "Sprint version not of form S<version number>[.<patch number>]: $version_tag"
-      echo "Examples of valid sprint versions: S108, S156.8"
-      exit 1
-    fi
+    echo "$version_number"
   fi
 }
 
 #
-# Returns TRUE if the specified sprint branch for the specified project already exists.
+# Returns specified patch number if it is not an empty string. 
+# If undefined the latest tagged sprint version of the specified project is read from the repository.
+# This patch number increased by one will be returned. 
 #
 # This function should be used as follows:
+# sprint_version=$(get_sprint_version $project $version_number $patch_number)
 #
-# if [ $(sprint_branch_exists $project $sprint_version) == FALSE ]; then echo create branch; fi
+# Note: assert_valid_sprint_version_tag has to be invoked before invoking this function.
 #
-sprint_branch_exists()
+get_sprint_patch()
 {
-  project=$1
-  sprint_version=$2
-  if [[ `_list_branches $project sprint` =~ S$sprint_version.x ]]; then
-    echo TRUE
+  local project=$1
+  local version_number=$2
+  local patch_number=$3
+
+  if [ "$patch_number" == "" ]; then
+    path=$project/tags/sprint/S$version_number.x
+    if [ $(_path_exists $path) == TRUE ]; then
+      latest=`_list_folder $path|cut -d/ -f1|cut -d. -f2|sort -nr|head -1`
+      if [ "$latest" == "" ]; then
+        echo 0
+      else
+        echo $(($latest + 1))
+      fi
+    else
+      echo 0
+    fi
   else
-    echo FALSE
+    echo $patch_number
   fi
 }
 
@@ -58,27 +100,72 @@ sprint_branch_exists()
 #
 create_sprint_branch_if_necessary()
 {
-  project=$1
-  sprint_version=$2
-  dir_name=S$sprint_version.x
-  if [[ ! `_list_branches $project sprint` =~ $dir_name ]]; then
+  local project=$1
+  local sprint_version=$2
+  
+  local dir_name=S$sprint_version.x
+  local path="$project/branches/sprint/$dir_name"
+  if [ $(_path_exists $path) == FALSE ]; then
     _start_batch
-    _batch mkdir "$REPOSITORY_URL/$project/branches/sprint/$dir_name"
-    projects="$(_list_projects "$project/trunk") $project gradle build_resources"
+    _mkdir "$path"
+    local projects="$(_list_projects "$project/trunk") $project gradle build_resources"
     for p in $projects; do
-      _batch cp "$REPOSITORY_URL/$p/trunk" "$REPOSITORY_URL/$project/branches/sprint/$dir_name/$p"
+      _batch cp "$REPOSITORY_URL/$p/trunk" "$REPOSITORY_URL/$path/$p"
     done
-    log_message="Create sprint branch '$dir_name' for project $project"
+    local log_message="Create sprint branch '$dir_name' for project $project"
     echo $log_message
     _submit_batch "$log_message"
   fi
 }
+
+#
+# Tags the specified sprint branch of specified project. 
+# Creates <project>/tags/sprint/S<sprint_version>.x and copies to this folder
+# the branch <project>/branch/sprint/S<sprint_version>.x as tag <sprint_version>.<patch number>
+#
+copy_sprint_branch_to_tag()
+{
+  local project=$1
+  local sprint_version=$2
+  local patch_number=$3
+  
+  local dir_name=S$sprint_version.x
+  local tag_name=S$sprint_version.$patch_number
+  local path="$project/tags/sprint/$dir_name"
+  if [ $(_path_exists "$path/$tag_name") == TRUE ]; then
+    _error "There exists already a sprint tag '$tag_name' for project $project."
+    _error "You can use the sprint version 'S$sprint_version' without specifying the patch number."
+    _error "The tagging script will find out the next available patch number."
+    exit 1
+  fi
+  _start_batch
+  _mkdir "$path"
+  _batch cp "$REPOSITORY_URL/$project/branches/sprint/$dir_name" "$REPOSITORY_URL/$path/$tag_name"
+  local log_message="Create sprint tag '$tag_name' for project $project"
+  echo $log_message
+  _submit_batch "$log_message"
+}
+
 
 ################################### private functions (only used in this script) ################
 
 _start_batch()
 {
   tag_batch_command_count=0
+}
+
+_mkdir()
+{
+  local path=$1
+  
+  local paths=()
+  while [ $(_path_exists $path) == FALSE ]; do
+    paths[${#paths[*]}]=$path
+    local path=${path%/*}
+  done
+  for ((i=$((${#paths[*]} - 1)); i >= 0; i--)); do
+    _batch mkdir "$REPOSITORY_URL/${paths[$i]}"
+  done
 }
 
 _batch()
@@ -89,7 +176,8 @@ _batch()
 
 _submit_batch()
 {
-  log_message=$1
+  local log_message=$1
+  
   if [ $SVN_MUCC_AVAILABLE == TRUE ]; then
     tag_batch_command_mucc=(--message "$log_message")
     for ((i=0; i < $tag_batch_command_count; i++)); do
@@ -106,8 +194,9 @@ _submit_batch()
 
 _add_mucc_command_item()
 {
-  cmd=$1
+  local cmd=$1
   shift
+  
   tag_batch_command_mucc[${#tag_batch_command_mucc[*]}]=$cmd
   if [[ $cmd == "cp" ]]; then
     tag_batch_command_mucc[${#tag_batch_command_mucc[*]}]="HEAD"
@@ -119,24 +208,48 @@ _add_mucc_command_item()
 
 _list_branches() 
 {
-  project=$1
-  branch_type=$2
-  "$SVN_CMD" list --non-interactive "$REPOSITORY_URL/$project/branches/$branch_type"
+  local project=$1
+  local branch_type=$2
+  
+  local path="$project/branches/$branch_type"
+  if [ $(_path_exists $path) == TRUE ]; then
+    _list_folder "$path"
+  fi
+}
+
+_path_exists()
+{
+  local path=$1
+  
+  _list_folder $path > /dev/null 2>&1
+  if [ $? == 0 ]; then
+    echo TRUE
+  else
+    echo FALSE
+  fi
+}
+
+_list_folder()
+{
+  local path=$1
+  
+  "$SVN_CMD" list --non-interactive "$REPOSITORY_URL/$path"
 }
 
 _list_projects()
 {
-  path=$1
+  local path=$1
+  
   settings_gradle=`"$SVN_CMD" cat --non-interactive "$REPOSITORY_URL/$path/settings.gradle"`
   echo $settings_gradle|awk '/includeFlat/ {line=substr($0,12); while(match(line,",[\t ]*$")) {getline;line=(line $0)}; gsub(",", " ", line); gsub("'\''", "", line); print line}'
 }
 
 _svn_with_logmessage()
 {
-  log_message=$1
-  shift
-  cmd=$1
-  shift
+  local log_message=$1
+  local cmd=$2
+  shift 2
+  
   _svn_with_echo $cmd --message "$log_message" "$@"
 }
 
@@ -158,24 +271,31 @@ _assert_repository()
 {
   "$SVN_CMD" -q --non-interactive --version > /dev/null
   if [ $? != 0 ]; then
-    echo "Subversion client client '$SVN_CMD' not available"
-    echo "Please make Subversion command line client available or set variable \$SVN_CMD in script 'utilities.sh' correctly."
+    _error "Subversion client client '$SVN_CMD' not available"
+    _error "Please make Subversion command line client available or set variable SVN_CMD in script '$BASE/utilities.sh' correctly."
     exit 1
   fi
   "$SVN_CMD" info --non-interactive "$REPOSITORY_URL" > /dev/null
   if [ $? != 0 ]; then
-    echo "Subversion repository '$REPOSITORY_URL' not available"
-    echo "Please check that the variable \$REPOSITORY_URL in script 'utilities.sh' is set correctly."
+    _error "Subversion repository '$REPOSITORY_URL' not available"
+    _error "Please check that the variable REPOSITORY_URL in script '$BASE/utilities.sh' is set correctly."
     exit 1
   fi
-  "$SVN_MUCC" --version > /dev/null 2>&1
+  "$SVN_MUCC" --help > /dev/null 2>&1
   if [ $? == 0 ]; then
     SVN_MUCC_AVAILABLE=TRUE
-    echo "Command '$SVN_MUCC' available"
+    echo "Command '$SVN_MUCC' available."
   else
     SVN_MUCC_AVAILABLE=FALSE
-    echo "Command '$SVN_MUCC' not available"
+    echo "Command '$SVN_MUCC' not available."
+    echo -n "The command 'svnmucc' allows to create and copy several folders in the repository "
+    echo "in one transaction leading to only one new revision."
   fi
+}
+
+_error()
+{
+  echo "ERROR> $@" >&2
 }
 
 _assert_repository
