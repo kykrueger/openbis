7b90f5f7ae254ebadf93da0b5a77fe4c98005a6a
pkupczyk <pkupczyk>
2014-07-14 13:07:07 +0000

    SSDM-133 : Remove .classpath, .project, build/ from gradle-built projects - convert more projects
    SVN: 32035

--- screening/source/core-plugins/screening/4/dss/drop-boxes/simple-dropbox/simple-dropbox.py
+++ screening/source/core-plugins/screening/4/dss/drop-boxes/simple-dropbox/simple-dropbox.py
@@ -1,28 +1,13 @@
 #! /usr/bin/env python
 def process(transaction):
-    expid = "/DEFAULT/DEFAULT/DEFAULT"
+    expid = "/TEST/TEST-PROJECT/DEMO-EXP-SIMPLE"
     exp = transaction.getExperiment(expid)
      
     if None == exp:
         exp = transaction.createNewExperiment(expid, "TEST-EXPERIMENT")
    
-    container = transaction.getDataSetForUpdate("20140627153354751-4102")
-
-    contained = transaction.createNewDataSet()
-    contained.setDataSetType("UNKNOWN")
-    contained.setExperiment(exp)
-
-    contained2 = transaction.createNewDataSet()
-    contained2.setDataSetType("UNKNOWN")
-    contained2.setExperiment(exp)
-
-    containedList = list(container.getContainedDataSetCodes())
-    containedList = [ contained.getDataSetCode() ] + containedList + [ contained2.getDataSetCode() ]
-
-    container.setContainedDataSetCodes(containedList)
-
-    print 'Contained data sets: ' + str(containedList)
-
-    transaction.createNewFile(contained, "contained")
-    transaction.createNewFile(contained2, "contained2")
-
+    dataSet = transaction.createNewDataSet()
+          
+    dataSet.setDataSetType("UNKNOWN")
+    dataSet.setExperiment(exp)
+    transaction.moveFile(transaction.getIncoming().getAbsolutePath(), dataSet)
