bc1a702b429bf54bc1ec70b84c9a885d72a980a7
pkupczyk <pkupczyk>
2014-07-14 12:53:27 +0000

    SSDM-133 : Remove .classpath, .project, build/ from gradle-built projects - convert more projects
    SVN: 32034

--- screening/source/core-plugins/screening/4/dss/drop-boxes/simple-dropbox/simple-dropbox.py
+++ screening/source/core-plugins/screening/4/dss/drop-boxes/simple-dropbox/simple-dropbox.py
@@ -1,13 +1,28 @@
 #! /usr/bin/env python
 def process(transaction):
-    expid = "/TEST/TEST-PROJECT/DEMO-EXP-SIMPLE"
+    expid = "/DEFAULT/DEFAULT/DEFAULT"
     exp = transaction.getExperiment(expid)
      
     if None == exp:
         exp = transaction.createNewExperiment(expid, "TEST-EXPERIMENT")
    
-    dataSet = transaction.createNewDataSet()
-          
-    dataSet.setDataSetType("UNKNOWN")
-    dataSet.setExperiment(exp)
-    transaction.moveFile(transaction.getIncoming().getAbsolutePath(), dataSet)
+    container = transaction.getDataSetForUpdate("20140627153354751-4102")
+
+    contained = transaction.createNewDataSet()
+    contained.setDataSetType("UNKNOWN")
+    contained.setExperiment(exp)
+
+    contained2 = transaction.createNewDataSet()
+    contained2.setDataSetType("UNKNOWN")
+    contained2.setExperiment(exp)
+
+    containedList = list(container.getContainedDataSetCodes())
+    containedList = [ contained.getDataSetCode() ] + containedList + [ contained2.getDataSetCode() ]
+
+    container.setContainedDataSetCodes(containedList)
+
+    print 'Contained data sets: ' + str(containedList)
+
+    transaction.createNewFile(contained, "contained")
+    transaction.createNewFile(contained2, "contained2")
+
