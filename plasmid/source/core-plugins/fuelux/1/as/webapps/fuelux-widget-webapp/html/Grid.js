997128e549a572f7c40237870a7f0c5aba15e03f
pkupczyk <pkupczyk>
2014-10-01 06:32:16 +0000

    SSDM-970 : ELN UI - PAPER - Generic grid - refactoring
    SVN: 32532

--- plasmid/source/core-plugins/fuelux/1/as/webapps/fuelux-widget-webapp/html/Grid.js
+++ plasmid/source/core-plugins/fuelux/1/as/webapps/fuelux-widget-webapp/html/Grid.js
@@ -15,22 +15,7 @@ $.extend(Grid.prototype, {
 
 		$.get("Grid.html", function(template) {
 			thisGrid.panel.html(template);
-
-			var columnList = thisGrid.panel.find(".columnDropdown").find("ul");
-			columnList.click(function(e) {
-				e.stopPropagation();
-			});
-
-			thisGrid.columns.forEach(function(column, columnIndex) {
-				var checkbox = $("<input>").attr("type", "checkbox").attr("value", column.property).attr("checked", "checked");
-				checkbox.change(function() {
-					thisGrid.panel.repeater('render');
-				});
-				var label = $("<label>").attr("role", "menuitem").addClass("checkbox").text(column.label).append(checkbox);
-				var item = $("<li>").attr("role", "presentation").append(label);
-				columnList.append(item);
-			});
-
+			thisGrid.renderColumnDropdown();
 			thisGrid.panel.repeater({
 				defaultView : "list",
 				dataSource : function(options, callback) {
@@ -39,6 +24,7 @@ $.extend(Grid.prototype, {
 					}
 				},
 				list_selectable : false,
+				list_noItemsHTML : 'No items found',
 				list_rowRendered : function(helpers, callback) {
 					$(helpers.item).click(function() {
 						var rowIndex = helpers.item[0].rowIndex;
@@ -50,121 +36,166 @@ $.extend(Grid.prototype, {
 					});
 
 					callback();
-				},
-				list_noItemsHTML : 'No items found'
+				}
 			});
 		});
 
 		return thisGrid.panel;
 	},
 
+	renderColumnDropdown : function() {
+		var thisGrid = this;
+
+		var columnList = thisGrid.panel.find(".columnDropdown").find("ul");
+		columnList.click(function(e) {
+			e.stopPropagation();
+		});
+
+		thisGrid.columns.forEach(function(column, columnIndex) {
+			var checkbox = $("<input>").attr("type", "checkbox").attr("value", column.property).attr("checked", "checked");
+			checkbox.change(function() {
+				thisGrid.panel.repeater('render');
+			});
+			var label = $("<label>").attr("role", "menuitem").addClass("checkbox").text(column.label).append(checkbox);
+			var item = $("<li>").attr("role", "presentation").append(label);
+			columnList.append(item);
+		});
+	},
+
+	getAllColumns : function() {
+		return this.columns;
+	},
+
+	getVisibleColumns : function() {
+		var thisGrid = this;
+		var columns = [];
+
+		thisGrid.panel.find(".columnDropdown").find("input:checked").each(function(index, element) {
+			thisGrid.getAllColumns().forEach(function(column) {
+				var checkbox = $(element);
+				if (column.property == checkbox.val()) {
+					columns.push(column);
+				}
+			});
+		});
+
+		// add a dummy empty column (repeater does not properly handle visibility of the last column)
+		columns.push({
+			label : null,
+			property : null,
+			sortable : false
+		});
+
+		return columns;
+	},
+
+	filterData : function(dataList, filter) {
+		var thisGrid = this;
+
+		if (filter) {
+			filter = filter.toLowerCase();
+			dataList = dataList.filter(function(data) {
+				return thisGrid.columns.some(function(column) {
+					if (column.filter) {
+						return column.filter(data, filter);
+					} else {
+						var value = "" + data[column.property];
+						return value != null && value.toLowerCase().indexOf(filter) != -1;
+					}
+				});
+			});
+		}
+
+		return dataList;
+	},
+
+	sortData : function(dataList, sortProperty, sortDirection) {
+		var thisGrid = this;
+
+		if (sortProperty && sortDirection) {
+			var sortColumn = null;
+			thisGrid.columns.forEach(function(column) {
+				if (column.property == sortProperty) {
+					sortColumn = column;
+				}
+			});
+			if (sortColumn) {
+				var sortFunction = null;
+				var sortDirection = sortDirection == "asc" ? 1 : -1;
+
+				if (sortColumn.sort) {
+					sortFunction = sortColumn.sort;
+				} else {
+					sortFunction = function(data1, data2) {
+						var value1 = data1[sortColumn.property];
+						var value2 = data2[sortColumn.property];
+						return naturalSort(value1, value2);
+					};
+				}
+
+				dataList.sort(function(data1, data2) {
+					return sortDirection * sortFunction(data1, data2);
+				});
+			}
+		}
+
+		return dataList;
+	},
+
+	renderData : function(dataList) {
+		var thisGrid = this;
+		var items = [];
+
+		dataList.forEach(function(data) {
+			var item = {};
+			thisGrid.getVisibleColumns().forEach(function(column) {
+				var value = null;
+				if (column.render) {
+					value = column.render(data);
+				} else {
+					value = data[column.property];
+				}
+				item[column.property] = value;
+			});
+			items.push(item);
+		});
+
+		return items;
+	},
+
 	list : function(options, callback) {
 		var thisGrid = this;
 
 		thisGrid.getDataList(function(dataList) {
-			var result = {
-				"columns" : []
-			};
 
-			// columns
-			thisGrid.panel.find(".columnDropdown").find("input:checked").each(function(index, element) {
-				thisGrid.columns.forEach(function(column) {
-					var checkbox = $(element);
-					if (column.property == checkbox.val()) {
-						result.columns.push(column);
-					}
-				});
-			});
+			dataList = thisGrid.filterData(dataList, options.search);
+			dataList = thisGrid.sortData(dataList, options.sortProperty, options.sortDirection);
 
-			// add a dummy empty column (repeater does not properly handle
-			// visibility of the last column)
-			result.columns.push({
-				label : null,
-				property : null,
-				sortable : false
-			});
-
-			// filter
-			if (options.search) {
-				var filter = options.search.toLowerCase();
-				dataList = dataList.filter(function(data) {
-					return thisGrid.columns.some(function(column) {
-						if (column.filter) {
-							return column.filter(data, filter);
-						} else {
-							var value = "" + data[column.property];
-							return value != null && value.toLowerCase().indexOf(filter) != -1;
-						}
-					});
-				});
-			}
-
-			// sort
-			if (options.sortDirection && options.sortProperty) {
-				var sortColumn = null;
-				thisGrid.columns.forEach(function(column) {
-					if (column.property == options.sortProperty) {
-						sortColumn = column;
-					}
-				});
-				if (sortColumn) {
-					var sortFunction = null;
-					var sortDirection = options.sortDirection == "asc" ? 1 : -1;
-
-					if (sortColumn.sort) {
-						sortFunction = sortColumn.sort;
-					} else {
-						sortFunction = function(data1, data2) {
-							var value1 = data1[sortColumn.property];
-							var value2 = data2[sortColumn.property];
-							return naturalSort(value1, value2);
-						};
-					}
-
-					dataList.sort(function(data1, data2) {
-						return sortDirection * sortFunction(data1, data2);
-					});
-				}
-			}
-
-			// page
-			result.count = dataList.length;
-			result.datas = [];
-			result.items = [];
-
+			var result = {};
 			var defaultPageSize = 50;
 			var startIndex = options.pageIndex * (options.pageSize || defaultPageSize);
 			var endIndex = startIndex + (options.pageSize || defaultPageSize);
-			endIndex = (endIndex <= result.count) ? endIndex : result.count;
+			endIndex = (endIndex <= result.count) ? endIndex : dataList.length;
 
+			result.count = dataList.length;
+			result.datas = [];
+			result.items = [];
+			result.columns = thisGrid.getVisibleColumns();
 			result.page = options.pageIndex;
 			result.pages = Math.ceil(result.count / (options.pageSize || defaultPageSize));
 			result.start = startIndex + 1;
 			result.end = endIndex;
 
-			// render
-			var items = [];
-			for (var i = startIndex; i < endIndex; i++) {
-				var data = dataList[i];
-				var item = {};
-				thisGrid.columns.forEach(function(column) {
-					var value = null;
-					if (column.render) {
-						value = column.render(data);
-					} else {
-						value = data[column.property];
-					}
-					item[column.property] = value;
-				});
-				result.datas.push(data);
+			dataList = dataList.slice(startIndex, endIndex);
+			itemList = thisGrid.renderData(dataList);
+			itemList.forEach(function(item, index) {
+				result.datas.push(dataList[index]);
 				result.items.push(item);
-			}
+			});
 
-			thisGrid.result = result;
-
-			// add some delay (repeater does not properly layout columns without
-			// it)
+			// add some delay (repeater does not properly layout columns without it)
 			setTimeout(function() {
+				thisGrid.result = result;
 				callback(result);
 			}, 1);
 		});
