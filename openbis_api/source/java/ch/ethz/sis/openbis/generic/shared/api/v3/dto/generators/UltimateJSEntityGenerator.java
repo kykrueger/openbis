c70d925aca37a24568ab4e30b78c0058ca3f24a8
juanf <juanf>
2014-11-26 10:02:39 +0000

    SSDM-1211 : V3 API - JS facade (Ultimate JS Generator Ongoing work)
    SVN: 32925

--- openbis_api/source/java/ch/ethz/sis/openbis/generic/shared/api/v3/dto/generators/UltimateJSEntityGenerator.java
+++ openbis_api/source/java/ch/ethz/sis/openbis/generic/shared/api/v3/dto/generators/UltimateJSEntityGenerator.java
@@ -6,36 +6,55 @@ import java.io.IOException;
 
 public class UltimateJSEntityGenerator
 {
+    private static final String API_PROJECT_SOURCE_FOLDER = "/Users/juanf/Documents/workspace/openbis_api/source/java/";
+    private static final String[] CLASSES_TO_CONVERT = new String[]{
+        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.attachment.Attachment",
+        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.attachment.AttachmentFetchOptions",
+        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.dataset.DataSet",
+        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.dataset.DataSetFetchOptions",
+        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.experiment.Experiment",
+        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.experiment.ExperimentFetchOptions",
+        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.experiment.ExperimentType",
+        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.experiment.ExperimentTypeFetchOptions",
+        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.person.Person",
+        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.person.Person"
+    };
+    
+    private static final String JSTEST_PROJECT_SOURCE_FOLDER = "/Users/juanf/Documents/workspace/js-test/servers/common/core-plugins/tests/1/as/webapps/openbis-v3-api-test/html/dto/";
+    
     public static void main(String[] args) throws IOException {
-        String toTranslate = readFileAsString("/Users/juanf/Documents/workspace/openbis_api/source/java/ch/ethz/sis/openbis/generic/shared/api/v3/dto/entity/attachment/Attachment.java");
-
-        toTranslate = toTranslate
-            //Remove Java specific features
-            .replaceAll("package.+;", "")
-            .replaceAll("import.+;", "")
-            .replaceAll("@JsonObject.+\\)", "")
-            .replaceAll("@JsonProperty", "")
-            .replaceAll("@JsonIgnore", "")
-            .replaceAll("@Override", "")
-            .replaceAll("private static final long serialVersionUID.+;", "")
-            //Translate private parameters to var
-            .replaceAll("private .+ ", "var _")
-            .replaceAll("this.", "_")
-            //Translate Class to function
-            .replaceAll("public class ([\\w\\[\\]]+).*", "function $1()")
-            //Translate methods
-            .replaceAll("public [\\w\\[\\]]+ ", "function ")
-            //Remove function parameter types
-            .replaceAll("\\([\\w\\[\\]]+ ", "(")
-            //Translate Exceptions
-            .replaceAll("throw new .+\\(\"(.+)\"\\);", "throw '$1';")
-            //Remove Comments
-            .replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)", "");
-        
-        System.out.println(toTranslate);
+        for (String classToConvert:CLASSES_TO_CONVERT) {
+            String jsClass = translateFromJavaToJS(readFileAsString(API_PROJECT_SOURCE_FOLDER + classToConvert.replace('.', '/') + ".java"));
+            System.out.println(jsClass);
+        }
     }
     
-    private static String readFileAsString(String filePath) throws IOException {
+    private static final String translateFromJavaToJS(String toTranslate) {
+        return toTranslate
+                //Remove Java specific features
+                .replaceAll("package.+;", "")
+                .replaceAll("import.+;", "")
+                .replaceAll("@JsonObject.+\\)", "")
+                .replaceAll("@JsonProperty", "")
+                .replaceAll("@JsonIgnore", "")
+                .replaceAll("@Override", "")
+                .replaceAll("private static final long serialVersionUID.+;", "")
+                //Translate private parameters to var
+                .replaceAll("private .+ ", "var _")
+                .replaceAll("this.", "_")
+                //Translate Class to function
+                .replaceAll("public class ([\\w\\[\\]]+).*", "function $1()")
+                //Translate methods
+                .replaceAll("public [\\w\\[\\]]+ ", "function ")
+                //Remove function parameter types
+                .replaceAll("\\([\\w\\[\\]]+ ", "(")
+                //Translate Exceptions
+                .replaceAll("throw new .+\\(\"(.+)\"\\);", "throw '$1';")
+                //Remove Comments
+                .replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)", "");
+    }
+    
+    private static final String readFileAsString(String filePath) throws IOException {
         StringBuffer fileData = new StringBuffer();
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
         char[] buf = new char[1024];
