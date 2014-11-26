48c99463e4e94dd461d36cdc82c848f47aeb2181
fedoreno <fedoreno>
2014-11-26 14:22:51 +0000

    SSDM-1211 : V3 API - JS facade (Ultimate JS Generator Ongoing work: last version)
    SVN: 32933

--- openbis_api/source/java/ch/ethz/sis/openbis/generic/shared/api/v3/dto/generators/UltimateJSEntityGenerator.java
+++ openbis_api/source/java/ch/ethz/sis/openbis/generic/shared/api/v3/dto/generators/UltimateJSEntityGenerator.java
@@ -16,46 +16,48 @@ import ch.ethz.sis.openbis.generic.shared.api.v3.dto.generators.uglify.UglifyOpt
 
 public class UltimateJSEntityGenerator
 {
-    private static final String API_PROJECT_SOURCE_FOLDER = "/Users/juanf/Documents/workspace/openbis_api/source/java/";
-//    private static final String API_PROJECT_SOURCE_FOLDER = "/Users/fedoreno/projects/work/openbis/openbis_api/source/java/";
-//    private static final String JSTEST_PROJECT_SOURCE_FOLDER = "/Users/juanf/Documents/workspace/js-test/servers/common/core-plugins/tests/1/as/webapps/openbis-v3-api-test/html/dto/";
-    
-    private static final String[] CLASSES_TO_CONVERT = new String[]{
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.attachment.Attachment",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.attachment.AttachmentFetchOptions",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.dataset.DataSet",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.dataset.DataSetFetchOptions",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.experiment.Experiment",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.experiment.ExperimentFetchOptions",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.experiment.ExperimentType",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.experiment.ExperimentTypeFetchOptions",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.person.Person",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.person.PersonFetchOptions",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.project.Project",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.project.ProjectFetchOptions",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.property.PropertyFetchOptions",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.sample.Sample",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.sample.SampleFetchOptions",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.sample.SampleType",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.sample.SampleTypeFetchOptions",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.space.Space",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.space.SpaceFetchOptions",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.tag.Tag",
-        "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.tag.TagFetchOptions"
+    private static final String API_PROJECT_ROOT_FOLDER = "/Users/fedoreno/projects/work/openbis";
+
+    private static final String API_PROJECT_SOURCE_FOLDER = API_PROJECT_ROOT_FOLDER + "/openbis_api/source/java/";
+
+    private static final String JSTEST_PROJECT_SOURCE_FOLDER = API_PROJECT_ROOT_FOLDER
+            + "/js-test/servers/common/core-plugins/tests/1/as/webapps/openbis-v3-api-test/html/dto/";
+
+    private static final String[] CLASSES_TO_CONVERT = new String[] {
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.attachment.Attachment",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.attachment.AttachmentFetchOptions",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.dataset.DataSet",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.dataset.DataSetFetchOptions",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.experiment.Experiment",
+            "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.experiment.ExperimentUpdate",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.experiment.ExperimentFetchOptions",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.experiment.ExperimentType",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.experiment.ExperimentTypeFetchOptions",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.person.Person",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.person.PersonFetchOptions",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.project.Project",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.project.ProjectFetchOptions",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.property.PropertyFetchOptions",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.sample.Sample",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.sample.SampleFetchOptions",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.sample.SampleType",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.sample.SampleTypeFetchOptions",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.space.Space",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.space.SpaceFetchOptions",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.tag.Tag",
+            // "ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.tag.TagFetchOptions"
     };
-    
-    public static void main(String[] args) throws IOException {
-        for (String classToConvert:CLASSES_TO_CONVERT) {
+
+    public static void main(String[] args) throws IOException
+    {
+        for (String classToConvert : CLASSES_TO_CONVERT)
+        {
             String javaClass = readFileAsString(API_PROJECT_SOURCE_FOLDER + classToConvert.replace('.', '/') + ".java");
             String jsClass = translateFromJavaToJS(javaClass);
-//            whriteStringAsFile(JSTEST_PROJECT_SOURCE_FOLDER + getSimpleName(classToConvert) + ".js", jsClass);
+            // whriteStringAsFile(JSTEST_PROJECT_SOURCE_FOLDER + getSimpleName(classToConvert) + ".js", jsClass);
+            System.out.println(jsClass);
             System.out.println(prettyPrint(jsClass));
         }
-    }
-    
-    private static String getSimpleName(String fullClassName) {
-        int lastIndexOfDot = fullClassName.lastIndexOf('.');
-        return fullClassName.substring(lastIndexOfDot+1);
     }
 
     private static final String translateFromJavaToJS(String toTranslate)
@@ -69,11 +71,18 @@ public class UltimateJSEntityGenerator
                 .replaceAll("@JsonIgnore", "")
                 .replaceAll("@Override", "")
                 .replaceAll("private static final long serialVersionUID.+;", "")
+                // Remove generic parameters
+                .replaceAll("<[\\w, ]+>", "")
+                // And nested generic parameters
+                .replaceAll("<[\\w, ]+>", "")
+                // Remove array declarations
+                .replaceAll("\\[\\]", "")
                 // Translate private parameters to var
-                .replaceAll("private .+ ", "var _")
+                .replaceAll("private\\s+\\w+\\s+", "var _")
                 .replaceAll("this.", "_")
                 // Translate Class to function
-                .replaceAll("public class ([\\w\\[\\]]+).*", "function $1()")
+                .replaceAll("public class ([\\w]+)\\s*(?:implements Serializable)?\\s*[\\n\\s]*\\{",
+                        "function $1()\n{\n\tthis['@type'] = '$1';")
                 // Translate methods
                 .replaceAll("public [\\w\\[\\]]+ ", "function ")
                 // Remove function parameter types
@@ -88,10 +97,12 @@ public class UltimateJSEntityGenerator
                 .replaceAll("==", "===")
                 .replaceAll("!=", "!==")
                 // Remove Comments
-                .replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)", "");
+                .replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)", "")
+        ;
     }
-    
-    private static final void whriteStringAsFile(String filePath,String string) throws IOException {
+
+    private static final void whriteStringAsFile(String filePath, String string) throws IOException
+    {
         Writer out = null;
         try
         {
@@ -102,37 +113,40 @@ public class UltimateJSEntityGenerator
             out.close();
         }
     }
-    
-    private static final String readFileAsString(String filePath) throws IOException {
+
+    private static final String readFileAsString(String filePath) throws IOException
+    {
         StringBuffer fileData = new StringBuffer();
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), "UTF8"));
         char[] buf = new char[1024];
-        int numRead=0;
-        while((numRead=reader.read(buf)) != -1){
+        int numRead = 0;
+        while ((numRead = reader.read(buf)) != -1)
+        {
             String readData = String.valueOf(buf, 0, numRead);
             fileData.append(readData);
         }
         reader.close();
         return fileData.toString();
     }
-    
-    private static String prettyPrint(String jsClass) throws IOException {
-      UglifyOptions options = new UglifyOptions();
-      options.beautify = true;
-      options.noMangle = true;
-      options.noSqueeze = true;
-      options.noSeqs = true;
-      
-      ArrayList<String> optionsArgList = options.toArgList();
-      
-      File temp = File.createTempFile("temp", "");
-      whriteStringAsFile(temp.getAbsolutePath(), jsClass);
-      
-      optionsArgList.add(temp.getAbsolutePath());
-      String[] args = new String[optionsArgList.size()];
-      
-      args = optionsArgList.toArray(args);
-      UglifyJS uglifyjs = new UglifyJS();
-      return uglifyjs.uglify(args);
+
+    private static String prettyPrint(String jsClass) throws IOException
+    {
+        UglifyOptions options = new UglifyOptions();
+        options.beautify = true;
+        options.noMangle = true;
+        options.noSqueeze = true;
+        options.noSeqs = true;
+
+        ArrayList<String> optionsArgList = options.toArgList();
+
+        File temp = File.createTempFile("temp", "");
+        whriteStringAsFile(temp.getAbsolutePath(), jsClass);
+
+        optionsArgList.add(temp.getAbsolutePath());
+        String[] args = new String[optionsArgList.size()];
+
+        args = optionsArgList.toArray(args);
+        UglifyJS uglifyjs = new UglifyJS();
+        return uglifyjs.uglify(args);
     }
 }
