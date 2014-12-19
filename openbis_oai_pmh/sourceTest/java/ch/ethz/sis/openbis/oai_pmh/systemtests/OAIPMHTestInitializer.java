d85d79702a5db2f67bfd581d0cbe4a97658b1252
pkupczyk <pkupczyk>
2014-12-19 10:41:48 +0000

    SSDM-1369 : CKAN - create automated tests
    SVN: 33097

---
+++ openbis_oai_pmh/sourceTest/java/ch/ethz/sis/openbis/oai_pmh/systemtests/OAIPMHTestInitializer.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.ethz.sis.openbis.oai_pmh.systemtests;
+
+import ch.systemsx.cisd.openbis.generic.server.util.TestInitializer;
+
+/**
+ * @author pkupczyk
+ */
+public class OAIPMHTestInitializer
+{
+
+    private static String dbKind = "test_oai_pmh";
+
+    private static String getDBKind()
+    {
+        return dbKind;
+    }
+
+    private static String getDBKindForIndexing()
+    {
+        return getDBKind() + "_indexing";
+    }
+
+    public static void init()
+    {
+        TestInitializer.setScriptFolderForEmptyDB("../openbis/source");
+        TestInitializer.setScriptFolderForTestDB("../openbis/sourceTest");
+        TestInitializer.setDBKind(getDBKind());
+        TestInitializer.setDBKindForIndexing(getDBKindForIndexing());
+        TestInitializer.setCreateDBFromScratch(false);
+    }
+
+}
