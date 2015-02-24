cdb184484ec9c4fe6671824ebdd209cb0507a3ea
felmer <felmer>
2015-02-24 07:22:57 +0000

    SSDM-1491: Using sis-jhdf5 lib instead of cisd-jhdf5. HDF5ContainerBasedHierarchicalContentNodeTest.testLegacyH5arFile added to test an old .h5ar container.
    SVN: 33508

--- openbis-common/sourceTest/java/ch/systemsx/cisd/openbis/common/io/hierarchical_content/HDF5ContainerBasedHierarchicalContentNodeTest.java
+++ openbis-common/sourceTest/java/ch/systemsx/cisd/openbis/common/io/hierarchical_content/HDF5ContainerBasedHierarchicalContentNodeTest.java
@@ -73,32 +73,47 @@ public class HDF5ContainerBasedHierarchicalContentNodeTest extends AssertJUnit
     }
     
     @Test
+    public void testLegacyH5arFile()
+    {
+        IHierarchicalContent content =
+                new DefaultFileBasedHierarchicalContentFactory().asHierarchicalContent(
+                        TEST_DATA_FOLDER, null);
+        IHierarchicalContentNode node = content.getNode("thumbnails_old.h5ar");
+        assertH5arExampleContent(node);
+    }
+    
+    @Test
     public void testH5arFile()
     {
         IHierarchicalContent content =
                 new DefaultFileBasedHierarchicalContentFactory().asHierarchicalContent(
                         TEST_DATA_FOLDER, null);
-        IHierarchicalContentNode node = content.getNode("thumbnails.h5ar");
+        IHierarchicalContentNode node = content.getNode("thumbnails_new.h5ar");
+        assertH5arExampleContent(node);
+    }
+
+    private void assertH5arExampleContent(IHierarchicalContentNode node)
+    {
         List<IHierarchicalContentNode> nodes = getSortedChildren(node);
         
-        assertEquals("3361fd20 24242 wA1_d1-1_cCy3.png\n" + 
-        		"609f3183 29353 wA1_d1-1_cDAPI.png\n" + 
-        		"b68f97cf 27211 wA1_d1-1_cGFP.png\n" + 
-        		"e2c7c34f 28279 wA1_d1-2_cCy3.png\n" + 
-        		"1bf73b61 22246 wA1_d1-2_cDAPI.png\n" + 
-        		"58e14da9 22227 wA1_d1-2_cGFP.png\n" + 
-        		"b312b087 31570 wA1_d2-1_cCy3.png\n" + 
-        		"e7082b23 28267 wA1_d2-1_cDAPI.png\n" + 
-        		"fb7f320e 26972 wA1_d2-1_cGFP.png\n" + 
-        		"d367dd9d 34420 wA1_d2-2_cCy3.png\n" + 
-        		"15e1f3b0 28070 wA1_d2-2_cDAPI.png\n" + 
-        		"34bcde32 27185 wA1_d2-2_cGFP.png\n" + 
-        		"a97cff4e 28916 wA1_d3-1_cCy3.png\n" + 
-        		"6f0abf6f 30079 wA1_d3-1_cDAPI.png\n" + 
-        		"5ba6ae39 28072 wA1_d3-1_cGFP.png\n" + 
-        		"f8d4cfc7 26367 wA1_d3-2_cCy3.png\n" + 
-        		"aeb12b1a 25086 wA1_d3-2_cDAPI.png\n" + 
-        		"ced4332a 22199 wA1_d3-2_cGFP.png", getNamesChecksumsAndSizes(nodes));
+        assertEquals("e15e2b61 36424 wA1_d1-1_cCy3.png\n" + 
+                "6e584065 43322 wA1_d1-1_cDAPI.png\n" + 
+                "9f5d677c 40505 wA1_d1-1_cGFP.png\n" + 
+                "3456d921 43096 wA1_d1-2_cCy3.png\n" + 
+                "771adbb5 32444 wA1_d1-2_cDAPI.png\n" + 
+                "e5e10f4c 32961 wA1_d1-2_cGFP.png\n" + 
+                "3c137a44 48062 wA1_d2-1_cCy3.png\n" + 
+                "e301bfc9 41913 wA1_d2-1_cDAPI.png\n" + 
+                "9286ab95 40327 wA1_d2-1_cGFP.png\n" + 
+                "4ec4697d 51564 wA1_d2-2_cCy3.png\n" + 
+                "92563064 42019 wA1_d2-2_cDAPI.png\n" + 
+                "c21f2b93 41219 wA1_d2-2_cGFP.png\n" + 
+                "7b32fe5a 43449 wA1_d3-1_cCy3.png\n" + 
+                "cca44db2 45041 wA1_d3-1_cDAPI.png\n" + 
+                "2a1a67d 42168 wA1_d3-1_cGFP.png\n" + 
+                "9e2b87e5 39980 wA1_d3-2_cCy3.png\n" + 
+                "38aea6fe 37051 wA1_d3-2_cDAPI.png\n" + 
+                "bb59344e 32815 wA1_d3-2_cGFP.png", getNamesChecksumsAndSizes(nodes));
     }
     
     private static String getNamesChecksumsAndSizes(List<IHierarchicalContentNode> nodes)
