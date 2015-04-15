3811ac1216c4b72286bea35a2542dad3347dea44
juanf <juanf>
2015-04-15 09:38:14 +0000

    SSDM-1728: YeastLab Data Curation - Report
    SVN: 33836

--- plasmid/source/core-plugins/stellingmigration/1/dss/drop-boxes/stellingconsistency/consistency.py
+++ plasmid/source/core-plugins/stellingmigration/1/dss/drop-boxes/stellingconsistency/consistency.py
@@ -8,16 +8,20 @@ import xml.etree.ElementTree as ET
 ## Definitions
 ##
 
-sampleTypesToVerify = ["YEAST","POMBE"];
-logLevelsToPrint = ["ERROR", "REPORT", "MANUAL-FIX", "AUTO-FIX-2"]; #INFO not included, use it for debug only
+sampleTypesToVerify = ["YEAST","POMBE"]
+logLevelsToPrint = ["ERROR", "REPORT", "MANUAL-FIX"] #INFO not included, use it for debug only
 
 ##
 ## Logging
 ##
 
-numberOfManualFixes = 0;
-numberOfAutoFixesDeletes = 0;
-numberOfAutoFixesLost = 0;
+numberOfManualFixes = 0
+numberOfAutoFixesDeletes = 0
+numberOfAutoFixesLost = 0
+
+def logManualFix(message, creator, sampleIdentifier, affectedSampleToBeAnnotated, creatorOfAncestor, ancestor):
+    log("MANUAL-FIX", message + "\t" + creator + "\t" + sampleIdentifier + "\t" + affectedSampleToBeAnnotated + "\t" + creatorOfAncestor + "\t" + ancestor)
+    
 def log(level, message):
     if level == "MANUAL-FIX":
         global numberOfManualFixes
@@ -29,7 +33,7 @@ def log(level, message):
         global numberOfAutoFixesLost
         numberOfAutoFixesLost = numberOfAutoFixesLost + 1
     if level in logLevelsToPrint:
-        print "[" + level + "] " + message;
+        print "[" + level + "] " + message
 
 ##
 ## Cache
@@ -68,8 +72,8 @@ def getSampleByPermId(tr, permId):
 ## Main Methods
 ##
 def process(tr):
-    log("REPORT", "START VERIFICATION REPORT!");
-    
+    log("REPORT", "START VERIFICATION REPORT!")
+    logManualFix("MANUAL-FIX", "AFFECTED SAMPLE CREATOR", "AFFECTED SAMPLE", "ANNOTATION OF AFFECTED SAMPLE", "AFFECTED ANCESTOR CREATOR", "AFFECTED ANCESTOR")
     for sampleType in sampleTypesToVerify:
         samples = getSamplesByType(tr, sampleType)
         global currentCache
@@ -81,10 +85,10 @@ def process(tr):
     global numberOfManualFixes
     global numberOfAutoFixesDeletes
     global numberOfAutoFixesLost
-    log("REPORT", "FOUND " + str(numberOfAutoFixesDeletes) + " AUTOMATIC DELETE FIXES!");
-    log("REPORT", "FOUND " + str(numberOfAutoFixesLost) + " AUTOMATIC LOST FIXES!");
-    log("REPORT", "REQUIRED " + str(numberOfManualFixes) + " MANUAL FIXES!");
-    log("REPORT", "FINISH VERIFICATION REPORT!");
+    log("REPORT", "FOUND " + str(numberOfAutoFixesDeletes) + " AUTOMATIC DELETE FIXES!")
+    log("REPORT", "FOUND " + str(numberOfAutoFixesLost) + " AUTOMATIC LOST FIXES!")
+    log("REPORT", "REQUIRED " + str(numberOfManualFixes-1) + " MANUAL FIXES!") #-1 For the Header
+    log("REPORT", "FINISH VERIFICATION REPORT!")
 
 def verify(tr, sample):
     annotationsRoot = getAnnotationsRootNodeFromSample(sample)
@@ -99,31 +103,35 @@ def verify(tr, sample):
             try:
                 if isChild(sample, annotatedSampleIdentifier):
                     #This is an annotation from a parent, this is by default correct and don't needs further inspection.
-                    log("INFO", "GOOT ANNOTATION AT SAMPLE: " + sample.getSampleIdentifier() + " ANNOTATION: " + annotatedSampleIdentifier);
+                    log("INFO", "GOOT ANNOTATION AT SAMPLE: " + sample.getSampleIdentifier() + " ANNOTATION: " + annotatedSampleIdentifier)
                 else:
                     foundAnnotationAndAncestor = getAnnotationAndAncestor(annotatedSampleIdentifier, sample.getParentSampleIdentifiers())
                     foundAnnotation = foundAnnotationAndAncestor[0]
                     foundAncestor = foundAnnotationAndAncestor[1]
                     if foundAnnotation is not None and foundAncestor is not None:
-                        log("INFO", "BAD ANNOTATION FOUND - ON:" + sample.getSampleIdentifier() + " ANNOTATION:" + annotatedSampleIdentifier);
+                        log("INFO", "BAD ANNOTATION FOUND - ON:" + sample.getSampleIdentifier() + " ANNOTATION:" + annotatedSampleIdentifier)
                         if areAnnotationsEqual(annotation, foundAnnotation):
-                            log("AUTO-FIX", "CASE 1 - GOOD REPEATED ANNOTATION THAT CAN BE DELETED - " + sample.getSampleIdentifier() + " " + annotatedSampleIdentifier);
+                            log("AUTO-FIX", "CASE 1 - GOOD REPEATED ANNOTATION THAT CAN BE DELETED - AT SAMPLE: " + sample.getSampleIdentifier() + " FOR ANNOTATION: " + annotatedSampleIdentifier + " FOUND ORIGINAL AT: " + foundAncestor.getSampleIdentifier())
                         else:
-                            log("MANUAL-FIX", "CASE 3 - THE ANNOTATION: " + annotatedSampleIdentifier + " IS DIFFERENT AT SAMPLE: " + sample.getSampleIdentifier() + " AND ORIGINAL ANCESTOR:" + foundAncestor.getSampleIdentifier());
+                            #log("MANUAL-FIX", "CASE 3 - THE ANNOTATION: " + annotatedSampleIdentifier + " IS DIFFERENT AT SAMPLE: " + sample.getSampleIdentifier() + " AND ORIGINAL ANCESTOR:" + foundAncestor.getSampleIdentifier())
+                            logManualFix("Case 3 - The annotation is different on the sample and his ancestor.", sample.getSample().getRegistrator().getUserId(), sample.getSampleIdentifier(), annotatedSampleIdentifier, foundAncestor.getSample().getRegistrator().getUserId(), foundAncestor.getSampleIdentifier())
                     elif foundAncestor is None:
-                        log("MANUAL-FIX", "CASE 1 - THE ANNOTATED SAMPLE IS NOT AN ANCESTOR - FOR SAMPLE: " + sample.getSampleIdentifier() + " ANNOTATION WITH MISSING ANCESTOR:" + annotatedSampleIdentifier);
+                        logManualFix("Case 1 - The annotated sample is not an ancestor, is missing on the hierarchy for some reason.", sample.getSample().getRegistrator().getUserId(), sample.getSampleIdentifier(), annotatedSampleIdentifier, "?", "?")
+                        #log("MANUAL-FIX", "CASE 1 - THE ANNOTATED SAMPLE IS NOT AN ANCESTOR - FOR SAMPLE: " + sample.getSampleIdentifier() + " ANNOTATION WITH MISSING ANCESTOR:" + annotatedSampleIdentifier)
                     elif foundAnnotation is None:
-                        log("MANUAL-FIX", "CASE 2 - THE ANNOTATED SAMPLE IS NOT ANNOTATED WHERE IT SHOULD - FOR SAMPLE: " + sample.getSampleIdentifier() + " ANNOTATION: " + annotatedSampleIdentifier +" NOT AT " + foundAncestor.getSampleIdentifier());
+                        logManualFix("Case 2 - The annotated sample is not annotated on the appropriate ancestor.", sample.getSample().getRegistrator().getUserId(), sample.getSampleIdentifier(), annotatedSampleIdentifier, foundAncestor.getSample().getRegistrator().getUserId(), foundAncestor.getSampleIdentifier())
+                        #log("MANUAL-FIX", "CASE 2 - THE ANNOTATED SAMPLE IS NOT ANNOTATED WHERE IT SHOULD - FOR SAMPLE: " + sample.getSampleIdentifier() + " ANNOTATION: " + annotatedSampleIdentifier +" NOT AT " + foundAncestor.getSampleIdentifier())
             except Exception, err:
-                log("ERROR", "PROCESSING ANNOTATIONS XML CHILD " + sample.getSampleIdentifier() + " ERR: " + str(err));
+                log("ERROR", "PROCESSING ANNOTATIONS XML CHILD " + sample.getSampleIdentifier() + " ERR: " + str(err))
     #2.Missing Parents Annotations
     for parentIdentifier in requiredAnnotationsFound:
         if not requiredAnnotationsFound[parentIdentifier]:
-            log("MANUAL-FIX", "CASE 4 - MISSING ANNOTATIONS ON SAMPLE: " + sample.getSampleIdentifier() + " FOR PARENT:" + parentIdentifier);
+            #log("MANUAL-FIX", "CASE 4 - MISSING ANNOTATIONS ON SAMPLE: " + sample.getSampleIdentifier() + " FOR PARENT:" + parentIdentifier)
+            logManualFix("Case 4 - Missing annotation on sample for parent.", sample.getSample().getRegistrator().getUserId(), sample.getSampleIdentifier(), parentIdentifier, "-", "-")
     #3.Missing Annotations LOST
     for parentAnnotationIdentifier in requiredAnnotationsFromParents:
         if not requiredAnnotationsFromParents[parentAnnotationIdentifier]:
-            log("AUTO-FIX-2", "CASE 2 - MISSING LOST ANNOTATIONS ON SAMPLE: " + sample.getSampleIdentifier() + " FOR LOST:" + parentAnnotationIdentifier);
+            log("AUTO-FIX-2", "CASE 2 - MISSING LOST ANNOTATIONS ON SAMPLE: " + sample.getSampleIdentifier() + " FOR LOST ANNOTATION: " + parentAnnotationIdentifier + " PRESENT INTO ONE OF THE PARENTS")
 
 def getRequiredAnnotationsFromParents(sample):
     requiredAnnotationsFromParents = {}
@@ -133,24 +141,24 @@ def getRequiredAnnotationsFromParents(sample):
             parentAnnotationsRoot = getAnnotationsRootNodeFromSample(parent)
             if parentAnnotationsRoot is not None:
                 for parentAnnotation in parentAnnotationsRoot:
-                    parentAnnotationIdentifier = parentAnnotation.attrib["identifier"];
+                    parentAnnotationIdentifier = parentAnnotation.attrib["identifier"]
                     if "/FRP" in parentAnnotationIdentifier: #Only require Plasmids
                         requiredAnnotationsFromParents[parentAnnotationIdentifier] = False
-    return requiredAnnotationsFromParents;
+    return requiredAnnotationsFromParents
 
 def getRequiredAnnotations(sample):
     requiredAnnotationsFound = {}
     for parentIdentifier in sample.getParentSampleIdentifiers():
         if "/FRP" in parentIdentifier: #Only require Plasmids
-            requiredAnnotationsFound[parentIdentifier] = False;
-    return requiredAnnotationsFound;
+            requiredAnnotationsFound[parentIdentifier] = False
+    return requiredAnnotationsFound
 
 def areAnnotationsEqual(annotationA, annotationB):
     for key in annotationA.attrib:
         value = annotationA.attrib[key]
         if key != "CONTAINED":
             if value != getValueOrNull(annotationB.attrib, key):
-                log("INFO", "EQUALITY FAILED FOR " + key + ": - " + value + " " + str(getValueOrNull(annotationB.attrib, key)));
+                log("INFO", "EQUALITY FAILED FOR " + key + ": - " + value + " " + str(getValueOrNull(annotationB.attrib, key)))
                 return False
     return True
 
@@ -189,5 +197,5 @@ def getAnnotationsRootNodeFromSample(sample):
         try:
             return ET.fromstring(annotations)
         except Exception:
-            log("ERROR", "READING ANNOTATIONS XML FOR " + sample.getSampleIdentifier());
+            log("ERROR", "READING ANNOTATIONS XML FOR " + sample.getSampleIdentifier())
     return None
