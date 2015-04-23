416116163b868641e741a5372d29651c51cda5fa
juanf <juanf>
2015-04-23 14:47:40 +0000

    SSDM-1802 : ELN - UI: correct table size (Fix that works on Chrome and Firefox but not Safari)
    SVN: 33903

--- plasmid/source/core-plugins/newbrowser/1/as/webapps/newbrowser/html/lib/grid/js/Grid.js
+++ plasmid/source/core-plugins/newbrowser/1/as/webapps/newbrowser/html/lib/grid/js/Grid.js
@@ -370,8 +370,13 @@ $.extend(Grid.prototype, {
 						$(tableHeads[hIdx]).remove();
 					}
 				}
-				$(window).trigger('resize'); // HACK: Fixes table rendering issues when refreshing the grid on fuelux 3.1.0 for all browsers
-//				$(thisGrid.panel).hide().show(0); // HACK: Fixes Chrome rendering issues when refreshing the grid on fuelux 3.1.0
+				//HACK:	Legacy Hacks no longer needed
+				//$(window).trigger('resize'); // HACK: Fixes table rendering issues when refreshing the grid on fuelux 3.1.0 for all browsers
+				//$(thisGrid.panel).hide().show(0); // HACK: Fixes Chrome rendering issues when refreshing the grid on fuelux 3.1.0
+				
+				// HACK: Fix that only works if there is only one table at a time
+				var newWidth = $(".repeater-list-wrapper > .table").width();
+				$(".repeater").width(newWidth);
 			}, 1);
 		});
 	},
