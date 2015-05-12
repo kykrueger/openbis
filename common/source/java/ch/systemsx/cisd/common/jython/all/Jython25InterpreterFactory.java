ced1e01da1018914d20a2560581f85a31dd2a3d5
jakubs <jakubs>
2015-05-12 14:52:59 +0000

    SSDM-1794 jython 2.7 - preparations
    SVN: 33971

---
+++ common/source/java/ch/systemsx/cisd/common/jython/all/Jython25InterpreterFactory.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2015 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.common.jython.all;
+
+import org.python.core.PyBaseCode;
+import org.python.core.PyFunction;
+import org.python.core.PyInteger;
+import org.python.core.PyObject;
+
+import ch.systemsx.cisd.common.jython.JythonUtils;
+import ch.systemsx.cisd.common.jython.PythonInterpreter;
+
+public class Jython25InterpreterFactory implements IJythonInterpreterFactory
+{
+    @Override
+    public IJythonInterpreter createInterpreter()
+    {
+        return new Jython25Interpreter();
+    }
+}
+
+class Jython25Function implements IJythonFunction
+{
+    PyFunction function;
+
+    Jython25Function(PyFunction function)
+    {
+        this.function = function;
+    }
+
+    @Override
+    public IJythonObject invoke(Object... arguments)
+    {
+        PyObject result = JythonUtils.invokeFunction(function, arguments);
+        if (result == null)
+        {
+            return null;
+        }
+        return new Jython25Object(result);
+    }
+
+    @Override
+    public int getArgumentCount()
+    {
+        if (this.function.func_code instanceof PyBaseCode)
+        {
+            return ((PyBaseCode) this.function.func_code).co_argcount;
+        }
+        return -1;
+    }
+}
+
+class Jython25Object implements IJythonObject
+{
+    PyObject pyObject;
+
+    public Jython25Object(PyObject object)
+    {
+        this.pyObject = object;
+    }
+
+    @Override
+    public String getJythonType()
+    {
+        return pyObject.getClass().toString();
+    }
+
+    @Override
+    public boolean isInteger()
+    {
+        return pyObject instanceof PyInteger;
+    }
+
+    @Override
+    public int asInteger()
+    {
+        if (false == isInteger())
+        {
+            throw new IllegalStateException("Object is not integer");
+        }
+        return ((PyInteger) pyObject).asInt();
+    }
+
+}
+
+class Jython25Interpreter implements IJythonInterpreter
+{
+    private PythonInterpreter interpreter;
+
+    public Jython25Interpreter()
+    {
+        interpreter = PythonInterpreter.createIsolatedPythonInterpreter();
+    }
+
+    @Override
+    public void exec(String scriptString, String scriptFile)
+    {
+        interpreter.exec(scriptString, scriptFile);
+    }
+
+    @Override
+    public void set(String variableName, Object object)
+    {
+        interpreter.set(variableName, object);
+    }
+
+    @Override
+    public void releaseResources()
+    {
+        interpreter.releaseResources();
+    }
+
+    @Override
+    public IJythonFunction tryJythonFunction(String name)
+    {
+        PyFunction function = JythonUtils.tryJythonFunction(interpreter, name);
+        if (function == null)
+        {
+            return null;
+        }
+        else
+        {
+            return new Jython25Function(function);
+        }
+    }
+}
---
+++ common/source/java/ch/systemsx/cisd/common/jython27/JythonUtils.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2011 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.common.jython27;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.python27.core.Py;
+import org.python27.core.PyDictionary;
+import org.python27.core.PyFunction;
+import org.python27.core.PyObject;
+import org.python27.core.PySequenceList;
+
+import ch.systemsx.cisd.common.filesystem.FileUtilities;
+import ch.systemsx.cisd.common.jython.evaluator.EvaluatorException;
+import ch.systemsx.cisd.common.shared.basic.string.StringUtils;
+
+/**
+ * Jython utility methods.
+ * 
+ * @author Kaloyan Enimanev
+ */
+public class JythonUtils
+{
+
+    /**
+     * Converts a {@link PyDictionary} to a Java map.
+     * 
+     * @return a map equivalent to the given Jython dictionary.
+     */
+    public static Map<String, String> convertPyDictToMap(PyDictionary result)
+    {
+        Map<String, String> javaMap = new HashMap<String, String>();
+        for (Object item : result.items())
+        {
+            PySequenceList tuple = (PySequenceList) item;
+            javaMap.put(tuple.get(0).toString(), tuple.get(1).toString());
+        }
+        return javaMap;
+    }
+
+    /**
+     * Tries to get a function defined in jython script
+     * 
+     * @return a Jython function object, or <code>null</code> if function doesn't exist.
+     */
+    public static PyFunction tryJythonFunction(PythonInterpreter27 interpreter, String functionName)
+    {
+        try
+        {
+            PyFunction function = interpreter.get(functionName, PyFunction.class);
+            return function;
+        } catch (Exception e)
+        {
+            return null;
+        }
+    }
+
+    /**
+     * Turn all arguments into a python objects, and calls the specified function.
+     */
+    public static PyObject invokeFunction(PyFunction function, Object... args)
+    {
+        PyObject[] pyArgs = new PyObject[args.length];
+        for (int i = 0; i < args.length; i++)
+        {
+            pyArgs[i] = Py.java2py(args[i]);
+        }
+        return function.__call__(pyArgs);
+    }
+
+    /**
+     * @return script string from file with given path
+     * @throws EvaluatorException if the file doesn't exist or is empty
+     */
+    public static String extractScriptFromPath(String scriptPath) throws EvaluatorException
+    {
+        File scriptFile = new File(scriptPath);
+        if (false == scriptFile.exists())
+        {
+            throw new EvaluatorException("Plugin script [" + scriptPath
+                    + "] specified in the configuration doesn't exist.");
+        } else
+        {
+            String scriptString = FileUtilities.loadToString(scriptFile);
+            if (StringUtils.isBlank(scriptString))
+            {
+                throw new EvaluatorException("Plugin script [" + scriptPath
+                        + "] specified in the configuration is empty.");
+            } else
+            {
+                try
+                {
+                    return scriptString + "\n";
+                } catch (EvaluatorException ex)
+                {
+                    throw new EvaluatorException(ex.getMessage() + " [" + scriptPath + "]");
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns a python path that contains folders the specified scripts are located in. Returns null if no scripts have been specified.
+     */
+    public static String[] getScriptDirectoryPythonPath(String... scriptPaths)
+    {
+        if (scriptPaths == null || scriptPaths.length == 0)
+        {
+            return null;
+        }
+
+        List<String> pythonPath = new ArrayList<String>();
+
+        for (String scriptPath : scriptPaths)
+        {
+            if (scriptPath != null)
+            {
+                File scriptFile = new File(scriptPath);
+                File scriptDirectory = scriptFile.getParentFile();
+
+                if (scriptDirectory != null)
+                {
+                    pythonPath.add(scriptDirectory.getAbsolutePath());
+                }
+            }
+        }
+
+        if (pythonPath.isEmpty())
+        {
+            return null;
+        } else
+        {
+            return pythonPath.toArray(new String[pythonPath.size()]);
+        }
+    }
+}
