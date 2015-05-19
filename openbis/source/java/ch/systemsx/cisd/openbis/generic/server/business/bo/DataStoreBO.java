3d8e34d87543c6be460a0e02323773cb2eb08775
jakubs <jakubs>
2015-05-19 15:36:27 +0000

    SSDM-1914 Improve error message in case when custom import plugin has incorrect data store id
    SVN: 34000

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/business/bo/DataStoreBO.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/business/bo/DataStoreBO.java
@@ -16,6 +16,7 @@
 
 package ch.systemsx.cisd.openbis.generic.server.business.bo;
 
+import ch.systemsx.cisd.common.exceptions.ConfigurationFailureException;
 import ch.systemsx.cisd.openbis.generic.server.business.IDataStoreServiceFactory;
 import ch.systemsx.cisd.openbis.generic.server.dataaccess.IDAOFactory;
 import ch.systemsx.cisd.openbis.generic.shared.IDataStoreService;
@@ -36,6 +37,8 @@ public class DataStoreBO implements IDataStoreBO
 
     private DataStorePE dataStore;
 
+    private String dataStoreCode;
+
     public DataStoreBO(IDAOFactory daoFactory, Session session, IDataStoreServiceFactory dssFactory)
     {
         this.daoFactory = daoFactory;
@@ -46,14 +49,18 @@ public class DataStoreBO implements IDataStoreBO
     @Override
     public void loadByCode(String dssCode)
     {
+        this.dataStoreCode = dssCode;
         this.dataStore = daoFactory.getDataStoreDAO().tryToFindDataStoreByCode(dssCode);
     }
 
     @Override
     public void uploadFile(String dropboxName, CustomImportFile customImportFile)
     {
-        assert dataStore != null : "data store not loaded.";
-
+        if (dataStore == null)
+        {
+            throw new ConfigurationFailureException("The data store " + this.dataStoreCode + " for custom import of a dropbox " + dropboxName
+                    + " doesn't exist.");
+        }
         IDataStoreService service = dssFactory.create(dataStore.getRemoteUrl());
         service.putDataSet(session.getSessionToken(), dropboxName, customImportFile);
     }
