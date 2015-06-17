1f260ff74285e00198ebf1e011a897b4049c8aa7
barillac <barillac>
2015-06-17 08:51:53 +0000

    adapted to Weis lab def
    SVN: 34191

---
+++ plasmid/source/core-plugins/weismigration/1/compatibility/at.py
@@ -0,0 +1,9 @@
+import re
+
+def calculate_Onuc(): 
+	result = (len(re.findall("A|a|T|t", entity.propertyValue('SEQUENCE'))) * 100)/len(re.findall("A|a|C|c|G|g|T|t|U|u", entity.propertyValue('SEQUENCE')))	
+
+	return result
+     
+def calculate():
+	return calculate_Onuc()
