e36fb6348ef7fbf35267d454a8f0cb8563601de1
barillac <barillac>
2015-06-17 08:51:29 +0000

    adapted to Weis lab def
    SVN: 34189

--- plasmid/source/core-plugins/weismigration/1/compatibility/gc.py
+++ plasmid/source/core-plugins/weismigration/1/compatibility/gc.py
@@ -1,7 +1,7 @@
 import re
 
 def calculate_Onuc(): 
-	result = (len(re.findall("G|g|C|c", entity.propertyValue('SEQUENCE'))) * 100)/len(re.findall("A|a|C|c|G|g|T|t|R|r|Y|y|M|m|K|k|S|s|W|w|H|h|b|B|D|d|X|x|n|N", entity.propertyValue('SEQUENCE')))	
+	result = (len(re.findall("G|g|C|c", entity.propertyValue('SEQUENCE'))) * 100)/len(re.findall("A|a|C|c|G|g|T|t|U|u", entity.propertyValue('SEQUENCE')))	
 
 	return result
      
