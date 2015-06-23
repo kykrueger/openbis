e04b5c7a1cf7a1bb19f9976542640471988c02b1
pkupczyk <pkupczyk>
2015-06-23 15:03:12 +0000

    SSDM-2005 : V3 AS API - JS tests - more space tests
    SVN: 34258

--- js-test/servers/common/core-plugins/tests/1/as/webapps/openbis-v3-api-test/html/test/space.js
+++ js-test/servers/common/core-plugins/tests/1/as/webapps/openbis-v3-api-test/html/test/space.js
@@ -1,6 +1,69 @@
-define([ 'jquery', 'underscore', 'openbis', 'test/common' ], function($, _, openbis, common) {
+define([ 'jquery', 'underscore', 'openbis', 'test/common', 'dto/entity/space/SpaceCreation', 'dto/entity/space/SpaceUpdate', 'dto/deletion/space/SpaceDeletionOptions' ], function($, _, openbis,
+		common, SpaceCreation, SpaceUpdate, SpaceDeletionOptions) {
 	return function() {
 		QUnit.module("Space tests");
+
+		QUnit.test("createSpaces()", function(assert) {
+			var c = new common(assert);
+			var done = assert.async();
+
+			var creation = new SpaceCreation();
+			creation.setCode("CREATE_JSON_SPACE_" + (new Date().getTime()));
+			creation.setDescription("test description");
+
+			$.when(c.createFacadeAndLogin(), c.createSpaceFetchOptions()).then(function(facade, fetchOptions) {
+				return facade.createSpaces([ creation ]).then(function(permIds) {
+					return facade.mapSpaces(permIds, fetchOptions).done(function() {
+						facade.logout()
+					});
+				});
+			}).done(function(spaces) {
+				c.assertObjectsCount(Object.keys(spaces), 1);
+
+				var space = spaces[creation.getCode()];
+				c.assertEqual(space.getCode(), creation.getCode(), "Code");
+				c.assertEqual(space.getDescription(), creation.getDescription(), "Description");
+				done();
+			}).fail(function(error) {
+				c.fail(error.message);
+				done();
+			});
+		});
+
+		QUnit.test("updateSpaces()", function(assert) {
+			var c = new common(assert);
+			var done = assert.async();
+
+			var creation = new SpaceCreation();
+			creation.setCode("CREATE_JSON_SPACE_" + (new Date().getTime()));
+			creation.setDescription("test description");
+
+			var update = new SpaceUpdate();
+			update.setDescription("test description 2");
+
+			$.when(c.createFacadeAndLogin(), c.createSpaceFetchOptions()).then(function(facade, fetchOptions) {
+				return facade.createSpaces([ creation ]).then(function(permIds) {
+
+					update.setSpaceId(permIds[0]);
+
+					return facade.updateSpaces([ update ]).then(function() {
+						return facade.mapSpaces(permIds, fetchOptions).done(function() {
+							facade.logout()
+						});
+					});
+				});
+			}).done(function(spaces) {
+				c.assertObjectsCount(Object.keys(spaces), 1);
+
+				var space = spaces[creation.getCode()];
+				c.assertEqual(space.getCode(), creation.getCode(), "Code");
+				c.assertEqual(space.getDescription(), update.getDescription().getValue(), "Description");
+				done();
+			}).fail(function(error) {
+				c.fail(error.message);
+				done();
+			});
+		});
 
 		QUnit.test("mapSpaces()", function(assert) {
 			var c = new common(assert);
@@ -48,6 +111,33 @@ define([ 'jquery', 'underscore', 'openbis', 'test/common' ], function($, _, open
 
 				var space = spaces[0];
 				c.assertEqual(space.getCode(), "TEST", "Code");
+				done();
+			}).fail(function(error) {
+				c.fail(error.message);
+				done();
+			});
+		});
+
+		QUnit.test("deleteSpaces()", function(assert) {
+			var c = new common(assert);
+			var done = assert.async();
+
+			var creation = new SpaceCreation();
+			creation.setCode("CREATE_JSON_SPACE_" + (new Date().getTime()));
+
+			var deletion = new SpaceDeletionOptions();
+			deletion.setReason("test reason");
+
+			$.when(c.createFacadeAndLogin(), c.createSpaceFetchOptions()).then(function(facade, fetchOptions) {
+				return facade.createSpaces([ creation ]).then(function(permIds) {
+					return facade.deleteSpaces(permIds, deletion).then(function() {
+						return facade.mapSpaces(permIds, fetchOptions).done(function() {
+							facade.logout()
+						});
+					});
+				});
+			}).done(function(spaces) {
+				c.assertObjectsCount(Object.keys(spaces), 0);
 				done();
 			}).fail(function(error) {
 				c.fail(error.message);
