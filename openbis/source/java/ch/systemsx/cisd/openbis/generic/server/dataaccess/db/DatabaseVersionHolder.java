40e579ad0f8912c3335fb8e005d82234ad8499f3
juanf <juanf>
2015-06-29 14:16:48 +0000

    SSDM-1610 : Allow Hiding of Properties when Registering a new Sample
    SVN: 34300

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
@@ -24,7 +24,7 @@ package ch.systemsx.cisd.openbis.generic.server.dataaccess.db;
 public final class DatabaseVersionHolder
 {
     /** Current version of the database. */
-    private static final String DATABASE_VERSION = "145"; // S202
+    private static final String DATABASE_VERSION = "146"; // S208
 
     private DatabaseVersionHolder()
     {
