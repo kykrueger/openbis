665b890904814a3e754ad2825657143f7568b309
kohleman <kohleman>
2015-08-26 14:15:35 +0000

    added python.path and added creation of a new property
    SVN: 34527

--- deep_sequencing_unit/sourceTest/java/ch/ethz/bsse/cisd/dsu/dss/systemtests/DSUDropboxSystemTest.java
+++ deep_sequencing_unit/sourceTest/java/ch/ethz/bsse/cisd/dsu/dss/systemtests/DSUDropboxSystemTest.java
@@ -19,6 +19,12 @@ package ch.ethz.bsse.cisd.dsu.dss.systemtests;
 import org.testng.annotations.BeforeSuite;
 
 import ch.systemsx.cisd.openbis.datastoreserver.systemtests.GenericDropboxSystemTest;
+import ch.systemsx.cisd.openbis.generic.shared.ICommonServer;
+import ch.systemsx.cisd.openbis.generic.shared.basic.dto.DataType;
+import ch.systemsx.cisd.openbis.generic.shared.basic.dto.DataTypeCode;
+import ch.systemsx.cisd.openbis.generic.shared.basic.dto.EntityKind;
+import ch.systemsx.cisd.openbis.generic.shared.basic.dto.NewETPTAssignment;
+import ch.systemsx.cisd.openbis.generic.shared.basic.dto.PropertyType;
 
 /**
  * @author pkupczyk
@@ -30,14 +36,38 @@ public abstract class DSUDropboxSystemTest extends GenericDropboxSystemTest
     @BeforeSuite
     public void beforeSuite() throws Exception
     {
+        initializePythonImportPath();
         DSUTestInitializer.init();
         super.beforeSuite();
+        initializeMasterData();
     }
-    
+
     @Override
     protected String getDropboxIncomingDirectoryName()
     {
         return getDropboxName();
+    }
+
+    private void initializePythonImportPath()
+    {
+        System.setProperty("python.path", "sourceTest/core-plugins/illumina-qgf/1/jython-lib");
+    }
+
+    private void initializeMasterData()
+    {
+        String sessionToken = getGeneralInformationService().tryToAuthenticateForAllServices("kohleman", "password");
+        ICommonServer commonServer = getBean("common-server");
+        PropertyType propertyType = new PropertyType();
+        propertyType.setCode("CONTROL_SOFTWARE_VERSION");
+        propertyType.setDataType(new DataType(DataTypeCode.VARCHAR));
+        propertyType.setLabel("Control Software Version");
+        propertyType.setDescription(propertyType.getLabel());
+        NewETPTAssignment assignment = new NewETPTAssignment();
+        assignment.setPropertyTypeCode(propertyType.getCode());
+        assignment.setEntityTypeCode("ILLUMINA_FLOW_CELL");
+        assignment.setEntityKind(EntityKind.SAMPLE);
+        assignment.setOrdinal(1000L);
+        commonServer.registerAndAssignPropertyType(sessionToken, propertyType, assignment);
     }
 
 }
