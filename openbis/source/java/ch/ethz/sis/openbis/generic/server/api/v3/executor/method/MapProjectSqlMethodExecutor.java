85661eca86f657534092b2b0751d619ada0be22c
felmer <felmer>
2015-09-15 13:52:48 +0000

    SSDM-2318: Introducing MapProjectTechIdByIdExecutor and using it in MapProjectSqlMethodExecutor
    SVN: 34653

--- openbis/source/java/ch/ethz/sis/openbis/generic/server/api/v3/executor/method/MapProjectSqlMethodExecutor.java
+++ openbis/source/java/ch/ethz/sis/openbis/generic/server/api/v3/executor/method/MapProjectSqlMethodExecutor.java
@@ -16,22 +16,16 @@
 
 package ch.ethz.sis.openbis.generic.server.api.v3.executor.method;
 
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import ch.ethz.sis.openbis.generic.server.api.v3.executor.IOperationContext;
 import ch.ethz.sis.openbis.generic.server.api.v3.executor.common.IMapObjectByIdExecutor;
-import ch.ethz.sis.openbis.generic.server.api.v3.executor.project.IMapProjectByIdExecutor;
+import ch.ethz.sis.openbis.generic.server.api.v3.executor.project.IMapProjectTechIdByIdExecutor;
 import ch.ethz.sis.openbis.generic.server.api.v3.translator.ITranslator;
 import ch.ethz.sis.openbis.generic.server.api.v3.translator.entity.project.sql.IProjectSqlTranslator;
 import ch.ethz.sis.openbis.generic.shared.api.v3.dto.entity.project.Project;
 import ch.ethz.sis.openbis.generic.shared.api.v3.dto.fetchoptions.project.ProjectFetchOptions;
 import ch.ethz.sis.openbis.generic.shared.api.v3.dto.id.project.IProjectId;
-import ch.systemsx.cisd.openbis.generic.shared.dto.ProjectPE;
 
 /**
  * @author pkupczyk
@@ -42,7 +36,7 @@ public class MapProjectSqlMethodExecutor extends AbstractMapMethodExecutor<IProj
 {
 
     @Autowired
-    private IMapProjectByIdExecutor mapExecutor;
+    private IMapProjectTechIdByIdExecutor mapExecutor;
 
     @Autowired
     private IProjectSqlTranslator translator;
@@ -50,23 +44,7 @@ public class MapProjectSqlMethodExecutor extends AbstractMapMethodExecutor<IProj
     @Override
     protected IMapObjectByIdExecutor<IProjectId, Long> getMapExecutor()
     {
-        // TODO replace with IProjectId -> Long mapExecutor once there is one
-        return new IMapObjectByIdExecutor<IProjectId, Long>()
-            {
-                @Override
-                public Map<IProjectId, Long> map(IOperationContext context, Collection<? extends IProjectId> ids)
-                {
-                    Map<IProjectId, ProjectPE> peMap = mapExecutor.map(context, ids);
-                    Map<IProjectId, Long> idMap = new LinkedHashMap<IProjectId, Long>();
-
-                    for (Map.Entry<IProjectId, ProjectPE> peEntry : peMap.entrySet())
-                    {
-                        idMap.put(peEntry.getKey(), peEntry.getValue().getId());
-                    }
-
-                    return idMap;
-                }
-            };
+        return mapExecutor;
     }
 
     @Override
---
+++ openbis/source/java/ch/ethz/sis/openbis/generic/server/api/v3/executor/project/MapProjectTechIdByIdExecutor.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015 ETH Zuerich, SIS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.ethz.sis.openbis.generic.server.api.v3.executor.project;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import ch.ethz.sis.openbis.generic.server.api.v3.executor.IOperationContext;
+import ch.ethz.sis.openbis.generic.server.api.v3.executor.common.AbstractMapObjectByIdExecutor;
+import ch.ethz.sis.openbis.generic.server.api.v3.helper.common.IListObjectById;
+import ch.ethz.sis.openbis.generic.shared.api.v3.dto.id.project.IProjectId;
+
+/**
+ * 
+ *
+ * @author Franz-Josef Elmer
+ */
+@Component
+public class MapProjectTechIdByIdExecutor extends AbstractMapObjectByIdExecutor<IProjectId, Long> implements IMapProjectTechIdByIdExecutor
+{
+
+    @Override
+    protected List<IListObjectById<? extends IProjectId, Long>> createListers(IOperationContext context)
+    {
+        List<IListObjectById<? extends IProjectId, Long>> listers =
+                new LinkedList<IListObjectById<? extends IProjectId, Long>>();
+        listers.add(new ListProjectTechIdByIdentifier());
+        listers.add(new ListProjectTechIdByPermId());
+        return listers;
+    }
+
+}
