fef779cb2aca026025f9f4ac501e4cec3b3fe593
jakubs <jakubs>
2015-09-30 08:37:50 +0000

    SSDM-2550 - expose dss registration logger to dropbox transaction
    SVN: 34748

---
+++ datastore_server/source/java/ch/systemsx/cisd/etlserver/registrator/api/v2/IDSSRegistrationLogger.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2015 ETH Zuerich, CISD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.systemsx.cisd.etlserver.registrator.api.v2;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Logging interface exposed through a dropbox api.
+ * 
+ * @author Jakub Straszewski
+ */
+public interface IDSSRegistrationLogger
+{
+    public void info(Logger logger, String message);
+
+    public void warn(Logger logger, String message);
+
+    public void error(Logger logger, String message);
+
+    public void info(Logger logger, String message, Throwable ex);
+
+    public void warn(Logger logger, String message, Throwable ex);
+
+    public void error(Logger logger, String message, Throwable ex);
+}
