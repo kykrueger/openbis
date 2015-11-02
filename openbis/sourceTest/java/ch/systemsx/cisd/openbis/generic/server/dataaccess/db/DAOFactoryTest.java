4fb266a92b3049fbf289d5539b82d50a8005d481
felmer <felmer>
2015-11-02 13:24:22 +0000

    SSDM-2600: Enable one test case in DAOFactoryTest: Trying to switching project samples feature off.
    SVN: 34961

--- openbis/sourceTest/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DAOFactoryTest.java
+++ openbis/sourceTest/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DAOFactoryTest.java
@@ -1,50 +1,40 @@
 package ch.systemsx.cisd.openbis.generic.server.dataaccess.db;
 
-//@Test(groups = "db")
-public class DAOFactoryTest// extends AbstractDAOTest
-{
-    // @Resource(name = ExposablePropertyPlaceholderConfigurer.PROPERTY_CONFIGURER_BEAN_NAME)
-    // private ExposablePropertyPlaceholderConfigurer configurer;
-    //
-    // private BufferedAppender logRecorder;
-    //
-    // @BeforeMethod
-    // public void setUpLogRecorder()
-    // {
-    // logRecorder = new BufferedAppender(null, Level.INFO, ".*\\." + DAOFactory.class.getSimpleName());
-    // }
+import static org.testng.AssertJUnit.assertEquals;
 
-    // @Test(priority = -1)
-    public void testEnableAndDisableProjectSamples() throws Exception
+import javax.annotation.Resource;
+
+import org.apache.log4j.Level;
+import org.springframework.beans.factory.InitializingBean;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import ch.systemsx.cisd.common.logging.BufferedAppender;
+import ch.systemsx.cisd.common.spring.ExposablePropertyPlaceholderConfigurer;
+import ch.systemsx.cisd.openbis.generic.shared.Constants;
+
+//@Test(groups = "db")
+public class DAOFactoryTest extends AbstractDAOTest
+{
+    @Resource(name = ExposablePropertyPlaceholderConfigurer.PROPERTY_CONFIGURER_BEAN_NAME)
+    private ExposablePropertyPlaceholderConfigurer configurer;
+
+    private BufferedAppender logRecorder;
+
+    @BeforeMethod
+    public void setUpLogRecorder()
     {
-        // configurer.getResolvedProps().setProperty(Constants.PROJECT_SAMPLES_ENABLED_KEY, "true");
-        // ((InitializingBean) daoFactory).afterPropertiesSet();
-        // assertEquals("Enable project samples by dropping the trigger 'disable_project_level_samples'.",
-        // logRecorder.getLogContent());
-        //
-        // logRecorder.resetLogContent();
-        // configurer.getResolvedProps().setProperty(Constants.PROJECT_SAMPLES_ENABLED_KEY, "true");
-        // ((InitializingBean) daoFactory).afterPropertiesSet();
-        // assertEquals("Enable project samples by dropping the trigger 'disable_project_level_samples'.",
-        // logRecorder.getLogContent());
-        //
-        // logRecorder.resetLogContent();
-        // configurer.getResolvedProps().setProperty(Constants.PROJECT_SAMPLES_ENABLED_KEY, "false");
-        // ((InitializingBean) daoFactory).afterPropertiesSet();
-        // assertEquals("It is not possible to disable project samples feature. The system still considers project-samples-enabled=true.",
-        // logRecorder.getLogContent());
-        //
-        // logRecorder.resetLogContent();
-        // configurer.getResolvedProps().setProperty(Constants.PROJECT_SAMPLES_ENABLED_KEY, "false");
-        // ((InitializingBean) daoFactory).afterPropertiesSet();
-        // assertEquals("It is not possible to disable project samples feature. The system still considers project-samples-enabled=true.",
-        // logRecorder.getLogContent());
-        //
-        // logRecorder.resetLogContent();
-        // configurer.getResolvedProps().setProperty(Constants.PROJECT_SAMPLES_ENABLED_KEY, "true");
-        // ((InitializingBean) daoFactory).afterPropertiesSet();
-        // assertEquals("Enable project samples by dropping the trigger 'disable_project_level_samples'.",
-        // logRecorder.getLogContent());
+        logRecorder = new BufferedAppender(null, Level.INFO, ".*\\." + DAOFactory.class.getSimpleName());
+    }
+
+    @Test(priority = -1, groups = "project-samples")
+    public void testDisableProjectSamples() throws Exception
+    {
+        configurer.getResolvedProps().setProperty(Constants.PROJECT_SAMPLES_ENABLED_KEY, "false");
+        ((InitializingBean) daoFactory).afterPropertiesSet();
+        assertEquals("It is not possible to disable project samples feature. "
+                + "The system still considers project-samples-enabled=true.",
+                logRecorder.getLogContent());
     }
 
 }
