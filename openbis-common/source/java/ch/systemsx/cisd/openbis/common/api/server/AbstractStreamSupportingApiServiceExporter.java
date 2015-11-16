fa49bf54b78789cdd0a8071fa1832dfd3a56bfe7
jakubs <jakubs>
2015-11-16 15:44:22 +0000

    SSDM-2805: Add a post processing of beans to check if the http invoker exporters are secure
    SVN: 35068

--- openbis-common/source/java/ch/systemsx/cisd/openbis/common/api/server/AbstractStreamSupportingApiServiceExporter.java
+++ openbis-common/source/java/ch/systemsx/cisd/openbis/common/api/server/AbstractStreamSupportingApiServiceExporter.java
@@ -24,13 +24,14 @@ import ch.systemsx.cisd.common.api.IRpcService;
 import ch.systemsx.cisd.common.api.IRpcServiceNameServer;
 import ch.systemsx.cisd.common.api.RpcServiceInterfaceVersionDTO;
 import ch.systemsx.cisd.common.spring.ServiceExceptionTranslator;
+import ch.systemsx.cisd.common.spring.WhiteListStreamSupportingHttpInvokerExporter;
 
 /**
  * Abstract super class of all classes make an API available via {@link StreamSupportingHttpInvokerServiceExporter}.
  * 
  * @author Franz-Josef Elmer
  */
-public abstract class AbstractStreamSupportingApiServiceExporter extends StreamSupportingHttpInvokerServiceExporter
+public abstract class AbstractStreamSupportingApiServiceExporter extends WhiteListStreamSupportingHttpInvokerExporter
 {
     @Resource(name = IRpcServiceNameServer.PREFFERED_BEAN_NAME)
     private RpcServiceNameServer nameServer;
