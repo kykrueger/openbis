16b970b524d89ad8b066c70275e57da8984457af
juanf <juanf>
2015-12-01 14:13:35 +0000

    SSDM-2658 : Bugfix sample plate creation
    SVN: 35184

--- plasmid/source/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/views/SampleForm/SampleFormView.js
+++ plasmid/source/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/views/SampleForm/SampleFormView.js
@@ -272,7 +272,7 @@ function SampleFormView(sampleFormController, sampleFormModel) {
 		//
 		// Plate View
 		//
-		if(this._sampleFormModel.sample.sampleTypeCode === "PLATE") {
+		if(this._sampleFormModel.sample.sampleTypeCode === "PLATE" && this._sampleFormModel.mode !== FormMode.CREATE) {
 			var plateContainer = $("<div>", { 'id' : 'sample-form-plate-view' });
 			$formColumn.append($("<legend>").append("Plate"));
 			var plateController = new PlateController(this._sampleFormModel.sample, this._sampleFormModel.mode !== FormMode.EDIT);
