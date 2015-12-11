a355400b05050b60c243e631922c5e649c7c6bda
juanf <juanf>
2015-12-11 10:19:40 +0000

    SSDM-2899 : Our unicode utils class now supports UTF-16 and UTF-32 in addition to UTF-8, if no encoding is detected defaults to UTF-8.
    SVN: 35284

--- common/source/java/ch/systemsx/cisd/common/string/UnicodeUtils.java
+++ common/source/java/ch/systemsx/cisd/common/string/UnicodeUtils.java
@@ -16,11 +16,15 @@
 
 package ch.systemsx.cisd.common.string;
 
+import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import ch.systemsx.cisd.common.exceptions.EnvironmentFailureException;
 
@@ -31,9 +35,24 @@ import ch.systemsx.cisd.common.exceptions.EnvironmentFailureException;
  */
 public final class UnicodeUtils
 {
+    
+    public static final String UTF_8 = "UTF-8";
+    public static final String UTF_16 = "UTF-16";
+    public static final String UTF_32 = "UTF-32";
+    
     /** default Unicode encoding used for strings - UTF-8 */
-    public static final String DEFAULT_UNICODE_CHARSET = "UTF-8";
-
+    public static final String DEFAULT_UNICODE_CHARSET = UTF_8;
+    
+    
+    private static final Map<String, byte[]> supportedEncodings = new HashMap<String, byte[]>();
+    static {
+        supportedEncodings.put("UTF-8", new byte[]{ (byte) 0xEF, (byte) 0xBB, (byte) 0xBF });
+        supportedEncodings.put("UTF-16 LE", new byte[]{ (byte) 0xFF, (byte) 0xFE });
+        supportedEncodings.put("UTF-16 BE", new byte[]{ (byte) 0xFE, (byte) 0xFF });
+        supportedEncodings.put("UTF-32 LE", new byte[]{ (byte) 0xFE, (byte) 0xFF, (byte) 0x00, (byte) 0x00 });
+        supportedEncodings.put("UTF-32 BE", new byte[]{ (byte) 0x00, (byte) 0x00, (byte) 0xFE, (byte) 0xFF });
+    }
+    
     private UnicodeUtils()
     {
         // Can not be instantiated.
@@ -45,6 +64,28 @@ public final class UnicodeUtils
         return Charset.forName(UnicodeUtils.DEFAULT_UNICODE_CHARSET);
     }
 
+    private final static String getEncoding(BufferedInputStream bufferedInputStream) {
+        try {
+            if(bufferedInputStream.markSupported()) {
+                for(String encoding:supportedEncodings.keySet()) {
+                    byte[] BOM = supportedEncodings.get(encoding);
+                    byte[] BOMTest = new byte[BOM.length];
+                    bufferedInputStream.mark(BOM.length);
+                    bufferedInputStream.read(BOMTest, 0, BOM.length);
+                    bufferedInputStream.reset();
+                    if(Arrays.equals(BOM, BOMTest)) {
+                        String javaEncoding = encoding.split(" ")[0];
+                        return javaEncoding;
+                    }
+                }
+            }
+        } catch(Exception ex) {
+            
+        }
+        
+        return null;
+    }
+    
     /**
      * @return {@link Reader} that uses the default Unicode encoding.
      * @throws EnvironmentFailureException if the encoding is not supported (shouldn't happen).
@@ -53,7 +94,13 @@ public final class UnicodeUtils
     public final static Reader createReader(InputStream inputStream)
             throws EnvironmentFailureException
     {
-        return createReader(inputStream, DEFAULT_UNICODE_CHARSET);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+        String encoding = getEncoding(bufferedInputStream);
+        if(encoding == null) {
+            encoding = DEFAULT_UNICODE_CHARSET;
+        }
+        
+        return createReader(bufferedInputStream, encoding);
     }
 
     /**
