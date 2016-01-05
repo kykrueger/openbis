b389f7344c60e72259a823c2d3cab90786373a11
kohleman <kohleman>
2016-01-05 07:20:58 +0000

    making the data set type listing usable
    SVN: 35416

--- openbis_standard_technologies/dist/core-plugins/dataset-uploader/1/as/webapps/dataset-uploader/html/js/FormUtil.js
+++ openbis_standard_technologies/dist/core-plugins/dataset-uploader/1/as/webapps/dataset-uploader/html/js/FormUtil.js
@@ -1,6 +1,6 @@
 var FormUtil = new function() {
 	this.profile = null;
-	
+
 	//
 	// Form css classes
 	//
@@ -14,93 +14,88 @@ var FormUtil = new function() {
 	//
 	// Standard Form Fields
 	//
-	
+
 	this.getPhysicalDataSetsDropDown = function(code, dataSetTypes) {
 		var $component = $("<select>", { class : 'form-control ' });
 		$component.attr('id', code);
-		
+
 		$component.attr('required', '');
-		
+
 		$component.append($("<option>").attr('value', '').attr('selected', '').text(''));
-		
+
 		for(var i = 0; i < dataSetTypes.length; i++) {
 			if(dataSetTypes[i].dataSetKind === "PHYSICAL") {
 				var displayName = dataSetTypes[i].code;
 				if(dataSetTypes[i].description) {
-					var length = dataSetTypes[i].description.length > 40;
-					if(dataSetTypes[i].description.length > 40) {
-						displayName = dataSetTypes[i].description.substring(1,36) + " ...";
-					} else {
-						displayName = dataSetTypes[i].description;
-					}
+					displayName += " (" + dataSetTypes[i].description + ")";
 				}
 				$component.append($("<option>").attr('value',dataSetTypes[i].code).text(displayName));
 			}
 		}
-		
+
 		return $component;
 	}
-	
+
 	//
 	// Get Field with container to obtain a correct layout
 	//
 	this.getFieldForComponentWithLabel = function($component, label, postComponent) {
 		var $fieldset = $('<div>');
-		
+
 		var $controlGroup = $('<div>', {class : 'form-group'});
 		var requiredText = '';
 		if($component.attr('required')) {
 			requiredText = "&nbsp;(*)"
 		}
-		
+
 		var labelText = "";
 		if(label) {
 			labelText = label + requiredText + ":";
 		}
-		
+
 		var $controlLabel = $('<label>', { class : 'control-label ' + this.labelColumnClass }).html(labelText);
 		var $controls = $('<div>', { class : 'controls ' + this.controlColumnClass });
-			
+
 		$controlGroup.append($controlLabel);
 		$controlGroup.append($controls);
 		if(postComponent) {
 			$controlGroup.append(postComponent);
 		}
 		$fieldset.append($controlGroup);
-		
+
 		$controls.append($component);
-		
-		
+
+
 		return $fieldset;
 	}
-	
+
 	this.getFieldForTextWithLabel = function(text, label) {
 		var $fieldset = $('<div>');
-		
+
 		var $controlGroup = $('<div>', {class : 'form-group'});
-		
+
 		var $controlLabel = $('<label>', {class : 'control-label ' + this.labelColumnClass});
 		if(label) {
 			$controlLabel.text(label + ":");
 		}
-		
+
 		var $controls = $('<div>', {class : 'controls ' + this.controlColumnClass });
-		
+
 		$controlGroup.append($controlLabel);
 		$controlGroup.append($controls);
 		$fieldset.append($controlGroup);
-		
+
 		var $component = $("<p>", {'class' : 'form-control-static', 'style' : 'border:none; box-shadow:none; background:transparent; word-wrap: break-word; white-space: pre-line;'});
 		$component.text(text);
 		$controls.append($component);
-		
+
 		return $fieldset;
 	}
 
 	//
 	// Get Field from property
 	//
-	
+
 	this.getFieldForPropertyType = function(propertyType) {
 		var $component = null;
 		if (propertyType.dataType === "BOOLEAN") {
@@ -124,33 +119,33 @@ var FormUtil = new function() {
 		} else if (propertyType.dataType === "XML") {
 			$component = getTextBox(propertyType.code, propertyType.description, propertyType.mandatory);
 		}
-		
+
 		return $component;
 	}
-	
+
 	//
 	// Form Fields
 	//
 	var getBooleanField = function(id, alt) {
 		return $('<div>', {'class' : 'checkbox'}).append($('<input>', {'type' : 'checkbox', 'id' : id, 'alt' : alt, 'placeholder' : alt }));
 	}
-	
+
 	var getDropDownFieldForVocabulary = function(code, terms, alt, isRequired) {
 		var $component = $("<select>", {'placeholder' : alt, 'class' : 'form-control'});
 		$component.attr('id', code);
-		
+
 		if (isRequired) {
 			$component.attr('required', '');
 		}
-		
+
 		$component.append($("<option>").attr('value', '').attr('selected', '').text(''));
 		for(var i = 0; i < terms.length; i++) {
 			$component.append($("<option>").attr('value',terms[i].code).text(terms[i].label));
 		}
-		
+
 		return $component;
 	}
-	
+
 	var getInputField = function(type, id, alt, step, isRequired) {
 		var $component = $('<input>', {'type' : type, 'id' : id, 'alt' : alt, 'placeholder' : alt, 'class' : 'form-control'});
 		if (isRequired) {
@@ -161,7 +156,7 @@ var FormUtil = new function() {
 		}
 		return $component;
 	}
-	
+
 	var getTextBox = function(id, alt, isRequired) {
 		var $component = $('<textarea>', {'id' : id, 'alt' : alt, 'style' : 'height: 80px; width: 450px;', 'placeholder' : alt, 'class' : 'form-control'});
 		if (isRequired) {
@@ -169,7 +164,7 @@ var FormUtil = new function() {
 		}
 		return $component;
 	}
-	
+
 	var getDatePickerField = function(id, alt, isRequired) {
 		var $component = $('<div>', {'class' : 'form-group', 'style' : 'margin-left: 0px;', 'placeholder' : alt });
 		var $subComponent = $('<div>', {'class' : 'input-group date', 'id' : 'datetimepicker_' + id });
@@ -179,12 +174,12 @@ var FormUtil = new function() {
 		}
 		var $spanAddOn = $('<span>', {'class' : 'input-group-addon'})
 							.append($('<span>', {'class' : 'glyphicon glyphicon-calendar' }));
-		
+
 		$subComponent.append($input);
 		$subComponent.append($spanAddOn);
 		$subComponent.datetimepicker({ language: 'en' });
 		$component.append($subComponent);
-		
+
 		return $component;
 	}
 }
