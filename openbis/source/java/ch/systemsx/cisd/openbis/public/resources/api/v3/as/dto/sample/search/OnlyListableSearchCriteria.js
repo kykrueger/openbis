ed19ef67cddea1726771678f2fb8b1fd6338a97d
fedoreno <fedoreno>
2016-04-22 10:49:51 +0000

    SSDM-3519: properly inheriting js class
    SVN: 36267

--- openbis/source/java/ch/systemsx/cisd/openbis/public/resources/api/v3/as/dto/sample/search/OnlyListableSearchCriteria.js
+++ openbis/source/java/ch/systemsx/cisd/openbis/public/resources/api/v3/as/dto/sample/search/OnlyListableSearchCriteria.js
@@ -1,10 +1,11 @@
 /**
  * @author pkupczyk
  */
-define([ "stjs", "as/dto/sample/search/AbstractCompositeSearchCriteria" ], function(stjs, AbstractCompositeSearchCriteria) {
+define([ "stjs", "as/dto/common/search/AbstractCompositeSearchCriteria" ], function(stjs, AbstractCompositeSearchCriteria) {
 	var OnlyListableSearchCriteria = function() {
+		AbstractCompositeSearchCriteria.call(this);
 	};
-	stjs.extend(AbstractCompositeSearchCriteria, OnlyListableSearchCriteria, [ OnlyListableSearchCriteria ], function(constructor, prototype) {
+	stjs.extend(OnlyListableSearchCriteria, AbstractCompositeSearchCriteria, [ AbstractCompositeSearchCriteria ], function(constructor, prototype) {
 		prototype['@type'] = 'as.dto.sample.search.OnlyListableSearchCriteria';
 		constructor.serialVersionUID = 1;
 	}, {});
