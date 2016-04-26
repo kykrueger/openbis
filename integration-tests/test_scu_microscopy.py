13cfc569d4d86389776fd3f7e96d965f5b85d139
felmer <felmer>
2016-04-26 06:58:36 +0000

    SSDM-3505: get log info and test expectations for each example
    SVN: 36286

--- integration-tests/test_scu_microscopy.py
+++ integration-tests/test_scu_microscopy.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 import os
 import os.path
+import re
+import time
+from time import mktime
+from datetime import datetime
 
 import settings
 import systemtest.testcase
@@ -21,7 +25,7 @@ class TestCase(systemtest.testcase.TestCase):
 #        openbisController = self.createOpenbisController(dropDatabases=False)
         testDataFolder = self.getTestDataFolder(openbisController)
 #        self.dropTestExample(openbisController, testDataFolder, "ivmtcbeb087rjsmilcus17nu18")
-#        self.dropTestExample(openbisController, testDataFolder, "m9du561cidup7n0gdp97k8gh6u")
+        self.dropTestExample(openbisController, testDataFolder, "m9du561cidup7n0gdp97k8gh6u")
         self.dropTestExample(openbisController, testDataFolder, "okn3scc5tmhk199m6qk80d20op")
         for exampleName in sorted(os.listdir(testDataFolder)):
             if os.path.isdir("%s/%s" % (testDataFolder, exampleName)):
@@ -67,10 +71,56 @@ class TestCase(systemtest.testcase.TestCase):
         util.copyFromTo(testDataFolder, destination, exampleName)
         markerFile = "%s/.MARKER_is_finished_%s" % (destination, exampleName)
         open(markerFile, 'a').close()
-        openbisController.waitUntilDataSetRegistrationFinished(expectations.numberOfPhysicalDataSets, timeOutInMinutes = 20)
-        
-    def checkExpectations(self, openbisController, exampleName):
-        logFolder = "%/openbis/servers/datastore_server/log-registrations/succeeded" % openbisController.installPath
+        openbisController.waitUntilDataSetRegistrationFinished(expectations.numberOfPhysicalDataSets, 
+                                                               timeOutInMinutes = expectations.registrationTimeout)
+        logInfo = self.getLogInfo(openbisController, exampleName)
+        self.assertEquals("Number of experiments created", expectations.numberOfExperiments, logInfo.numberOfExperiments)
+        self.assertEquals("Number of samples created", expectations.numberOfSamples, logInfo.numberOfSamples)
+        self.assertEquals("Number of data sets created", expectations.numberOfDataSets, logInfo.numberOfDataSets)
+            
+    def getLogInfo(self, openbisController, exampleName):
+        logFolder = "%s/servers/datastore_server/log-registrations/succeeded" % openbisController.installPath
+        logFile = self.getLogFile(logFolder, exampleName)
+        logInfo = LogInfo(exampleName)
+        with open(logFile) as fid:
+            content = fid.readlines()
+            initial_time = datetime.fromtimestamp(mktime(time.strptime(content[0][0:19], '%Y-%m-%d %H:%M:%S')))
+            last_time = datetime.fromtimestamp(mktime(time.strptime(content[-1][0:19], '%Y-%m-%d %H:%M:%S')))
+            logInfo.jobTime = last_time - initial_time
+            for line in content:
+                if "Experiments created:" in line:
+                    logInfo.numberOfExperiments = self.getNumFromString(line)
+                if "Samples created:" in line:
+                    logInfo.numberOfSamples = self.getNumFromString(line)
+                if "Data sets created:" in line:
+                    logInfo.numberOfDataSets = self.getNumFromString(line)
+                if "Start registration" in line:
+                    initial_thumb_time = datetime.fromtimestamp(mktime(time.strptime(line[0:19], '%Y-%m-%d %H:%M:%S')))
+                if "Prepared registration" in line:
+                    last_thumb_time = datetime.fromtimestamp(mktime(time.strptime(line[0:19], '%Y-%m-%d %H:%M:%S')))
+            
+            logInfo.thumbnailTime = last_thumb_time - initial_thumb_time
+        util.printAndFlush(("Registration took %s seconds (%s seconds for thumbnail creation) " 
+                           + "to create %s experiments, %s samples, %s data sets") 
+                           % (logInfo.jobTime.seconds, logInfo.thumbnailTime.seconds, logInfo.numberOfExperiments, 
+                              logInfo.numberOfSamples, logInfo.numberOfDataSets))
+        return logInfo
+
+    def getNumFromString(self, string):
+        """Get the number at the end of a string."""
+    
+        m = re.search(r'\d+$', string)
+        if m is not None:
+            return int(m.group())
+        else:
+            return 0
+    
+    def getLogFile(self, logFolder, exampleName):
+        for f in os.listdir(logFolder):
+            if f.find(exampleName) > 0:
+                return "%s/%s" % (logFolder, f)
+        raise Exception("No log file for example %s found in %s." % (exampleName, logFolder))
+
         
 
 class Expectations(object):
@@ -80,7 +130,18 @@ class Expectations(object):
         numberOfAdditinalDataSets = int(properties['number-of-additional-data-sets'])
         self.numberOfPhysicalDataSets = self.numberOfSeries + 1 + numberOfAdditinalDataSets
         self.numberOfDataSets = self.numberOfPhysicalDataSets + self.numberOfSeries
+        self.numberOfExperiments = int(properties['number-of-experiments'])
+        self.numberOfSamples = int(properties['number-of-samples'])
+        self.registrationTimeout = int(properties['registration-timeout-in-minutes']);
 
+class LogInfo(object):
+    def __init__(self, exampleName):
+        self.exampleName = exampleName
+        self.numberOfExperiments = 0
+        self.numberOfSamples = 0
+        self.numberOfDataSets = 0
+        self.jobTime = 0
+        self.thumbnailTime = 0
 
     
 TestCase(settings, __file__).runTest()
