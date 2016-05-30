eb4b1230e8b7142742b417dda964b0df2cf58658
juanf <juanf>
2016-05-30 13:24:54 +0000

    SSDM-3092 : Export functionality, ongoing work
    SVN: 36592

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/dss/reporting-plugins/exports-api/exports-api.py
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/dss/reporting-plugins/exports-api/exports-api.py
@@ -25,7 +25,15 @@ from ch.ethz.sis.openbis.generic.asapi.v3 import IApplicationServerApi
 from ch.systemsx.cisd.openbis.dss.generic.server import DataStoreServer
 OPENBISURL = DataStoreServer.getConfigParameters().getServerURL() + "/openbis/openbis"
 
+from ch.ethz.sis.openbis.generic.asapi.v3.dto.project.search import ProjectSearchCriteria
+from ch.ethz.sis.openbis.generic.asapi.v3.dto.experiment.search import ExperimentSearchCriteria
+from ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.search import SampleSearchCriteria
+from ch.ethz.sis.openbis.generic.asapi.v3.dto.dataset.search import DataSetSearchCriteria
 
+from ch.ethz.sis.openbis.generic.asapi.v3.dto.project.fetchoptions import ProjectFetchOptions
+from ch.ethz.sis.openbis.generic.asapi.v3.dto.experiment.fetchoptions import ExperimentFetchOptions
+from ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.fetchoptions import SampleFetchOptions
+from ch.ethz.sis.openbis.generic.asapi.v3.dto.dataset.fetchoptions import DataSetFetchOptions
 
 def username(sessiontoken):
     m = re.compile('(.*)-[^-]*').match(sessiontoken)
@@ -37,7 +45,7 @@ def process(tr, params, tableBuilder):
 	isOk = False;
 	result = None;
 	
-	# Set user using the dropbox
+	# Set user using the Dropbox
 	tr.setUserId(userId);
 	
 	if method == "exportAll":
@@ -58,27 +66,55 @@ def process(tr, params, tableBuilder):
 		row.setCell("STATUS","FAIL");
 		row.setCell("MESSAGE", "Operation Failed");
 		
+
 def exportAll(tr, params):
-	entitiesToExport = [];
+	sessionToken = params.get("sessionToken");
+	entity = params.get("entity");
+	entityAsPythonMap = { "type" : entity.get("type"), "permId" : entity.get("permId") };
+	entitiesToExport = [entityAsPythonMap];
+	entitiesToExpand = deque([entityAsPythonMap]);
 	
-	entitiesToExpand = deque([params.get("entity")]);
 	while entitiesToExpand:
 		entityToExpand = entitiesToExpand.popleft();
-		type =  entityToExpand.get("type");
-		permId = entityToExpand.get("permId");
-		print "Expanding type: " + type + " permId: " + permId;
+		type =  entityToExpand["type"];
+		permId = entityToExpand["permId"];
+		print "Expanding type: " + str(type) + " permId: " + str(permId);
 		v3 = HttpInvokerUtils.createServiceStub(IApplicationServerApi, OPENBISURL + IApplicationServerApi.SERVICE_URL, 30 * 1000);
-# 		if type == "SPACE":
-# 			getSpaceProjects();
-# 		if type == "PROJECT":
-# 			getProjectSamples(); 
-# 			getProjectExperiments();
-# 		if type == "EXPERIMENT":
-# 			getExperimentSamples();
-# 			getExperimentDataSets();
-# 		if type == "SAMPLE":
-# 			getSampleDataSets();
+		
+		if type == "SPACE":
+			criteria = ProjectSearchCriteria();
+			criteria.withSpace().withCode().thatEquals(permId);
+			results = v3.searchProjects(sessionToken, criteria, ProjectFetchOptions());
+			print "Found: " + str(results.getTotalCount()) + " projects";
+			for project in results.getObjects():
+				entitiesToExport.append({ "type" : "PROJECT", "permId" : project.getPermId().getPermId() });
+				entitiesToExpand.append({ "type" : "PROJECT", "permId" : project.getPermId().getPermId() });
+		if type == "PROJECT":
+			criteria = ExperimentSearchCriteria();
+			criteria.withProject().withPermId().thatEquals(permId);
+			results = v3.searchExperiments(sessionToken, criteria, ExperimentFetchOptions());
+			print "Found: " + str(results.getTotalCount()) + " experiments";
+			for experiment in results.getObjects():
+				entitiesToExport.append({ "type" : "EXPERIMENT", "permId" : experiment.getPermId().getPermId() });
+				entitiesToExpand.append({ "type" : "EXPERIMENT", "permId" : experiment.getPermId().getPermId() });
+ 		if type == "EXPERIMENT":
+ 			criteria = SampleSearchCriteria();
+			criteria.withExperiment().withPermId().thatEquals(permId);
+			results = v3.searchSamples(sessionToken, criteria, SampleFetchOptions());
+			print "Found: " + str(results.getTotalCount()) + " samples";
+			for sample in results.getObjects():
+				entitiesToExport.append({ "type" : "SAMPLE", "permId" : sample.getPermId().getPermId() });
+				entitiesToExpand.append({ "type" : "SAMPLE", "permId" : sample.getPermId().getPermId() });
+ 		if type == "SAMPLE":
+ 			criteria = DataSetSearchCriteria();
+ 			criteria.withSample().withPermId().thatEquals(permId);
+ 			results = v3.searchDataSets(sessionToken, criteria, DataSetFetchOptions());
+ 			print "Found: " + str(results.getTotalCount()) + " datasets";
+			for dataset in results.getObjects():
+				entitiesToExport.append({ "type" : "DATASET", "permId" : dataset.getPermId().getPermId() });
+				entitiesToExpand.append({ "type" : "DATASET", "permId" : dataset.getPermId().getPermId() });
 	
+	print "Found " + str(len(entitiesToExport)) + " entities to export.";
 	params.put("entities", entitiesToExport);
 	return export(tr, params);
 
