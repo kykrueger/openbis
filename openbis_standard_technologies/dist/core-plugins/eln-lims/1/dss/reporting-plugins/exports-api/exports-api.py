f2864a36c51072e5a0f1c17e8e8aceedbca9ce02
juanf <juanf>
2016-05-31 11:11:40 +0000

    SSDM-3092 : Export functionality, ongoing work
    SVN: 36596

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/dss/reporting-plugins/exports-api/exports-api.py
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/dss/reporting-plugins/exports-api/exports-api.py
@@ -17,11 +17,17 @@
 from collections import deque
 import time
 
-from java.io import File
+#Java Core
+from java.io import ByteArrayInputStream
+from java.lang import String
 
 #To obtain the openBIS URL
 from ch.systemsx.cisd.openbis.dss.generic.server import DataStoreServer
+from __builtin__ import None
 OPENBISURL = DataStoreServer.getConfigParameters().getServerURL() + "/openbis/openbis"
+
+#V1 API - To manage session workspace
+from ch.systemsx.cisd.openbis.dss.client.api.v1 import DssComponentFactory
 
 #V3 API
 from ch.systemsx.cisd.common.spring import HttpInvokerUtils;
@@ -36,15 +42,9 @@ from ch.ethz.sis.openbis.generic.asapi.v3.dto.experiment.fetchoptions import Exp
 from ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.fetchoptions import SampleFetchOptions
 from ch.ethz.sis.openbis.generic.asapi.v3.dto.dataset.fetchoptions import DataSetFetchOptions
 
-def getThreadProperties(tr):
-  threadPropertyDict = {}
-  threadProperties = tr.getGlobalState().getThreadParameters().getThreadProperties()
-  for key in threadProperties:
-    try:
-      threadPropertyDict[key] = threadProperties.getProperty(key)
-    except:
-      pass
-  return threadPropertyDict
+#JSON
+from ch.ethz.sis.openbis.generic.server.sharedapi.v3.json import GenericObjectMapper;
+from com.fasterxml.jackson.databind import SerializationFeature
 
 def process(tr, params, tableBuilder):
 	method = params.get("method");
@@ -75,6 +75,7 @@ def process(tr, params, tableBuilder):
 
 def exportAll(tr, params):
 	sessionToken = params.get("sessionToken");
+	v3 = HttpInvokerUtils.createServiceStub(IApplicationServerApi, OPENBISURL + IApplicationServerApi.SERVICE_URL, 30 * 1000);
 	entity = params.get("entity");
 	entityAsPythonMap = { "type" : entity.get("type"), "permId" : entity.get("permId") };
 	entitiesToExport = [entityAsPythonMap];
@@ -85,7 +86,6 @@ def exportAll(tr, params):
 		type =  entityToExpand["type"];
 		permId = entityToExpand["permId"];
 		print "Expanding type: " + str(type) + " permId: " + str(permId);
-		v3 = HttpInvokerUtils.createServiceStub(IApplicationServerApi, OPENBISURL + IApplicationServerApi.SERVICE_URL, 30 * 1000);
 		
 		if type == "SPACE":
 			criteria = ProjectSearchCriteria();
@@ -103,19 +103,19 @@ def exportAll(tr, params):
 			for experiment in results.getObjects():
 				entitiesToExport.append({ "type" : "EXPERIMENT", "permId" : experiment.getPermId().getPermId() });
 				entitiesToExpand.append({ "type" : "EXPERIMENT", "permId" : experiment.getPermId().getPermId() });
- 		if type == "EXPERIMENT":
- 			criteria = SampleSearchCriteria();
+		if type == "EXPERIMENT":
+			criteria = SampleSearchCriteria();
 			criteria.withExperiment().withPermId().thatEquals(permId);
 			results = v3.searchSamples(sessionToken, criteria, SampleFetchOptions());
 			print "Found: " + str(results.getTotalCount()) + " samples";
 			for sample in results.getObjects():
 				entitiesToExport.append({ "type" : "SAMPLE", "permId" : sample.getPermId().getPermId() });
 				entitiesToExpand.append({ "type" : "SAMPLE", "permId" : sample.getPermId().getPermId() });
- 		if type == "SAMPLE":
- 			criteria = DataSetSearchCriteria();
- 			criteria.withSample().withPermId().thatEquals(permId);
- 			results = v3.searchDataSets(sessionToken, criteria, DataSetFetchOptions());
- 			print "Found: " + str(results.getTotalCount()) + " datasets";
+		if type == "SAMPLE":
+			criteria = DataSetSearchCriteria();
+			criteria.withSample().withPermId().thatEquals(permId);
+			results = v3.searchDataSets(sessionToken, criteria, DataSetFetchOptions());
+			print "Found: " + str(results.getTotalCount()) + " datasets";
 			for dataset in results.getObjects():
 				entitiesToExport.append({ "type" : "DATASET", "permId" : dataset.getPermId().getPermId() });
 				entitiesToExpand.append({ "type" : "DATASET", "permId" : dataset.getPermId().getPermId() });
@@ -124,12 +124,91 @@ def exportAll(tr, params):
 	params.put("entities", entitiesToExport);
 	return export(tr, params);
 
+def getFileName(rootDir, spaceCode, projCode, expCode, sampCode, dataCode, ext):
+	fileName = rootDir;
+	if spaceCode is not None:
+		fileName += "/" + spaceCode;
+	if projCode is not None:
+		fileName += "/" + projCode;
+	if expCode is not None:
+		fileName += "/" + expCode;
+	if sampCode is not None:
+		fileName += "/" + sampCode;
+	if dataCode is not None:
+		fileName += "/" + dataCode;
+	fileName += "." + ext;
+	return fileName;
+
 def export(tr, params):
-	#The temporal folder should be able to access the store, for that we create it close to it.
-	threadProperties = getThreadProperties(tr);
-	tempDir =  threadProperties[u'incoming-dir'] + "/tmp_eln/" + str(time.time());
-	print "Using " + tempDir + " as temp dir.";
-	tempDirFile = File(tempDir);
-	tempDirFile.mkdirs();
+	sessionToken = params.get("sessionToken");
+	v3 = HttpInvokerUtils.createServiceStub(IApplicationServerApi, OPENBISURL + IApplicationServerApi.SERVICE_URL, 30 * 1000);
+	objectMapper = GenericObjectMapper();
+	objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+	dss_component = DssComponentFactory.tryCreate(params.get("sessionToken"), OPENBISURL);
+	rootDir = str(time.time());
 	
+	for entity in params.get("entities"):
+		type =  entity["type"];
+		permId = entity["permId"];
+		print "exporting type: " + str(type) + " permId: " + str(permId);
+		entityObj = None;
+		entityFilePath = None;
+		
+		if type == "SPACE":
+			pass #Do nothing
+		if type == "PROJECT":
+			criteria = ProjectSearchCriteria();
+			criteria.withPermId().thatEquals(permId);
+			fetchOps = ProjectFetchOptions();
+			fetchOps.withSpace();
+			fetchOps.withRegistrator();
+			fetchOps.withModifier();
+			entityObj = v3.searchProjects(sessionToken, criteria, fetchOps).getObjects().get(0);
+			entityFilePath = getFileName(rootDir, entityObj.getSpace().getCode(), entityObj.getCode(), None, None, None, "json");
+		if type == "EXPERIMENT":
+			criteria = ExperimentSearchCriteria();
+			criteria.withPermId().thatEquals(permId);
+			fetchOps = ExperimentFetchOptions();
+			fetchOps.withType();
+			fetchOps.withProject().withSpace();
+			fetchOps.withRegistrator();
+			fetchOps.withModifier();
+			fetchOps.withProperties();
+			fetchOps.withTags();
+			entityObj = v3.searchExperiments(sessionToken, criteria, fetchOps).getObjects().get(0);
+			entityFilePath = getFileName(rootDir, entityObj.getProject().getSpace().getCode(), entityObj.getProject().getCode(), entityObj.getCode(), None, None, "json");
+		if type == "SAMPLE":
+			criteria = SampleSearchCriteria();
+			criteria.withPermId().thatEquals(permId);
+			fetchOps = SampleFetchOptions();
+			fetchOps.withType();
+			fetchOps.withExperiment().withProject().withSpace();
+			fetchOps.withRegistrator();
+			fetchOps.withModifier();
+			fetchOps.withProperties();
+			fetchOps.withTags();
+			fetchOps.withParents();
+			fetchOps.withChildren();
+			entityObj = v3.searchSamples(sessionToken, criteria, fetchOps).getObjects().get(0);
+			entityFilePath = getFileName(rootDir, entityObj.getExperiment().getProject().getSpace().getCode(), entityObj.getExperiment().getProject().getCode(), entityObj.getExperiment().getCode(), entityObj.getCode(), None, "json");
+		if type == "DATASET":
+			criteria = DataSetSearchCriteria();
+			criteria.withPermId().thatEquals(permId);
+			fetchOps = DataSetFetchOptions();
+			fetchOps.withType();
+			fetchOps.withSample().withExperiment().withProject().withSpace();
+			fetchOps.withRegistrator();
+			fetchOps.withModifier();
+			fetchOps.withProperties();
+			fetchOps.withTags();
+			fetchOps.withParents();
+			fetchOps.withChildren();
+			entityObj = v3.searchDataSets(sessionToken, criteria, fetchOps).getObjects().get(0);
+			entityFilePath = getFileName(rootDir, entityObj.getSample().getExperiment().getProject().getSpace().getCode(), entityObj.getSample().getExperiment().getProject().getCode(), entityObj.getSample().getExperiment().getCode(), entityObj.getSample().getCode(), entityObj.getCode(), "json");
+			
+		if entityObj is not None and entityFilePath is not None:
+			entityJson = String(objectMapper.writeValueAsString(entityObj));
+			dss_component.putFileToSessionWorkspace(entityFilePath, ByteArrayInputStream(entityJson.getBytes()));
+			print entityJson;
+			
 	return True
