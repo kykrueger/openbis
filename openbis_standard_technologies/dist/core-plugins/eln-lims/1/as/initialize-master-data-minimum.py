d2a9e0d23a80ce8b2cf4961621861d7e865f0b85
juanf <juanf>
2016-07-08 13:56:45 +0000

    SSDM-3816 : ELN Installer Fixes
    SVN: 36808

---
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/initialize-master-data-minimum.py
@@ -0,0 +1,62 @@
+#
+# Copyright 2014 ETH Zuerich, Scientific IT Services
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+##
+## Configuration
+##
+import elnTypes
+
+# MasterDataRegistrationTransaction Class
+import os
+import ch.systemsx.cisd.openbis.generic.server.jython.api.v1.DataType as DataType
+
+tr = service.transaction();
+
+##
+## Vocabulary Types
+##
+elnTypes.createVocabularyWithTerms(tr, elnTypes.ELN_TYPES_METADATA_MINI);
+    
+##
+## Property Types for annotations
+##
+
+##
+## DataSet Types
+##
+elnTypes.getCommentsScript(tr, "DATA_SET");
+elnTypes.createDataSetTypeWithProperties(tr, elnTypes.ELN_PREVIEW);
+elnTypes.createDataSetTypeWithProperties(tr, elnTypes.ATTACHMENT);
+elnTypes.createDataSetTypeWithProperties(tr, elnTypes.DRAWING_BOARD_EXPERIMENT);
+
+##
+## Experiment Types
+##
+elnTypes.getCommentsScript(tr, "EXPERIMENT");
+elnTypes.createExperimentTypeWithProperties(tr, elnTypes.MATERIALS);
+elnTypes.createExperimentTypeWithProperties(tr, elnTypes.METHODS);
+elnTypes.createExperimentTypeWithProperties(tr, elnTypes.BOARDS);
+elnTypes.createExperimentTypeWithProperties(tr, elnTypes.DEFAULT_EXPERIMENT);
+    
+##
+## Sample Types - Materials
+##
+
+##
+## Sample Types - Non Materials
+##
+elnTypes.EXPERIMENTAL_STEP_ANNOTATIONS = None;
+elnTypes.createSampleTypeWithProperties(tr, elnTypes.EXPERIMENTAL_STEP);
--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/dss/reporting-plugins/eln-lims-api/script.py
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/dss/reporting-plugins/eln-lims-api/script.py
@@ -320,58 +320,73 @@ def getFeaturesFromFeatureVector(tr, parameters, tableBuilder):
 def init(tr, parameters, tableBuilder):
 	inventorySpace = tr.getSpace("DEFAULT_LAB_NOTEBOOK");
 	if inventorySpace == None:
-		elnTypes = tr.getVocabularyForUpdate("ELN_TYPES_METADATA");
-		if elnTypes is not None: # We can only create the data if the ELN metadata is present, this is not true on highly customized systems.
+		elnTypesMetadata = tr.getVocabularyForUpdate("ELN_TYPES_METADATA");
+		elnTypesMetadataMini = tr.getVocabularyForUpdate("ELN_TYPES_METADATA_MINI");
+		if (elnTypesMetadata is not None) or (elnTypesMetadataMini is not None): # We can only create the data if the ELN metadata is present, this is not true on highly customized systems.
 			tr.createNewSpace("MATERIALS", None);
 			
-			tr.createNewProject("/MATERIALS/REAGENTS");
-			antibodyCollection = tr.createNewExperiment("/MATERIALS/REAGENTS/ANTIBODY_COLLECTION", 		"MATERIALS");
-			antibodyCollection.setPropertyValue("NAME", "Antibody Collection");
-			chemicalCollection = tr.createNewExperiment("/MATERIALS/REAGENTS/CHEMICAL_COLLECTION", 		"MATERIALS");
-			chemicalCollection.setPropertyValue("NAME", "Chemical Collection");
-			enzymeCollection = tr.createNewExperiment("/MATERIALS/REAGENTS/ENZYME_COLLECTION", 			"MATERIALS");
-			enzymeCollection.setPropertyValue("NAME", "Enzyme Collection");
-			mediaCollection = tr.createNewExperiment("/MATERIALS/REAGENTS/MEDIA_COLLECTION", 			"MATERIALS");
-			mediaCollection.setPropertyValue("NAME", "Media Collection");
-			solutionBufferCollection = tr.createNewExperiment("/MATERIALS/REAGENTS/SOLUTION_BUFFER_COLLECTION",	"MATERIALS");
-			solutionBufferCollection.setPropertyValue("NAME", "Solution Buffer Collection");
-			
-			tr.createNewProject("/MATERIALS/BACTERIA");
-			bacteriaCollection = tr.createNewExperiment("/MATERIALS/BACTERIA/BACTERIA_COLLECTION_1",		"MATERIALS");
-			bacteriaCollection.setPropertyValue("NAME", "Bacteria Collection 1");
-			
-			tr.createNewProject("/MATERIALS/CELL_LINES");
-			cellLineCollection1 = tr.createNewExperiment("/MATERIALS/CELL_LINES/CELL_LINE_COLLECTION_1",	"MATERIALS");
-			cellLineCollection1.setPropertyValue("NAME", "Cell Line Collection 1");
-			
-			tr.createNewProject("/MATERIALS/FLIES");
-			flyCollection1 = tr.createNewExperiment("/MATERIALS/FLIES/FLY_COLLECTION_1",				"MATERIALS");
-			flyCollection1.setPropertyValue("NAME", "Fly Collection 1");
-			
-			tr.createNewProject("/MATERIALS/YEASTS");
-			yeastCollection1 = tr.createNewExperiment("/MATERIALS/YEASTS/YEAST_COLLECTION_1",			"MATERIALS");
-			yeastCollection1.setPropertyValue("NAME", "Yeast Collection 1");
-			
-			tr.createNewProject("/MATERIALS/PLASMIDS");
-			plasmidCollection1 = tr.createNewExperiment("/MATERIALS/PLASMIDS/PLASMID_COLLECTION_1",		"MATERIALS");
-			plasmidCollection1.setPropertyValue("NAME", "Plasmid Collection 1");
-			
-			tr.createNewProject("/MATERIALS/POLYNUCLEOTIDES");
-			oligoCollection1 = tr.createNewExperiment("/MATERIALS/POLYNUCLEOTIDES/OLIGO_COLLECTION_1",	"MATERIALS");
-			oligoCollection1.setPropertyValue("NAME", "Oligo Collection 1");
-			rnaCollection1 = tr.createNewExperiment("/MATERIALS/POLYNUCLEOTIDES/RNA_COLLECTION_1",	"MATERIALS");
-			rnaCollection1.setPropertyValue("NAME", "RNA Collection 1");
+			##
+			## If not Mini
+			##
+			if elnTypesMetadata is not None:
+				tr.createNewProject("/MATERIALS/REAGENTS");
+				antibodyCollection = tr.createNewExperiment("/MATERIALS/REAGENTS/ANTIBODY_COLLECTION", 		"MATERIALS");
+				antibodyCollection.setPropertyValue("NAME", "Antibody Collection");
+				chemicalCollection = tr.createNewExperiment("/MATERIALS/REAGENTS/CHEMICAL_COLLECTION", 		"MATERIALS");
+				chemicalCollection.setPropertyValue("NAME", "Chemical Collection");
+				enzymeCollection = tr.createNewExperiment("/MATERIALS/REAGENTS/ENZYME_COLLECTION", 			"MATERIALS");
+				enzymeCollection.setPropertyValue("NAME", "Enzyme Collection");
+				mediaCollection = tr.createNewExperiment("/MATERIALS/REAGENTS/MEDIA_COLLECTION", 			"MATERIALS");
+				mediaCollection.setPropertyValue("NAME", "Media Collection");
+				solutionBufferCollection = tr.createNewExperiment("/MATERIALS/REAGENTS/SOLUTION_BUFFER_COLLECTION",	"MATERIALS");
+				solutionBufferCollection.setPropertyValue("NAME", "Solution Buffer Collection");
+				
+				tr.createNewProject("/MATERIALS/BACTERIA");
+				bacteriaCollection = tr.createNewExperiment("/MATERIALS/BACTERIA/BACTERIA_COLLECTION_1",		"MATERIALS");
+				bacteriaCollection.setPropertyValue("NAME", "Bacteria Collection 1");
+				
+				tr.createNewProject("/MATERIALS/CELL_LINES");
+				cellLineCollection1 = tr.createNewExperiment("/MATERIALS/CELL_LINES/CELL_LINE_COLLECTION_1",	"MATERIALS");
+				cellLineCollection1.setPropertyValue("NAME", "Cell Line Collection 1");
+				
+				tr.createNewProject("/MATERIALS/FLIES");
+				flyCollection1 = tr.createNewExperiment("/MATERIALS/FLIES/FLY_COLLECTION_1",				"MATERIALS");
+				flyCollection1.setPropertyValue("NAME", "Fly Collection 1");
+				
+				tr.createNewProject("/MATERIALS/YEASTS");
+				yeastCollection1 = tr.createNewExperiment("/MATERIALS/YEASTS/YEAST_COLLECTION_1",			"MATERIALS");
+				yeastCollection1.setPropertyValue("NAME", "Yeast Collection 1");
+				
+				tr.createNewProject("/MATERIALS/PLASMIDS");
+				plasmidCollection1 = tr.createNewExperiment("/MATERIALS/PLASMIDS/PLASMID_COLLECTION_1",		"MATERIALS");
+				plasmidCollection1.setPropertyValue("NAME", "Plasmid Collection 1");
+				
+				tr.createNewProject("/MATERIALS/POLYNUCLEOTIDES");
+				oligoCollection1 = tr.createNewExperiment("/MATERIALS/POLYNUCLEOTIDES/OLIGO_COLLECTION_1",	"MATERIALS");
+				oligoCollection1.setPropertyValue("NAME", "Oligo Collection 1");
+				rnaCollection1 = tr.createNewExperiment("/MATERIALS/POLYNUCLEOTIDES/RNA_COLLECTION_1",	"MATERIALS");
+				rnaCollection1.setPropertyValue("NAME", "RNA Collection 1");
+			##
+			##
+			##
 			
 			tr.createNewSpace("METHODS", None);
-			tr.createNewProject("/METHODS/PROTOCOLS");
-			generalProtocols = tr.createNewExperiment("/METHODS/PROTOCOLS/GENERAL_PROTOCOLS", 			"METHODS");
-			generalProtocols.setPropertyValue("NAME", "General Protocols");
-			
-			pcrProtocols = tr.createNewExperiment("/METHODS/PROTOCOLS/PCR_PROTOCOLS", 				"METHODS");
-			pcrProtocols.setPropertyValue("NAME", "PCR Protocols");
-			
-			westernBottingProtocols = tr.createNewExperiment("/METHODS/PROTOCOLS/WESTERN_BLOTTING_PROTOCOLS", 	"METHODS");
-			westernBottingProtocols.setPropertyValue("NAME", "Western Botting Protocols");
+			##
+			## If not Mini
+			##
+			if elnTypesMetadata is not None:
+				tr.createNewProject("/METHODS/PROTOCOLS");
+				generalProtocols = tr.createNewExperiment("/METHODS/PROTOCOLS/GENERAL_PROTOCOLS", 			"METHODS");
+				generalProtocols.setPropertyValue("NAME", "General Protocols");
+				
+				pcrProtocols = tr.createNewExperiment("/METHODS/PROTOCOLS/PCR_PROTOCOLS", 				"METHODS");
+				pcrProtocols.setPropertyValue("NAME", "PCR Protocols");
+				
+				westernBottingProtocols = tr.createNewExperiment("/METHODS/PROTOCOLS/WESTERN_BLOTTING_PROTOCOLS", 	"METHODS");
+				westernBottingProtocols.setPropertyValue("NAME", "Western Botting Protocols");
+			##
+			##
+			##
 			
 			tr.createNewSpace("DEFAULT_LAB_NOTEBOOK", None);
 			tr.createNewProject("/DEFAULT_LAB_NOTEBOOK/DEFAULT_PROJECT");
