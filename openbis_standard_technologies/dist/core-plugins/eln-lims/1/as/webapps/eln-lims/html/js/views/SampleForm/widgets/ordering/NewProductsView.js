c9da00c898cbfacfcc4be256445eb4f52b7de431
juanf <juanf>
2016-08-16 14:16:49 +0000

    SSDM-2732 : New products widget (added validation).
    SVN: 36918

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/views/SampleForm/widgets/ordering/NewProductsView.js
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/views/SampleForm/widgets/ordering/NewProductsView.js
@@ -17,7 +17,7 @@ function NewProductsView(newProductsController, newProductsModel) {
 											.append($("<th>").append("Code"))
 											.append($("<th>").append("Price"))
 											.append($("<th>").append("Currency"))
-											.append($("<th>").append("Provider"))
+											.append($("<th>").append("Supplier"))
 											.append($("<th>").append("Quantiy"))
 											.append($("<th>").append(FormUtil.getButtonWithIcon("glyphicon-plus", this.addNewProduct)));
 		
@@ -29,21 +29,74 @@ function NewProductsView(newProductsController, newProductsModel) {
 	}
 	
 	this.addNewProduct = function() {
-		var currencyVocabulary = profile.getVocabularyByCode("CURRENCY");
-		var currencyDropdown = FormUtil.getDropDownForTerms(null, currencyVocabulary.terms, "Currency", true);
+
 		
-		var $newProductsTableRow = $("<tr>")
-		.append($("<td>").append(FormUtil.getTextInputField(null, "Name", true)))
-		.append($("<td>").append(FormUtil.getTextInputField(null, "Code", true)))
-		.append($("<td>").append(FormUtil.getRealInputField(null, "Price", true)))
-		.append($("<td>").append(currencyDropdown))
-		.append($("<td>").append(FormUtil.getIntegerInputField(null, "Provider Code", true)))
-		.append($("<td>").append(FormUtil.getIntegerInputField(null, "Quantiy", true)))
-		.append($("<td>").append(FormUtil.getButtonWithIcon("glyphicon-minus", function() {
-			$(this).parent().parent().remove();
-		})));
+		mainController.serverFacade.searchWithType("SUPPLIER", null, false, function(suppliers){
+			var supplierTerms = [];
+			for(var sIdx = 0; sIdx < suppliers.length; sIdx++) {
+				supplierTerms.push({code : suppliers[sIdx].code, label : suppliers[sIdx].properties["NAME"]});
+			}
+			var supplierDropdown = FormUtil.getDropDownForTerms(null, supplierTerms, "Select a supplier", true);
+			
+			var currencyVocabulary = profile.getVocabularyByCode("CURRENCY");
+			var currencyDropdown = FormUtil.getDropDownForTerms(null, currencyVocabulary.terms, "Select a currency", true);
+			
+			var quantityField = FormUtil.getIntegerInputField(null, "Quantiy", true);
+			quantityField.change(function() {
+				var value = $(this).val();
+				try {
+					var valueParsed = parseInt(value);
+					if("" + valueParsed === "NaN") {
+						Util.showError("Please input a correct quantiy.");
+						$(this).val("");
+					} else {
+						$(this).val(valueParsed);
+					}
+				} catch(err) {
+					Util.showError("Please input a correct quantiy.");
+					$(this).val("");
+				}
+			});
+			
+			var priceField = FormUtil.getRealInputField(null, "Price", true);
+				priceField.change(function() {
+					var value = $(this).val();
+					try {
+						var valueParsed = parseFloat(value);
+						if("" + valueParsed === "NaN") {
+							Util.showError("Please input a correct price.");
+							$(this).val("");
+						} else {
+							$(this).val(valueParsed);
+						}
+					} catch(err) {
+						Util.showError("Please input a correct price.");
+						$(this).val("");
+					}
+				});
+			
+			var codeField = FormUtil.getTextInputField(null, "Code", true);
+			
+			var nameField = FormUtil.getTextInputField(null, "Name", true);
+				nameField.change(function() {
+					codeField.val($(this).val().toUpperCase().replace(" ","_"));
+				});
+			var $newProductsTableRow = $("<tr>")
+			.append($("<td>").append(nameField))
+			.append($("<td>").append(codeField))
+			.append($("<td>").append(priceField))
+			.append($("<td>").append(currencyDropdown))
+			.append($("<td>").append(supplierDropdown))
+			.append($("<td>").append(quantityField))
+			.append($("<td>").append(FormUtil.getButtonWithIcon("glyphicon-minus", function() {
+				$(this).parent().parent().remove();
+			})));
+			
+			$newProductsTableBody.append($newProductsTableRow);
+		});
 		
-		$newProductsTableBody.append($newProductsTableRow);
+		
+		
 	}
 	
 }
