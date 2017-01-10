3a768d9ca92058b108ae869e348d9d432c033881
anttil <anttil>
2017-01-10 13:46:17 +0000

    SSDM-4579: DSS version information not available (because of duplicate ch.systemsx.cisd.openbis.dss.BuildAndEnvironmentInfo in the classpath)
    SVN: 37576

--- plasmid/source/java/ch/systemsx/cisd/plasmid/BuildAndEnvironmentInfo.java
+++ plasmid/source/java/ch/systemsx/cisd/plasmid/BuildAndEnvironmentInfo.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package ch.systemsx.cisd.openbis.dss;
+package ch.systemsx.cisd.plasmid;
 
 import ch.systemsx.cisd.base.utilities.AbstractBuildAndEnvironmentInfo;
 
