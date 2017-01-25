e83945e0d33c32b58e11fb7cc7193c75279471bd
felmer <felmer>
2017-01-25 12:11:26 +0000

    SSDM-4187: Provide project-samples-enabled, api-version, archiving-configured, enabled-technologies in getServerInformation()
    SVN: 37622

--- openbis/source/java/ch/ethz/sis/openbis/generic/server/asapi/v3/ApplicationServerApi.java
+++ openbis/source/java/ch/ethz/sis/openbis/generic/server/asapi/v3/ApplicationServerApi.java
@@ -251,6 +251,7 @@ import ch.systemsx.cisd.openbis.generic.server.business.IPropertiesBatchManager;
 import ch.systemsx.cisd.openbis.generic.server.dataaccess.IDAOFactory;
 import ch.systemsx.cisd.openbis.generic.server.plugin.IDataSetTypeSlaveServerPlugin;
 import ch.systemsx.cisd.openbis.generic.server.plugin.ISampleTypeSlaveServerPlugin;
+import ch.systemsx.cisd.openbis.generic.shared.Constants;
 import ch.systemsx.cisd.openbis.generic.shared.IOpenBisSessionManager;
 import ch.systemsx.cisd.openbis.generic.shared.dto.Session;
 import ch.systemsx.cisd.openbis.generic.shared.dto.SessionContextDTO;
@@ -835,10 +836,10 @@ public class ApplicationServerApi extends AbstractServer<IApplicationServerApi> 
     {
         checkSession(sessionToken);
         Map<String, String> info = new HashMap<String, String>();
-        // info.put("api-version", getMajorVersion() + "." + getMinorVersion());
-        // info.put("project-samples-enabled", Boolean.toString(isProjectSamplesEnabled(null)));
-        // info.put("archiving-configured", Boolean.toString(isArchivingConfigured(null)));
-        // info.put("enabled-technologies", configurer.getResolvedProps().getProperty(Constants.ENABLED_MODULES_KEY));
+        info.put("api-version", getMajorVersion() + "." + getMinorVersion());
+        info.put("project-samples-enabled", Boolean.toString(isProjectSamplesEnabled(null)));
+        info.put("archiving-configured", Boolean.toString(isArchivingConfigured(null)));
+        info.put("enabled-technologies", configurer.getResolvedProps().getProperty(Constants.ENABLED_MODULES_KEY));
         // String disabledText = tryGetDisabledText();
         // if (disabledText != null)
         // {
