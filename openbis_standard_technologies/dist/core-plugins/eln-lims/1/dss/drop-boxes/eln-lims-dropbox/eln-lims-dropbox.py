909852dff67a721ef6b09a31c0e0ed68167a714c
juanf <juanf>
2017-01-30 12:54:43 +0000

    SSDM-4147 : Default ELN Dropbox
    SVN: 37641

---
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/dss/drop-boxes/eln-lims-dropbox/eln-lims-dropbox.py
@@ -0,0 +1,94 @@
+import os
+
+from ch.systemsx.cisd.openbis.generic.client.web.client.exception import UserFailureException
+
+INVALID_FORMAT_ERROR_MESSAGE = "Invalid format for the folder name, should follow the pattern <ENTITY_KIND>+<SPACE_CODE>+[<PROJECT_CODE>+<EXPERIMENT_CODE|<SAMPLE_CODE>]+<OPTIONAL_DATASET_TYPE>+<OPTIONAL_NAME>";
+FAILED_TO_PARSE_SAMPLE_ERROR_MESSAGE = "Failed to parse sample";
+FAILED_TO_PARSE_EXPERIMENT_ERROR_MESSAGE = "Failed to parse experiment";
+SAMPLE_MISSING_ERROR_MESSAGE = "Sample not found";
+EXPERIMENT_MISSING_ERROR_MESSAGE = "Experiment not found";
+MORE_THAN_ONE_FOLDER_ERROR_MESSAGE = "More than one folder found";
+
+def process(transaction):
+	incoming = transaction.getIncoming();
+	folderName = incoming.getName();
+	
+	if not folderName.startswith('.'):
+		datasetInfo = folderName.split("+");
+		entityKind = None;
+		sample = None;
+		experiment = None;
+		datasetType = None;
+		name = None;
+		
+		# Parse entity Kind
+		if len(datasetInfo) >= 1:
+			entityKind = datasetInfo[0];
+		else:
+			raise UserFailureException(INVALID_FORMAT_ERROR_MESSAGE + ":" + FAILED_TO_PARSE_ERROR_MESSAGE);
+		
+		# Parse entity Kind Format
+		if entityKind == "SAMPLE":
+			if len(datasetInfo) >= 3:
+				sampleSpace = datasetInfo[1];
+				sampleCode = datasetInfo[2];
+				sample = transaction.getSample("/" +sampleSpace + "/" + sampleCode);
+				if sample is None:
+					raise UserFailureException(INVALID_FORMAT_ERROR_MESSAGE + ":" + SAMPLE_MISSING_ERROR_MESSAGE);
+				if len(datasetInfo) >= 4:
+					datasetType = datasetInfo[3];
+				if len(datasetInfo) >= 5:
+					name = datasetInfo[4];
+				if len(datasetInfo) > 5:
+					raise UserFailureException(INVALID_FORMAT_ERROR_MESSAGE + ":" + FAILED_TO_PARSE_SAMPLE_ERROR_MESSAGE);
+			else:
+				raise UserFailureException(INVALID_FORMAT_ERROR_MESSAGE + ":" + FAILED_TO_PARSE_SAMPLE_ERROR_MESSAGE);
+		
+		if entityKind == "EXPERIMENT":
+			if len(datasetInfo) >= 4:
+				experimentSpace = datasetInfo[1];
+				projectCode = datasetInfo[2];
+				experimentCode = datasetInfo[3];
+				experiment = transaction.getExperiment("/" +experimentSpace + "/" + projectCode + "/" + experimentCode);
+				if experiment is None:
+					raise UserFailureException(INVALID_FORMAT_ERROR_MESSAGE + ":" + EXPERIMENT_MISSING_ERROR_MESSAGE);
+				if len(datasetInfo) >= 5:
+					datasetType = datasetInfo[4];
+				if len(datasetInfo) >= 6:
+					name = datasetInfo[5];
+				if len(datasetInfo) > 6:
+					raise UserFailureException(INVALID_FORMAT_ERROR_MESSAGE + ":" + FAILED_TO_PARSE_EXPERIMENT_ERROR_MESSAGE);
+			else:
+				raise UserFailureException(INVALID_FORMAT_ERROR_MESSAGE + ":" + FAILED_TO_PARSE_EXPERIMENT_ERROR_MESSAGE);
+		
+		# Create dataset
+		dataSet = None;
+		if datasetType is not None: #Set type if found
+			dataSet = transaction.createNewDataSet(datasetType);
+		else:
+			dataSet = transaction.createNewDataSet();
+		
+		if name is not None:
+			dataSet.setPropertyValue("NAME", name); #Set name if found
+		
+		# Set sample or experiment
+		if sample is not None:
+			dataSet.setSample(sample);
+		else:
+			dataSet.setExperiment(experiment);
+		
+		# Move folder to dataset
+		filesInFolder = incoming.listFiles();
+		
+		# Discard folders started with a . (hidden files)
+		itemsInFolder = 0;
+		datasetItem = None;
+		for item in filesInFolder:
+			if not item.getName().startswith('.'):
+				itemsInFolder = itemsInFolder + 1;
+				datasetItem = item;
+		
+		if itemsInFolder > 1:
+			raise UserFailureException(MORE_THAN_ONE_FOLDER_ERROR_MESSAGE);
+		else:
+			transaction.moveFile(datasetItem.getAbsolutePath(), dataSet);
