c4fd44822319a9e70806334d31097e615ebcccf4
juanf <juanf>
2017-01-30 13:41:49 +0000

    SSDM-4687 : Placeholders on rich text editor
    SVN: 37642

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/lib/ckeditor/js/config.js
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/lib/ckeditor/js/config.js
@@ -7,6 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.extraPlugins='confighelper';
 	config.stylesSet = false;
 	config.toolbarGroups = [
 	    					{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
