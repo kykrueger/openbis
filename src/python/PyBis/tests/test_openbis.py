57b903f02357105277645ba8b0d24cc683703355
Chandrasekhar Ramakrishnan <chandrasekhar.ramakrishnan@id.ethz.ch>
2017-03-17 15:43:48 +0100

    SSDM-4670: Added option to specify the data set code in regisration of a git data set.

--- src/python/PyBis/tests/test_openbis.py
+++ src/python/PyBis/tests/test_openbis.py
@@ -170,6 +170,16 @@ def test_new_git_data_set(openbis_instance):
     openbis_instance.delete_entity('DataSet', result.code, 'Testing.', capitalize=False)
 
 
+def test_new_git_data_set_with_code(openbis_instance):
+    dms_code, dms = create_external_data_management_system(openbis_instance)
+    data_set_code = openbis_instance.create_perm_id()
+    result = openbis_instance.new_git_data_set("GIT_REPO", "./", '12345', dms_code, "/DEFAULT/DEFAULT",
+                                               data_set_code=data_set_code)
+    assert result is not None
+    assert result.code == data_set_code
+    openbis_instance.delete_entity('DataSet', result.code, 'Testing.', capitalize=False)
+
+
 def test_create_perm_id(openbis_instance):
     perm_id = openbis_instance.create_perm_id()
     assert perm_id is not None
