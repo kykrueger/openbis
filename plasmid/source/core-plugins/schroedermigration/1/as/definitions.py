6e159ae4ead3392710cba7eece7d59435cd716ac
barillac <barillac>
2017-03-22 14:40:35 +0000

    schroedermigration added
    SVN: 37934

---
+++ plasmid/source/core-plugins/schroedermigration/1/as/definitions.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ch.systemsx.cisd.openbis.generic.server.jython.api.v1.DataType as DataType
+
+#
+# Helper Methods
+#
+def getPropertyDefinitionByCode(definition, code):
+    for property in definition:
+        if property[0] == code:
+            return property
+    return None
+
+#
+# Scripts
+#
+
+
+#
+# Storage 
+#
+
+def getStorageGroupPropertyCodes():
+    propertyCodes = [];
+    for property in getStorageGroupDefinition():
+        propertyCodes.append(property[0]);
+    return propertyCodes;
+
+def getStorageGroupDefinition():
+    return [
+    ["STORAGE_NAME",         "Physical Storage",        "location",         DataType.CONTROLLEDVOCABULARY,      "FREEZER",            "Storage Name",       None, None, False],
+    ["STORAGE_ROW",          "Physical Storage",        "Storage Row",      DataType.INTEGER,                    None,                "Storage Row",        None, None, False],
+    ["STORAGE_COLUMN",       "Physical Storage",        "Storage Column",   DataType.INTEGER,                    None,                "Storage Column",     None, None, False],
+    ["STORAGE_BOX_NAME",     "Physical Storage",        "box label",        DataType.VARCHAR,                    None,                "Storage Box Name",   None, None, False],
+    ["STORAGE_USER",         "Physical Storage",        "frozen by",        DataType.VARCHAR,                    None,                "Storage User Id",    None, None, False],
+    ["STORAGE_BOX_SIZE",     "Physical Storage",        "box size",         DataType.CONTROLLEDVOCABULARY,      "STORAGE_BOX_SIZE",   "Storage Box size",   None, None, False],
+    ["STORAGE_BOX_POSITION", "Physical Storage",        "position",         DataType.VARCHAR,                    None,                "Storage Box Position",    None, None, False]
+];
+
+
+
+#
+# Sample Types
+#
+
+cellDefinition = [
+    ["CO2",                             "General",                "%CO2",                                DataType.CONTROLLEDVOCABULARY,       "CO2",  "", None, None, False],
+    ["ATCC_NUM",                        "General",                "ATCC No.",                            DataType.VARCHAR,                    None,  "", None, None, False],
+    ["BARCODE",                         "General",                "barcode",                             DataType.VARCHAR,                    None,  "", None, None, False],
+    ["BARCODE_LABEL",                   "General",                "barcode label",                       DataType.VARCHAR,                    None,  "", None, None, False],
+    ["CELL_ID_NR",                      "General",                "cell_id_nr",                          DataType.VARCHAR,                    None,  "", None, None, False],
+    ["CELL_ID_NR_COPY",                 "General",                "cell_id_nr Copy",                     DataType.VARCHAR,                    None,  "", None, None, False],
+    ["NAME",                            "General",                "cell ID",                             DataType.VARCHAR,                    None,  "", None, None, False],
+    ["CELL_ID_COPY",                    "General",                "cell ID Copy",                        DataType.VARCHAR,                    None,  "", None, None, False],
+    ["CLONE",                           "General",                "clone #",                             DataType.INTEGER,                    None,  "", None, None, False],
+    ["COMPARE_FIELD",                   "General",                "compare field",                       DataType.VARCHAR,                    None,  "", None, None, False],
+    ["CONC1",                           "General",                "conc.1",                              DataType.REAL,                       None,  "", None, None, False],
+    ["CONC_UNIT1",                      "General",                "concentration unit 1",                DataType.CONTROLLEDVOCABULARY,       "CONC_UNITS",  "", None, None, False],
+    ["CONC2",                           "General",                "conc.2",                              DataType.REAL,                       None,  "", None, None, False],
+    ["CONC_UNIT2",                      "General",                "concentration unit 2",                DataType.CONTROLLEDVOCABULARY,       "CONC_UNITS",  "", None, None, False],
+    ["CONC3",                           "General",                "conc.3",                              DataType.REAL,                       None,  "", None, None, False],
+    ["CONC_UNIT3",                      "General",                "concentration unit 3",                DataType.CONTROLLEDVOCABULARY,       "CONC_UNITS",  "", None, None, False],
+    ["CONC4",                           "General",                "conc.4",                              DataType.REAL,                       None,  "", None, None, False],
+    ["CONC_UNIT4",                      "General",                "concentration unit 4",                DataType.CONTROLLEDVOCABULARY,       "CONC_UNITS",  "", None, None, False],
+    ["CREATION_DATE",                   "General",                "creation date",                       DataType.TIMESTAMP,                  None,  "", None, None, False],
+    ["DATE",                            "General",                "date",                                DataType.TIMESTAMP,                  None,  "", None, None, False],
+    ["FROZEN",                          "General",                "frozen",                              DataType.TIMESTAMP,                  None,  "", None, None, False],
+    ["GROWTH_TEMPERATURE",              "General",                "growth temperature",                  DataType.CONTROLLEDVOCABULARY,       "GROWTH_TEMPERATURE",  "", None, None, False],
+    ["INVESTIGATOR",                    "General",                "investigator",                        DataType.CONTROLLEDVOCABULARY,       "ALL_LAB_MEMBERS",  "", None, None, False],
+    ["LINE_NAME",                       "General",                "line name",                           DataType.VARCHAR,                    None,  "", None, None, False],
+    ["MEDIUM",                          "General",                "medium",                              DataType.CONTROLLEDVOCABULARY,       "MEDIUM",  "", None, None, False],
+    ["MEDIUM_SPECIALS",                 "General",                "medium specials",                     DataType.VARCHAR,                    None,  "", None, None, False],
+    ["MODIFICATION_DATE",               "General",                "modification date",                   DataType.TIMESTAMP,                  None,  "", None, None, False],
+    ["MODIFIED_BY",                     "General",                "modified by",                         DataType.CONTROLLEDVOCABULARY,       "ALL_LAB_MEMBERS",  "", None, None, False],
+    ["MYCOPLASM_TESTED",                "General",                "mycoplasm_tested",                    DataType.BOOLEAN,                    None,  "", None, None, False],
+    ["OBTAINED_FROM",                   "General",                "obtained from",                       DataType.CONTROLLEDVOCABULARY,       "CELL_OBTAINED_FROM",  "", None, None, False],
+    ["PAGE_NUMBER",                     "General",                "page number",                         DataType.VARCHAR,                    None,  "", None, None, False],
+    ["PARENTAL_CELL",                   "General",                "parental cell",                       DataType.VARCHAR,                    None,  "", None, None, False],
+    ["PASSAGE",                         "General",                "passage",                             DataType.VARCHAR,                    None,  "", None, None, False],
+    ["PERS_CELL_NUMBER",                "General",                "PersCellNumber",                      DataType.VARCHAR,                    None,  "", None, None, False],
+    ["PLASMID_ID1",                     "General",                "plasmid ID 1",                        DataType.VARCHAR,                    None,  "", None, None, False],
+    ["PLASMID_ID2",                     "General",                "plasmid ID 2",                        DataType.VARCHAR,                    None,  "", None, None, False],
+    ["SELECTION_PLASMID1",              "General",                "sel.1",                               DataType.CONTROLLEDVOCABULARY,       "SELECTION",  "", None, None, False],
+    ["SELECTION_PLASMID2",              "General",                "sel.2",                               DataType.CONTROLLEDVOCABULARY,       "SELECTION",  "", None, None, False],
+    ["SELECTION_PLASMID3",              "General",                "sel.3",                               DataType.CONTROLLEDVOCABULARY,       "SELECTION",  "", None, None, False],    
+    ["SELECTION_PLASMID4",              "General",                "sel.4",                               DataType.CONTROLLEDVOCABULARY,       "SELECTION",  "", None, None, False],
+    ["SERIAL_NUMBER",                   "General",                "serial number",                       DataType.INTEGER,                    None,  "", None, None, False],
+    ["SPECIES",                         "General",                "species",                             DataType.CONTROLLEDVOCABULARY,       "CELL_SPECIES",  "", None, None, False],
+    ["VIRUS_ID1",                       "General",                "virus ID 1",                          DataType.VARCHAR,                    None,  "", None, None, False],
+    ["VIRUS_ID2",                       "General",                "virus ID 2",                          DataType.VARCHAR,                    None,  "", None, None, False],
+    ["PUBLISHED_IN",                    "Reference",              "reference",                           DataType.VARCHAR,                    None,  "", None, None, False],
+    ["COMMENTS",                        "Comments",               "info",                                DataType.VARCHAR,                    None,  "", None, None, False]
+];
+
+
---
+++ plasmid/source/core-plugins/schroedermigration/1/dss/drop-boxes/schroedermigration/migration.py
@@ -0,0 +1,398 @@
+# some_file.py
+import sys
+import definitions
+import definitionsVoc
+import re
+import random
+from datetime import datetime
+from ch.systemsx.cisd.openbis.generic.shared.api.v1.dto import SearchCriteria, SearchSubCriteria
+import ch.systemsx.cisd.openbis.generic.server.jython.api.v1.DataType as DataType
+
+import java.lang.Class as Class
+import java.sql.DriverManager as DriverManager
+
+##
+## Generic Process Method
+##
+notMigratedEntities = {
+                       "ENTITY_TYPE" : {"ENTITY ID" : {"ERROR" : "TIMES" }}
+                      }
+
+def addNotMigratedEntity(type, entityID, error):
+    if type not in notMigratedEntities:
+        notMigratedEntities[type] = {}
+    if entityID not in notMigratedEntities[type]:
+        notMigratedEntities[type][entityID] = {}
+    if error not in notMigratedEntities[type][entityID]:
+        notMigratedEntities[type][entityID][error] = 1
+    else:
+        notMigratedEntities[type][entityID][error] += 1
+
+def printNotMigratedEntities():
+    print "--- Not Migrated Entities Report"
+    for type in notMigratedEntities:
+        print "Type: [" + str(type) + "]"
+        for id in notMigratedEntities[type]:
+            for error in notMigratedEntities[type][id]:
+                print "Id: [" + str(id) + "] Error: " + str(error) + " Times: " + str(notMigratedEntities[type][id][error])
+    print "---"
+
+def process(tr):
+    print "START!"
+    createDataHierarchy(tr)
+    for adaptor in adaptors:
+        print "- ADAPTOR [" + adaptor.__class__.__name__ + "] START"
+        while adaptor.next():
+            entity = adaptor.getEntity()
+            print "* ENTITY [" + str(entity.getIdentifier(tr)) + "]"
+            try:
+                if not entity.isInOpenBIS(tr):
+                    entity.write(tr)
+                    #print entity.getIdentifier(tr) + " - Updated"
+                else:
+                    addNotMigratedEntity(adaptor.__class__.__name__, entity.getIdentifier(tr), "Already in openBIS")
+            except Exception, error:
+                    addNotMigratedEntity(adaptor.__class__.__name__, entity.getIdentifier(tr), str(error.args))
+                    #print entity.getIdentifier(tr) + " - Already up to date"
+        print "- ADAPTOR [" + adaptor.__class__.__name__ + "] FINISH"
+    print "REPORT START"
+    printNotMigratedEntities()
+    definitionsVoc.printCreatedTerms()
+    print "REPORT FINISH"
+    print "FINISH!"
+
+##
+## Help Methods
+##
+def setEntityProperties(tr, definition, entity, properties):
+    for propertyCode, propertyValue in properties.iteritems():
+            propertyDefinition = definitions.getPropertyDefinitionByCode(definition, propertyCode)
+            if propertyValue is not None:
+                propertyValue =  unicode(propertyValue)
+
+            if propertyDefinition is not None and propertyDefinition[3] == DataType.TIMESTAMP and propertyValue is not None:
+                date_val = datetime.strptime(propertyValue, "%Y")
+                propertyValue = datetime.strftime(date_val, "%Y-%m-%d")
+            
+            if propertyDefinition is not None and propertyDefinition[3] == DataType.CONTROLLEDVOCABULARY and propertyValue is not None:
+                possiblePropertyValue = definitionsVoc.getVocabularyTermCodeForVocabularyAndTermLabel(propertyDefinition[4], propertyValue)
+                if possiblePropertyValue is not None:
+                    propertyValue = possiblePropertyValue
+                else:  #We rely on the Add Hock Terms if is None, since there is no API we create a new one
+                    #Create new vocabulary term
+                    codeToUse = re.sub(r'\W+','_',propertyValue)
+                    labelToUse = propertyValue
+                    if len(codeToUse) is 0:
+                        codeToUse = "None" + str(random.random())
+                    if len(codeToUse) > 60:
+                        codeToUse = codeToUse[:50]
+                    #Uses new vocabulary term
+                    newTerm = definitionsVoc.createVocabularyTerm(tr, propertyDefinition[4], codeToUse, labelToUse)
+                    propertyValue = newTerm.getCode()
+                    print "* WARNING ENTITY [" + entity.getCode() + "]: for Vocabulary [" + propertyDefinition[4] + "], found value not in list: [" + repr(labelToUse) + "]. Created new term with code [" + codeToUse + "]"
+            
+            if propertyDefinition is not None: #Sometimes special fields are added for other purposes, these should not be set
+                entity.setPropertyValue(propertyCode, propertyValue)
+
+##
+## Generic Pattern
+##
+class EntityAdaptor:
+    entities = None
+    entitiesIdx = None
+    definition = None
+    
+    def init(self):
+        self.entities = []
+        self.entitiesIdx = -1
+        pass
+    
+    def next(self):
+        if self.entities is None and self.entitiesIdx is None:
+            self.init()
+        self.entitiesIdx += 1
+        if len(self.entities) > self.entitiesIdx:
+            return True
+        else:
+            return False
+    
+    def addEntity(self, values):
+        self.entities.append(OpenBISDTO(values, self.definition))
+    
+    def getEntity(self):
+        return self.entities[self.entitiesIdx]
+    
+class OpenBISDTO:
+    values = {}
+    definition = None
+    
+    def __init__(self, values, definition):
+        self.values = values
+        self.definition = definition
+    
+    def getIdentifier(self, tr):
+        pass
+    
+    def isInOpenBIS(self, tr):
+        pass
+    
+    def write(self, tr):
+        pass
+
+##
+## Costumer specific logic: generic part
+##
+experimentCache = {}
+sampleCache = {}
+sampleID2Sample = {}
+
+def getExperimentForUpdate(experimentIdentifier, experimentType, tr):
+    if experimentIdentifier not in experimentCache:
+        #print "Cache failed " + experimentIdentifier + ":" + str(experimentType)
+        experiment = tr.getExperimentForUpdate(experimentIdentifier)
+        if experiment is None and experimentType is not None:
+            #print "Cache Create " + experimentIdentifier + ":" + str(experimentType)
+            experiment = tr.createNewExperiment(experimentIdentifier,         experimentType)
+        if experiment is not None:
+             experimentCache[experimentIdentifier] = experiment
+    else:
+        pass
+        #print "Cache hit " + experimentIdentifier + ":" + str(experimentType)
+    if experimentIdentifier not in experimentCache:
+         return None
+    else:
+         return experimentCache[experimentIdentifier]
+     
+def getSampleForUpdate(sampleIdentifier, sampleType, tr):
+    if sampleIdentifier not in sampleCache:
+         #print "Cache failed " + sampleIdentifier + ":" + str(sampleType)
+         sample = tr.getSampleForUpdate(sampleIdentifier)
+         if sample is None and sampleType is not None:
+             #print "Cache Create " + sampleIdentifier + ":" + str(sampleType)
+             if sampleType == "CELL":
+             	experiment = getExperimentForUpdate("/MATERIALS/CELL_LINES/CELL_LINE_COLLECTION_1", sampleType, tr)             
+             sample = tr.createNewSample(sampleIdentifier, sampleType)
+             sample.setExperiment(experiment)
+         if sample is not None:
+             sampleCache[sampleIdentifier] = sample
+    else:
+        pass
+        #print "Cache hit " + sampleIdentifier + ":" + str(sampleType)
+        
+    if sampleIdentifier not in sampleCache:
+         return None
+    else:
+         return sampleCache[sampleIdentifier]
+
+class FileMakerEntityAdaptor(EntityAdaptor):
+    connection = None
+    selectQuery = None
+    
+    def __init__(self, fileMakerConnString, fileMakerUser, fileMakerPass, db):
+        Class.forName("com.filemaker.jdbc.Driver").newInstance()
+        self.connection = DriverManager.getConnection(fileMakerConnString+db,fileMakerUser, fileMakerPass)
+    
+    def init(self):
+        EntityAdaptor.init(self)
+        
+        preparedStatement = self.connection.prepareStatement(self.selectQuery)
+        result = preparedStatement.executeQuery()
+        
+        while result.next():
+            values = {}
+            for property in self.definition:
+                values[property[0]] = result.getString(property[2])
+            self.addEntity(values)
+        result.close()
+        preparedStatement.close()
+
+##
+## Customer specific logic: different sample types
+##
+class FMSchroederOpenBISDTO(OpenBISDTO):
+        def isSampleCacheable(self):
+            return True
+        
+        def isInOpenBIS(self, tr):
+            code = self.getIdentifier(tr)
+            if (code is not None) and (' ' not in code):
+                if self.isSampleCacheable():
+                    sampleID2Sample[self.values["NAME"]] = self.values
+                sample = getSampleForUpdate("/MATERIALS/"+code, None, tr)
+                if sample is not None:
+                    lastModificationData = self.values["MODIFICATION_DATE"].strip()
+                    lastModificationData = str(datetime.strptime(lastModificationData, "%Y-%m-%d"))[:10]
+                    lastModificationOpenBIS = sample.getPropertyValue("MODIFICATION_DATE")[:10]
+                    return lastModificationOpenBIS == lastModificationData
+                else :
+                    return False
+            else:
+                print "* ERROR [" + str(code) + "] - Invalid Code found for '" + self.__class__.__name__ + "'"
+                raise Exception('Invalid Code found ' + str(code))
+
+class FMSchroederBoxAdaptor(FileMakerEntityAdaptor):
+    selectBoxQuery = None
+    entityIdFieldName = None
+    entityCodeFieldName = None
+    
+    def addEntity(self, values):
+        self.entities.append(FMSchroederEntityBoxOpenBISDTO(values, self.definition))
+        
+    def init(self):
+        #print "Reading boxes for: " + self.__class__.__name__
+        emptyBox = 0
+        boxes = {}
+        EntityAdaptor.init(self)
+        preparedStatement = self.connection.prepareStatement(self.selectBoxQuery)
+        result = preparedStatement.executeQuery()
+        while result.next():
+            entityId = unicode(result.getString(self.entityIdFieldName))
+            if entityId is not None:
+                if entityId in sampleID2Sample:
+                    antibodyNumber = sampleID2Sample[entityId][self.entityCodeFieldName]
+                    if antibodyNumber is not None:
+                        values = {}
+                        values["STORAGE_NAME"] = result.getString("location")
+                        values["STORAGE_ROW"] = "1"
+                        values["STORAGE_COLUMN"] = "1"
+                        values["STORAGE_BOX_NAME"] = result.getString("box label")
+                        values["STORAGE_BOX_SIZE"] = result.getString("box size")
+                        values["STORAGE_USER"] = result.getString("owner")
+                        values["STORAGE_BOX_POSITION"] = result.getString("position")
+                        
+                        allBoxes = []
+                        if antibodyNumber in boxes:
+                            allBoxes = boxes[antibodyNumber]
+                        else:
+                            boxes[antibodyNumber] = allBoxes
+                        allBoxes.append(values)
+                else:
+                    #The antibody is not there. What the *#%$&
+                    emptyBox += 1
+            else:
+                #The antibody is not there. What the *#%$&
+                emptyBox += 1
+        
+        print "- ERROR ADAPTOR Boxes positions with empty entityId for " + self.__class__.__name__ + ":" + str(emptyBox)
+        
+        for entiyCode, allBoxes in boxes.iteritems():
+            self.addEntity({
+                        "*CODE" : entiyCode,
+                        "*BOXESLIST" : allBoxes
+            })
+        
+        result.close()
+        preparedStatement.close()
+
+class FMSchroederEntityBoxOpenBISDTO(OpenBISDTO):
+    def getIdentifier(self, tr):
+        return self.values["*CODE"]
+    
+    def write(self, tr):
+        sample = getSampleForUpdate("/MATERIALS/"+self.values["*CODE"], None, tr)
+        print "* INFO Boxes size: " + str(len(self.values["*BOXESLIST"]))
+        #Delete old boxes
+        for boxNum in range(1, definitions.numberOfStorageGroups+1):
+            for propertyCode in definitions.getStorageGroupPropertyCodes():
+                sample.setPropertyValue(propertyCode + "_" + str(boxNum), None)
+        
+        #Add new boxes
+        boxNum = 0
+        for box in self.values["*BOXESLIST"]:
+            boxNum += 1
+            for propertyCode, propertyValue in box.iteritems():
+                if propertyCode == "STORAGE_NAME":
+                    freezerName = definitionsVoc.getVocabularyTermCodeForVocabularyAndTermLabel("FREEZER", propertyValue)
+                    if freezerName is None:
+                        #print repr("NOT FOUND FEEZER: " + self.values["ANTIBODY_ID_NR"] + " : '" + unicode(propertyValue) + "'")
+                        propertyValue = None
+                    else:
+                        propertyValue = freezerName
+                
+                if propertyValue is not None:
+                    propertyValue =  unicode(propertyValue)
+                    sample.setPropertyValue(propertyCode + "_" + str(boxNum), propertyValue)
+    
+    def isBoxPressent(self, boxSignature, tr):
+        sample = getSampleForUpdate("/MATERIALS/"+self.values["*CODE"], None, tr)
+        if sample is not None:
+            for boxNum in range(1, definitions.numberOfStorageGroups+1):
+                storedSignature = "";
+                for propertyCode in definitions.getStorageGroupPropertyCodes():
+                    propertyValue = sample.getPropertyValue(propertyCode + "_" + str(boxNum))
+                    if propertyValue is not None:
+                        propertyValue = unicode(propertyValue)
+                        storedSignature += propertyValue
+                if storedSignature == boxSignature:
+                    #print "Found Box " + storedSignature.encode('ascii', 'ignore')
+                    return True
+        return False
+    
+    def isInOpenBIS(self, tr):
+        for box in self.values["*BOXESLIST"]:
+            boxSignature = "";
+            for propertyCode in definitions.getStorageGroupPropertyCodes():
+                propertyValue = box[propertyCode]
+                if propertyCode == "STORAGE_NAME":
+                    propertyValue = definitionsVoc.getVocabularyTermCodeForVocabularyAndTermLabel("FREEZER", propertyValue)
+                if propertyCode == "STORAGE_USER":
+                    propertyValue = definitionsVoc.getVocabularyTermCodeForVocabularyAndTermLabel("LAB_MEMBERS_INITIALS", propertyValue)
+                    
+                if propertyValue is not None:
+                    propertyValue = unicode(propertyValue)
+                    boxSignature += propertyValue
+            if not self.isBoxPressent(boxSignature, tr):
+                return False
+        return True
+
+
+##
+## Cells
+##
+class CellAdaptor(FileMakerEntityAdaptor):
+    
+    def init(self):
+        self.selectQuery = "SELECT * FROM \"boxit cells\""
+        self.definition = definitions.cellDefinition
+        FileMakerEntityAdaptor.init(self)
+    
+    def addEntity(self, values):
+        self.entities.append(CellOpenBISDTO(values, self.definition))
+        
+class CellOpenBISDTO(FMSchroederOpenBISDTO):
+    def write(self, tr):
+        code = self.values["CELL_ID_NR_COPY"]
+        if code is not None and code.startswith("c_"):
+            sample = getSampleForUpdate("/MATERIALS/"+code,"CELL", tr)
+            setEntityProperties(tr, self.definition, sample, self.values);
+    
+    def getIdentifier(self, tr):
+        code = self.values["CELL_ID_NR_COPY"]
+        return code
+
+class CellBoxAdaptor(FMSchroederBoxAdaptor):
+    selectBoxQuery = "SELECT * FROM \"cell boxes\""
+    entityIdFieldName = "cell ID"
+    entityCodeFieldName = "CELL_ID_NR_COPY"
+
+
+
+
+
+fmConnString = "jdbc:filemaker://127.0.0.1/"
+#fmConnString = "jdbc:filemaker://fm.ethz.ch/"
+fmUser= "designer"
+fmPass = "seattle"
+
+adaptors = [ 
+
+            CellAdaptor(fmConnString, fmUser, fmPass, "boxit-cells"),
+            CellBoxAdaptor(fmConnString, fmUser, fmPass, "boxit-cells_boxes"),
+
+             ]
+           
+            
+            
+
+        
+        
