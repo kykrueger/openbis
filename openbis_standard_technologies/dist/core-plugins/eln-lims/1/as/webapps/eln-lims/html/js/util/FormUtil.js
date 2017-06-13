2492706e1df94271e87cb176a9d167d60b172fa6
juanf <juanf>
2017-06-13 09:34:34 +0000

    SSDM-5079 : corrected dropdown placeholder message
    SVN: 38343

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/util/FormUtil.js
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/util/FormUtil.js
@@ -190,7 +190,7 @@ var FormUtil = new function() {
 				$component.attr('required', '');
 			}
 			
-			$component.append($("<option>").attr('value', '').attr('selected', '').attr('disabled', '').text("Select an " + ELNDictionary.sample + " type"));
+			$component.append($("<option>").attr('value', '').attr('selected', '').attr('disabled', '').text("Select an Storage"));
 			for(var idx = 0; idx < storageConfigurations.length; idx++) {
 				var storageConfiguration = storageConfigurations[idx];
 				var label = null;
