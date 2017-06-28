16789f39ceab65a57d8d6a8dcfca309e5d4d014b
vermeul <swen@ethz.ch>
2017-06-29 01:07:39 +0200

    SSDM-5306: add sequence number if file already exists

--- src/python/ELNJupyter/elnjupyter/server.py
+++ src/python/ELNJupyter/elnjupyter/server.py
@@ -39,10 +39,24 @@ class CreateNotebook(tornado.web.RequestHandler):
 
         # create necessary directories
         os.makedirs(os.path.dirname(notebook_file), exist_ok=True)
+        
+        # add sequence to the filename if file already exists
+        notebook_file_new = notebook_file
+        i = 1
+        while os.path.isfile(notebook_file_new):
+            i += 1
+            notebook_file_new = "{} {}".format(notebook_file, i)
+        notebook_file = notebook_file_new
+
         with open(notebook_file, 'wb') as f:
             f.write(content)
         os.chown(notebook_file, user.pw_uid, user.pw_gid)
         print(notebook_file)
+        
+        link_to_notebook = {
+            "url": "http://localhost:8888/notebooks/" +  os.path.join(folder, os.path.basename(notebook_file))
+        }
+        self.write(json.dumps(link_to_notebook))
 
     def send_error(self, status_code=500, **kwargs):
         self.set_status(status_code)
