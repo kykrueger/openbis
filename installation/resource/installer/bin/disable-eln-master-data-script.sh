6f0ac084e100b0dd53258777ba503b214f12ffce
juanf <juanf>
2017-09-13 11:39:26 +0000

    SSDM-5584 : Bugfix, disable master data script in new version 3
    SVN: 38723

--- installation/resource/installer/bin/disable-eln-master-data-script.sh
+++ installation/resource/installer/bin/disable-eln-master-data-script.sh
@@ -8,6 +8,8 @@ if [ "$ELN_MASTER_DATA" == "false" ]; then
     echo "Disabling ELN master data script";
     mv $BASE/../servers/core-plugins/eln-lims/1/as/initializemasterdataminimum.py $BASE/../servers/core-plugins/eln-lims/1/as/initialize-master-data.py;
     mv $BASE/../servers/core-plugins/eln-lims/2/as/initializemasterdataminimum.py $BASE/../servers/core-plugins/eln-lims/2/as/initialize-master-data.py;
+    mv $BASE/../servers/core-plugins/eln-lims/3/as/initializemasterdataminimum.py $BASE/../servers/core-plugins/eln-lims/3/as/initialize-master-data.py;
 fi
 touch $BASE/../servers/core-plugins/eln-lims/1/as/.eln-master-data-installed
 touch $BASE/../servers/core-plugins/eln-lims/2/as/.eln-master-data-installed
+touch $BASE/../servers/core-plugins/eln-lims/3/as/.eln-master-data-installed
