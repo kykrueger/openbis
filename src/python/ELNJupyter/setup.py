0297967827cf3b94a856de67195a4491e0ba10ac
vermeul <swen@ethz.ch>
2017-10-02 17:05:17 +0200

    updated to 1.2.3

--- src/python/ELNJupyter/setup.py
+++ src/python/ELNJupyter/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='elnjupyter',
-    version='0.1.2',
+    version='0.1.3',
     description='A webservice to create jupyter notebooks in the users home directory',
     url='https://sissource.ethz.ch/sis/pybis/',
     author='SIS | ID | ETH Zuerich',
