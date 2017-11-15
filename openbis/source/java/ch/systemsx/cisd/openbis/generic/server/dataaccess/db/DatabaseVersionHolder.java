66cae01899f1b928dbc7d9ee8eb544a2baaf555a
pkupczyk <pkupczyk>
2017-11-15 18:35:44 +0000

    SSDM-5661 : AS V3 API : Support for semantic annotations - add unique constraint to semantic_annotations table on perm_id column
    SVN: 38908

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
@@ -24,7 +24,7 @@ package ch.systemsx.cisd.openbis.generic.server.dataaccess.db;
 public final class DatabaseVersionHolder
 {
     /** Current version of the database. */
-    private static final String DATABASE_VERSION = "167"; // S261
+    private static final String DATABASE_VERSION = "168"; // S266
 
     private DatabaseVersionHolder()
     {
