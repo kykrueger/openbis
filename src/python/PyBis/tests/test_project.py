0ab5f8d538cb13bbea328891f6ed76a09c5de07a
vermeul <swen@ethz.ch>
2017-12-19 17:00:15 +0100

    fixed project attachments, added tests

--- src/python/PyBis/tests/test_project.py
+++ src/python/PyBis/tests/test_project.py
@@ -4,6 +4,7 @@ import re
 
 import pytest
 import time
+import os
 from pybis import DataSet
 from pybis import Openbis
 
@@ -28,3 +29,23 @@ def test_create_delete_project(space):
     with pytest.raises(ValueError):
         project_no_longer_exists=o.get_project(project_name)
         assert "project {} should have been deleted".format(project_name) is None
+    
+
+def test_create_project_with_attachment(space):
+    o=space.openbis
+
+    timestamp = time.strftime('%a_%y%m%d_%H%M%S').upper()
+    project_name = 'project_'+timestamp
+    filename = os.path.join(os.path.dirname(__file__), 'testfile')
+
+    if not os.path.exists(filename):
+        raise ValueError("File not found: {}".format(filename))
+
+    project=o.new_project(space=space, code=project_name, attachments=filename)
+    assert project.attachments is not None
+    project.save()
+
+    project_exists=o.get_project(project_name)
+    assert project_exists is not None
+    assert project_exists.attachments is not None
+    
