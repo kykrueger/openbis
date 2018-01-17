f78686701a2119cd5a71de48c0eddf3bd977749b
felmer <felmer>
2018-01-17 07:03:06 +0000

    SSDM-5724: No role assignments for projects if project authorization is disabled.
    SVN: 39113

--- openbis/source/java/ch/ethz/sis/openbis/generic/server/asapi/v3/executor/roleassignment/CreateRoleAssignmentExecutor.java
+++ openbis/source/java/ch/ethz/sis/openbis/generic/server/asapi/v3/executor/roleassignment/CreateRoleAssignmentExecutor.java
@@ -41,6 +41,7 @@ import ch.ethz.sis.openbis.generic.server.asapi.v3.helper.entity.progress.Create
 import ch.systemsx.cisd.common.exceptions.UserFailureException;
 import ch.systemsx.cisd.openbis.generic.server.business.bo.DataAccessExceptionTranslator;
 import ch.systemsx.cisd.openbis.generic.server.dataaccess.IDAOFactory;
+import ch.systemsx.cisd.openbis.generic.shared.authorization.IAuthorizationConfig;
 import ch.systemsx.cisd.openbis.generic.shared.basic.dto.RoleWithHierarchy.RoleCode;
 import ch.systemsx.cisd.openbis.generic.shared.dto.AuthorizationGroupPE;
 import ch.systemsx.cisd.openbis.generic.shared.dto.PersonPE;
@@ -76,6 +77,9 @@ public class CreateRoleAssignmentExecutor
     @Autowired
     private ISetRoleAssignmentProjectExecutor setRoleAssignmentProjectExecutor;
     
+    @Autowired
+    private IAuthorizationConfig authorizationConfig;
+
     @Override
     protected IObjectId getId(RoleAssignmentPE entity)
     {
@@ -139,6 +143,10 @@ public class CreateRoleAssignmentExecutor
         if (creation.getSpaceId() != null && creation.getProjectId() != null)
         {
             throw new UserFailureException("A space and a project have been specified.");
+        }
+        if (creation.getProjectId() != null && authorizationConfig.isProjectLevelEnabled() == false)
+        {
+            throw new UserFailureException("Project authorization isn't enabled.");
         }
     }
 
