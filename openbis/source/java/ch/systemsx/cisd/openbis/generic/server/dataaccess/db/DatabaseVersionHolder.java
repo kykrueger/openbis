2da35d95172fd7e86416862613c9ec0d9b97b182
Antti Luomi <antti.luomi@id.ethz.ch>
2018-03-05 07:26:02 +0100

    SSDM-5645: Remove schema change

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
@@ -24,7 +24,7 @@ package ch.systemsx.cisd.openbis.generic.server.dataaccess.db;
 public final class DatabaseVersionHolder
 {
     /** Current version of the database. */
-    private static final String DATABASE_VERSION = "170"; // S270?
+    private static final String DATABASE_VERSION = "169"; // S268
 
     private DatabaseVersionHolder()
     {
