b19cf80e96074c58046f739838b38d119c28688b
Yves Noirjean <yves.noirjean@id.ethz.ch>
2018-03-23 14:02:52 +0100

    obis: vagrant - fixed git-annex installation for all users

--- src/vagrant/obis/initialize/install_obis.sh
+++ src/vagrant/obis/initialize/install_obis.sh
@@ -7,15 +7,17 @@ sudo pip3 install pyOpenSSL
 
 if [ ! -L /usr/bin/git ]; then
 
-    pushd ~obis $@ > /dev/null
+    pushd / $@ > /dev/null
 
-    wget https://downloads.kitenet.net/git-annex/linux/current/git-annex-standalone-amd64.tar.gz
-    tar -xvvf git-annex-standalone-amd64.tar.gz
+    sudo wget https://downloads.kitenet.net/git-annex/linux/current/git-annex-standalone-amd64.tar.gz
+    sudo tar -xvvf git-annex-standalone-amd64.tar.gz
 
-    sudo ln -s /home/obis/git-annex.linux/git /usr/bin/git
-    sudo ln -s /home/obis/git-annex.linux/git-annex /usr/bin/git-annex
+    sudo ln -s /git-annex.linux/git /usr/bin/git
+    sudo ln -s /git-annex.linux/git-annex /usr/bin/git-annex
 
-    sudo chown -R obis:obis /home/obis/git-annex.linux
+    sudo chmod -R 777 /git-annex.linux
+    sudo chmod -R 777 /usr/bin/git
+    sudo chmod -R 777 /usr/bin/git-annex
 
     popd $@ > /dev/null
 
--- src/vagrant/obis/initialize/install_openbis.sh
+++ src/vagrant/obis/initialize/install_openbis.sh
@@ -1,13 +1,19 @@
 #!/bin/env bash
 
-pushd . $@ > /dev/null
+if [ ! -L /openbis_installed ]; then
 
-ob_dir=$(find /vagrant/initialize/ -maxdepth 1 -type d -name openBIS-installation-standard-technologies-* | head -n1)
-echo "Installing openBIS from $ob_dir"
-cp /vagrant/config/openbis/console.properties $ob_dir
-chmod a+rw "$ob_dir/console.properties"
+    pushd . $@ > /dev/null
 
-sudo -u openbis cd ~openbis/
-sudo su openbis -c "export ADMIN_PASSWORD=admin && export ETLSERVER_PASSWORD=etlserver && $ob_dir/run-console.sh"
+    ob_dir=$(find /vagrant/initialize/ -maxdepth 1 -type d -name openBIS-installation-standard-technologies-* | head -n1)
+    echo "Installing openBIS from $ob_dir"
+    cp /vagrant/config/openbis/console.properties $ob_dir
+    chmod a+rw "$ob_dir/console.properties"
 
-popd $@ > /dev/null
+    sudo -u openbis cd ~openbis/
+    sudo su openbis -c "export ADMIN_PASSWORD=admin && export ETLSERVER_PASSWORD=etlserver && $ob_dir/run-console.sh"
+
+    sudo touch /openbis_installed
+
+    popd $@ > /dev/null
+
+fi
