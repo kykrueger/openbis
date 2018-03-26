296cb56ec8a2a99dd18805ae79d5bd00a523172f
Yves Noirjean <yves.noirjean@id.ethz.ch>
2018-03-26 15:19:22 +0200

    obis: vagrant - fixing connection from obisclient to obisserver machine

--- src/vagrant/obis/initialize/install_openbis.sh
+++ src/vagrant/obis/initialize/install_openbis.sh
@@ -12,6 +12,8 @@ if [ ! -L /openbis_installed ]; then
     sudo -u openbis cd ~openbis/
     sudo su openbis -c "export ADMIN_PASSWORD=admin && export ETLSERVER_PASSWORD=etlserver && $ob_dir/run-console.sh"
 
+    sudo su openbis -c "sed -i '/host-address = /c\host-address = https://obisserver' /home/openbis/servers/datastore_server/etc/service.properties"
+
     sudo touch /openbis_installed
     sudo chmod 777 /openbis_installed
 
