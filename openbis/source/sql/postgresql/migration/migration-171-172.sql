492d3cd951ff891c54ef8a6d9fc09454230a6ebe
Antti Luomi <antti.luomi@id.ethz.ch>
2018-05-11 09:15:31 +0200

    Revert "SSDM-6070: Polyhub - CORE: Flag to Archive - DB"
    This reverts commit a05b3f0c61924c72cd183e0ad7eb5a9ea0ad75ce.

--- openbis/source/sql/postgresql/migration/migration-171-172.sql
+++
@@ -1,7 +0,0 @@
--- Migration from 171 to 172
-
-------------------------------------------------------------------------------------
---  Purpose:  SSDM-6070: Add archiving request flag to physical datasets
-------------------------------------------------------------------------------------
-
-ALTER TABLE external_data ADD COLUMN archiving_requested BOOLEAN_CHAR NOT NULL DEFAULT 'F'
