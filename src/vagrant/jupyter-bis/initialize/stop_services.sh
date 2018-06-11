b1077e52a24688d9de711450cd2c2b91e57751cc
Yves Noirjean <yves.noirjean@id.ethz.ch>
2018-06-11 14:19:09 +0200

    removed pybis/src/vagrant

--- src/vagrant/jupyter-bis/initialize/stop_services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-# Stop openbis and jupyterhub
-
-# stop openbis
-sudo -u openbis /home/openbis/bin/alldown.sh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$DIR/stop_jupyterhub.sh
