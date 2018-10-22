a6c614f66bff2800ea5143777924a210ce6eed2a
juanf <juanf@bsse-bs-dock-dhcp-490.ethz.ch>
2018-10-22 13:53:20 +0200

    SSDM-7275 : New plugin system for the Microscopy plugin

---
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/plugins/ELNLIMSPlugin.js
@@ -0,0 +1,27 @@
+function ELNLIMSPlugin() {
+	this.init();
+}
+
+$.extend(ELNLIMSPlugin.prototype, {
+	init: function() {
+	
+	},
+	sampleTypeDefinitionsExtension : {
+	
+	},
+	dataSetTypeDefinitionsExtension : {
+	
+	},
+	sampleFormTop : function($container, model) {
+	
+	},
+	sampleFormBottom : function($container, model) {
+	
+	},
+	dataSetFormTop : function($container, model) {
+	
+	},
+	dataSetFormBottom : function($container, model) {
+	
+	}
+});
