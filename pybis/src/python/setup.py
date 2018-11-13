04d52677f91b6ee4c476fcafeb8497546bcbd96f
vermeul <swen@ethz.ch>
2018-11-13 16:51:41 +0100

    optimised package for pypi, now displays README

--- pybis/src/python/setup.py
+++ pybis/src/python/setup.py
@@ -4,21 +4,21 @@ import sys
 if sys.version_info < (3,3):
     sys.exit('Sorry, Python < 3.3 is not supported')
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='PyBIS',
-    version= '1.7.2',
+    version= '1.7.3',
     author='Swen Vermeul |  ID SIS | ETH Zürich',
     author_email='swen@ethz.ch',
     description='openBIS connection and interaction, optimized for using with Jupyter',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://sissource.ethz.ch/sispub/openbis/tree/master/pybis',
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     license='BSD',
     install_requires=[
         'pytest',
