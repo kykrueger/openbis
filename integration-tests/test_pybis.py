6fc6796d537d6f10e3c471d65cdf6e48f9fe2809
Yves Noirjean <yves.noirjean@id.ethz.ch>
2018-12-11 11:08:08 +0100

    SSDM-7634: added minimal pybis test

---
+++ integration-tests/test_pybis.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# encoding=utf8
+#!/usr/bin/python
+#
+# Requirement:
+#   The pybis module must be available.
+
+import settings
+import systemtest.testcase
+import pybis
+
+class TestCase(systemtest.testcase.TestCase):
+
+    def execute(self):
+        self._init_openbis()
+        openbis = self._get_openbis()
+        self._test_login(openbis)
+        self._test_server_information(openbis)
+        self._test_logout(openbis)
+
+    def _init_openbis(self):
+        self.installOpenbis()
+        openbisController = self.createOpenbisController()
+        openbisController.createTestDatabase("openbis")
+        openbisController.allUp()
+
+    def _get_openbis(self):
+        return pybis.Openbis(url="https://localhost:8443", verify_certificates=False)
+
+    def _test_login(self, openbis):
+        openbis.login('admin', 'admin', save_token=True)
+        assert openbis.is_session_active() == True
+
+    def _test_server_information(self, openbis):
+        server_information = openbis.get_server_information()
+        assert type(server_information.get_major_version()) is int
+        assert type(server_information.get_minor_version()) is int
+
+    def _test_logout(self, openbis):
+        openbis.logout()
+        assert openbis.is_session_active() == False
+
+TestCase(settings, __file__).runTest()
