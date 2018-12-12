cbd0b6f48ab7a523878b44613357f7b2f46c5f3d
felmer <franz-josef.elmer@id.ethz.ch>
2018-12-12 15:14:45 +0100

    SSDM-7430: Disable eln-lims technology until new way of importing master data is finished

--- integration-tests/test_openbis_sync_big.py
+++ integration-tests/test_openbis_sync_big.py
@@ -31,7 +31,8 @@ def sslwrap(func):
 
 ssl.wrap_socket = sslwrap(ssl.wrap_socket)
 
-ALL_TECHNOLOGIES = ['screening', 'proteomics', 'illumina-ngs', 'eln-lims', 'microscopy', 'flow']
+#ALL_TECHNOLOGIES = ['screening', 'proteomics', 'illumina-ngs', 'eln-lims', 'microscopy', 'flow']
+ALL_TECHNOLOGIES = ['screening', 'proteomics', 'illumina-ngs', 'microscopy', 'flow']
 DATA_SOURCE_AS_PORT = '9000'
 DATA_SOURCE_DSS_PORT = '9001'
 HARVESTER_AS_PORT = '9002'
