7176178a179bddc5d8bf687589cdfbea9b8d11cc
vermeul <swen@ethz.ch>
2019-01-09 16:19:42 +0100

    changed to standard docker installation

--- pybis/src/python/tests/test_dataset.py
+++ pybis/src/python/tests/test_dataset.py
@@ -6,12 +6,13 @@ import pytest
 import time
 
 def test_create_delete_dataset(space):
+    timestamp = time.strftime('%a_%y%m%d_%H%M%S').upper()
     o=space.openbis
 
     dataset_type = 'UNKNOWN'
     dataset = o.new_dataset(
         type='UNKNOWN',
-        sample='/DEFAULT/DEFAULT',
+        sample='/DEFAULT/DEFAULT/DEFAULT',
         files=['testfile']
     )
 
@@ -26,17 +27,13 @@ def test_create_delete_dataset(space):
     dataset_by_permId = o.get_dataset(dataset.permId)
     assert dataset_by_permId is not None
 
-    dataset_by_permId = space.get_dataset(dataset.permId)
-    assert dataset_by_permId is not None
-
-
-    # get it by identifier
-    dataset_by_identifier = o.get_dataset(dataset.identifier)
-    assert dataset_by_identifier is not None
-
-    dataset_by_identifier = space.get_dataset(dataset.identifier)
-    assert dataset_by_identifier is not None
-
+    permId = dataset.permId
     dataset.delete('dataset creation test on '+timestamp)
+#
+#    # get by permId should now throw an error
+#    with pytest.raises(ValueError):
+#        dataset.by_permId = o.get_dataset(permId)
+#        
+    
 
 
