3d69e40a5c8f23e40d6e548a0d8707af1fb428f1
Cezary Czernecki <ccezary@ethz.ch>
2019-03-06 14:45:09 +0100

    SSDM-7271 update strategies

--- openbis_standard_technologies/dist/core-plugins/xls-import/1/as/services/xls-import-api/entrypoint.py
+++ openbis_standard_technologies/dist/core-plugins/xls-import/1/as/services/xls-import-api/entrypoint.py
@@ -1,26 +1,50 @@
 from ch.ethz.sis.openbis.generic.asapi.v3.dto.operation import SynchronousOperationExecutionOptions
-from parsers import get_creations_from, CreationToOperationParser
+from parsers import get_creations_from, CreationOrUpdateToOperationParser
 from processors import OpenbisDuplicatesHandler, PropertiesLabelHandler, DuplicatesHandler, unify_properties_representation_of
 from search_engines import SearchEngine
 from utils import FileHandler
+from ch.systemsx.cisd.common.exceptions import UserFailureException
 
 
 def process(context, parameters):
-    xls_byte_arrays = parameters.get('xls')
-    scripts = parameters.get('scripts')
+    """
+        Excel import AS service.
+        For extensive documentation of usage and Excel layout, 
+        please visit https://wiki-bsse.ethz.ch/display/openBISDoc/Excel+import+service
+        
+        :param context: Standard Openbis AS Service context object
+        :param parameters: Contains two elemens
+                        {
+                            'xls' : excel byte blob,    - mandatory
+                            'scripts': {                - optional
+                                file path: loaded file
+                            },
+                            update_mode: [IGNORE_EXISTING|FAIL_IF_EXISTS|UPDATE_IF_EXISTS] - optional, default FAIL_IF_EXISTS
+                                                                                             This only takes duplicates that are ON THE SERVER
+                        }
+        :return: Openbis's execute operations result string. It should contain report on what was created.
+    """
+    xls_byte_arrays = parameters.get('xls', None)
+    scripts = parameters.get('scripts', {})
+    update_mode = parameters.get('update_mode', None)
+    if xls_byte_arrays is None:
+        raise UserFailureException('Excel sheet has not been provided. "xls" parameter is None')
+    if update_mode not in ['IGNORE_EXISTING', 'FAIL_IF_EXISTS', 'UPDATE_IF_EXISTS']:
+        raise UserFailureException('Update mode has to be one of following: IGNORE_EXISTING FAIL_IF_EXISTS UPDATE_IF_EXISTS but was ' + (str(update_mode) if update_mode else 'None'))
     creations = get_creations_from(xls_byte_arrays, FileHandler(scripts))
     distinct_creations = DuplicatesHandler.get_distinct_creations(creations)
     sessionToken = context.sessionToken
     api = context.applicationService
     search_engine = SearchEngine(api, sessionToken)
     existing_elements = search_engine.find_all_existing_elements(distinct_creations)
-    server_duplicates_handler = OpenbisDuplicatesHandler(distinct_creations, existing_elements)
-    creations = server_duplicates_handler.remove_existing_elements_from_creations()
-    creations = server_duplicates_handler.rewrite_parentchild_creationid_to_permid()
     entity_kinds = search_engine.find_existing_entity_kind_definitions_for(creations)
     existing_vocabularies = search_engine.find_all_existing_vocabularies()
     existing_unified_kinds = unify_properties_representation_of(creations, entity_kinds, existing_vocabularies, existing_elements)
     creations = PropertiesLabelHandler.rewrite_property_labels_to_codes(creations, existing_unified_kinds)
-    operations = CreationToOperationParser.parse(creations)
+    server_duplicates_handler = OpenbisDuplicatesHandler(distinct_creations, existing_elements, update_mode)
+    creations = server_duplicates_handler.rewrite_parentchild_creationid_to_permid()
+    creations = server_duplicates_handler.handle_existing_elements_in_creations()
+    operations = CreationOrUpdateToOperationParser.parse(creations)
     res = str(api.executeOperations(sessionToken, operations, SynchronousOperationExecutionOptions()).getResults())
     return res
+
--- openbis_standard_technologies/dist/core-plugins/xls-import/1/as/services/xls-import-api/processors/openbis_duplicates_handler.py
+++ openbis_standard_technologies/dist/core-plugins/xls-import/1/as/services/xls-import-api/processors/openbis_duplicates_handler.py
@@ -1,26 +1,31 @@
 from parsers import SpaceDefinitionToCreationParser, ProjectDefinitionToCreationParser, \
-    ExperimentDefinitionToCreationParser, ScriptDefinitionToCreationParser, SampleDefinitionToCreationParser
+    ExperimentDefinitionToCreationParser, ScriptDefinitionToCreationParser, SampleDefinitionToCreationParser, CreationToUpdateParser
 
 from utils.openbis_utils import create_sample_identifier_string
+
+FAIL_IF_EXISTS = "FAIL_IF_EXISTS"
+IGNORE_EXISTING = "IGNORE_EXISTING"
+UPDATE_IF_EXISTS = "UPDATE_IF_EXISTS"
 
 
 class OpenbisDuplicatesHandler(object):
 
-    def __init__(self, creations, existing_elements):
+    def __init__(self, creations, existing_elements, duplicates_strategy=FAIL_IF_EXISTS):
         self.creations = creations
         self.existing_elements = existing_elements
+        self.duplicates_strategy = duplicates_strategy
 
     def rewrite_parentchild_creationid_to_permid(self):
         if ProjectDefinitionToCreationParser.type in self.creations:
             for creation in self.creations[ProjectDefinitionToCreationParser.type]:
                 for existing_element in self.existing_elements[SpaceDefinitionToCreationParser.type]:
-                    if existing_element.code == creation.spaceId.creationId:
+                    if existing_element.code == str(creation.spaceId):
                         creation.spaceId = existing_element.permId
                         break
         if ExperimentDefinitionToCreationParser.type in self.creations:
             for creation in self.creations[ExperimentDefinitionToCreationParser.type]:
                 for existing_element in self.existing_elements[ProjectDefinitionToCreationParser.type]:
-                    if existing_element.code == creation.projectId.creationId:
+                    if existing_element.code == str(creation.projectId):
                         creation.projectId = existing_element.permId
                         break
         if SampleDefinitionToCreationParser.type in self.creations:
@@ -79,20 +84,64 @@ class OpenbisDuplicatesHandler(object):
 
         return self.creations
 
-    def remove_existing_elements_from_creations(self):
-        for creations_type, existing_elements in self.existing_elements.items():
-            if not creations_type in self.creations:
-                continue
-            if creations_type == SampleDefinitionToCreationParser.type:
-                existing_object_codes = [obj.identifier.identifier for obj in existing_elements]
-                self.creations[creations_type] = list(filter(
-                    lambda creation: creation.code is None or create_sample_identifier_string(
-                        creation) not in existing_object_codes, self.creations[creations_type]))
-            else:
-                distinct_property_name = self._get_distinct_property_name(creations_type)
-                self.creations[creations_type] = self._filter_creations_from_existing_objects(creations_type,
-                                                                                              existing_elements,
-                                                                                              distinct_property_name)
+    def handle_existing_elements_in_creations(self):
+
+        if self.duplicates_strategy == UPDATE_IF_EXISTS:
+            duplicates_list = {}
+            for creations_type, existing_elements in self.existing_elements.items():
+                if not creations_type in self.creations:
+                    continue
+                if creations_type == SampleDefinitionToCreationParser.type:
+                    existing_object_codes = [obj.identifier.identifier for obj in existing_elements]
+                    duplicates_list[creations_type] = list(filter(
+                        lambda creation: creation.code is not None or create_sample_identifier_string(
+                            creation) in existing_object_codes, self.creations[creations_type]))
+                    self.creations[creations_type] = list(filter(
+                        lambda creation: creation.code is None or create_sample_identifier_string(
+                            creation) not in existing_object_codes, self.creations[creations_type]))
+                else:
+                    distinct_property_name = self._get_distinct_property_name(creations_type)
+                    duplicates_list[creations_type] = self.get_creations_for_existing_objects(creations_type, existing_elements, distinct_property_name)
+                    self.creations[creations_type] = self._filter_creations_from_existing_objects(creations_type,
+                                                                                                  existing_elements,
+                                                                                                  distinct_property_name)
+
+                updates = CreationToUpdateParser.parse(duplicates_list, self.existing_elements)
+                for update_type, update in updates.items():
+                    self.creations[update_type] = update
+
+        if self.duplicates_strategy == FAIL_IF_EXISTS:
+            duplicates_list = {}
+            for creations_type, existing_elements in self.existing_elements.items():
+                if not creations_type in self.creations:
+                    continue
+                if creations_type == SampleDefinitionToCreationParser.type:
+                    existing_object_codes = [obj.identifier.identifier for obj in existing_elements]
+                    duplicates = list(filter(lambda creation: creation.code is not None and create_sample_identifier_string(creation) in existing_object_codes, self.creations[creations_type]))
+                    if duplicates:
+                        duplicates_list[creations_type] = duplicates
+                else:
+                    distinct_property_name = self._get_distinct_property_name(creations_type)
+                    duplicates = self.get_creations_for_existing_objects(creations_type, existing_elements, distinct_property_name)
+                    if duplicates:
+                        duplicates_list[creations_type] = duplicates
+            if duplicates_list:
+                raise Exception("Some of the objects you are trying to create already exist on the server. An error is being thrown when FAIL_IF_EXISTS flag is on. Existing elements are: " + str(duplicates_list))
+
+        if self.duplicates_strategy == IGNORE_EXISTING:
+            for creations_type, existing_elements in self.existing_elements.items():
+                if not creations_type in self.creations:
+                    continue
+                if creations_type == SampleDefinitionToCreationParser.type:
+                    existing_object_codes = [obj.identifier.identifier for obj in existing_elements]
+                    self.creations[creations_type] = list(filter(
+                        lambda creation: creation.code is None or create_sample_identifier_string(
+                            creation) not in existing_object_codes, self.creations[creations_type]))
+                else:
+                    distinct_property_name = self._get_distinct_property_name(creations_type)
+                    self.creations[creations_type] = self._filter_creations_from_existing_objects(creations_type,
+                                                                                                  existing_elements,
+                                                                                                  distinct_property_name)
         return self.creations
 
     def _get_distinct_property_name(self, creation_type):
@@ -104,4 +153,9 @@ class OpenbisDuplicatesHandler(object):
     def _filter_creations_from_existing_objects(self, creations_type, existing_objects, attr):
         existing_object_codes = [getattr(obj, attr) for obj in existing_objects]
         return list(filter(lambda creation: getattr(creation, attr) not in existing_object_codes,
+                           self.creations[creations_type]))
+
+    def get_creations_for_existing_objects(self, creations_type, existing_objects, attr):
+        existing_object_codes = [getattr(obj, attr) for obj in existing_objects]
+        return list(filter(lambda creation: getattr(creation, attr) in existing_object_codes,
                            self.creations[creations_type]))
