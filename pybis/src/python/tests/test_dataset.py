4aa40620909f4ddf2d50e8ae3b949e3708c17b9a
vermeul <swen@ethz.ch>
2019-03-13 13:50:03 +0100

    added more tests to datasets, such as wrong attributes and incorrect properties

--- pybis/src/python/tests/test_dataset.py
+++ pybis/src/python/tests/test_dataset.py
@@ -9,15 +9,27 @@ def test_create_delete_dataset(space):
     timestamp = time.strftime('%a_%y%m%d_%H%M%S').upper()
     o=space.openbis
 
-    dataset_type = 'UNKNOWN'
     dataset = o.new_dataset(
-        type='UNKNOWN',
-        sample='/DEFAULT/DEFAULT/DEFAULT',
-        files=['testfile']
+        type   = 'RAW_DATA',
+        sample = '/DEFAULT/DEFAULT/DEFAULT',
+        files  = ['testfile'],
+        props  = {'name': 'some good name', 'notes': 'my notes' }
     )
-
+    
     assert dataset is not None
-    assert dataset.permId is None
+    assert dataset.permId is None  # object is not saved
+    assert dataset.p is not None
+    assert dataset.p.name == 'some good name'
+    assert dataset.p.notes == 'my notes'
+
+    with pytest.raises(Exception):
+        dataset.non_existing_attribute = "invalid attribute"
+        assert "attribute does not exist, should fail" is None
+
+    with pytest.raises(Exception):
+        dataset.p.non_existing_property = "invalid propery"
+        assert "property does not exist, should fail" is None
+    
     dataset.save()
 
     # now there should appear a permId
@@ -28,12 +40,13 @@ def test_create_delete_dataset(space):
     assert dataset_by_permId is not None
 
     permId = dataset.permId
+    assert dataset is not None
     dataset.delete('dataset creation test on '+timestamp)
-#
-#    # get by permId should now throw an error
-#    with pytest.raises(ValueError):
-#        dataset.by_permId = o.get_dataset(permId)
-#        
+
+    # get by permId should now throw an error
+    with pytest.raises(Exception):
+        deleted_ds = o.get_dataset(permId)
+        
     
 
 
