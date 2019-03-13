08777fd49f0d1759415ad33dbd1e3c803ba609af
vermeul <swen@ethz.ch>
2019-03-13 23:14:34 +0100

    removed attribute check in creation of new sample (now done in attribute.py)

--- pybis/src/python/pybis/sample.py
+++ pybis/src/python/pybis/sample.py
@@ -28,8 +28,6 @@ class Sample(OpenBisObject):
 
         if kwargs is not None:
             for key in kwargs:
-                if key not in sample_definitions['attrs_new']:
-                    raise ValueError("{} is not a valid attribute for a Sample".format(key))
                 setattr(self, key, kwargs[key])
 
         if getattr(self, 'parents') is None:
