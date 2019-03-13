aa4a7ebee7becb0e538e90887bede935eda4522d
vermeul <swen@ethz.ch>
2019-03-13 14:04:20 +0100

    added paging test

--- pybis/src/python/tests/test_dataset.py
+++ pybis/src/python/tests/test_dataset.py
@@ -9,6 +9,7 @@ def test_create_delete_dataset(space):
     timestamp = time.strftime('%a_%y%m%d_%H%M%S').upper()
     o=space.openbis
 
+
     dataset = o.new_dataset(
         type   = 'RAW_DATA',
         sample = '/DEFAULT/DEFAULT/DEFAULT',
@@ -32,15 +33,22 @@ def test_create_delete_dataset(space):
     
     dataset.save()
 
+    # test paging
+    current_datasets = o.get_datasets(start_with=1, count=1)
+    assert current_datasets is not None
+    # we cannot assert == 1, because search is delayed due to lucene search...
+    assert len(current_datasets) <= 1
+
     # now there should appear a permId
     assert dataset.permId is not None
 
     # get it by permId
     dataset_by_permId = o.get_dataset(dataset.permId)
     assert dataset_by_permId is not None
-
     permId = dataset.permId
     assert dataset is not None
+
+    # delete datasets
     dataset.delete('dataset creation test on '+timestamp)
 
     # get by permId should now throw an error
