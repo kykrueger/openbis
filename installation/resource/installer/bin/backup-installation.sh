dc370218c9f80fe477ab947367dff7bfe15cb758
felmer <franz-josef.elmer@id.ethz.ch>
2019-04-30 11:11:06 +0200

    SSDM-6453: improve upgrade inconsitency logic in backup-installation.sh

--- installation/resource/installer/bin/backup-installation.sh
+++ installation/resource/installer/bin/backup-installation.sh
@@ -16,6 +16,7 @@ if [ ${BASE#/} == ${BASE} ]; then
     BASE="`pwd`/${BASE}"
 fi
 
+# check for inconsistence upgrade
 INSTALLER_JAR=`ps aux|grep openBIS-installer.jar|grep -v grep|awk '{for(i=1;i<=NF;i++){if($i~/openBIS-installer\.jar/){print $i}}}'`
 UPGRADE_VERSION=`java -cp $BASE InstallerVariableAccess $INSTALLER_JAR version.number`
 UPGRADE_REVISION=`java -cp $BASE InstallerVariableAccess $INSTALLER_JAR revision.number`
@@ -24,10 +25,32 @@ VERSION_REVISION=${BUILD_INFO%*:*}
 VERSION=${VERSION_REVISION%:*}
 REVISION=${VERSION_REVISION#*:}
 echo "existing version: $VERSION-r$REVISION"
-echo "installer jar: $INSTALLER_JAR, version: $UPGRADE_VERSION-r$UPGRADE_REVISION"
-if [[ $UPGRADE_VERSION < $VERSION || ($UPGRADE_VERSION == $VERSION && $UPGRADE_REVISION -lt $REVISION)]]; then
-    echo "ERROR: Can not upgrade to an older version: Current version is $VERSION-r$REVISION and upgrade version is $UPGRADE_VERSION-r$UPGRADE_REVISION"
-    exit 1
+echo "upgrade  version: $UPGRADE_VERSION-r$UPGRADE_REVISION (installer: $INSTALLER_JAR)"
+ERROR_MSG="ERROR: Can not upgrade from version $VERSION-r$REVISION to version $UPGRADE_VERSION-r$UPGRADE_REVISION"
+if [[ $VERSION != 'SNAPSHOT' && $UPGRADE_VERSION != 'SNAPSHOT' ]]; then # upgrade from non-snapshot -> non-snapshot
+    if [[ ${VERSION:0:1} == 'S' && ${UPGRADE_VERSION:0:1} != 'S' ]]; then # upgrade from sprint -> release
+        if [[ ${UPGRADE_VERSION:0:2} == '13' ]]; then
+            FIRST_RELEASE_REVISION='28762'
+        elif [[ ${UPGRADE_VERSION:0:2} == '16' ]]; then
+            FIRST_RELEASE_REVISION='36515'
+        elif [[ ${UPGRADE_VERSION:0:2} == '18' ]]; then
+            FIRST_RELEASE_REVISION='1530174746'
+        else
+            FIRST_RELEASE_REVISION=$REVISION
+        fi
+        if [[ $REVISION -gt $FIRST_RELEASE_REVISION ]]; then
+            echo $ERROR_MSG
+            exit 1
+        fi
+    elif [[ ${VERSION:0:1} != 'S' && ${UPGRADE_VERSION:0:1} == 'S' ]]; then # upgrade from release -> sprint
+        if [[ $REVISION -gt $UPGRADE_REVISION ]]; then
+            echo $ERROR_MSG
+            exit 1
+        fi
+    elif [[ $UPGRADE_VERSION < $VERSION ]]; then # upgrade from sprint -> sprint or release -> release
+        echo $ERROR_MSG
+        exit 1
+    fi
 fi
 
 BACKUP_DIR=$1
