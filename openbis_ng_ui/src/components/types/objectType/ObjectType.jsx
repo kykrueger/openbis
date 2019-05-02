8e1c6b7cc2bd9db97129068beb208411bda8664c
pkupczyk <piotr.kupczyk@id.ethz.ch>
2019-05-02 22:27:57 +0200

    SSDM-7569 NEW openBIS UI - General Template/Infrastructure for Forms - Visualisation/Creation/Edit - very basic object type form

--- openbis_ng_ui/src/components/types/objectType/ObjectType.jsx
+++ openbis_ng_ui/src/components/types/objectType/ObjectType.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 import TextField from '@material-ui/core/TextField'
 import Checkbox from '@material-ui/core/Checkbox'
 import Button from '@material-ui/core/Button'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
 import logger from '../../../common/logger.js'
 import {facade, dto} from '../../../services/openbis.js'
 
@@ -17,15 +23,29 @@ class ObjectType extends React.Component {
   }
 
   componentDidMount(){
-    facade.getSampleTypes([new dto.EntityTypePermId(this.props.objectId)], new dto.SampleTypeFetchOptions()).then(map => {
+    let id = new dto.EntityTypePermId(this.props.objectId)
+    let fo = new dto.SampleTypeFetchOptions()
+    fo.withPropertyAssignments().withPropertyType()
+    fo.withPropertyAssignments().sortBy().code()
+
+    facade.getSampleTypes([id], fo).then(map => {
       let objectType = map[this.props.objectId]
       if(objectType){
         this.setState(() => {
           return {
             loaded: true,
-            fields: {
+            data: {
+              code: objectType.code,
               description: objectType.description || '',
-              listable: objectType.listable
+              listable: objectType.listable,
+              properties: objectType.propertyAssignments.map(assignment => ({
+                permId: assignment.permId,
+                code: assignment.propertyType.code,
+                label: assignment.propertyType.label,
+                description: assignment.propertyType.description,
+                ordinal: assignment.ordinal,
+                mandatory: assignment.mandatory
+              }))
             }
           }
         })
@@ -38,8 +58,8 @@ class ObjectType extends React.Component {
       let value = _.has(event.target, 'checked') ? event.target.checked : event.target.value
       this.setState((prevState) => ({
         ...prevState,
-        fields: {
-          ...prevState.fields,
+        data: {
+          ...prevState.data,
           [name]: value
         }
       }))
@@ -47,7 +67,7 @@ class ObjectType extends React.Component {
   }
 
   handleSave(){
-    let {description, listable} = this.state.fields
+    let {description, listable} = this.state.data
     let update = new dto.SampleTypeUpdate()
     update.setTypeId(new dto.EntityTypePermId(this.props.objectId))
     update.setDescription(description)
@@ -62,18 +82,46 @@ class ObjectType extends React.Component {
       return <div></div>
     }
 
-    let { description, listable } = this.state.fields
+    let { code, description, listable, properties } = this.state.data
 
     return (
       <div>
-        <div>{this.props.objectId}</div>
+        <h2>{code}</h2>
         <form>
-          <div>
-            <TextField label='Description' value={description} onChange={this.handleChange('description')} />
-          </div>
-          <div>
-            <Checkbox checked={listable} value='listable' onChange={this.handleChange('listable')} />
-          </div>
+          <FormControlLabel
+            label="Description"
+            labelPlacement="top"
+            control={
+              <TextField value={description} onChange={this.handleChange('description')} />
+            }
+          />
+          <FormControlLabel
+            label="Listable"
+            labelPlacement="top"
+            control={
+              <Checkbox checked={listable} value='listable' onChange={this.handleChange('listable')} />
+            }
+          />
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Code</TableCell>
+                <TableCell>Label</TableCell>
+                <TableCell>Description</TableCell>
+                <TableCell>Mandatory</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {properties.map(property => (
+                <TableRow key={property.permId}>
+                  <TableCell>{property.code}</TableCell>
+                  <TableCell>{property.label}</TableCell>
+                  <TableCell>{property.description}</TableCell>
+                  <TableCell>{property.mandatory ? 'true' : 'false'}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
           <div>
             <Button variant='contained' color='primary' onClick={this.handleSave}>Save</Button>
           </div>
