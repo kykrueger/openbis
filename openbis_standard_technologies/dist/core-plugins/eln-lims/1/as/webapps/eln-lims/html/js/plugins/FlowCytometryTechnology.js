65273238aa3add7382b241388ec3f6e6ac3c5190
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2019-05-08 06:25:17 -0700

    Add toolbar configuration for the flow cytometry objects. Still needs refinement.

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/plugins/FlowCytometryTechnology.js
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/plugins/FlowCytometryTechnology.js
@@ -3,80 +3,153 @@ function FlowCytometryTechnology() {
 }
 
 $.extend(FlowCytometryTechnology.prototype, ELNLIMSPlugin.prototype, {
-	init: function() {
-		
-	},
-	forcedDisableRTF : [],
-	forceMonospaceFont : [],
-	sampleTypeDefinitionsExtension : {
-				"FACS_ARIA_EXPERIMENT" : {
-					"SAMPLE_PARENTS_HINT" : [{
-						"LABEL" : "Organization Units",
-						"TYPE": "ORGANIZATION_UNIT",
-						"ANNOTATION_PROPERTIES" : []
-					}]
-				},
-				"INFLUX_EXPERIMENT" : {
-					"SAMPLE_PARENTS_HINT" : [{
-						"LABEL" : "Organization Units",
-						"TYPE": "ORGANIZATION_UNIT",
-						"ANNOTATION_PROPERTIES" : []
-					}]
-				},
-				"LSR_FORTESSA_EXPERIMENT" : {
-					"SAMPLE_PARENTS_HINT" : [{
-						"LABEL" : "Organization Units",
-						"TYPE": "ORGANIZATION_UNIT",
-						"ANNOTATION_PROPERTIES" : []
-					}]
-				},
-				"MOFLO_XDP_EXPERIMENT" : {
-					"SAMPLE_PARENTS_HINT" : [{
-						"LABEL" : "Organization Units",
-						"TYPE": "ORGANIZATION_UNIT",
-						"ANNOTATION_PROPERTIES" : []
-					}]
-				},
-				"S3E_EXPERIMENT" : {
-					"SAMPLE_PARENTS_HINT" : [{
-						"LABEL" : "Organization Units",
-						"TYPE": "ORGANIZATION_UNIT",
-						"ANNOTATION_PROPERTIES" : []
-					}]
-				},
-				"FACS_ARIA_PLATE" : { "SHOW_ON_NAV" : true },
-				"FACS_ARIA_SPECIMEN" : { "SHOW_ON_NAV" : true },
-				"FACS_ARIA_TUBE" : { "SHOW_ON_NAV" : true },
-				"FACS_ARIA_TUBESET" : { "SHOW_ON_NAV" : true },
-				"FACS_ARIA_WELL" : { "SHOW_ON_NAV" : true },
-				"INFLUX_SPECIMEN" : { "SHOW_ON_NAV" : true },
-				"INFLUX_TUBE" : { "SHOW_ON_NAV" : true },
-				"INFLUX_TUBESET" : { "SHOW_ON_NAV" : true },
-				"LSR_FORTESSA_PLATE" : { "SHOW_ON_NAV" : true },
-				"LSR_FORTESSA_SPECIMEN" : { "SHOW_ON_NAV" : true },
-				"LSR_FORTESSA_TUBE" : { "SHOW_ON_NAV" : true },
-				"LSR_FORTESSA_TUBESET" : { "SHOW_ON_NAV" : true },
-				"LSR_FORTESSA_WELL" : { "SHOW_ON_NAV" : true },
-				"MOFLO_XDP_SPECIMEN" : { "SHOW_ON_NAV" : true },
-				"MOFLO_XDP_TUBE" : { "SHOW_ON_NAV" : true },
-				"MOFLO_XDP_TUBESET" : { "SHOW_ON_NAV" : true },
-				"S3E_TUBE" : { "SHOW_ON_NAV" : true },
-				"S3E_TUBESET" : { "SHOW_ON_NAV" : true },
-				"SE3_SPECIMEN" : { "SHOW_ON_NAV" : true }
-	},
-	dataSetTypeDefinitionsExtension : {
+	init: function () {
 
 	},
-	sampleFormTop : function($container, model) {
-
-    },
-	sampleFormBottom : function($container, model) {
-	
+	forcedDisableRTF: [],
+	forceMonospaceFont: [],
+	sampleTypeDefinitionsExtension: {
+		"FACS_ARIA_EXPERIMENT": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SAMPLE_PARENTS_HINT": [{
+				"LABEL": "Organization Units",
+				"TYPE": "ORGANIZATION_UNIT",
+				"ANNOTATION_PROPERTIES": []
+			}]
+		},
+		"INFLUX_EXPERIMENT": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SAMPLE_PARENTS_HINT": [{
+				"LABEL": "Organization Units",
+				"TYPE": "ORGANIZATION_UNIT",
+				"ANNOTATION_PROPERTIES": []
+			}]
+		},
+		"LSR_FORTESSA_EXPERIMENT": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SAMPLE_PARENTS_HINT": [{
+				"LABEL": "Organization Units",
+				"TYPE": "ORGANIZATION_UNIT",
+				"ANNOTATION_PROPERTIES": []
+			}]
+		},
+		"MOFLO_XDP_EXPERIMENT": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SAMPLE_PARENTS_HINT": [{
+				"LABEL": "Organization Units",
+				"TYPE": "ORGANIZATION_UNIT",
+				"ANNOTATION_PROPERTIES": []
+			}]
+		},
+		"S3E_EXPERIMENT": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SAMPLE_PARENTS_HINT": [{
+				"LABEL": "Organization Units",
+				"TYPE": "ORGANIZATION_UNIT",
+				"ANNOTATION_PROPERTIES": []
+			}]
+		},
+		"FACS_ARIA_SPECIMEN": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"FACS_ARIA_TUBE": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"FACS_ARIA_TUBESET": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"INFLUX_SPECIMEN": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"INFLUX_TUBE": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"INFLUX_TUBESET": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"LSR_FORTESSA_PLATE": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"LSR_FORTESSA_SPECIMEN": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"LSR_FORTESSA_TUBE": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"LSR_FORTESSA_TUBESET": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"LSR_FORTESSA_WELL": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"MOFLO_XDP_SPECIMEN": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"MOFLO_XDP_TUBE": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"MOFLO_XDP_TUBESET": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"S3E_TUBE": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"S3E_TUBESET": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		},
+		"S3E_SPECIMEN": {
+			"TOOLBAR": { CREATE: false, EDIT: false, MOVE: false, COPY: false, DELETE: false, PRINT: true, HIERARCHY_GRAPH: true, HIERARCHY_TABLE: true, UPLOAD_DATASET: false, UPLOAD_DATASET_HELPER: false, EXPORT_ALL: true, EXPORT_METADATA: true },
+			"SHOW_ON_NAV": true
+		}
 	},
-	dataSetFormTop : function($container, model) {
+	dataSetTypeDefinitionsExtension: {
+		"FACS_ARIA_FCSFILE": {
+			"DATASET_PARENTS_DISABLED": true,
+			"TOOLBAR": { EDIT: false, MOVE: false, ARCHIVE: true, DELETE: false, HIERARCHY_TABLE: true, EXPORT_ALL: true, EXPORT_METADATA: true }
+		},
+		"INFLUX_FCSFILE": {
+			"DATASET_PARENTS_DISABLED": true,
+			"TOOLBAR": { EDIT: false, MOVE: false, ARCHIVE: true, DELETE: false, HIERARCHY_TABLE: true, EXPORT_ALL: true, EXPORT_METADATA: true }
+		},
+		"LSR_FORTESSA_FCSFILE": {
+			"DATASET_PARENTS_DISABLED": true,
+			"TOOLBAR": { EDIT: false, MOVE: false, ARCHIVE: true, DELETE: false, HIERARCHY_TABLE: true, EXPORT_ALL: true, EXPORT_METADATA: true }
+		},
+		"MOFLO_XDP_FCSFILE": {
+			"DATASET_PARENTS_DISABLED": true,
+			"TOOLBAR": { EDIT: false, MOVE: false, ARCHIVE: true, DELETE: false, HIERARCHY_TABLE: true, EXPORT_ALL: true, EXPORT_METADATA: true }
+		},
+		"S3E_ARIA_FCSFILE": {
+			"DATASET_PARENTS_DISABLED": true,
+			"TOOLBAR": { EDIT: false, MOVE: false, ARCHIVE: true, DELETE: false, HIERARCHY_TABLE: true, EXPORT_ALL: true, EXPORT_METADATA: true }
+		}
+	},
+	sampleFormTop: function ($container, model) {
 
 	},
-	dataSetFormBottom : function($container, model) {
-		
+	sampleFormBottom: function ($container, model) {
+
+	},
+	dataSetFormTop: function ($container, model) {
+
+	},
+	dataSetFormBottom: function ($container, model) {
+
 	}
 });
