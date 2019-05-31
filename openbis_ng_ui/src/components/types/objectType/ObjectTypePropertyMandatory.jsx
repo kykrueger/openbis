9a80652982aac911c9ddf0f3504900b032d307ba
pkupczyk <piotr.kupczyk@id.ethz.ch>
2019-05-31 19:38:23 +0200

    SSDM-7569 NEW openBIS UI - General Template/Infrastructure for Forms - Visualisation/Creation/Edit - make 'mandatory' field editable only after a mouse click

--- openbis_ng_ui/src/components/types/objectType/ObjectTypePropertyMandatory.jsx
+++ openbis_ng_ui/src/components/types/objectType/ObjectTypePropertyMandatory.jsx
@@ -1,17 +1,22 @@
+import _ from 'lodash'
 import React from 'react'
 import Checkbox from '@material-ui/core/Checkbox'
+import EditableField from '../../common/form/EditableField.jsx'
+import {withStyles} from '@material-ui/core/styles'
 import logger from '../../../common/logger.js'
+
+const styles = () => ({
+  checkbox: {
+    padding: '0px'
+  },
+})
 
 class ObjectTypePropertyMandatory extends React.Component {
 
   constructor(props){
     super(props)
-    this.handleClick = this.handleClick.bind(this)
+    this.renderField = this.renderField.bind(this)
     this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleClick(event){
-    event.stopPropagation()
   }
 
   handleChange(event){
@@ -22,18 +27,32 @@ class ObjectTypePropertyMandatory extends React.Component {
   render(){
     logger.log(logger.DEBUG, 'ObjectTypePropertyMandatory.render')
 
-    const {property} = this.props
+    return <EditableField renderField={this.renderField} />
+  }
 
-    return (
-      <Checkbox
-        checked={property.mandatory}
-        value='mandatory'
-        onClick={this.handleClick}
-        onChange={this.handleChange}
-      />
-    )
+  renderField({ref, edited, handleBlur}){
+    const {classes, property} = this.props
+
+    if(edited){
+      return (
+        <Checkbox
+          checked={property.mandatory}
+          value='mandatory'
+          onChange={this.handleChange}
+          onBlur={handleBlur}
+          inputRef={ref}
+          classes={{ root: classes.checkbox }}
+        />
+      )
+    }else{
+      return (
+        <span>{property.mandatory ? 'true' : 'false'}</span>
+      )
+    }
   }
 
 }
 
-export default ObjectTypePropertyMandatory
+export default _.flow(
+  withStyles(styles),
+)(ObjectTypePropertyMandatory)
