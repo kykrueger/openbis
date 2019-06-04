fed130b868bff47e0cf09558bac81bfe6f52cdaf
pkupczyk <piotr.kupczyk@id.ethz.ch>
2019-06-04 19:18:15 +0200

    SSDM-7594 : NEW openBIS UI - General Template/Infrastructure for Tables - Visualisation/Creation/Edit - global search field

--- openbis_ng_ui/src/components/common/menu/Menu.jsx
+++ openbis_ng_ui/src/components/common/menu/Menu.jsx
@@ -3,8 +3,13 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
+import TextField from '@material-ui/core/TextField'
+import InputAdornment from '@material-ui/core/InputAdornment'
 import Button from '@material-ui/core/Button'
+import SearchIcon from '@material-ui/icons/Search'
+import CloseIcon from '@material-ui/icons/Close'
 import LogoutIcon from '@material-ui/icons/PowerSettingsNew'
+import { fade } from '@material-ui/core/styles/colorManipulator'
 import {connect} from 'react-redux'
 import {withStyles} from '@material-ui/core/styles'
 import logger from '../../../common/logger.js'
@@ -12,10 +17,37 @@ import * as actions from '../../../store/actions/actions.js'
 import * as selectors from '../../../store/selectors/selectors.js'
 import * as pages from '../../../store/consts/pages.js'
 
-const styles = () => ({
+const styles = (theme) => ({
   tabs: {
     flexGrow: 1,
+  },
+  search: {
+    color: theme.palette.common.white,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    borderRadius: theme.shape.borderRadius,
+    paddingLeft: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    marginRight: theme.spacing.unit * 2,
+  },
+  searchInput: {
+    transition: theme.transitions.create('width'),
+    width: '200px',
+    '&:focus': {
+      width: '300px',
+    },
+  },
+  searchIcon: {
+    paddingLeft: theme.spacing.unit / 2,
+    paddingRight: theme.spacing.unit,
+    cursor: 'default'
+  },
+  searchClear: {
+    cursor: 'pointer'
   }
+
 })
 
 function mapStateToProps(state){
@@ -46,6 +78,25 @@ class Menu extends React.Component {
             <Tab value={pages.TYPES} label="Types"/>
             <Tab value={pages.USERS} label="Users"/>
           </Tabs>
+          <TextField
+            placeholder="Search..."
+            InputProps={{
+              disableUnderline: true,
+              startAdornment: (
+                <InputAdornment>
+                  <SearchIcon classes={{ root: classes.searchIcon }} />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment>
+                  <CloseIcon classes={{ root: classes.searchClear }}/>
+                </InputAdornment>
+              ),
+              classes: {
+                root: classes.search,
+                input: classes.searchInput
+              }
+            }}/>
           <Button
             variant="contained"
             color="primary"
