881b8191eee486b5f2b24c9fec5805a25b74630b
vermeul <swen@ethz.ch>
2019-06-20 22:37:47 +0200

    added LICENSE and other files to get ready for pypi

--- obis/src/python/setup.py
+++ obis/src/python/setup.py
@@ -2,28 +2,31 @@ import os
 
 from setuptools import setup
 
+# include all man files
 data_dir = os.path.join('man','man1')
 data_files = [(d, [os.path.join(d,f) for f in files])
     for d, folders, files in os.walk(data_dir)]
 
-setup(name='obis',
-      version='0.1.0',
-      description='Local data management with assistance from OpenBIS.',
-      url='https://sissource.ethz.ch/sis/pybis/',
-      author='SIS | ID | ETH Zuerich',
-      author_email='chandrasekhar.ramakrishnan@id.ethz.ch',
-      license='BSD',
-      packages=['obis', 'obis.dm', 'obis.dm.commands', 'obis.scripts'],
-      data_files=data_files,
-      package_data={'obis' : ['dm/git-annex-attributes']},
-      install_requires=[
-          'pyOpenSSL',
-          'pytest',
-          'pybis',
-          'click'
-      ],
-      entry_points='''
-        [console_scripts]
-        obis=obis.scripts.cli:main
-      ''',
-      zip_safe=False)
+setup(
+    name='obis',
+    version='0.1.0',
+    description='Local data management with assistance from OpenBIS.',
+    url='https://sissource.ethz.ch/sis/pybis/',
+    author='SIS | ID | ETH Zuerich',
+    author_email='swen@ethz.ch',
+    license='Apache Software License Version 2.0',
+    packages=['obis', 'obis.dm', 'obis.dm.commands', 'obis.scripts'],
+    data_files=data_files,
+    package_data={'obis' : ['dm/git-annex-attributes']},
+    install_requires=[
+        'pyOpenSSL',
+        'pytest',
+        'pybis',
+        'click'
+    ],
+    entry_points='''
+      [console_scripts]
+      obis=obis.scripts.cli:main
+    ''',
+    zip_safe=False,
+)
