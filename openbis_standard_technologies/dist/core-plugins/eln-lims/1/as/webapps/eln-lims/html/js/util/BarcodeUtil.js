04608c52a4238dd65e9c762e8d582c39692ce3bd
Juan Fuentes <juanf@bs-mbpr28.d.ethz.ch>
2019-10-04 14:40:24 +0200

    SSDM-8750 : Basic permId barcode in sample form

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/util/BarcodeUtil.js
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/util/BarcodeUtil.js
@@ -5,15 +5,22 @@ var BarcodeUtil = new function() {
             'action' : 'javascript:void(0);'
         });
 
-        var $btnAccept = $('<input>', { 'type': 'submit', 'class' : 'btn btn-primary', 'value' : 'Accept' });
-        var $btnCancel = $('<input>', { 'type': 'submit', 'class' : 'btn', 'value' : 'Cancel' });
+        var $btnAccept = $('<input>', { 'type': 'submit', 'class' : 'btn btn-primary', 'value' : 'Download' });
+        $btnAccept.click(function(event) {
+            var canvas = document.getElementById('barcode-canvas');
+            canvas.toBlob(function (blob) {
+                saveAs(blob, entity.permId + '.png');
+            }, 'image/png', 1);
+        });
+
+        var $btnCancel = $('<input>', { 'type': 'submit', 'class' : 'btn', 'value' : 'Close' });
 		$btnCancel.click(function(event) {
 			Util.unblockUI();
 		});
 
 		$window.append($('<legend>').append("Barcode"));
 	    $window.append($('<br>'));
-	    $window.append($('<canvas>', { id : "barcode-canvas", width : 1, height : 1, style : "border:1px solid #fff;visibility:hidden" }));
+	    $window.append($('<center>').append($('<canvas>', { id : "barcode-canvas", width : 1, height : 1, style : "border:1px solid #fff;visibility:hidden" })));
 	    $window.append($('<br>'));
 	    $window.append($btnAccept).append('&nbsp;').append($btnCancel);
 
@@ -46,7 +53,7 @@ var BarcodeUtil = new function() {
 
     this.generateBarcode = function(canvasId, barcodeType, text, altx) {
         var elt  = symdesc[barcodeType];
-        var opts = "includetext";
+        var opts = {};
         var rot  = "N";
         var monochrome = true;
         var scaleX = 2;
@@ -58,22 +65,6 @@ var BarcodeUtil = new function() {
         canvas.height = 1;
         canvas.width  = 1;
         canvas.style.visibility = 'hidden';
-
-        // Convert the options to a dictionary object, so we can pass alttext with
-        // spaces.
-        var tmp = opts.split(' ');
-        opts = {};
-        for (var i = 0; i < tmp.length; i++) {
-            if (!tmp[i]) {
-                continue;
-            }
-            var eq = tmp[i].indexOf('=');
-            if (eq == -1) {
-                opts[tmp[i]] = true;
-            } else {
-                opts[tmp[i].substr(0, eq)] = tmp[i].substr(eq+1);
-            }
-        }
 
         // Add the alternate text
         if (altx) {
@@ -105,14 +96,12 @@ var BarcodeUtil = new function() {
         bw.bitmap().pad(+opts.paddingwidth*scaleX || 0,
                         +opts.paddingheight*scaleY || 0);
 
-        var ts0 = Date.now();
         try {
             // Call into the BWIPP cross-compiled code.
             BWIPP()(bw, elt.sym, text, opts);
 
             // Allow the font manager to demand-load any required fonts
             // before calling render().
-            var ts1 = Date.now();
             bwipjs_fonts.loadfonts(function(e) {
                 if (e) {
                     Util.manageError(e);
