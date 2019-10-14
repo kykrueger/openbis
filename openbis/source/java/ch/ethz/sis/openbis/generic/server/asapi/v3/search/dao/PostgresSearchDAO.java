d72b3470b0fbb7681f38de3ca1c50f3b34a69338
Viktor Kovtun <viktor.kovtun@id.ethz.ch>
2019-10-14 17:22:41 +0200

    SSDM-8226 Change PostgresSearchDAO class to use SQLLexemes class instead of string SQL literals.

--- openbis/source/java/ch/ethz/sis/openbis/generic/server/asapi/v3/search/dao/PostgresSearchDAO.java
+++ openbis/source/java/ch/ethz/sis/openbis/generic/server/asapi/v3/search/dao/PostgresSearchDAO.java
@@ -31,6 +31,17 @@ import ch.ethz.sis.openbis.generic.server.asapi.v3.search.sql.ISQLExecutor;
 import ch.ethz.sis.openbis.generic.server.asapi.v3.search.translator.SelectQuery;
 import ch.ethz.sis.openbis.generic.server.asapi.v3.search.translator.Translator;
 
+import static ch.ethz.sis.openbis.generic.server.asapi.v3.search.translator.SQLLexemes.DISTINCT;
+import static ch.ethz.sis.openbis.generic.server.asapi.v3.search.translator.SQLLexemes.FROM;
+import static ch.ethz.sis.openbis.generic.server.asapi.v3.search.translator.SQLLexemes.IN;
+import static ch.ethz.sis.openbis.generic.server.asapi.v3.search.translator.SQLLexemes.LP;
+import static ch.ethz.sis.openbis.generic.server.asapi.v3.search.translator.SQLLexemes.NL;
+import static ch.ethz.sis.openbis.generic.server.asapi.v3.search.translator.SQLLexemes.QU;
+import static ch.ethz.sis.openbis.generic.server.asapi.v3.search.translator.SQLLexemes.RP;
+import static ch.ethz.sis.openbis.generic.server.asapi.v3.search.translator.SQLLexemes.SELECT;
+import static ch.ethz.sis.openbis.generic.server.asapi.v3.search.translator.SQLLexemes.SP;
+import static ch.ethz.sis.openbis.generic.server.asapi.v3.search.translator.SQLLexemes.UNNEST;
+import static ch.ethz.sis.openbis.generic.server.asapi.v3.search.translator.SQLLexemes.WHERE;
 import static ch.systemsx.cisd.openbis.generic.shared.dto.ColumnNames.ID_COLUMN;
 
 public class PostgresSearchDAO implements ISQLSearchDAO
@@ -62,9 +73,9 @@ public class PostgresSearchDAO implements ISQLSearchDAO
     @Override
     public Set<Long> findChildIDs(final TableMapper tableMapper, final Set<Long> parentIdSet)
     {
-        final String query = "SELECT DISTINCT " + tableMapper.getRelationshipsTableChildIdField() + "\n" +
-                "FROM " + tableMapper.getRelationshipsTable() + "\n" +
-                "WHERE " + tableMapper.getRelationshipsTableParentIdField() + " IN (SELECT unnest(?))";
+        final String query = SELECT + SP + DISTINCT + SP + tableMapper.getRelationshipsTableChildIdField() + NL +
+                FROM + SP + tableMapper.getRelationshipsTable() + NL +
+                WHERE + SP + tableMapper.getRelationshipsTableParentIdField() + SP + IN + SP + LP + SELECT + SP + UNNEST + LP + QU + RP + RP;
         final List<Object> args = Collections.singletonList(parentIdSet.toArray(new Long[0]));
         final List<Map<String, Object>> queryResultList = sqlExecutor.execute(query, args);
         return queryResultList.stream().map(stringObjectMap -> (Long) stringObjectMap.get(tableMapper.getRelationshipsTableChildIdField()))
@@ -74,9 +85,9 @@ public class PostgresSearchDAO implements ISQLSearchDAO
     @Override
     public Set<Long> findParentIDs(final TableMapper tableMapper, final Set<Long> childIdSet)
     {
-        final String query = "SELECT DISTINCT " + tableMapper.getRelationshipsTableParentIdField() + "\n" +
-                "FROM " + tableMapper.getRelationshipsTable() + "\n" +
-                "WHERE " + tableMapper.getRelationshipsTableChildIdField() + " IN (SELECT unnest(?))";
+        final String query = SELECT + SP + DISTINCT + SP + tableMapper.getRelationshipsTableParentIdField() + NL +
+                FROM + SP + tableMapper.getRelationshipsTable() + NL +
+                WHERE + SP + tableMapper.getRelationshipsTableChildIdField() + SP + IN + SP + LP + SELECT + SP + UNNEST + LP + QU + RP + RP;
         final List<Object> args = Collections.singletonList(childIdSet.toArray(new Long[0]));
         final List<Map<String, Object>> queryResultList = sqlExecutor.execute(query, args);
         return queryResultList.stream().map(stringObjectMap -> (Long) stringObjectMap.get(tableMapper.getRelationshipsTableParentIdField()))
