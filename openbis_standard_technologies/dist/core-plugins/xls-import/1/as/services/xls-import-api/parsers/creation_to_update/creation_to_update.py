cd72d5a6b68e7470b60751e5b717fce0aa2db5a2
Mariia Pukhliakova <mariia.pukhliakova@id.ethz.ch>
2019-10-24 16:46:19 +0200

    SSDM-8914 add test for Project update

--- openbis_standard_technologies/dist/core-plugins/xls-import/1/as/services/xls-import-api/parsers/creation_to_update/creation_to_update.py
+++ openbis_standard_technologies/dist/core-plugins/xls-import/1/as/services/xls-import-api/parsers/creation_to_update/creation_to_update.py
@@ -31,7 +31,7 @@ def get_existing_element_based_on(creation_type, creation, existing_elements):
         existing_element = list(filter(
             lambda existing_element: creation.code is not None and create_sample_identifier_string(
                 creation) == existing_element.identifier.identifier, existing_elements))
-    if creation_type == ProjectDefinitionToCreationType:
+    elif creation_type == ProjectDefinitionToCreationType:
         existing_element = list(filter(
             lambda existing_element: create_project_identifier_string(creation) == str(existing_element.identifier),
             existing_elements))
