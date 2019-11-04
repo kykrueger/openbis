3450aced6487fa53c568552a16b185f86d2dc598
felmer <franz-josef.elmer@id.ethz.ch>
2019-11-04 14:39:55 +0100

    SSDM-4600: V3 API: add update rights for experiments and data sets. System tests in Java.
    (cherry picked from commit 6f70d43666cc1aaa526074355b9a18ca8121955a)

--- openbis/source/java/ch/ethz/sis/openbis/generic/server/asapi/v3/executor/rights/GetRightsExecutor.java
+++ openbis/source/java/ch/ethz/sis/openbis/generic/server/asapi/v3/executor/rights/GetRightsExecutor.java
@@ -28,68 +28,188 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.common.id.IObjectId;
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.dataset.id.IDataSetId;
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.experiment.id.IExperimentId;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.rights.Right;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.rights.Rights;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.rights.fetchoptions.RightsFetchOptions;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.id.ISampleId;
 import ch.ethz.sis.openbis.generic.server.asapi.v3.executor.IOperationContext;
+import ch.ethz.sis.openbis.generic.server.asapi.v3.executor.dataset.IDataSetAuthorizationExecutor;
+import ch.ethz.sis.openbis.generic.server.asapi.v3.executor.dataset.IMapDataSetByIdExecutor;
+import ch.ethz.sis.openbis.generic.server.asapi.v3.executor.experiment.IExperimentAuthorizationExecutor;
+import ch.ethz.sis.openbis.generic.server.asapi.v3.executor.experiment.IMapExperimentByIdExecutor;
 import ch.ethz.sis.openbis.generic.server.asapi.v3.executor.sample.IMapSampleByIdExecutor;
 import ch.ethz.sis.openbis.generic.server.asapi.v3.executor.sample.ISampleAuthorizationExecutor;
 import ch.systemsx.cisd.common.exceptions.AuthorizationFailureException;
+import ch.systemsx.cisd.openbis.generic.shared.dto.DataPE;
+import ch.systemsx.cisd.openbis.generic.shared.dto.ExperimentPE;
 import ch.systemsx.cisd.openbis.generic.shared.dto.SamplePE;
 
 /**
  * @author Franz-Josef Elmer
- *
  */
 @Component
 public class GetRightsExecutor implements IGetRightsExecutor
 {
     @Autowired
     private IMapSampleByIdExecutor mapSampleByIdExecutor;
-    
+
     @Autowired
     private ISampleAuthorizationExecutor sampleAuthorizationExecutor;
+
+    @Autowired
+    private IMapExperimentByIdExecutor mapExperimentByIdExecutor;
+
+    @Autowired
+    private IExperimentAuthorizationExecutor experimentAuthorizationExecutor;
+
+    @Autowired
+    private IMapDataSetByIdExecutor mapDataSetByIdExecutor;
+
+    @Autowired
+    private IDataSetAuthorizationExecutor dataSetAuthorizationExecutor;
+
+    private List<Handler> handlers;
+
+    public GetRightsExecutor()
+    {
+        handlers = new ArrayList<>();
+        handlers.add(new SampleHandler());
+        handlers.add(new ExperimentHandler());
+        handlers.add(new DataSetHandler());
+    }
 
     @Override
     public Map<IObjectId, Rights> getRights(IOperationContext context, List<? extends IObjectId> objectIds, RightsFetchOptions fetchOptions)
     {
         Map<IObjectId, Rights> result = new HashMap<>();
-        List<ISampleId> sampleIds = new ArrayList<>();
         for (IObjectId id : objectIds)
+        {
+            for (Handler handler : handlers)
+            {
+                handler.handle(id);
+            }
+        }
+        for (Handler handler : handlers)
+        {
+            handler.addRights(context, result);
+        }
+        return result;
+    }
+
+    private static interface Handler
+    {
+        void handle(IObjectId id);
+
+        void addRights(IOperationContext context, Map<IObjectId, Rights> rightsByIds);
+    }
+
+    private class SampleHandler implements Handler
+    {
+        private List<ISampleId> sampleIds = new ArrayList<>();
+
+        @Override
+        public void handle(IObjectId id)
         {
             if (id instanceof ISampleId)
             {
-                ISampleId sampleId = (ISampleId) id;
-                sampleIds.add(sampleId);
+                sampleIds.add((ISampleId) id);
             }
         }
-        Map<ISampleId, SamplePE> map = mapSampleByIdExecutor.map(context, sampleIds);
-        Set<Entry<ISampleId, SamplePE>> entrySet = map.entrySet();
-        for (Entry<ISampleId, SamplePE> entry : entrySet)
+
+        @Override
+        public void addRights(IOperationContext context, Map<IObjectId, Rights> rightsByIds)
         {
-            Set<Right> rights = new HashSet<>();
-            ISampleId id = entry.getKey();
-            SamplePE object = entry.getValue();
-            
-            try
+            Map<ISampleId, SamplePE> map = mapSampleByIdExecutor.map(context, sampleIds);
+            for (Entry<ISampleId, SamplePE> entry : map.entrySet())
             {
-                sampleAuthorizationExecutor.canUpdate(context, id, object);
-                rights.add(Right.UPDATE);
-            } catch (AuthorizationFailureException e)
-            {
-                // silently ignored
+                Set<Right> rights = new HashSet<>();
+                ISampleId id = entry.getKey();
+                SamplePE object = entry.getValue();
+
+                try
+                {
+                    sampleAuthorizationExecutor.canUpdate(context, id, object);
+                    rights.add(Right.UPDATE);
+                } catch (AuthorizationFailureException e)
+                {
+                    // silently ignored
+                }
+                rightsByIds.put(id, new Rights(rights));
             }
-            result.put(id, new Rights(rights));
         }
-        
-        return result;
     }
-    
-    private enum RightsDetector
+
+    private class ExperimentHandler implements Handler
     {
-        SAMPLE;
-        
-        
+        private List<IExperimentId> experimentIds = new ArrayList<>();
+
+        @Override
+        public void handle(IObjectId id)
+        {
+            if (id instanceof IExperimentId)
+            {
+                experimentIds.add((IExperimentId) id);
+            }
+        }
+
+        @Override
+        public void addRights(IOperationContext context, Map<IObjectId, Rights> rightsByIds)
+        {
+            Map<IExperimentId, ExperimentPE> map = mapExperimentByIdExecutor.map(context, experimentIds);
+            for (Entry<IExperimentId, ExperimentPE> entry : map.entrySet())
+            {
+                Set<Right> rights = new HashSet<>();
+                IExperimentId id = entry.getKey();
+                ExperimentPE object = entry.getValue();
+
+                try
+                {
+                    experimentAuthorizationExecutor.canUpdate(context, id, object);
+                    rights.add(Right.UPDATE);
+                } catch (AuthorizationFailureException e)
+                {
+                    // silently ignored
+                }
+                rightsByIds.put(id, new Rights(rights));
+            }
+        }
+    }
+
+    private class DataSetHandler implements Handler
+    {
+        private List<IDataSetId> dataSetIds = new ArrayList<>();
+
+        @Override
+        public void handle(IObjectId id)
+        {
+            if (id instanceof IDataSetId)
+            {
+                dataSetIds.add((IDataSetId) id);
+            }
+        }
+
+        @Override
+        public void addRights(IOperationContext context, Map<IObjectId, Rights> rightsByIds)
+        {
+            Map<IDataSetId, DataPE> map = mapDataSetByIdExecutor.map(context, dataSetIds);
+            for (Entry<IDataSetId, DataPE> entry : map.entrySet())
+            {
+                Set<Right> rights = new HashSet<>();
+                IDataSetId id = entry.getKey();
+                DataPE object = entry.getValue();
+
+                try
+                {
+                    dataSetAuthorizationExecutor.canUpdate(context, id, object);
+                    rights.add(Right.UPDATE);
+                } catch (AuthorizationFailureException e)
+                {
+                    // silently ignored
+                }
+                rightsByIds.put(id, new Rights(rights));
+            }
+        }
     }
 }
--- openbis/sourceTest/java/ch/ethz/sis/openbis/systemtest/asapi/v3/GetRightsTest.java
+++ openbis/sourceTest/java/ch/ethz/sis/openbis/systemtest/asapi/v3/GetRightsTest.java
@@ -24,6 +24,8 @@ import java.util.Map;
 import org.testng.annotations.Test;
 
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.common.id.IObjectId;
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.dataset.id.DataSetPermId;
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.experiment.id.ExperimentIdentifier;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.rights.Rights;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.rights.fetchoptions.RightsFetchOptions;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.id.SampleIdentifier;
@@ -40,6 +42,38 @@ public class GetRightsTest extends AbstractTest
         String sessionToken = v3api.login(TEST_ROLE_V3, PASSWORD);
         IObjectId s1 = new SampleIdentifier("/TEST-SPACE/CP-TEST-4");
         IObjectId s2 = new SampleIdentifier("/CISD/CP-TEST-1");
+
+        // When
+        Map<IObjectId, Rights> map = v3api.getRights(sessionToken, Arrays.asList(s1, s2), new RightsFetchOptions());
+
+        // Then
+        assertEquals(map.get(s1).getRights().toString(), "[]");
+        assertEquals(map.get(s2).getRights().toString(), "[UPDATE]");
+    }
+
+    @Test
+    public void testGetExperimentRights()
+    {
+        // Given
+        String sessionToken = v3api.login(TEST_ROLE_V3, PASSWORD);
+        IObjectId s1 = new ExperimentIdentifier("/TEST-SPACE/TEST-PROJECT/EXP-SPACE-TEST");
+        IObjectId s2 = new ExperimentIdentifier("/CISD/NEMO/EXP1");
+
+        // When
+        Map<IObjectId, Rights> map = v3api.getRights(sessionToken, Arrays.asList(s1, s2), new RightsFetchOptions());
+
+        // Then
+        assertEquals(map.get(s1).getRights().toString(), "[]");
+        assertEquals(map.get(s2).getRights().toString(), "[UPDATE]");
+    }
+
+    @Test
+    public void testGetDataSetRights()
+    {
+        // Given
+        String sessionToken = v3api.login(TEST_ROLE_V3, PASSWORD);
+        IObjectId s1 = new DataSetPermId("20120619092259000-22");
+        IObjectId s2 = new DataSetPermId("20081105092259000-21");
 
         // When
         Map<IObjectId, Rights> map = v3api.getRights(sessionToken, Arrays.asList(s1, s2), new RightsFetchOptions());
