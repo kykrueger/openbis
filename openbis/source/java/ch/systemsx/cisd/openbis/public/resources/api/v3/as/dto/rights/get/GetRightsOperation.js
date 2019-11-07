9542d1d8c7f108924404d3027455cfb18b79e518
felmer <franz-josef.elmer@id.ethz.ch>
2019-11-07 13:28:35 +0100

    SSDM-4600: Bug fixed in GetRightsOperation.js

--- openbis/source/java/ch/systemsx/cisd/openbis/public/resources/api/v3/as/dto/rights/get/GetRightsOperation.js
+++ openbis/source/java/ch/systemsx/cisd/openbis/public/resources/api/v3/as/dto/rights/get/GetRightsOperation.js
@@ -1,6 +1,6 @@
 define([ "stjs", "as/dto/common/get/GetObjectsOperation" ], function(stjs, GetObjectsOperation) {
 	var GetRightsOperation = function(objectIds, fetchOptions) {
-		GetRightsOperation.call(this, objectIds, fetchOptions);
+		GetObjectsOperation.call(this, objectIds, fetchOptions);
 	};
 	stjs.extend(GetRightsOperation, GetObjectsOperation, [ GetObjectsOperation ], function(constructor, prototype) {
 		prototype['@type'] = 'as.dto.rights.get.GetRightsOperation';
