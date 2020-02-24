99f1d2d47b93d631dd2ec9fa2a8444dfa33782b8
Viktor Kovtun <viktor.kovtun@id.ethz.ch>
2020-02-24 10:36:51 +0100

    SSDM-8226 Switched search of data set types to the new SQL translator.

--- openbis/source/java/ch/ethz/sis/openbis/generic/server/asapi/v3/executor/dataset/SearchDataSetTypesOperationExecutor.java
+++ openbis/source/java/ch/ethz/sis/openbis/generic/server/asapi/v3/executor/dataset/SearchDataSetTypesOperationExecutor.java
@@ -16,6 +16,8 @@
 
 package ch.ethz.sis.openbis.generic.server.asapi.v3.executor.dataset;
 
+import ch.ethz.sis.openbis.generic.server.asapi.v3.executor.IOperationContext;
+import ch.ethz.sis.openbis.generic.server.asapi.v3.search.planner.DataSetTypeSearchManager;
 import ch.ethz.sis.openbis.generic.server.asapi.v3.search.planner.ISearchManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -49,6 +51,9 @@ public class SearchDataSetTypesOperationExecutor
     @Autowired
     private IDataSetTypeTranslator translator;
 
+    @Autowired
+    private DataSetTypeSearchManager dataSetTypeSearchManager;
+
     @Override
     protected Class<? extends SearchObjectsOperation<DataSetTypeSearchCriteria, DataSetTypeFetchOptions>> getOperationClass()
     {
@@ -74,8 +79,14 @@ public class SearchDataSetTypesOperationExecutor
     }
 
     @Override
+    protected SearchObjectsOperationResult<DataSetType> doExecute(final IOperationContext context, final SearchObjectsOperation<DataSetTypeSearchCriteria, DataSetTypeFetchOptions> operation)
+    {
+        return doExecuteNewSearch(context, operation);
+    }
+
+    @Override
     protected ISearchManager<DataSetTypeSearchCriteria, DataSetType, Long> getSearchManager() {
-        throw new RuntimeException("This method is not implemented yet.");
+        return dataSetTypeSearchManager;
     }
 
 }
