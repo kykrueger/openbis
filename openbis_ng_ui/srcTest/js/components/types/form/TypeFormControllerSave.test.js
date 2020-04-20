89c71aec7d2ffbb03ba0e37dc3c44ead0f40c025
pkupczyk <piotr.kupczyk@id.ethz.ch>
2020-04-20 22:12:06 +0200

    SSDM-7583 : ObjectType form - extend "Save" automated tests

--- openbis_ng_ui/srcTest/js/components/types/form/TypeFormControllerSave.test.js
+++ openbis_ng_ui/srcTest/js/components/types/form/TypeFormControllerSave.test.js
@@ -6,6 +6,38 @@ import openbis from '@srcTest/js/services/openbis.js'
 
 jest.mock('@src/js/components/types/form/TypeFormFacade')
 
+const LOCAL_PROPERTY_TYPE = new openbis.PropertyType()
+LOCAL_PROPERTY_TYPE.setCode('TEST_TYPE.TEST_PROPERTY_TYPE')
+LOCAL_PROPERTY_TYPE.setLabel('TEST_LABEL')
+LOCAL_PROPERTY_TYPE.setDescription('TEST_DESCRIPTION')
+LOCAL_PROPERTY_TYPE.setDataType(openbis.DataType.INTEGER)
+
+const LOCAL_PROPERTY_ASSIGNMENT = new openbis.PropertyAssignment()
+LOCAL_PROPERTY_ASSIGNMENT.setPropertyType(LOCAL_PROPERTY_TYPE)
+
+const SAMPLE_TYPE_WITH_LOCAL_PROPERTY = new openbis.SampleType()
+SAMPLE_TYPE_WITH_LOCAL_PROPERTY.setCode('TEST_TYPE')
+SAMPLE_TYPE_WITH_LOCAL_PROPERTY.setGeneratedCodePrefix('TEST_PREFIX')
+SAMPLE_TYPE_WITH_LOCAL_PROPERTY.setPropertyAssignments([
+  LOCAL_PROPERTY_ASSIGNMENT
+])
+
+const GLOBAL_PROPERTY_TYPE = new openbis.PropertyType()
+GLOBAL_PROPERTY_TYPE.setCode('TEST_PROPERTY_TYPE')
+GLOBAL_PROPERTY_TYPE.setLabel('TEST_LABEL')
+GLOBAL_PROPERTY_TYPE.setDescription('TEST_DESCRIPTION')
+GLOBAL_PROPERTY_TYPE.setDataType(openbis.DataType.INTEGER)
+
+const GLOBAL_PROPERTY_ASSIGNMENT = new openbis.PropertyAssignment()
+GLOBAL_PROPERTY_ASSIGNMENT.setPropertyType(GLOBAL_PROPERTY_TYPE)
+
+const SAMPLE_TYPE_WITH_GLOBAL_PROPERTY = new openbis.SampleType()
+SAMPLE_TYPE_WITH_GLOBAL_PROPERTY.setCode('TEST_TYPE')
+SAMPLE_TYPE_WITH_GLOBAL_PROPERTY.setGeneratedCodePrefix('TEST_PREFIX')
+SAMPLE_TYPE_WITH_GLOBAL_PROPERTY.setPropertyAssignments([
+  GLOBAL_PROPERTY_ASSIGNMENT
+])
+
 let context = null
 let facade = null
 let controller = null
@@ -31,31 +63,20 @@ afterEach(() => {
 
 describe('TypeFormController.handleSave', () => {
   test('validation', async () => {
-    const TEST_PROPERTY_TYPE_DTO = new openbis.PropertyType()
-    TEST_PROPERTY_TYPE_DTO.setCode('TEST_PROPERTY_TYPE')
-    TEST_PROPERTY_TYPE_DTO.setLabel('TEST_LABEL')
-    TEST_PROPERTY_TYPE_DTO.setDescription('TEST_DESCRIPTION')
-    TEST_PROPERTY_TYPE_DTO.setDataType(openbis.DataType.INTEGER)
+    const SAMPLE_TYPE = new openbis.SampleType()
 
-    const TEST_PROPERTY_ASSIGNMENT = new openbis.PropertyAssignment()
-    TEST_PROPERTY_ASSIGNMENT.setPropertyType(TEST_PROPERTY_TYPE_DTO)
-
-    const TEST_SAMPLE_TYPE_DTO = new openbis.SampleType()
-    TEST_SAMPLE_TYPE_DTO.setCode('TEST_TYPE')
-    TEST_SAMPLE_TYPE_DTO.setPropertyAssignments([TEST_PROPERTY_ASSIGNMENT])
-
-    facade.loadType.mockReturnValue(Promise.resolve(TEST_SAMPLE_TYPE_DTO))
+    facade.loadType.mockReturnValue(Promise.resolve(SAMPLE_TYPE))
     facade.loadUsages.mockReturnValue(Promise.resolve({}))
 
     await controller.load()
-    controller.handleSelectionChange('section', { id: 'section-0' })
+    controller.handleAddSection()
     controller.handleAddProperty()
 
     expect(context.getState()).toMatchObject({
       selection: {
         type: 'property',
         params: {
-          id: 'property-1'
+          id: 'property-0'
         }
       },
       type: {
@@ -64,14 +85,6 @@ describe('TypeFormController.handleSave', () => {
       properties: [
         {
           id: 'property-0',
-          code: TEST_PROPERTY_TYPE_DTO.getCode(),
-          dataType: TEST_PROPERTY_TYPE_DTO.getDataType(),
-          label: TEST_PROPERTY_TYPE_DTO.getLabel(),
-          description: TEST_PROPERTY_TYPE_DTO.getDescription(),
-          errors: {}
-        },
-        {
-          id: 'property-1',
           code: null,
           dataType: openbis.DataType.VARCHAR,
           label: null,
@@ -87,25 +100,18 @@ describe('TypeFormController.handleSave', () => {
       selection: {
         type: 'type',
         params: {
-          part: 'generatedCodePrefix'
+          part: 'code'
         }
       },
       type: {
         errors: {
+          code: 'Code cannot be empty',
           generatedCodePrefix: 'Generated code prefix cannot be empty'
         }
       },
       properties: [
         {
           id: 'property-0',
-          code: TEST_PROPERTY_TYPE_DTO.getCode(),
-          dataType: TEST_PROPERTY_TYPE_DTO.getDataType(),
-          label: TEST_PROPERTY_TYPE_DTO.getLabel(),
-          description: TEST_PROPERTY_TYPE_DTO.getDescription(),
-          errors: {}
-        },
-        {
-          id: 'property-1',
           code: null,
           dataType: openbis.DataType.VARCHAR,
           label: null,
@@ -121,113 +127,328 @@ describe('TypeFormController.handleSave', () => {
   })
 
   test('add property', async () => {
-    const TEST_PROPERTY_TYPE_DTO = new openbis.PropertyType()
-    TEST_PROPERTY_TYPE_DTO.setCode('TEST_PROPERTY_TYPE')
-    TEST_PROPERTY_TYPE_DTO.setLabel('TEST_LABEL')
-    TEST_PROPERTY_TYPE_DTO.setDescription('TEST_DESCRIPTION')
-    TEST_PROPERTY_TYPE_DTO.setDataType(openbis.DataType.INTEGER)
+    const SAMPLE_TYPE = new openbis.SampleType()
+    SAMPLE_TYPE.setCode('TEST_TYPE')
+    SAMPLE_TYPE.setGeneratedCodePrefix('TEST_PREFIX')
 
-    const TEST_PROPERTY_ASSIGNMENT = new openbis.PropertyAssignment()
-    TEST_PROPERTY_ASSIGNMENT.setPropertyType(TEST_PROPERTY_TYPE_DTO)
-
-    const TEST_SAMPLE_TYPE_DTO = new openbis.SampleType()
-    TEST_SAMPLE_TYPE_DTO.setCode('TEST_TYPE')
-    TEST_SAMPLE_TYPE_DTO.setGeneratedCodePrefix('TEST_PREFIX')
-    TEST_SAMPLE_TYPE_DTO.setPropertyAssignments([TEST_PROPERTY_ASSIGNMENT])
-
-    facade.loadType.mockReturnValue(Promise.resolve(TEST_SAMPLE_TYPE_DTO))
+    facade.loadType.mockReturnValue(Promise.resolve(SAMPLE_TYPE))
     facade.loadUsages.mockReturnValue(Promise.resolve({}))
     facade.executeOperations.mockReturnValue(Promise.resolve({}))
 
     await controller.load()
 
-    expect(context.getState()).toMatchObject({
-      selection: null,
-      properties: [
-        { id: 'property-0', code: TEST_PROPERTY_TYPE_DTO.getCode() }
-      ],
-      sections: [
-        {
-          id: 'section-0',
-          properties: ['property-0']
-        }
-      ]
-    })
-
-    controller.handleSelectionChange('section', { id: 'section-0' })
+    controller.handleAddSection()
     controller.handleAddProperty()
     controller.handleChange('property', {
-      id: 'property-1',
+      id: 'property-0',
       field: 'code',
       value: 'NEW_CODE'
     })
     controller.handleChange('property', {
-      id: 'property-1',
+      id: 'property-0',
       field: 'label',
       value: 'NEW_LABEL'
     })
     controller.handleChange('property', {
-      id: 'property-1',
+      id: 'property-0',
       field: 'description',
       value: 'NEW_DESCRIPTION'
     })
 
-    expect(context.getState()).toMatchObject({
-      selection: {
-        type: 'property',
-        params: {
-          id: 'property-1'
-        }
-      },
-      properties: [
-        { id: 'property-0', code: TEST_PROPERTY_TYPE_DTO.getCode() },
-        {
-          id: 'property-1',
-          code: 'NEW_CODE',
-          label: 'NEW_LABEL',
-          description: 'NEW_DESCRIPTION'
-        }
-      ],
-      sections: [
-        {
-          id: 'section-0',
-          properties: ['property-0', 'property-1']
-        }
-      ]
+    await controller.handleSave()
+
+    expectExecuteOperations([
+      createPropertyTypeOperation(
+        'TEST_TYPE.NEW_CODE',
+        openbis.DataType.VARCHAR,
+        'NEW_LABEL'
+      ),
+      setPropertyAssignmentOperation(
+        SAMPLE_TYPE.getCode(),
+        'TEST_TYPE.NEW_CODE',
+        false
+      )
+    ])
+  })
+
+  test('update local property assignment', async () => {
+    facade.loadType.mockReturnValue(
+      Promise.resolve(SAMPLE_TYPE_WITH_LOCAL_PROPERTY)
+    )
+    facade.loadUsages.mockReturnValue(Promise.resolve({}))
+    facade.executeOperations.mockReturnValue(Promise.resolve({}))
+
+    await controller.load()
+
+    controller.handleChange('property', {
+      id: 'property-0',
+      field: 'mandatory',
+      value: true
     })
 
     await controller.handleSave()
 
-    expect(facade.executeOperations).toHaveBeenCalledTimes(1)
-    const operations = facade.executeOperations.mock.calls[0][0]
-
-    expect(operations).toMatchObject([
-      createPropertyTypeOperation('TEST_TYPE.NEW_CODE'),
-      updateSampleTypeOperation(TEST_SAMPLE_TYPE_DTO.getCode(), [
-        propertyAssignment(TEST_PROPERTY_TYPE_DTO.getCode()),
-        propertyAssignment('TEST_TYPE.NEW_CODE')
-      ])
+    expectExecuteOperations([
+      setPropertyAssignmentOperation(
+        SAMPLE_TYPE_WITH_LOCAL_PROPERTY.getCode(),
+        LOCAL_PROPERTY_TYPE.getCode(),
+        true
+      )
     ])
   })
 
-  function createPropertyTypeOperation(propertyTypeCode) {
+  test('update local property type if possible', async () => {
+    facade.loadType.mockReturnValue(
+      Promise.resolve(SAMPLE_TYPE_WITH_LOCAL_PROPERTY)
+    )
+    facade.loadUsages.mockReturnValue(Promise.resolve({}))
+    facade.executeOperations.mockReturnValue(Promise.resolve({}))
+
+    await controller.load()
+
+    controller.handleChange('property', {
+      id: 'property-0',
+      field: 'label',
+      value: 'Updated label'
+    })
+
+    await controller.handleSave()
+
+    expectExecuteOperations([
+      updatePropertyTypeOperation(
+        LOCAL_PROPERTY_TYPE.getCode(),
+        'Updated label'
+      ),
+      setPropertyAssignmentOperation(
+        SAMPLE_TYPE_WITH_LOCAL_PROPERTY.getCode(),
+        LOCAL_PROPERTY_TYPE.getCode(),
+        LOCAL_PROPERTY_ASSIGNMENT.isMandatory()
+      )
+    ])
+  })
+
+  test('update local property type if not possible', async () => {
+    facade.loadType.mockReturnValue(
+      Promise.resolve(SAMPLE_TYPE_WITH_LOCAL_PROPERTY)
+    )
+    facade.loadUsages.mockReturnValue(Promise.resolve({}))
+    facade.executeOperations.mockReturnValue(Promise.resolve({}))
+
+    await controller.load()
+
+    controller.handleChange('property', {
+      id: 'property-0',
+      field: 'dataType',
+      value: openbis.DataType.BOOLEAN
+    })
+
+    await controller.handleSave()
+
+    expectExecuteOperations([
+      deletePropertyAssignmentOperation(
+        SAMPLE_TYPE_WITH_LOCAL_PROPERTY.getCode(),
+        LOCAL_PROPERTY_TYPE.getCode(),
+        false
+      ),
+      deletePropertyTypeOperation(LOCAL_PROPERTY_TYPE.getCode()),
+      createPropertyTypeOperation(
+        LOCAL_PROPERTY_TYPE.getCode(),
+        openbis.DataType.BOOLEAN,
+        LOCAL_PROPERTY_TYPE.getLabel()
+      ),
+      setPropertyAssignmentOperation(
+        SAMPLE_TYPE_WITH_LOCAL_PROPERTY.getCode(),
+        LOCAL_PROPERTY_TYPE.getCode(),
+        LOCAL_PROPERTY_ASSIGNMENT.isMandatory()
+      )
+    ])
+  })
+
+  test('update global property assignment', async () => {
+    facade.loadType.mockReturnValue(
+      Promise.resolve(SAMPLE_TYPE_WITH_GLOBAL_PROPERTY)
+    )
+    facade.loadUsages.mockReturnValue(Promise.resolve({}))
+    facade.executeOperations.mockReturnValue(Promise.resolve({}))
+
+    await controller.load()
+
+    controller.handleChange('property', {
+      id: 'property-0',
+      field: 'mandatory',
+      value: true
+    })
+
+    await controller.handleSave()
+
+    expectExecuteOperations([
+      setPropertyAssignmentOperation(
+        SAMPLE_TYPE_WITH_GLOBAL_PROPERTY.getCode(),
+        GLOBAL_PROPERTY_TYPE.getCode(),
+        true
+      )
+    ])
+  })
+
+  test('update global property type', async () => {
+    facade.loadType.mockReturnValue(
+      Promise.resolve(SAMPLE_TYPE_WITH_GLOBAL_PROPERTY)
+    )
+    facade.loadUsages.mockReturnValue(Promise.resolve({}))
+    facade.executeOperations.mockReturnValue(Promise.resolve({}))
+
+    await controller.load()
+
+    controller.handleChange('property', {
+      id: 'property-0',
+      field: 'label',
+      value: 'Updated label'
+    })
+
+    await controller.handleSave()
+
+    const newPropertyTypeCode =
+      SAMPLE_TYPE_WITH_GLOBAL_PROPERTY.getCode() +
+      '.' +
+      GLOBAL_PROPERTY_TYPE.getCode()
+
+    expectExecuteOperations([
+      deletePropertyAssignmentOperation(
+        SAMPLE_TYPE_WITH_GLOBAL_PROPERTY.getCode(),
+        GLOBAL_PROPERTY_TYPE.getCode(),
+        false
+      ),
+      createPropertyTypeOperation(
+        newPropertyTypeCode,
+        GLOBAL_PROPERTY_TYPE.getDataType(),
+        'Updated label'
+      ),
+      setPropertyAssignmentOperation(
+        SAMPLE_TYPE_WITH_GLOBAL_PROPERTY.getCode(),
+        newPropertyTypeCode,
+        GLOBAL_PROPERTY_ASSIGNMENT.isMandatory()
+      )
+    ])
+  })
+
+  test('delete local property', async () => {
+    facade.loadType.mockReturnValue(
+      Promise.resolve(SAMPLE_TYPE_WITH_LOCAL_PROPERTY)
+    )
+    facade.loadUsages.mockReturnValue(Promise.resolve({}))
+    facade.executeOperations.mockReturnValue(Promise.resolve({}))
+
+    await controller.load()
+
+    controller.handleSelectionChange('property', { id: 'property-0' })
+    controller.handleRemove()
+
+    await controller.handleSave()
+
+    expectExecuteOperations([
+      deletePropertyAssignmentOperation(
+        SAMPLE_TYPE_WITH_LOCAL_PROPERTY.getCode(),
+        LOCAL_PROPERTY_TYPE.getCode(),
+        false
+      ),
+      deletePropertyTypeOperation(LOCAL_PROPERTY_TYPE.getCode()),
+      setPropertyAssignmentOperation(SAMPLE_TYPE_WITH_LOCAL_PROPERTY.getCode())
+    ])
+  })
+
+  test('delete global property', async () => {
+    facade.loadType.mockReturnValue(
+      Promise.resolve(SAMPLE_TYPE_WITH_GLOBAL_PROPERTY)
+    )
+    facade.loadUsages.mockReturnValue(Promise.resolve({}))
+    facade.executeOperations.mockReturnValue(Promise.resolve({}))
+
+    await controller.load()
+
+    controller.handleSelectionChange('property', { id: 'property-0' })
+    controller.handleRemove()
+
+    await controller.handleSave()
+
+    expectExecuteOperations([
+      deletePropertyAssignmentOperation(
+        SAMPLE_TYPE_WITH_GLOBAL_PROPERTY.getCode(),
+        GLOBAL_PROPERTY_TYPE.getCode(),
+        false
+      ),
+      setPropertyAssignmentOperation(SAMPLE_TYPE_WITH_GLOBAL_PROPERTY.getCode())
+    ])
+  })
+
+  function createPropertyTypeOperation(
+    propertyTypeCode,
+    propertyDataType,
+    propertyTypeLabel
+  ) {
     const creation = new openbis.PropertyTypeCreation()
     creation.setCode(propertyTypeCode)
+    creation.setLabel(propertyTypeLabel)
     return new openbis.CreatePropertyTypesOperation([creation])
   }
 
-  function updateSampleTypeOperation(typeCode, propertyAssignments) {
+  function updatePropertyTypeOperation(propertyTypeCode, propertyTypeLabel) {
+    const update = new openbis.PropertyTypeUpdate()
+    update.setTypeId(new openbis.PropertyTypePermId(propertyTypeCode))
+    update.setLabel(propertyTypeLabel)
+    return new openbis.UpdatePropertyTypesOperation([update])
+  }
+
+  function deletePropertyTypeOperation(propertyTypeCode) {
+    const id = new openbis.PropertyTypePermId(propertyTypeCode)
+    const options = new openbis.PropertyTypeDeletionOptions()
+    options.setReason('deleted via ng_ui')
+    return new openbis.DeletePropertyTypesOperation([id], options)
+  }
+
+  function setPropertyAssignmentOperation(
+    typeCode,
+    propertyCode,
+    propertyMandatory
+  ) {
+    const assignments = []
+    if (propertyCode) {
+      let assignment = new openbis.PropertyAssignmentCreation()
+      assignment.setPropertyTypeId(new openbis.PropertyTypePermId(propertyCode))
+      assignment.setMandatory(propertyMandatory)
+      assignments.push(assignment)
+    }
+
     const update = new openbis.SampleTypeUpdate()
     update.setTypeId(
       new openbis.EntityTypePermId(typeCode, openbis.EntityKind.SAMPLE)
     )
-    update.getPropertyAssignments().set(propertyAssignments.reverse())
+    update.getPropertyAssignments().set(assignments)
+
     return new openbis.UpdateSampleTypesOperation([update])
   }
 
-  function propertyAssignment(propertyCode) {
-    let creation = new openbis.PropertyAssignmentCreation()
-    creation.setPropertyTypeId(new openbis.PropertyTypePermId(propertyCode))
-    return creation
+  function deletePropertyAssignmentOperation(typeCode, propertyCode, force) {
+    const assignmentId = new openbis.PropertyAssignmentPermId(
+      new openbis.EntityTypePermId(typeCode, openbis.EntityKind.SAMPLE),
+      new openbis.PropertyTypePermId(propertyCode)
+    )
+
+    const update = new openbis.SampleTypeUpdate()
+    update.setTypeId(
+      new openbis.EntityTypePermId(typeCode, openbis.EntityKind.SAMPLE)
+    )
+    update.getPropertyAssignments().remove([assignmentId])
+    update.getPropertyAssignments().setForceRemovingAssignments(force)
+
+    return new openbis.UpdateSampleTypesOperation([update])
+  }
+
+  function expectExecuteOperations(expectedOperations) {
+    expect(facade.executeOperations).toHaveBeenCalledTimes(1)
+    const actualOperations = facade.executeOperations.mock.calls[0][0]
+    expect(actualOperations.length).toEqual(expectedOperations.length)
+    actualOperations.forEach((actualOperation, index) => {
+      expect(actualOperation).toMatchObject(expectedOperations[index])
+    })
   }
 })
