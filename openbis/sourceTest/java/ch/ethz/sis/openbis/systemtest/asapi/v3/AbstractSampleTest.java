7e3d9d26ecc814a5a076552a8033a132387d1a77
felmer <franz-josef.elmer@id.ethz.ch>
2020-06-16 08:41:46 +0200

    SSDM-9367: sample_identifier checked also in UpdateSampleTests

--- openbis/sourceTest/java/ch/ethz/sis/openbis/systemtest/asapi/v3/AbstractSampleTest.java
+++ openbis/sourceTest/java/ch/ethz/sis/openbis/systemtest/asapi/v3/AbstractSampleTest.java
@@ -1,12 +1,5 @@
 package ch.ethz.sis.openbis.systemtest.asapi.v3;
 
-import java.util.List;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.entitytype.id.EntityTypePermId;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.create.SampleCreation;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.space.id.SpacePermId;
@@ -18,9 +11,6 @@ import ch.systemsx.cisd.openbis.generic.shared.basic.dto.PropertyType;
 
 public class AbstractSampleTest extends AbstractTest
 {
-    @Autowired
-    protected SessionFactory sessionFactory;
-
     protected static SampleCreation masterPlateCreation(String spaceCode, String code)
     {
         SampleCreation creation = new SampleCreation();
@@ -55,25 +45,5 @@ public class AbstractSampleTest extends AbstractTest
         assignment.setOrdinal(1000L);
 
         commonServer.registerAndAssignPropertyType(sessionToken, propertyType, assignment);
-    }
-
-    @SuppressWarnings("rawtypes")
-    protected String getSampleIdentifier(String permId)
-    {
-        Session session = sessionFactory.getCurrentSession();
-        Query query = session.createSQLQuery("select sample_identifier from samples where perm_id = :permId")
-                .setParameter("permId", permId);
-        List<?> result = query.getResultList();
-        try
-        {
-            for (Object object : result)
-            {
-                return String.valueOf(object);
-            }
-            return null;
-        } finally
-        {
-            session.flush();
-        }
     }
 }
