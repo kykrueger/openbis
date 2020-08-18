109597ea5beb6fddb9d54d9b8b24ec7d3cf878b8
pkupczyk <piotr.kupczyk@id.ethz.ch>
2020-08-18 15:05:54 +0200

    NG_UI : vocabulary form : SSDM-10060 - extend tests with filtering

--- openbis_ng_ui/srcTest/js/components/types/form/VocabularyFormComponent.test.js
+++ openbis_ng_ui/srcTest/js/components/types/form/VocabularyFormComponent.test.js
@@ -28,6 +28,7 @@ describe('VocabularyFormComponent', () => {
   test('load new', testLoadNew)
   test('load existing', testLoadExisting)
   test('sort', testSort)
+  test('filter', testFilter)
   test('select term', testSelectTerm)
   test('add term', testAddTerm)
   test('remove term', testRemoveTerm)
@@ -280,6 +281,69 @@ async function testSort() {
         { values: { 'label.value': 'Term 2' } },
         { values: { 'label.value': 'Term 1' } }
       ]
+    }
+  })
+}
+
+async function testFilter() {
+  const form = await mountNew()
+
+  const labels = [
+    'some 1',
+    'SOME 2',
+    'Some 3',
+    'another 1',
+    'ANOTHER 2',
+    'Another 3'
+  ]
+
+  for (let i = 0; i < labels.length; i++) {
+    form.getButtons().getAddTerm().click()
+    await form.update()
+    form.getParameters().getTerm().getLabel().change(labels[i])
+    await form.update()
+  }
+
+  form.getGrid().getColumns()[1].getFilter().change('some')
+  await form.update()
+
+  form.expectJSON({
+    grid: {
+      columns: [
+        { field: 'code.value', filter: { value: null } },
+        { field: 'label.value', filter: { value: 'some' } },
+        { field: 'description.value', filter: { value: null } },
+        { field: 'official.value', filter: { value: null } }
+      ],
+      rows: [
+        { values: { 'label.value': 'some 1' } },
+        { values: { 'label.value': 'SOME 2' } },
+        { values: { 'label.value': 'Some 3' } }
+      ],
+      paging: {
+        range: '1-3 of 3'
+      }
+    }
+  })
+
+  form.getGrid().getColumns()[1].getFilter().change('1')
+  await form.update()
+
+  form.expectJSON({
+    grid: {
+      columns: [
+        { field: 'code.value', filter: { value: null } },
+        { field: 'label.value', filter: { value: '1' } },
+        { field: 'description.value', filter: { value: null } },
+        { field: 'official.value', filter: { value: null } }
+      ],
+      rows: [
+        { values: { 'label.value': 'some 1' } },
+        { values: { 'label.value': 'another 1' } }
+      ],
+      paging: {
+        range: '1-2 of 2'
+      }
     }
   })
 }
