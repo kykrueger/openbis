504e15381da6c0c97e42dde23d2234b225719f00
pkupczyk <piotr.kupczyk@id.ethz.ch>
2020-08-24 16:46:18 +0200

    NG_UI : vocabulary form : SSDM-7579 - add a new logging method that makes it easier to track incorrect component state and props changes which degradate the performance

--- openbis_ng_ui/src/js/common/logger.js
+++ openbis_ng_ui/src/js/common/logger.js
@@ -1,9 +1,14 @@
+import diff from '@src/js/common/diff.js'
+
 const ERROR = { name: 'ERROR', importance: 4 }
 const INFO = { name: 'INFO', importance: 3 }
 const DEBUG = { name: 'DEBUG', importance: 2 }
 const TRACE = { name: 'TRACE', importance: 1 }
 
 let currentLevel = INFO
+
+const states = new Map()
+const props = new Map()
 
 let isLevelEnabled = level => {
   return level.importance >= currentLevel.importance
@@ -13,6 +18,28 @@ let log = (level, message, ...params) => {
   if (isLevelEnabled(level)) {
     // eslint-disable-next-line no-console
     console.log(new Date().toISOString() + ' ' + message, ...params)
+  }
+}
+
+let logComponent = (level, message, component) => {
+  if (isLevelEnabled(level)) {
+    group(level, message)
+
+    group(level, message + ': state')
+    const prevState = states.get(component)
+    const newState = component.state
+    diff(prevState, newState, 'root')
+    states.set(component, newState)
+    groupEnd(level)
+
+    group(level, message + ': props')
+    const prevProps = props.get(component)
+    const newProps = component.props
+    diff(prevProps, newProps, 'root')
+    props.set(component, newProps)
+    groupEnd(level)
+
+    groupEnd(level)
   }
 }
 
@@ -36,6 +63,7 @@ export default {
   DEBUG,
   TRACE,
   log,
+  logComponent,
   group,
   groupEnd,
   isLevelEnabled
