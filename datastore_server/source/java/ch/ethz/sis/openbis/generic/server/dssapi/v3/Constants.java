97bdc254cd8d015ee0e1ecab2fc632a77b8ca402
felmer <franz-josef.elmer@id.ethz.ch>
2020-09-22 13:40:22 +0200

    SSDM-10191: resolve some of the unwanted dependencies by moving string constants to classes containing only such constants

---
+++ datastore_server/source/java/ch/ethz/sis/openbis/generic/server/dssapi/v3/Constants.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2020 ETH Zuerich, SIS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.ethz.sis.openbis.generic.server.dssapi.v3;
+
+/**
+ * @author Franz-Josef Elmer
+ *
+ */
+public class Constants
+{
+
+    /**
+     * Name of this service for which it is registered as Spring bean
+     */
+    public static final String INTERNAL_SERVICE_NAME = "data-store-server_INTERNAL";
+    public static final String INTERNAL_BEAN_NAME = "v3-exporter";
+
+}
