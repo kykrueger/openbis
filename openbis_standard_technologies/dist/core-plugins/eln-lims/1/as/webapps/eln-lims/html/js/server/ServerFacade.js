902d4eed4ac3c286c55ea0f2ae4fff325126cc21
Juan Fuentes <juanf@bs-mbpr465.d.ethz.ch>
2020-10-07 10:38:19 +0200

    SSDM-10295 : Bugfix, widgets are now updated by system

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/server/ServerFacade.js
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/server/ServerFacade.js
@@ -99,20 +99,11 @@ function ServerFacade(openbisServer) {
     }
 
     this.setCustomWidgetSettings = function(widgetSettings, doneCallback) {
-        require([ "openbis", "as/dto/property/update/PropertyTypeUpdate", "as/dto/property/id/PropertyTypePermId", "as/dto/common/update/ListUpdateActionAdd" ],
-        function(openbis, PropertyTypeUpdate, PropertyTypePermId) {
-            var ptus = [];
-            for(var uIdx = 0; uIdx < widgetSettings.length; uIdx++) {
-                var ptu = new PropertyTypeUpdate();
-                ptu.setTypeId(new PropertyTypePermId(widgetSettings[uIdx]["Property Type"]));
-                var luaa = new ListUpdateActionAdd();
-                luaa.setItems([{"custom_widget" : widgetSettings[uIdx]["Widget"] }])
-                ptu.setMetaDataActions([luaa]);
-                ptus.push(ptu);
-            }
-            mainController.openbisV3.updatePropertyTypes(ptus).done(function() {
-                doneCallback();
-            });
+        this.customELNASAPI({
+            "method" : "setCustomWidgetSettings",
+            "widgetSettings" : widgetSettings,
+        }, function(result) {
+            doneCallback();
         });
     }
 
