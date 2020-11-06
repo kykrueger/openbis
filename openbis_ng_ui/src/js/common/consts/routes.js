4b1e5980848716b7c96829a8344ab8279994cef5
pkupczyk <piotr.kupczyk@id.ethz.ch>
2020-11-06 14:13:28 +0100

    NG_UI : plugins : SSDM-10401 - refactored the routes definition

--- openbis_ng_ui/src/js/common/consts/routes.js
+++ openbis_ng_ui/src/js/common/consts/routes.js
@@ -3,362 +3,160 @@ import { compile, match } from 'path-to-regexp'
 import pages from '@src/js/common/consts/pages.js'
 import objectTypes from '@src/js/common/consts/objectType.js'
 
-const routes = {
-  TYPES: {
-    format: params => {
-      return _doFormat(params, '/types', {
-        page: pages.TYPES
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/types', {
-        page: pages.TYPES
-      })
-    }
-  },
-  TYPES_SEARCH: {
-    format: params => {
-      return _doFormat(params, '/types-search/:id', {
-        page: pages.TYPES,
-        type: objectTypes.SEARCH
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/types-search/:id', {
-        page: pages.TYPES,
-        type: objectTypes.SEARCH
-      })
-    }
-  },
-  NEW_OBJECT_TYPE: {
-    format: params => {
-      return _doFormat(params, '/new-object-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.NEW_OBJECT_TYPE
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/new-object-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.NEW_OBJECT_TYPE
-      })
-    }
-  },
-  OBJECT_TYPE: {
-    format: params => {
-      return _doFormat(params, '/object-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.OBJECT_TYPE
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/object-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.OBJECT_TYPE
-      })
-    }
-  },
-  NEW_COLLECTION_TYPE: {
-    format: params => {
-      return _doFormat(params, '/new-collection-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.NEW_COLLECTION_TYPE
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/new-collection-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.NEW_COLLECTION_TYPE
-      })
-    }
-  },
-  COLLECTION_TYPE: {
-    format: params => {
-      return _doFormat(params, '/collection-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.COLLECTION_TYPE
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/collection-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.COLLECTION_TYPE
-      })
-    }
-  },
-  NEW_DATA_SET_TYPE: {
-    format: params => {
-      return _doFormat(params, '/new-dataset-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.NEW_DATA_SET_TYPE
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/new-dataset-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.NEW_DATA_SET_TYPE
-      })
-    }
-  },
-  DATA_SET_TYPE: {
-    format: params => {
-      return _doFormat(params, '/dataset-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.DATA_SET_TYPE
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/dataset-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.DATA_SET_TYPE
-      })
-    }
-  },
-  NEW_MATERIAL_TYPE: {
-    format: params => {
-      return _doFormat(params, '/new-material-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.NEW_MATERIAL_TYPE
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/new-material-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.NEW_MATERIAL_TYPE
-      })
-    }
-  },
-  MATERIAL_TYPE: {
-    format: params => {
-      return _doFormat(params, '/material-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.MATERIAL_TYPE
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/material-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.MATERIAL_TYPE
-      })
-    }
-  },
-  NEW_VOCABULARY_TYPE: {
-    format: params => {
-      return _doFormat(params, '/new-vocabulary-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.NEW_VOCABULARY_TYPE
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/new-vocabulary-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.NEW_VOCABULARY_TYPE
-      })
-    }
-  },
-  VOCABULARY_TYPE: {
-    format: params => {
-      return _doFormat(params, '/vocabulary-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.VOCABULARY_TYPE
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/vocabulary-type/:id', {
-        page: pages.TYPES,
-        type: objectTypes.VOCABULARY_TYPE
-      })
-    }
-  },
-  USERS: {
-    format: params => {
-      return _doFormat(params, '/users', {
-        page: pages.USERS
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/users', {
-        page: pages.USERS
-      })
-    }
-  },
-  USERS_SEARCH: {
-    format: params => {
-      return _doFormat(params, '/users-search/:id', {
-        page: pages.USERS,
-        type: objectTypes.SEARCH
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/users-search/:id', {
-        page: pages.USERS,
-        type: objectTypes.SEARCH
-      })
-    }
-  },
-  NEW_USER: {
-    format: params => {
-      return _doFormat(params, '/new-user/:id', {
-        page: pages.USERS,
-        type: objectTypes.NEW_USER
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/new-user/:id', {
-        page: pages.USERS,
-        type: objectTypes.NEW_USER
-      })
-    }
-  },
-  USER: {
-    format: params => {
-      return _doFormat(params, '/user/:id', {
-        page: pages.USERS,
-        type: objectTypes.USER
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/user/:id', {
-        page: pages.USERS,
-        type: objectTypes.USER
-      })
-    }
-  },
-  NEW_USER_GROUP: {
-    format: params => {
-      return _doFormat(params, '/new-user-group/:id', {
-        page: pages.USERS,
-        type: objectTypes.NEW_USER_GROUP
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/new-user-group/:id', {
-        page: pages.USERS,
-        type: objectTypes.NEW_USER_GROUP
-      })
-    }
-  },
-  USER_GROUP: {
-    format: params => {
-      return _doFormat(params, '/user-group/:id', {
-        page: pages.USERS,
-        type: objectTypes.USER_GROUP
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/user-group/:id', {
-        page: pages.USERS,
-        type: objectTypes.USER_GROUP
-      })
-    }
-  },
-  TOOLS: {
-    format: params => {
-      return _doFormat(params, '/tools', {
-        page: pages.TOOLS
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/tools', {
-        page: pages.TOOLS
-      })
-    }
-  },
-  TOOLS_SEARCH: {
-    format: params => {
-      return _doFormat(params, '/tools-search/:id', {
-        page: pages.TOOLS,
-        type: objectTypes.SEARCH
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/tools-search/:id', {
-        page: pages.TOOLS,
-        type: objectTypes.SEARCH
-      })
-    }
-  },
-  NEW_DYNAMIC_PROPERTY_PLUGIN: {
-    format: params => {
-      return _doFormat(params, '/new-dynamic-property-plugin/:id', {
-        page: pages.TOOLS,
-        type: objectTypes.NEW_DYNAMIC_PROPERTY_PLUGIN
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/new-dynamic-property-plugin/:id', {
-        page: pages.TOOLS,
-        type: objectTypes.NEW_DYNAMIC_PROPERTY_PLUGIN
-      })
-    }
-  },
-  DYNAMIC_PROPERTY_PLUGIN: {
-    format: params => {
-      return _doFormat(params, '/dynamic-property-plugin/:id', {
-        page: pages.TOOLS,
-        type: objectTypes.DYNAMIC_PROPERTY_PLUGIN
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/dynamic-property-plugin/:id', {
-        page: pages.TOOLS,
-        type: objectTypes.DYNAMIC_PROPERTY_PLUGIN
-      })
-    }
-  },
-  NEW_ENTITY_VALIDATION_PLUGIN: {
-    format: params => {
-      return _doFormat(params, '/new-entity-validation-plugin/:id', {
-        page: pages.TOOLS,
-        type: objectTypes.NEW_ENTITY_VALIDATION_PLUGIN
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/new-entity-validation-plugin/:id', {
-        page: pages.TOOLS,
-        type: objectTypes.NEW_ENTITY_VALIDATION_PLUGIN
-      })
-    }
-  },
-  ENTITY_VALIDATION_PLUGIN: {
-    format: params => {
-      return _doFormat(params, '/entity-validation-plugin/:id', {
-        page: pages.TOOLS,
-        type: objectTypes.ENTITY_VALIDATION_PLUGIN
-      })
-    },
-    parse: path => {
-      return _doParse(path, '/entity-validation-plugin/:id', {
-        page: pages.TOOLS,
-        type: objectTypes.ENTITY_VALIDATION_PLUGIN
-      })
-    }
-  },
-  DEFAULT: {
-    format: () => {
+class Route {
+  constructor(pattern, params) {
+    this.pattern = pattern
+    this.params = params
+  }
+
+  format(params) {
+    if (_.isMatch(params, this.params)) {
+      let toPath = compile(this.pattern, { encode: encodeURIComponent })
       return {
-        path: '/',
-        match: 0
+        path: toPath(params),
+        specificity: Object.keys(this.params).length
       }
-    },
-    parse: () => {
+    } else {
+      return null
+    }
+  }
+
+  parse(path) {
+    let toPathObject = match(this.pattern, { decode: decodeURIComponent })
+    let pathObject = toPathObject(path)
+    if (pathObject) {
       return {
-        path: '/',
-        page: pages.TYPES
+        path: pathObject.path,
+        ...pathObject.params,
+        ...this.params
       }
+    } else {
+      return null
     }
   }
 }
 
+class DefaultRoute {
+  format() {
+    return {
+      path: '/',
+      specificity: 0
+    }
+  }
+  parse() {
+    return {
+      path: '/',
+      page: pages.TYPES
+    }
+  }
+}
+
+const routes = {
+  TYPES: new Route('/types', {
+    page: pages.TYPES
+  }),
+  TYPES_SEARCH: new Route('/types-search/:id', {
+    page: pages.TYPES,
+    type: objectTypes.SEARCH
+  }),
+  NEW_OBJECT_TYPE: new Route('/new-object-type/:id', {
+    page: pages.TYPES,
+    type: objectTypes.NEW_OBJECT_TYPE
+  }),
+  OBJECT_TYPE: new Route('/object-type/:id', {
+    page: pages.TYPES,
+    type: objectTypes.OBJECT_TYPE
+  }),
+  NEW_COLLECTION_TYPE: new Route('/new-collection-type/:id', {
+    page: pages.TYPES,
+    type: objectTypes.NEW_COLLECTION_TYPE
+  }),
+  COLLECTION_TYPE: new Route('/collection-type/:id', {
+    page: pages.TYPES,
+    type: objectTypes.COLLECTION_TYPE
+  }),
+  NEW_DATA_SET_TYPE: new Route('/new-dataset-type/:id', {
+    page: pages.TYPES,
+    type: objectTypes.NEW_DATA_SET_TYPE
+  }),
+  DATA_SET_TYPE: new Route('/dataset-type/:id', {
+    page: pages.TYPES,
+    type: objectTypes.DATA_SET_TYPE
+  }),
+  NEW_MATERIAL_TYPE: new Route('/new-material-type/:id', {
+    page: pages.TYPES,
+    type: objectTypes.NEW_MATERIAL_TYPE
+  }),
+  MATERIAL_TYPE: new Route('/material-type/:id', {
+    page: pages.TYPES,
+    type: objectTypes.MATERIAL_TYPE
+  }),
+  NEW_VOCABULARY_TYPE: new Route('/new-vocabulary-type/:id', {
+    page: pages.TYPES,
+    type: objectTypes.NEW_VOCABULARY_TYPE
+  }),
+  VOCABULARY_TYPE: new Route('/vocabulary-type/:id', {
+    page: pages.TYPES,
+    type: objectTypes.VOCABULARY_TYPE
+  }),
+  USERS: new Route('/users', {
+    page: pages.USERS
+  }),
+  USERS_SEARCH: new Route('/users-search/:id', {
+    page: pages.USERS,
+    type: objectTypes.SEARCH
+  }),
+  NEW_USER: new Route('/new-user/:id', {
+    page: pages.USERS,
+    type: objectTypes.NEW_USER
+  }),
+  USER: new Route('/user/:id', {
+    page: pages.USERS,
+    type: objectTypes.USER
+  }),
+  NEW_USER_GROUP: new Route('/new-user-group/:id', {
+    page: pages.USERS,
+    type: objectTypes.NEW_USER_GROUP
+  }),
+  USER_GROUP: new Route('/user-group/:id', {
+    page: pages.USERS,
+    type: objectTypes.USER_GROUP
+  }),
+  TOOLS: new Route('/tools', {
+    page: pages.TOOLS
+  }),
+  TOOLS_SEARCH: new Route('/tools-search/:id', {
+    page: pages.TOOLS,
+    type: objectTypes.SEARCH
+  }),
+  NEW_DYNAMIC_PROPERTY_PLUGIN: new Route('/new-dynamic-property-plugin/:id', {
+    page: pages.TOOLS,
+    type: objectTypes.NEW_DYNAMIC_PROPERTY_PLUGIN
+  }),
+  DYNAMIC_PROPERTY_PLUGIN: new Route('/dynamic-property-plugin/:id', {
+    page: pages.TOOLS,
+    type: objectTypes.DYNAMIC_PROPERTY_PLUGIN
+  }),
+  NEW_ENTITY_VALIDATION_PLUGIN: new Route('/new-entity-validation-plugin/:id', {
+    page: pages.TOOLS,
+    type: objectTypes.NEW_ENTITY_VALIDATION_PLUGIN
+  }),
+  ENTITY_VALIDATION_PLUGIN: new Route('/entity-validation-plugin/:id', {
+    page: pages.TOOLS,
+    type: objectTypes.ENTITY_VALIDATION_PLUGIN
+  }),
+  DEFAULT: new DefaultRoute()
+}
+
 function format(params) {
   let keys = Object.keys(routes)
-  let best = { match: 0, path: null }
+  let best = { specificity: 0, path: null }
 
   for (let i = 0; i < keys.length; i++) {
     let route = routes[keys[i]]
     try {
       let result = route.format(params)
-      if (result && result.match > best.match) {
+      if (result && result.specificity > best.specificity) {
         best = result
       }
     } catch (err) {
@@ -367,18 +165,6 @@ function format(params) {
   }
 
   return best.path
-}
-
-function _doFormat(actualParams, pattern, requiredParams) {
-  if (_.isMatch(actualParams, requiredParams)) {
-    let toPath = compile(pattern, { encode: encodeURIComponent })
-    return {
-      path: toPath(actualParams),
-      match: Object.keys(requiredParams).length
-    }
-  } else {
-    return null
-  }
 }
 
 function parse(path) {
@@ -391,20 +177,6 @@ function parse(path) {
     }
   }
   return null
-}
-
-function _doParse(path, pattern, extraParams) {
-  let toPathObject = match(pattern, { decode: decodeURIComponent })
-  let pathObject = toPathObject(path)
-  if (pathObject) {
-    return {
-      path: pathObject.path,
-      ...pathObject.params,
-      ...extraParams
-    }
-  } else {
-    return null
-  }
 }
 
 export default {
