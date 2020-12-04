027325b5948b28c4a6eaec9f7d385b0731722fb4
Juan Fuentes <juanf@bs-mbpr465.d.ethz.ch>
2020-12-04 16:37:33 +0100

    SSDM-10525 : Move storage boxes graphical glitch, using pattern to be able to be used for tests

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/views/StorageManager/widgets/GridView.js
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/views/StorageManager/widgets/GridView.js
@@ -165,7 +165,12 @@ function GridView(gridModel) {
 						}).bind(this, sample));
 					}
 
-					var storageBoxId = Util.guid();
+                    var rowLabel = posX;
+                    if(this._gridModel.useLettersOnRows) {
+                        rowLabel = Util.getLetterForNumber(rowLabel);
+                    }
+                    var storageBoxId = this._gridModel.gridId + "-" + rowLabel + "-" + posY + "-storage-box-" + i;
+
 					var labelContainer = $("<div>", { class: "storageBox", id : storageBoxId }).text(labels[i].displayName);
 					if (sample) {
 						var tooltip = PrintUtil.getTable(sample, false, optSampleTitle, 'inspectorWhiteFont', 
