1c527532c5e8a64448acff0e36956fe341efc551
pkupczyk <piotr.kupczyk@id.ethz.ch>
2021-01-04 10:29:58 +0100

    NG_UI : plugins evaluation : SSDM-10431 - properly render 'null' result

--- openbis_ng_ui/src/js/components/tools/form/plugin/PluginFormEvaluateResults.jsx
+++ openbis_ng_ui/src/js/components/tools/form/plugin/PluginFormEvaluateResults.jsx
@@ -47,24 +47,20 @@ class PluginFormEvaluateResults extends React.PureComponent {
       classes
     } = this.props
 
-    if (result) {
-      if (plugin.pluginType === openbis.PluginType.DYNAMIC_PROPERTY) {
-        return (
-          <Typography className={classes.result} data-part='result'>
-            {result.value}
-          </Typography>
-        )
-      } else if (plugin.pluginType === openbis.PluginType.ENTITY_VALIDATION) {
-        return (
-          <Typography className={classes.result} data-part='result'>
-            {result.error}
-          </Typography>
-        )
-      } else {
-        throw new Error('Unsupported pluginType: ' + plugin.pluginType)
-      }
+    if (plugin.pluginType === openbis.PluginType.DYNAMIC_PROPERTY) {
+      return (
+        <Typography className={classes.result} data-part='result'>
+          {(result && result.value) || 'null'}
+        </Typography>
+      )
+    } else if (plugin.pluginType === openbis.PluginType.ENTITY_VALIDATION) {
+      return (
+        <Typography className={classes.result} data-part='result'>
+          {(result && result.error) || 'null'}
+        </Typography>
+      )
     } else {
-      return null
+      throw new Error('Unsupported pluginType: ' + plugin.pluginType)
     }
   }
 }
