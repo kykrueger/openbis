9c36d6d7973761e8cccf746e664276829761464c
vermeul <swen@ethz.ch>
2021-05-10 16:17:54 +0200

    bumped version to 1.18.10

--- pybis/src/python/setup.py
+++ pybis/src/python/setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='PyBIS',
-    version= '1.18.9',
+    version= '1.18.10',
     author='Swen Vermeul • ID SIS • ETH Zürich',
     author_email='swen@ethz.ch',
     description='openBIS connection and interaction, optimized for using with Jupyter',
